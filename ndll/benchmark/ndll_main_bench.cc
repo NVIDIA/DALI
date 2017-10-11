8dab078581e600ac7c2171eff8fa1f814c9899d9
Trevor Gale <tgale@nvidia.com>
2017-10-10 19:22:34 -0700

    Removed unused caching allocator code. Still in branch mem-usage-refactor if were interested

--- ndll/benchmark/ndll_main_bench.cc
+++ ndll/benchmark/ndll_main_bench.cc
@@ -2,15 +2,10 @@
 
 #include "ndll/common.h"
 #include "ndll/pipeline/data/allocator.h"
-#include "ndll/pipeline/data/caching_device_allocator.h"
 #include "ndll/pipeline/init.h"
 
 int main(int argc, char **argv) {
-  // Try out the caching allocator wrappers
-  ndll::CachingDeviceAllocator *gpu_allocator =
-    new ndll::CachingDeviceAllocator(new ndll::GPUAllocator);
-  
-  ndll::NDLLInit(new ndll::PinnedCPUAllocator, gpu_allocator);
+  ndll::NDLLInit(new ndll::PinnedCPUAllocator, new ndll::GPUAllocator);
   ::benchmark::Initialize(&argc, argv);
   ::benchmark::RunSpecifiedBenchmarks();
 }
