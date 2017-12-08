a1e52674a15df2caa4edf4fca366f82f62fd9427
Simon Layton <slayton58@gmail.com>
2017-12-08 09:49:29 -0500

    FileStoreReader base and initial LMDB reader

--- ndll/file_store/file_store_reader.h
+++ ndll/file_store/file_store_reader.h
@@ -6,9 +6,12 @@
 #include <string>
 #include <vector>
 
+#include "ndll/common.h"
 #include "ndll/error_handling.h"
 
-class Sample {
+namespace ndll {
+
+struct Sample {
     size_t nbytes;
     void *data;
 };
@@ -18,12 +21,6 @@ using Options = std::map<std::string, std::string>;
 class FileStoreReader {
  public:
   FileStoreReader(std::string uri, Options& options) {
-    // Read an initial number of samples to fill our
-    // sample buffer
-    for (int i = 0; i < initial_buffer_fill_; ++i) {
-      sample_buffer_.push_back(ReadSample());
-		}
-
     // initialize a random distribution -- this will be
     // used to pick from our sample buffer
     dis = std::uniform_int_distribution<>(0, 1048576);
@@ -32,7 +29,16 @@ class FileStoreReader {
 
   // Get a random read sample
   Sample ReadOne() {
+    // perform an iniital buffer fill if it hasn't already happened
+    if (!initial_buffer_filled_) {
+      // Read an initial number of samples to fill our
+      // sample buffer
+      for (int i = 0; i < initial_buffer_fill_; ++i) {
+        sample_buffer_.push_back(ReadSample());
+      }
 
+      initial_buffer_filled_ = true;
+    }
     // choose the random index
     int idx = dis(e_) % sample_buffer_.size();
     Sample elem = sample_buffer_[idx];
@@ -51,11 +57,7 @@ class FileStoreReader {
   // Read an actual sample from the FileStore,
   // used to populate the sample buffer for "shuffled"
   // reads.
-  virtual Sample ReadSample() {
-    // error out here
-    NDLL_ENFORCE(false, "Base ReadSample should never be called");
-    return Sample{};
-  }
+  virtual Sample ReadSample() = 0;
 
  protected:
   std::vector<Sample> sample_buffer_;
@@ -63,10 +65,13 @@ class FileStoreReader {
   // number of samples to initialize buffer with
   // ~1 minibatch seems reasonable
   const int initial_buffer_fill_ = 1024;
+  bool initial_buffer_filled_ = false;
 
   // rng
   std::default_random_engine e_;
   std::uniform_int_distribution<> dis;
 };
+
+}; // namespace ndll
 
 #endif
---
+++ ndll/file_store/lmdb.h
@@ -0,0 +1,81 @@
+#ifndef NDLL_FILE_STORE_LMDB_H_
+#define NDLL_FILE_STORE_LMDB_H_
+
+#ifdef USE_LMDB
+
+#include <lmdb.h>
+#include "ndll/file_store/file_store_reader.h"
+
+namespace ndll {
+
+namespace {
+  inline void CHECK_LMDB(int status) {
+    NDLL_ENFORCE(status == MDB_SUCCESS,
+        "LMDB Error: " + string(mdb_strerror(status)));
+  }
+
+  bool SeekLMDB(MDB_cursor* cursor, MDB_cursor_op op, MDB_val& key, MDB_val& value) {
+    int status = mdb_cursor_get(cursor, &key, &value, op);
+
+
+    if (status == MDB_NOTFOUND) {
+      // reached the end of the db
+      return false;
+    } else {
+      CHECK_LMDB(status);
+      return true;
+    }
+  }
+}
+
+class LMDBReader : public FileStoreReader {
+ public:
+  LMDBReader(std::string uri, Options& options)
+    : FileStoreReader(uri, options) {
+    CHECK_LMDB(mdb_env_create(&mdb_env_));
+    auto mdb_flags = MDB_RDONLY | MDB_NOTLS | MDB_NOLOCK;
+    CHECK_LMDB(mdb_env_open(mdb_env_, uri.c_str(), mdb_flags, 0664));
+
+    CHECK_LMDB(mdb_txn_begin(mdb_env_, NULL, MDB_RDONLY, &mdb_transaction_));
+    CHECK_LMDB(mdb_dbi_open(mdb_transaction_, NULL, 0, &mdb_dbi_));
+    CHECK_LMDB(mdb_cursor_open(mdb_transaction_, mdb_dbi_, &mdb_cursor_));
+  }
+  ~LMDBReader() {
+    mdb_cursor_close(mdb_cursor_);
+    mdb_dbi_close(mdb_env_, mdb_dbi_);
+    mdb_txn_abort(mdb_transaction_);
+    mdb_env_close(mdb_env_);
+    mdb_env_ = nullptr;
+  }
+
+  Sample ReadSample() {
+    // assume cursor is valid, read next, loop to start if necessary
+    Sample sample;
+
+    bool ok = SeekLMDB(mdb_cursor_, MDB_NEXT, key_, value_);
+
+    if (!ok) {
+      SeekLMDB(mdb_cursor_, MDB_FIRST, key_, value_);
+    }
+
+    sample.nbytes= value_.mv_size;
+    sample.data = value_.mv_data;
+
+    return sample;
+  }
+ private:
+  MDB_env* mdb_env_;
+  MDB_cursor* mdb_cursor_;
+  MDB_dbi mdb_dbi_;
+  MDB_txn* mdb_transaction_;
+
+  // values
+  MDB_val key_, value_;
+
+};
+
+}; // namespace ndll
+
+#endif // USE_LMDB
+
+#endif // NDLL_FILE_STORE_LMDB_H_
