7615b16c4f1d63632705695cd1c3ed397dd77d2a
Simon Layton <slayton58@gmail.com>
2018-01-08 13:08:29 -0500

    Add Alexnet pipeline based on Caffe LMDB dataset
    Adds parser for Caffe protobuf
    Full reader with LMDB + Caffe parser

---
+++ ndll/pipeline/caffe_reader_op.cc
@@ -0,0 +1,12 @@
+#include "ndll/pipeline/caffe_reader_op.h"
+
+namespace ndll {
+
+NDLL_REGISTER_CPU_OPERATOR(CaffeReader, CaffeReader);
+
+OPERATOR_SCHEMA(CaffeReader)
+  .DocStr("Read (Image, label) pairs from a Caffe LMDB")
+  .NumInput(0)
+  .NumOutput(2);  // (Images, Labels)
+
+}  // namespace ndll
