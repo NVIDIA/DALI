83d5d440d80a9a442ceaab4d35f1b684f313dee3
Andrei <andreii@nvidia.com>
2018-01-18 11:54:38 -0800

    Lint problems from previous check-in fixed

--- ndll/pipeline/operators/sphere.cu
+++ ndll/pipeline/operators/sphere.cu
@@ -21,8 +21,9 @@ __global__ void BatchedSphereKernel(const uint8 *in_batch,
 NDLLError_t BatchedSphere(const uint8 *in_batch,
                           int N, const Dims &dims,
                           uint8 *out_batch, cudaStream_t stream) {
-    BatchedSphereKernel<<<N, dim3(32, 32), 0, stream>>>(in_batch, dims[0], dims[1], dims[2], out_batch);
+    BatchedSphereKernel<<<N, dim3(32, 32), 0, stream>>>
+            (in_batch, dims[0], dims[1], dims[2], out_batch);
     return NDLLSuccess;
 }
 
-}
+}  // namespace ndll
