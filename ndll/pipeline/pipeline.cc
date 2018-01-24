3de78288e6dd5d0b597acb1854e09db35d86719c
Simon Layton <slayton58@gmail.com>
2018-01-24 14:04:15 -0500

    Refactor due to issues in C2 integration
    Split argument.h into header / implementation (linking issues)
    Build libhybrid_decode as static (linking issues)

--- ndll/pipeline/pipeline.cc
+++ ndll/pipeline/pipeline.cc
@@ -1,4 +1,5 @@
 // Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+#include "ndll/pipeline/argument.h"
 #include "ndll/pipeline/pipeline.h"
 
 #include <google/protobuf/message.h>
