bc9f3ba4360e0659513c8661e7f202840914d03d
Serge Panev <spanev@nvidia.com>
2018-02-20 18:50:44 -0800

    Update notebooks for CI

--- qa/L0_jupyter/test.sh
+++ qa/L0_jupyter/test.sh
@@ -3,6 +3,10 @@ set -e
 
 cd /opt/ndll/examples
 
+pip install jupyter matplotlib
+
+mkdir /opt/ndll/idx_files
+
 ls *.ipynb | xargs -i jupyter nbconvert \
                    --to notebook --execute \
                    --ExecutePreprocessor.kernel_name=python2 \
@@ -10,3 +14,4 @@ ls *.ipynb | xargs -i jupyter nbconvert \
                    --output output.ipynb {}
 
 rm output.ipynb
+rm -r /opt/ndll/idx_files
