4645afe6fafee391903b8851320757e0a725c08c
Serge Panev <spanev@nvidia.com>
2018-02-21 10:01:09 -0800

    Change ci jupyter output to tmp files

--- qa/L0_jupyter/test.sh
+++ qa/L0_jupyter/test.sh
@@ -5,12 +5,10 @@ cd /opt/ndll/examples
 
 pip install jupyter matplotlib
 
-mkdir /opt/ndll/idx_files
+mkdir -p /opt/ndll/idx_files
 
 ls *.ipynb | xargs -i jupyter nbconvert \
                    --to notebook --execute \
                    --ExecutePreprocessor.kernel_name=python2 \
                    --ExecutePreprocessor.timeout=300 \
-                   --output /dev/null {}
-
-rm -r /opt/ndll/idx_files
+                   --output output.ipynb {}
