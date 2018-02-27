c253166a9b4998c94c0560fa17163adcb5a1ab0a
ptredak <ptredak@nvidia.com>
2018-02-27 10:51:40 -0800

    Refactor SampleWorkspace

--- ndll/pipeline/workspace/sample_workspace.cc
+++ ndll/pipeline/workspace/sample_workspace.cc
@@ -4,46 +4,6 @@
 namespace ndll {
 
 template <>
-bool SampleWorkspace::InputIsType<CPUBackend>(int idx) {
-  NDLL_ENFORCE(idx >= 0, "Negative index not supported.");
-  NDLL_ENFORCE((size_t)idx < input_index_map_.size(),
-      "Index out of range." + std::to_string(idx) +
-      " not in range [0, " + std::to_string(input_index_map_.size())
-      + ")");
-  return input_index_map_[idx].first;
-}
-
-template <>
-bool SampleWorkspace::InputIsType<GPUBackend>(int idx) {
-  NDLL_ENFORCE(idx >= 0, "Negative index not supported.");
-  NDLL_ENFORCE((size_t)idx < input_index_map_.size(),
-      "Index out of range." + std::to_string(idx) +
-      " not in range [0, " + std::to_string(input_index_map_.size())
-      + ")");
-  return !input_index_map_[idx].first;
-}
-
-template <>
-bool SampleWorkspace::OutputIsType<CPUBackend>(int idx) {
-  NDLL_ENFORCE(idx >= 0, "Negative index not supported.");
-  NDLL_ENFORCE((size_t)idx < output_index_map_.size(),
-      "Index out of range." + std::to_string(idx) +
-      " not in range [0, " + std::to_string(output_index_map_.size())
-      + ")");
-  return output_index_map_[idx].first;
-}
-
-template <>
-bool SampleWorkspace::OutputIsType<GPUBackend>(int idx) {
-  NDLL_ENFORCE(idx >= 0, "Negative index not supported.");
-  NDLL_ENFORCE((size_t)idx < output_index_map_.size(),
-      "Index out of range." + std::to_string(idx) +
-      " not in range [0, " + std::to_string(output_index_map_.size())
-      + ")");
-  return !output_index_map_[idx].first;
-}
-
-template <>
 const Tensor<CPUBackend>& SampleWorkspace::Input(int idx) const {
   NDLL_ENFORCE(idx >= 0, "Negative index not supported.");
   NDLL_ENFORCE((size_t)idx < input_index_map_.size(),
@@ -72,20 +32,6 @@ const Tensor<GPUBackend>& SampleWorkspace::Input(int idx) const {
 }
 
 template <>
-void SampleWorkspace::AddInput(shared_ptr<Tensor<CPUBackend>> input) {
-  cpu_inputs_.push_back(input);
-  input_index_map_.push_back(
-      std::make_pair(true, cpu_inputs_.size()-1));
-}
-
-template <>
-void SampleWorkspace::AddInput(shared_ptr<Tensor<GPUBackend>> input) {
-  gpu_inputs_.push_back(input);
-  input_index_map_.push_back(
-      std::make_pair(false, gpu_inputs_.size()-1));
-}
-
-template <>
 Tensor<CPUBackend>* SampleWorkspace::Output(int idx) {
   NDLL_ENFORCE_VALID_INDEX((size_t)idx, output_index_map_.size());
 
@@ -103,20 +49,6 @@ Tensor<GPUBackend>* SampleWorkspace::Output(int idx) {
   NDLL_ENFORCE(!tensor_meta.first, "Output Tensor with given "
       "index does not have the calling backend type (GPUBackend)");
   return gpu_outputs_[tensor_meta.second].get();
-}
-
-template <>
-void SampleWorkspace::AddOutput(shared_ptr<Tensor<CPUBackend>> output) {
-  cpu_outputs_.push_back(output);
-  output_index_map_.push_back(
-      std::make_pair(true, cpu_outputs_.size()-1));
-}
-
-template <>
-void SampleWorkspace::AddOutput(shared_ptr<Tensor<GPUBackend>> output) {
-  gpu_outputs_.push_back(output);
-  output_index_map_.push_back(
-      std::make_pair(false, gpu_outputs_.size()-1));
 }
 
 }  // namespace ndll
