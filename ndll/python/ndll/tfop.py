564d0d7bf62bbbef8c9d9a09b4eeb4c0067592f5
Serge Panev <spanev@nvidia.com>
2018-03-20 14:37:43 -0700

    Fix typo

--- ndll/python/ndll/tfop.py
+++ ndll/python/ndll/tfop.py
@@ -6,7 +6,7 @@ def ndllTFOp():
         ndllop_module = tf.load_op_library(libndllop)
         ndllop = ndllop_module.ndll
     except Exception:
-        print(libndllop + " not install: add /usr/local/lib to LD_LIBRARY_PATH")
+        print(libndllop + " not found: add /usr/local/lib/ to LD_LIBRARY_PATH")
         libndllop = '/opt/ndll/build/ndll/libndllop.so'
         ndllop_module = tf.load_op_library(libndllop)
         ndllop = ndllop_module.ndll
