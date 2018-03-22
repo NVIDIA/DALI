5f7b88d5bd0f4120b0ec0ea7a01d934944b68362
Serge Panev <spanev@nvidia.com>
2018-03-21 19:17:19 -0700

    Add nvcnn script from TF

--- ndll/tensorflow/ndllop.cc
+++ ndll/tensorflow/ndllop.cc
@@ -10,15 +10,42 @@
 
 namespace tf = tensorflow;
 
+tf::TensorShape NdllToShape(std::vector<ndll::Index> ns) {
+  tf::TensorShape ts;
+  for (int i = 0; i < static_cast<int>(ns.size()); ++i) {
+    ts.InsertDim(i, ns[i]);
+  }
+  return ts;
+}
+
 REGISTER_OP("Ndll")
   .Attr("serialized_pipeline: string")
   .Attr("batch_size: int = 128")
   .Attr("num_threads: int = 2")
   .Attr("device_id: int = 0")
   .Output("batch: float")
-  .Output("label: float");
-  // ignoring shape for now
-  // .SetShapeFn(shape_inference::UnknownShape);
+  .Output("label: float")
+  .SetShapeFn([](tf::shape_inference::InferenceContext* c) {
+    std::string serialized_pipeline;
+    TF_RETURN_IF_ERROR(c->GetAttr("serialized_pipeline", &serialized_pipeline));
+    // TODO(spanev) tmp solution: get static shape from pipeline
+   /* {
+      PipelineHandle pipe_handle;
+      std::vector<ndll::Index> data_tensor_shape = ShapeAt(&pipe_handle, 0);
+      std::vector<ndll::Index> label_tensor_shape = ShapeAt(&pipe_handle, 1);
+      tf::TensorShape data_shape = NdllToShape(data_tensor_shape);
+      tf::TensorShape label_shape = NdllToShape(label_tensor_shape);
+      tf::shape_inference::ShapeHandle data_s;
+      tf::shape_inference::ShapeHandle label_s;
+      TF_RETURN_IF_ERROR(c->MakeShapeFromTensorShape(data_shape, &data_s));
+      TF_RETURN_IF_ERROR(c->MakeShapeFromTensorShape(label_shape, &label_s));
+      c->set_output(0, data_s);
+      c->set_output(1, label_s);
+      delete(&pipe_handle);
+    }
+    */
+    return tf::Status::OK();
+  });
 
 class NdllOp : public tf::OpKernel {
  public:
@@ -34,6 +61,7 @@ class NdllOp : public tf::OpKernel {
     OP_REQUIRES_OK(context, context->GetAttr("batch_size", &batch_size));
     OP_REQUIRES_OK(context, context->GetAttr("num_threads", &num_threads));
     OP_REQUIRES_OK(context, context->GetAttr("device_id", &device_id));
+    LOG_LINE << "Initializing...\n";
 
     CreatePipeline(&pipe_handle_,
                    serialized_pipeline.c_str(),
@@ -41,25 +69,23 @@ class NdllOp : public tf::OpKernel {
                    batch_size,
                    num_threads,
                    device_id);
+
+    LOG_LINE << "Pipeline created\n";
     Run(&pipe_handle_);
+    LOG_LINE << "After first run\n";
   }
 
   ~NdllOp() override {
     DeletePipeline(&pipe_handle_);
   }
 
-  inline tf::TensorShape NdllToShape(std::vector<ndll::Index> ns) {
-    tf::TensorShape ts;
-    for (int i = 0; i < static_cast<int>(ns.size()); ++i) {
-      ts.InsertDim(i, ns[i]);
-    }
-    return ts;
-  }
-
   void Compute(tf::OpKernelContext* context) override {
+    LOG_LINE << "Computing...\n";
     Run(&pipe_handle_);
+    LOG_LINE << "Computing...\n";
     Output(&pipe_handle_);
 
+    LOG_LINE << "After output...\n";
     ndll::DeviceWorkspace* ws = pipe_handle_.ws;
 
     // Classification
