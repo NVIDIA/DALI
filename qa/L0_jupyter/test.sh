a7dc04382055299b27d100398c3756c4353d1d94
jwoolley <jwoolley@nvidia.com>
2018-06-15 06:18:25 -0700

    Fix the dependencies for the Jupyter test

--- qa/L0_jupyter/test.sh
+++ qa/L0_jupyter/test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -e
 
-cd /opt/dali/examples
+apt-get update
 
-pip install jupyter numpy==1.11.1 matplotlib python-opencv==3.1.0
+# glib-2.0 depends on python2, so reinstall the desired python afterward to make sure defaults are right
+apt-get install -y --no-install-recommends glib-2.0
+apt-get install -y --no-install-recommends --reinstall python$PYVER python$PYVER-dev
 
+pip install jupyter numpy==1.11.1 matplotlib opencv-python==3.1.0
+
+# attempt to run jupyter on all example notebooks
 mkdir -p /opt/dali/idx_files
 
+cd /opt/dali/examples
 ls *.ipynb | xargs -i jupyter nbconvert \
                    --to notebook --execute \
                    --ExecutePreprocessor.kernel_name=python2 \
