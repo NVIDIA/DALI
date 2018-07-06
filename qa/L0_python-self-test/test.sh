32c19e6b10cceddae1b38139d532189075db5094
Janusz <39967756+JanuszL@users.noreply.github.com>
2018-07-06 15:32:21 +0200

    Add Tensorflow 1.7 support (#24)
    Add python script providing list of env setup permutations
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/L0_python-self-test/test.sh
+++ qa/L0_python-self-test/test.sh
@@ -1,16 +1,38 @@
 #!/bin/bash -e
+# used pip packages
+pip_packages="nose opencv-python numpy"
 
 pushd ../..
+topdir=$(pwd)
 
 # Install dependencies
 # Note: glib-2.0 depends on python2, so reinstall the desired python afterward to make sure defaults are right
 apt-get update
 apt-get install -y --no-install-recommends glib-2.0
 apt-get install -y --no-install-recommends --reinstall python$PYVER python$PYVER-dev
-pip install nose numpy==1.11.1 opencv-python==3.1.0
+
+cd dali/test/python
+
+count=$($topdir/qa/setup_packages.py -n -u $pip_packages)
+
+for i in `seq 0 $count`;
+do
+    # install pacakges
+    inst=$($topdir/qa/setup_packages.py -i $i -u $pip_packages)
+    if [ -n "$inst" ]
+    then
+      pip install $inst
+    fi
+    # test code
+    nosetests --verbose test_pipeline.py
+    # remove pacakges
+    remove=$($topdir/qa/setup_packages.py -r  -u $pip_packages)
+    if [ -n "$remove" ]
+    then
+      pip uninstall -y $remove
+    fi
+done 
 
 # Run python tests
-cd dali/test/python
-nosetests --verbose test_pipeline.py
 
 popd
