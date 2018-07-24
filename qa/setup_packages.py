2b581b0e963715ba55c686a4c780164b70b00405
Cliff Woolley <jwoolley@nvidia.com>
2018-07-24 11:32:58 -0700

    Further test TF binary compat
    Tested in CI pipeline 32385.
    
    Signed-off-by: Cliff Woolley <jwoolley@nvidia.com>

--- qa/setup_packages.py
+++ qa/setup_packages.py
@@ -20,7 +20,7 @@ except ImportError:
 packages = {"numpy" : ["1.11.1"],
             "opencv-python" : ["3.1.0"],
             "mxnet-cu90" : ["1.3.0b20180612"],
-            "tensorflow-gpu" : ["1.7", "1.8", "1.9"],
+            "tensorflow-gpu" : ["1.7", "1.8", "1.9", "1.10.0rc0"],
             "pytorch" : ["http://download.pytorch.org/whl/cu90/torch-0.4.0-{0}.whl"]
             }
 
