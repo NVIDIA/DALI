1b1797834e3edeeea45ee4c3f854b65aa77ad86b
ptredak <ptredak@nvidia.com>
2018-07-27 14:18:39 -0700

    Bumb version to 0.2
    Signed-off-by: ptredak <ptredak@nvidia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2018, NVIDIA Corporation'
 author = u'NVIDIA Corporation'
 
 # The short X.Y version
-version = u'0.1'
+version = u'0.2'
 # The full version, including alpha/beta/rc tags
-release = u'0.1.1'
+release = u'0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
