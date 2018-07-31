5e810500e72c576db9000ae423ac0809499af0c0
Serge Panev <spanev@nvidia.com>
2018-07-31 11:08:27 -0700

    Add COCOReader operator - a reader that reads COCO dataset
    Signed-off-by: Serge Panev <spanev@nvidia.com>

---
+++ dali/pipeline/operators/reader/coco_reader_op.h
@@ -0,0 +1,113 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_PIPELINE_OPERATORS_READER_COCO_READER_OP_H_
+#define DALI_PIPELINE_OPERATORS_READER_COCO_READER_OP_H_
+
+#include <fstream>
+#include <map>
+
+#include "dali/pipeline/operators/reader/reader_op.h"
+#include "dali/pipeline/operators/reader/loader/file_loader.h"
+#include "dali/pipeline/operators/reader/parser/coco_parser.h"
+#include "dali/util/json.h"
+
+namespace dali {
+
+class COCOReader : public DataReader<CPUBackend> {
+ public:
+  explicit COCOReader(const OpSpec& spec)
+  : DataReader<CPUBackend>(spec),
+    annotations_filename_(spec.GetArgument<std::string>("annotations_file")) {
+    ParseAnnotationFiles();
+    // TODO pass image_id vector to FileLoader
+    loader_.reset(new FileLoader(spec, image_id_pairs));
+    parser_.reset(new COCOParser(spec, annotations_multimap_));
+  }
+
+  DEFAULT_READER_DESTRUCTOR(COCOReader, CPUBackend);
+
+  void RunImpl(SampleWorkspace* ws, const int i) override {
+    const int idx = ws->data_idx();
+
+    auto* raw_data = prefetched_batch_[idx];
+
+    parser_->Parse(raw_data->data<uint8_t>(), raw_data->size(), ws);
+
+    return;
+  }
+
+ protected:
+  void ParseAnnotationFiles() {
+    // Loading raw json into the RAM
+    std::ifstream f(annotations_filename_);
+    DALI_ENFORCE(f, "Could not open JSON annotations file");
+    std::vector<char> raw_json;
+    f.seekg(0, std::ios::end);
+    raw_json.reserve(f.tellg());
+    f.seekg(0, std::ios::beg);
+    raw_json.assign((std::istreambuf_iterator<char>(f)),
+                    std::istreambuf_iterator<char>());
+
+    // Parsing the JSON into image vector and annotation multimap
+    char *source = reinterpret_cast<char*>(raw_json.data());
+    auto j = nlohmann::json::parse(source);
+
+    // Parse images
+    auto images = j.find("images");
+    DALI_ENFORCE(images != j.end(), "`images` not found in JSON annotions file");
+    for (auto& im : *images) {
+      auto id_it = im.find("id");
+      DALI_ENFORCE(id_it != im.end(), "`id` not found in JSON annotions file");
+      auto file_name_it = im.find("file_name");
+      DALI_ENFORCE(file_name_it != im.end(), "`file_name` not found in JSON annotions file");
+
+      int id = id_it.value().get<int>();
+      std::string file_name = file_name_it.value().get<std::string>();
+
+      image_id_pairs.push_back(std::make_pair(file_name, id));
+    }
+
+    // Parse annotations
+    auto annotations = j.find("annotations");
+    DALI_ENFORCE(annotations != j.end(), "`annotations` not found in JSON annotions file");
+    int annotation_size = (*annotations).size();
+
+    for (auto& an : *annotations) {
+      auto image_id_it = an.find("image_id");
+      DALI_ENFORCE(image_id_it != an.end(), "`image_id` not found in JSON annotions file");
+      auto category_id_it = an.find("category_id");
+      DALI_ENFORCE(category_id_it != an.end(), "`category_id` not found in JSON annotions file");
+      auto bbox_it = an.find("bbox");
+      DALI_ENFORCE(bbox_it != an.end(), "`bbox` not found in JSON annotions file");
+
+      int image_id = image_id_it.value().get<int>();
+      int category_id = category_id_it.value().get<int>();
+      std::vector<float> bbox = bbox_it.value().get<std::vector<float>>();
+
+      annotations_multimap_.insert(
+          std::make_pair(image_id,
+            Annotation(bbox[0], bbox[1], bbox[2], bbox[3], category_id)));
+    }
+  }
+
+  std::string annotations_filename_;
+  std::multimap<int, Annotation> annotations_multimap_;
+  std::vector<std::pair<std::string, int>> image_id_pairs;
+  USE_READER_OPERATOR_MEMBERS(CPUBackend);
+};
+
+}  // namespace dali
+
+#endif  // DALI_PIPELINE_OPERATORS_READER_COCO_READER_OP_H_
---
+++ dali/pipeline/operators/reader/loader/file_loader.cc
@@ -0,0 +1,101 @@
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#include "dali/common.h"
+#include "dali/pipeline/operators/reader/loader/file_loader.h"
+#include "dali/util/file.h"
+
+namespace dali {
+
+void filesystem::assemble_file_list(const std::string& path, int label,
+                        std::vector<std::pair<std::string, int>> *file_label_pairs) {
+  DIR *dir = opendir(path.c_str());
+  struct dirent *entry;
+
+  const std::vector<std::string> valid_extensions({".jpg", ".jpeg", ".png", ".bmp"});
+
+  while ((entry = readdir(dir))) {
+    std::string full_path = path + "/" + std::string{entry->d_name};
+    struct stat s;
+    stat(full_path.c_str(), &s);
+    if (S_ISREG(s.st_mode)) {
+      std::string full_path_lowercase = full_path;
+      std::transform(full_path_lowercase.begin(), full_path_lowercase.end(),
+                     full_path_lowercase.begin(), ::tolower);
+      for (const std::string& s : valid_extensions) {
+        size_t pos = full_path_lowercase.rfind(s);
+        if (pos != std::string::npos && pos + s.size() == full_path_lowercase.size()) {
+          file_label_pairs->push_back(std::make_pair(full_path, label));
+          break;
+        }
+      }
+    }
+  }
+  closedir(dir);
+}
+
+vector<std::pair<string, int>> filesystem::traverse_directories(const std::string& path) {
+  // open the root
+  DIR *dir = opendir(path.c_str());
+
+  DALI_ENFORCE(dir != nullptr,
+      "Directory " + path + " could not be opened.");
+
+  struct dirent *entry;
+
+  std::vector<std::pair<std::string, int>> file_label_pairs;
+  std::vector<std::string> dir_path_list;
+
+  while ((entry = readdir(dir))) {
+    struct stat s;
+    std::string full_path = path + "/" + std::string(entry->d_name);
+    int ret = stat(full_path.c_str(), &s);
+    DALI_ENFORCE(ret == 0,
+        "Could not access " + full_path + " during directory traversal.");
+    if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
+    if (S_ISDIR(s.st_mode)) {
+      dir_path_list.push_back(full_path);
+    }
+  }
+  // sort directories to preserve class alphabetic order, as readdir could
+  // return unordered dir list. Otherwise file reader for training and validation
+  // could return directories with the same names in completely different order
+  std::sort(dir_path_list.begin(), dir_path_list.end());
+  for (unsigned dir_count = 0; dir_count < dir_path_list.size(); ++dir_count) {
+      assemble_file_list(dir_path_list[dir_count], dir_count, &file_label_pairs);
+  }
+  printf("read %lu files from %lu directories\n", file_label_pairs.size(), dir_path_list.size());
+
+  closedir(dir);
+
+  return file_label_pairs;
+}
+void FileLoader::ReadSample(Tensor<CPUBackend>* tensor) {
+  auto image_pair = image_label_pairs_[current_index_++];
+
+  // handle wrap-around
+  if (current_index_ == Size()) {
+    current_index_ = 0;
+  }
+
+  FileStream *current_image = FileStream::Open(image_pair.first);
+  Index image_size = current_image->Size();
+
+  // resize tensor to hold [image, label]
+  tensor->Resize({image_size + static_cast<Index>(sizeof(int))});
+
+  // copy the image
+  current_image->Read(tensor->mutable_data<uint8_t>(), image_size);
+
+  // close the file handle
+  current_image->Close();
+
+  // copy the label
+  *(reinterpret_cast<int*>(&tensor->mutable_data<uint8_t>()[image_size])) = image_pair.second;
+}
+
+Index FileLoader::Size() {
+  return static_cast<Index>(image_label_pairs_.size());
+}
+}  // namespace dali
