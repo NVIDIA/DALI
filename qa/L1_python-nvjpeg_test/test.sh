64a6231ae8b3268e2c3ef550853082bc6c521e46
Janusz Lisiecki <jlisiecki@nvidia.com>
2018-08-20 15:27:04 +0200

    Put common test code into one file
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/L1_python-nvjpeg_test/test.sh
+++ qa/L1_python-nvjpeg_test/test.sh
@@ -3,34 +3,14 @@
 pip_packages=""
 
 pushd ../..
-topdir=$(pwd)
-
-# Install dependencies
-# Note: glib-2.0 depends on python2, so reinstall the desired python afterward to make sure defaults are right
-apt-get update
-apt-get install -y --no-install-recommends glib-2.0
-apt-get install -y --no-install-recommends --reinstall python$PYVER python$PYVER-dev
 
 cd dali/test/python
 
-count=$($topdir/qa/setup_packages.py -n -u $pip_packages)
-
-for i in `seq 0 $count`;
-do
-    # install pacakges
-    inst=$($topdir/qa/setup_packages.py -i $i -u $pip_packages)
-    if [ -n "$inst" ]
-    then
-      pip install $inst
-    fi
+test_body() {
     # test code
     python test_data_containers.py
-    # remove pacakges
-    remove=$($topdir/qa/setup_packages.py -r  -u $pip_packages)
-    if [ -n "$remove" ]
-    then
-      pip uninstall -y $remove
-    fi
-done
+}
+
+source ../../../qa/test_template.sh
 
 popd
