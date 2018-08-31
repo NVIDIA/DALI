1588b32a80a09f041da010aaef76e61cf235b125
Andrei <andreii@nvidia.com>
2018-08-31 12:18:07 -0700

    New files for Crop operators added
    Signed-off-by: Andrei <andreii@nvidia.com>

---
+++ dali/pipeline/operators/crop/crop_test.cc
@@ -0,0 +1,36 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dali/test/dali_test_matching.h"
+
+namespace dali {
+
+template <typename ImgType>
+class CropTest : public GenericMatchingTest<ImgType> {
+};
+
+typedef ::testing::Types<RGB, BGR, Gray> Types;
+TYPED_TEST_CASE(CropTest, Types);
+
+const bool addImageType = true;
+
+TYPED_TEST(CropTest, CropNumber) {
+  this->RunTest({"Crop", {"crop", "224", DALI_INT32}}, addImageType);
+}
+
+TYPED_TEST(CropTest, CropVector) {
+  this->RunTest({"Crop", {"crop", "224, 256", DALI_INT_VEC}}, addImageType);
+}
+
+}  // namespace dali
