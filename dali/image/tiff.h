b7cf68ecadcde2309ec057f2851a02567975bc50
Michał Szołucha <mszolucha@nvidia.com>
2018-09-13 13:27:58 +0200

    Buffer for tiff implementation

--- dali/image/tiff.h
+++ dali/image/tiff.h
@@ -1,3 +1,17 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef DALI_TIFF_H
 #define DALI_TIFF_H
 
@@ -10,6 +24,57 @@
 
 namespace dali {
 
+
+class tiff_buffer {
+public:
+
+    tiff_buffer(std::string buffer, bool little_endian = false) :
+            little_endian_(little_endian) {
+        stream_ = std::istringstream(buffer);
+        buffer_size_ = buffer.length();
+    }
+
+
+    template<typename ValueType>
+    ValueType read(unsigned int offset = 0) {
+        assert(stream_.good());
+        assert(offset + sizeof(ValueType) < buffer_size_);
+        static_assert(std::is_integral<ValueType>::value, "Only integral values supported");
+
+        stream_.seekg(offset);
+        ValueType ret;
+        stream_.read(reinterpret_cast<char *>(&ret), sizeof(ret));
+        if (little_endian_) {
+            convert_le(&ret);
+        }
+        assert(stream_.good());
+        return ret;
+    }
+
+
+private:
+
+    template<typename T>
+    void convert_le(T *value) {
+        static_assert(std::is_integral<T>::value, "Converting floating point value unsupported");
+
+        char *value_bytes = reinterpret_cast<char *>(value);
+        std::vector<char> value_copy(value_bytes, value_bytes + sizeof(T));
+
+        {
+            int i = 0;
+            for (auto it = value_copy.rbegin(); it != value_copy.rend(); ++it, ++i) {
+                value_bytes[i] = *it;
+            }
+        }
+    }
+
+
+    std::istringstream stream_;
+    size_t buffer_size_;
+    bool little_endian_;
+};
+
 /**
  * @brief Returns 'true' if input compressed image is a tiff
  */
@@ -20,6 +85,7 @@ extern bool CheckIsTiff(const unsigned char *tiff);
  * @brief Get dimensions of tiff encoded image
  */
 extern DALIError_t GetTiffImageDims(const unsigned char *tiff, int size, int *h, int *w);
+
 
 /**
  * @brief Decodes 'tiff' into the buffer pointed to by 'image'
---
+++ dali/image/tiff_test.cc
@@ -0,0 +1,66 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tiff.h"
+#include <dali/test/dali_test.h>
+
+#include <sstream>
+
+using namespace std;
+
+namespace dali {
+
+class MyBufferTest : public DALITest {
+protected:
+    std::string bin = {'\x41', '\x42', '\x43', '\x44', '\x45', '\x46', '\x47', '\x48', '\x49', '\x4a', '\x4b', '\x4c',
+                       '\x4d', '\x4e', '\x4f', '\x50'};
+};
+
+TEST_F(MyBufferTest, TiffBufferBigEndianTest) {
+    tiff_buffer buf(bin);
+    EXPECT_EQ(65, buf.read<char>());
+    EXPECT_EQ(16961, buf.read<short>());
+    EXPECT_EQ(1145258561, buf.read<int>());
+    EXPECT_EQ(5208208757389214273, buf.read<long>());
+    EXPECT_EQ(65, buf.read<unsigned char>());
+    EXPECT_EQ(16961, buf.read<unsigned short>());
+    EXPECT_EQ(1145258561, buf.read<unsigned int>());
+    EXPECT_EQ(5208208757389214273, buf.read<unsigned long>());
+}
+
+
+TEST_F(MyBufferTest, TiffBufferLittleEndianTest) {
+    tiff_buffer buf(bin, true);
+    EXPECT_EQ(65, buf.read<char>());
+    EXPECT_EQ(16706, buf.read<short>());
+    EXPECT_EQ(1094861636, buf.read<int>());
+    EXPECT_EQ(4702394921427289928, buf.read<long>());
+    EXPECT_EQ(65, buf.read<unsigned char>());
+    EXPECT_EQ(16706, buf.read<unsigned short>());
+    EXPECT_EQ(1094861636, buf.read<unsigned int>());
+    EXPECT_EQ(4702394921427289928, buf.read<unsigned long>());
+}
+
+
+TEST_F(MyBufferTest, TiffBufferOffsetTest) {
+    tiff_buffer buf_big(bin);
+    EXPECT_EQ(75, buf_big.read<char>(10));
+    EXPECT_EQ(19274, buf_big.read<short>(9));
+    tiff_buffer buf_little(bin, true);
+    EXPECT_EQ(75, buf_little.read<char>(10));
+    EXPECT_EQ(1145390663, buf_little.read<int>(3));
+    EXPECT_EQ(4774735094265366601, buf_little.read<long>(1));
+}
+
+}  // namespace dali
