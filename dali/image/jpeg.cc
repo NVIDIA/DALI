dc514b5f8b3ae841d6e114a2d482321be098c6a0
Janusz <39967756+JanuszL@users.noreply.github.com>
2018-09-20 20:11:25 +0200

    Fix parsing of JPEG headers (#175)
    * Fix parsing of JPEG headers for host decoder, add more tests
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>
    
    * Update comment for jpeg.cc code inspiration
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/image/jpeg.cc
+++ dali/image/jpeg.cc
@@ -57,37 +57,32 @@ void PrintSubsampling(int sampling) {
 // Slightly modified from  https://github.com/apache/incubator-mxnet/blob/master/plugin/opencv/cv_api.cc
 // http://www.64lines.com/jpeg-width-height
 // Gets the JPEG size from the array of data passed to the function, file reference: http://www.obrador.com/essentialjpeg/headerinfo.htm
+// Final adjustments based on https://github.com/scardine/image_size
 bool get_jpeg_size(const uint8 *data, size_t data_size, int *height, int *width) {
   // Check for valid JPEG image
   unsigned int i = 0;  // Keeps track of the position within the file
-  if (data[i] == 0xFF && data[i+1] == 0xD8 && data[i+2] == 0xFF && data[i+3] == 0xE0) {
+  if (data[i] == 0xFF && data[i+1] == 0xD8) {
     i += 4;
-    // Check for valid JPEG header (null terminated JFIF)
-    if (data[i+2] == 'J' && data[i+3] == 'F' && data[i+4] == 'I'
-        && data[i+5] == 'F' && data[i+6] == 0x00) {
-      // Retrieve the block length of the first block since
-      // the first block will not contain the size of file
-      uint16_t block_length = data[i] * 256 + data[i+1];
-      while (i < data_size) {
-        i+=block_length;  // Increase the file index to get to the next block
-        if (i >= data_size) return false;  // Check to protect against segmentation faults
-        if (data[i] != 0xFF) return false;  // Check that we are truly at the start of another block
-        if (data[i+1] == 0xC0) {
-          // 0xFFC0 is the "Start of frame" marker which contains the file size
-          // The structure of the 0xFFC0 block is quite simple
-          // [0xFFC0][ushort length][uchar precision][ushort x][ushort y]
-          *height = data[i+5]*256 + data[i+6];
-          *width = data[i+7]*256 + data[i+8];
-          return true;
-        } else {
-          i+=2;  // Skip the block marker
-          block_length = data[i] * 256 + data[i+1];  // Go to the next block
-        }
+    // Retrieve the block length of the first block since
+    // the first block will not contain the size of file
+    uint16_t block_length = data[i] * 256 + data[i+1];
+    while (i < data_size) {
+      i+=block_length;  // Increase the file index to get to the next block
+      if (i >= data_size) return false;  // Check to protect against segmentation faults
+      if (data[i] != 0xFF) return false;  // Check that we are truly at the start of another block
+      if (data[i+1] >= 0xC0 && data[i+1] <= 0xC3) {
+        // 0xFFC0 is the "Start of frame" marker which contains the file size
+        // The structure of the 0xFFC0 block is quite simple
+        // [0xFFC0][ushort length][uchar precision][ushort x][ushort y]
+        *height = data[i+5]*256 + data[i+6];
+        *width = data[i+7]*256 + data[i+8];
+        return true;
+      } else {
+        i+=2;  // Skip the block marker
+        block_length = data[i] * 256 + data[i+1];  // Go to the next block
       }
-      return false;  // If this point is reached then no size was found
-    } else {
-      return false;  // Not a valid JFIF string
     }
+    return false;  // If this point is reached then no size was found
   } else {
     return false;  // Not a valid SOI header
   }
