88232553265406b476e2ff6b474bee5d87232254
Przemyslaw Tredak <ptrendx@gmail.com>
2018-09-22 01:23:57 -0700

    Fix reporing of the end of epoch in MXNet and pyTorch plugins (#180)
    Signed-off-by: ptredak <ptredak@nvidia.com>

--- dali/python/nvidia/dali/plugin/mxnet.py
+++ dali/python/nvidia/dali/plugin/mxnet.py
@@ -116,7 +116,7 @@ class DALIGenericIterator(object):
             batch = self._first_batch
             self._first_batch = None
             return batch
-        if self._counter > self._size:
+        if self._counter >= self._size:
             raise StopIteration
         # Gather outputs
         outputs = []
@@ -177,7 +177,7 @@ class DALIGenericIterator(object):
         DALI iterators do not support resetting before the end of the epoch
         and will ignore such request.
         """
-        if self._counter > self._size:
+        if self._counter >= self._size:
             self._counter = self._counter % self._size
         else:
             logging.warning("DALI iterator does not support resetting while epoch is not finished. Ignoring...")
