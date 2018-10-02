63a6c310dc818437da175dd6786d900ae12c06ea
Michał Szołucha <mszolucha@nvidia.com>
2018-10-02 13:38:54 +0200

    Catering for lint
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/pipeline/operators/geometric/bb_flip_test.cc
+++ dali/pipeline/operators/geometric/bb_flip_test.cc
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "dali/test/dali_test_single_op.h"
-
 #include <tuple>
+#include "dali/test/dali_test_single_op.h"
 
 namespace dali {
 
