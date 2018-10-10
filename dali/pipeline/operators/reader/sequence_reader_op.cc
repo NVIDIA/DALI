d0c98a1c36072af680f3f07fb4476fde1c1d98d2
Krzysztof Lecki <klecki@nvidia.com>
2018-10-10 14:41:43 +0200

    Implement SequenceReaderOp with Loader and Parser
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

---
+++ dali/pipeline/operators/reader/sequence_reader_op.cc
@@ -0,0 +1,46 @@
+// Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+
+#include "dali/pipeline/operators/reader/sequence_reader_op.h"
+
+namespace dali {
+
+void SequenceReader::RunImpl(SampleWorkspace* ws, const int i) {
+  const int idx = ws->data_idx();
+
+  auto* sequence = prefetched_batch_[idx];
+
+  parser_->Parse(*sequence, ws);
+}
+
+DALI_REGISTER_OPERATOR(SequenceReader, SequenceReader, CPU);
+
+DALI_SCHEMA(SequenceReader)
+    .DocStr(
+        "Read [Frame] sequences from a directory representing collection of "
+        "streams")
+    .NumInput(0)
+    .NumOutput(1)  // ([Frames])
+    .AddArg("file_root",
+            R"code(Path to a directory containing streams (directories representing streams).)code",
+            DALI_STRING)
+    .AddArg("sequence_length",
+            R"code(Lenght of sequence to load for each sample)code", DALI_INT32)
+    .AddOptionalArg("image_type",
+            R"code(The color space of input and output image)code", DALI_RGB, false)
+    .AddParent("LoaderBase");
+
+}  // namespace dali
