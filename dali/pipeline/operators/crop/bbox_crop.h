7aaacdad47bc72f847450a00d04ad4476939109e
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2018-10-22 16:20:09 +0200

    Fix invalid types in benchmarks
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

--- dali/pipeline/operators/crop/bbox_crop.h
+++ dali/pipeline/operators/crop/bbox_crop.h
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef DALI_PIPELINE_OPERATORS_CROP_CROP_H_
-#define DALI_PIPELINE_OPERATORS_CROP_CROP_H_
+#ifndef DALI_PIPELINE_OPERATORS_CROP_BBOX_CROP_H_
+#define DALI_PIPELINE_OPERATORS_CROP_BBOX_CROP_H_
 
 #include <vector>
 #include <utility>
