a75824e97690824b9c01b05bf2a95640d851d0cd
Serge Panev <spanev@nvidia.com>
2018-10-30 11:05:57 +0100

    Add qa L1 test for TensorFlow + DALI RN50 end-to-end
    Signed-off-by: Serge Panev <spanev@nvidia.com>

---
+++ qa/L1_tensorflow-dali_test/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+# used pip packages
+pip_packages="opencv-python tensorflow-gpu"
+
+pushd ../..
+
+cd docs/examples/tensorflow/demo
+
+mkdir -p idx-files/
+
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+
+test_body() {
+    # test code
+    for file in $(ls /data/imagenet/train-val-tfrecord-480-subset);
+    do
+        python ../../../../tools/tfrecord2idx /data/imagenet/train-val-tfrecord-480-subset/${file} \
+            idx-files/${file}.idx;
+    done
+
+    mpiexec --allow-run-as-root --bind-to socket -np ${NUM_GPUS} \
+        python -u resnet.py --layers=18 \
+        --data_dir=/data/imagenet/train-val-tfrecord-480-subset --data_idx_dir=idx-files/ \
+        --precision=fp16 --num_iter=100  --iter_unit=batch --display_every=50 \
+        --batch=256 --dali_cpu --log_dir=dali-log
+}
+
+source ../../../../qa/test_template.sh
+
+popd
