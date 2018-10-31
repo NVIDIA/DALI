aed1d9f488e3f1ef214a4af89ec1f017da7705f1
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2018-10-31 15:39:10 +0100

    Add skeleton of GPU slice
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

---
+++ dali/pipeline/operators/crop/slice.cu
@@ -0,0 +1,61 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dali/pipeline/operators/crop/slice.h"
+
+namespace dali {
+
+template <>
+void Slice<GPUBackend>::DataDependentSetup(DeviceWorkspace *ws) {
+//  // Assumes xywh
+//  const auto &input = ws->Input<CPUBackend>(0);
+//  const auto &begin = ws->Input<CPUBackend>(1);
+//
+//  const int H = input.shape()[0];
+//  const int W = input.shape()[1];
+//
+//  const auto &size = ws->Input<CPUBackend>(2);
+//
+//  crop_width_[ws->data_idx()] = static_cast<int>(size.template data<float>()[0]);
+//  crop_height_[ws->data_idx()] = static_cast<int>(size.template data<float>()[1]);
+//
+//  per_sample_dimensions_[ws->thread_idx()] = std::make_pair(H, W);
+//
+//  const int crop_y = begin.template data<float>()[1];
+//  const int crop_x = begin.template data<float>()[0];
+//
+//  per_sample_crop_[ws->thread_idx()] = std::make_pair(crop_y, crop_x);
+}
+
+template <>
+void Slice<GPUBackend>::RunImpl(DeviceWorkspace *ws, int idx) {
+//  DataDependentSetup(ws);
+//
+//  Crop<CPUBackend>::RunImpl(ws, idx);
+}
+
+template <>
+void Slice<GPUBackend>::SetupSharedSampleParams(DeviceWorkspace *ws) {
+//  DALI_ENFORCE(ws->NumInput() == 3, "Expected 3 inputs. Received: " +
+//                                        std::to_string(ws->NumInput()));
+//
+//  if (output_type_ == DALI_NO_TYPE) {
+//    const auto &input = ws->Input<CPUBackend>(0);
+//    output_type_ = input.type().id();
+//  }
+}
+
+DALI_REGISTER_OPERATOR(Slice, Slice<GPUBackend>, GPU);
+
+}  // namespace dali
