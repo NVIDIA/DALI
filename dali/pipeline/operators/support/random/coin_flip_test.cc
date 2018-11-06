c092a0067599ee89b695765f06175de4a196b85a
Michał Szołucha <mszolucha@nvidia.com>
2018-11-06 12:38:06 +0100

    test new api proposal
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/pipeline/operators/support/random/coin_flip_test.cc
+++ dali/pipeline/operators/support/random/coin_flip_test.cc
@@ -1,30 +1,39 @@
-//#include <dali/pipeline/data/backend.h>
-//#include "dali/test/dali_operator_test.h"
+#include <dali/pipeline/data/backend.h>
+#include "dali/test/dali_operator_test.h"
 
 namespace dali {
 
-//class CoinFlipTest : public DaliOperatorTest<int, float> {
-//    int SetInputs() const override {
-//        return -1;
-//    }
-//
-//    float SetAnticipatedOutputs() const override {
-//        return -1;
-//    }
-//
-//
-//    std::string SetOperator() const override {
-//        return "CoinFlip";
-//    }
-//
-//
-//    bool Verify(float outputs, float anticipated_outputs) const override {
-//        return false;
-//    }
-//};
-//
-//TEST_F(CoinFlipTest, AlwaysTrueTest) {
-//    this->RunTest({{"probability", 0.f}});
-//}
+class CoinFlipV2Test : public DaliOperatorTest<int, float> {
+
+
+  int SetInputs() const override {
+    return -1;
+  }
+
+
+  std::string SetOperator() const override {
+    return "CoinFlip";
+  }
+
+
+  bool Verify(float outputs, float anticipated_outputs) const override {
+    auto diff = outputs - anticipated_outputs;
+    diff = diff < 0 ? -diff : diff;
+    return diff < epsilon_;
+  }
+
+
+ private:
+  float epsilon_ = 0.0001f;
+};
+
+TEST_F(CoinFlipV2Test, Always1) {
+  this->RunTest({{"probability", 1.f}}, 1);
+}
+
+
+TEST_F(CoinFlipV2Test, Always0) {
+  this->RunTest({{"probability", 0.f}}, 0);
+}
 
 }  // namespace dali
--- dali/test/dali_operator_test.h
+++ dali/test/dali_operator_test.h
@@ -8,110 +8,132 @@
 namespace dali {
 
 template<typename T>
-struct is_tuple;
+struct is_tuple : std::false_type {
+};
+
+template<typename... T>
+struct is_tuple<std::tuple<T...>> : std::true_type {
+};
 
 template<typename Inputs, typename Outputs>
 class DaliOperatorTest : public ::testing::Test {
-    //is_tuple
-public:
-    using Arguments =  std::map<std::string, float>; // TODO custom type
+  //is_tuple
+ public:
+  using Arguments =  std::map<std::string, float>; // TODO boost::any
 
 //    template<typename Backend>
-    void RunTest(Arguments operator_arguments) {
-        //is base of Backend
-//        auto op_spec = CreateOpSpec(operator_name_, operator_arguments);
-//        AddOperatorToPipeline(op_spec);
-//        RunPipeline(op_spec);
-//        ASSERT_TRUE(Verify(outputs_, anticipated_outputs_)) << "Działka\n";
+  void RunTest(Arguments operator_arguments, Outputs anticipated_outputs) {
+    //is base of Backend
+    auto op_spec = CreateOpSpec(operator_name_, operator_arguments);
+    AddOperatorToPipeline(op_spec);
+    outputs_ = RunPipeline<CPUBackend>(op_spec);
+    ASSERT_TRUE(Verify(outputs_, anticipated_outputs));
+  }
+
+
+ private:
+  virtual Inputs SetInputs() const = 0;
+
+  virtual std::string SetOperator() const = 0;
+
+  virtual bool Verify(Outputs outputs, Outputs anticipated_outputs) const = 0;
+
+
+  void SetUp() final {
+    inputs_ = SetInputs();
+    operator_name_ = SetOperator();
+    InitPipeline();
+    AddInputsToPipeline("input");
+    CreateWorkspace();
+  }
+
+
+  void TearDown() final {
+
+  }
+
+
+  void InitPipeline() {
+    if (!pipeline_.get()) {
+      pipeline_.reset(new Pipeline(batch_size_, num_threads_, 0));
     }
+  }
 
 
-private:
-    virtual Inputs SetInputs() const = 0;
-
-    virtual Outputs SetAnticipatedOutputs() const = 0;
-
-    virtual std::string SetOperator() const = 0;
-
-    virtual bool Verify(Outputs outputs, Outputs anticipated_outputs) const = 0;
+//  template<typename Backend>
+  void AddInputsToPipeline(std::string input_name) {
+    pipeline_->AddExternalInput(input_name);
+    auto tensor_list = ToTensorList<CPUBackend>(inputs_);
+    pipeline_->SetExternalInput(input_name, tensor_list);
+  }
 
 
-    void SetUp() override final {
-//        inputs_ = SetInputs();
-//        anticipated_outputs_ = SetAnticipatedOutputs();
-//        operator_name_ = SetOperator();
-//        InitPipeline();
-//        AddInputsToPipeline("input");
-//        CreateWorkspace();
+  void AddOperatorToPipeline(OpSpec op_spec) {
+    pipeline_->AddOperator(op_spec);
+  }
+
+
+  DeviceWorkspace CreateWorkspace() {
+    DeviceWorkspace ws;
+    return ws;
+  }
+
+
+  /**
+   * TODO
+   * @tparam Backend
+   * @param spec
+   * @return
+   */
+  template<typename Backend>
+  Outputs RunPipeline(OpSpec spec) {
+    for (int i = 0; i < spec.NumOutput(); ++i)
+      vecoutputs_.push_back(std::make_pair(spec.OutputName(i), spec.OutputDevice(i)));
+    pipeline_->Build(vecoutputs_);
+    pipeline_->RunCPU();
+    pipeline_->RunGPU();
+    pipeline_->Outputs(&workspace_);
+    auto i = workspace_.Output<Backend>(0);
+    auto ptr = i->template data<int>();
+    return *ptr;
+  }
+
+
+  Outputs GetOutputsFromWorkspace(DeviceWorkspace ws) {
+
+  }
+
+
+//  template<typename Backend>
+  OpSpec CreateOpSpec(std::string operator_name, Arguments operator_arguments) {
+    OpSpec opspec = OpSpec(operator_name);
+    for (const auto &arg : operator_arguments) {
+      opspec.AddArg(arg.first, arg.second);
     }
+    opspec.AddOutput("cf_out", "cpu");
+    return opspec;
+  }
 
 
-    void TearDown() override final {
+  template<typename Backend>
+  TensorList<Backend> ToTensorList(const Inputs &inputs) {
+    //is_tuple
+    TensorList<Backend> tensor_list;
+    tensor_list.Resize({{1}});
+    auto ptr = tensor_list.template mutable_tensor<Inputs>(0);
+    ptr[0] = inputs;
 
-    }
+    return tensor_list;
+  };
 
-
-    void InitPipeline() {
-//        if (!pipeline_.get()) {
-//            pipeline_.reset(new Pipeline(batch_size_, num_threads_, 0));
-//        }
-    }
-
-
-//    void AddInputsToPipeline(std::string input_name) {
-//        pipeline_->AddExternalInput(input_name);
-//        auto tensor_list = ToTensorList<CPUBackend>(inputs_);
-//        TensorList<CPUBackend> tensor_list;
-//        pipeline_->SetExternalInput(input_name, tensor_list);
-//    }
-
-
-    void AddOperatorToPipeline(OpSpec op_spec) {
-//        pipeline_->AddOperator(op_spec);
-    }
-
-
-//    DeviceWorkspace CreateWorkspace() {
-//        DeviceWorkspace ws;
-//        return ws;
-//    }
-
-
-    void RunPipeline(OpSpec spec) {
-//        for (int i = 0; i < spec.NumOutput(); ++i)
-//            vecoutputs_.push_back(std::make_pair(spec.OutputName(i), spec.OutputDevice(i)));
-//        pipeline_->Build(vecoutputs_);
-//        pipeline_->RunCPU();
-//        pipeline_->RunGPU();
-//        pipeline_->Outputs(&workspace_);
-//        auto i = workspace_.Output<CPUBackend>(0);
-//        auto ptr  = i->data<int>();
-    }
-
-
-    Outputs GetOutputsFromWorkspace(DeviceWorkspace ws) {
-
-    }
-
-    OpSpec CreateOpSpec(std::string operator_name, Arguments operator_arguments) {
-//        return OpSpec(operator_name).AddArg("probability",1.f).AddOutput("cf_out","cpu");
-    }
-
-
-    template<typename Backend>
-    TensorList<Backend> ToTensorList(const Inputs &inputs) {
-        //is_tuple
-//        return TensorList<Backend>();
-    };
-
-//    Inputs inputs_;
-//    Outputs outputs_, anticipated_outputs_;
-//    std::string operator_name_;
-//    std::shared_ptr<Pipeline> pipeline_;
-//    DeviceWorkspace workspace_;
-//    int batch_size_ = 32;
-//    int num_threads_ = 2;
-//    vector<std::pair<string, string>> vecoutputs_;
+  Inputs inputs_;
+  Outputs outputs_;
+  std::string operator_name_;
+  std::shared_ptr<Pipeline> pipeline_;
+  DeviceWorkspace workspace_;
+  int batch_size_ = 32;
+  int num_threads_ = 2;
+  vector<std::pair<string, string>> vecoutputs_;
 };
 
 }  // namespace dali
