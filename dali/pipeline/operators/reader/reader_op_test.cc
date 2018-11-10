778cff81d82980504790678e4ad5d65b35580063
Krzysztof Lecki <klecki@nvidia.com>
2018-11-09 16:18:32 -0800

    Test for sequence reader
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/operators/reader/reader_op_test.cc
+++ dali/pipeline/operators/reader/reader_op_test.cc
@@ -119,4 +119,50 @@ TYPED_TEST(ReaderTest, SimpleTest) {
   return;
 }
 
+
+TYPED_TEST(ReaderTest, SequenceTest) {
+  Pipeline pipe(128, 4, 0);
+
+  pipe.AddOperator(
+      OpSpec("SequenceReader")
+      .AddArg("file_root", image_folder + "/frames/")
+      .AddArg("sequence_length", 3)
+      .AddArg("image_type", DALI_RGB)
+      .AddOutput("seq_out", "cpu"));
+
+  std::vector<std::pair<string, string>> outputs = {{"seq_out", "cpu"}};
+  pipe.Build(outputs);
+
+  DeviceWorkspace ws;
+  for (int i = 0; i < 4; ++i) {
+    printf(" ======= ITER %d ======\n", i);
+    pipe.RunCPU();
+    pipe.RunGPU();
+    pipe.Outputs(&ws);
+    auto shape = ws.Output<CPUBackend>(0)->AsTensor()->shape();
+    // We have NFHWC format
+    const auto batch_size = shape[0];
+    const auto frame_count = shape[1];
+    const auto H = shape[2];
+    const auto W = shape[3];
+    const auto C = shape[4];
+    const auto frame_size = H * W * C;
+    const auto seq_size = frame_size * frame_count;
+    for (int sample = 0; sample < batch_size; sample++) {
+      // We read samples sequentially. We have 2 "videos" of 16 frames,
+      // as sequence do not cross the boundary of one video, the highest starting frame is
+      // 13 (counting from 0, the last 3-element sequence is [13, 14, 15]).
+      auto start_frame = (i * batch_size + sample) % (16 - 3 + 1);
+      for (int frame = 0; frame < frame_count; frame++) {
+        auto off = sample * seq_size + frame * frame_size;
+        auto val = ws.Output<CPUBackend>(0)->AsTensor()->data<uint8_t>()[off];
+        decltype(val) expected = start_frame + frame;
+        ASSERT_EQ(val, expected);
+      }
+    }
+  }
+
+  return;
+}
+
 };  // namespace dali
