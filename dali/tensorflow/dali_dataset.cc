ba2d79f05e2f78924ac5acedde28a0a86bab94ca
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2018-11-27 12:15:35 +0100

    Add initial file with headers and warnings disabled
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

---
+++ dali/tensorflow/dali_dataset.cc
@@ -0,0 +1,25 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreorder"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#include "tensorflow/core/framework/common_shape_fns.h"
+#include "tensorflow/core/framework/dataset.h"
+#include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#pragma GCC diagnostic pop
+#pragma GCC diagnostic pop
+
