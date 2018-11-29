f30869ad79233fa2fb1e13d865a95f4075fbfb7d
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2018-11-29 14:45:39 +0100

    Complete python class and refactor to PEP8
    Signed-off-by: Pablo Ribalta Lorenzo <pribalta@nvidia.com>

--- dali/python/nvidia/dali/plugin/tf.py
+++ dali/python/nvidia/dali/plugin/tf.py
@@ -12,79 +12,99 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tensorflow as tf
 import os
 import glob
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
 
 _tf_plugins = glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libdali_tf*.so'))
 _dali_tf_module = None
 for _libdali_tf in _tf_plugins:
-  try:
-    _dali_tf_module = tf.load_op_library(_libdali_tf)
-    break
-  # if plugin is not compatible skip it
-  except tf.errors.NotFoundError:
-    pass
+    try:
+        _dali_tf_module = tf.load_op_library(_libdali_tf)
+        break
+    # if plugin is not compatible skip it
+    except tf.errors.NotFoundError:
+        pass
 else:
-  raise Exception('No matching DALI plugin found for installed TensorFlow version')
+    raise Exception('No matching DALI plugin found for installed TensorFlow version')
 
 _dali_tf = _dali_tf_module.dali
 
-def DALIIteratorWrapper(pipeline = None, serialized_pipeline = None, **kwargs):
-  """
-TF Plugin Wrapper
 
-This operator works in the same way as DALI TensorFlow plugin, with the exception that is also accepts Pipeline objects as the input and serializes it internally. For more information, please look **TensorFlow Plugin API reference** in the documentation.
-  """
-  if serialized_pipeline is None:
-    serialized_pipeline = pipeline.serialize()
-  return _dali_tf(serialized_pipeline=serialized_pipeline, **kwargs)
+def DALIIteratorWrapper(pipeline=None, serialized_pipeline=None, **kwargs):
+    """
+  TF Plugin Wrapper
+
+  This operator works in the same way as DALI TensorFlow plugin, with the exception that is also accepts Pipeline objects as the input and serializes it internally. For more information, please look **TensorFlow Plugin API reference** in the documentation.
+    """
+    if serialized_pipeline is None:
+        serialized_pipeline = pipeline.serialize()
+    return _dali_tf(serialized_pipeline=serialized_pipeline, **kwargs)
 
 
 def DALIIterator():
     return DALIIteratorWrapper
 
+
 # Vanilla raw operator legacy
 def DALIRawIterator():
     return _dali_tf
 
+
 dali_dataset_module = _dali_tf_module.dali_dataset
 
+
 class DALIDataset(tf.data.Dataset):
+    """A `Dataset` wrapping DALI iterators spread across a number of devices."""
 
-# todo: Add validation for types of parameters
-  def __init__(self, image_type=None, label_type=None, output_shape=None):
-    super(DALIDataset, self).__init__()
+    def __init__(self, pipeline=None, shape=None, dtypes=None, devices=None):
+        """Creates a `DALIDataset`.
+       Args:
+         pipeline: A`nvidia.dali.Pipeline` defining the augmentation to be performed
+         shapes: A `tf.TensorShape` object with the expected features output shape
+         dtypes: A collection of `tf.Dtype` with the type of the output features and labels
+         devices: A collection of `int` with device ids
 
-    if image_type:
-      self._image_type = image_type
-    else:
-      raise ValueError('No value provided for parameter \'image_type\'')
+       """
+        super(DALIDataset, self).__init__()
 
-    if label_type:
-      self._label_type = label_type
-    else:
-      raise ValueError('No value provided for parameter \'label_type\'')
+        if pipeline:
+            self._pipeline = pipeline
+        else:
+            raise ValueError('No value provided for parameter \'pipeline\'')
 
-    if output_shape:
-      self._output_shape = output_shape
-    else:
-      raise ValueError('No value provided for parameter \'output_shape\'')
+        if shape:
+            self._shape = shape
+        else:
+            raise ValueError('No value provided for parameter \'shape\'')
 
-  def _as_variant_tensor(self):
-    return dali_dataset_module.dali_dataset()
+        if dtypes:
+            self._dtypes = dtypes
+        else:
+            raise ValueError('No value provided for parameter \'dtypes\'')
 
-  @property
-  def output_types(self):
-    return self._image_type, self._label_type
+        if devices:
+            self._devices = devices
+        else:
+            raise ValueError('No value provided for parameter \'devices\'')
 
-  @property
-  def output_shapes(self):
-    return self._output_shape
+    def _as_variant_tensor(self):
+        return dali_dataset_module.dali_dataset(self._pipeline, self._shape, self._dtypes, self._devices)
 
-  @property
-  def output_classes(self):
-    raise NotImplementedError
+    @property
+    def output_types(self):
+        return self._dtypes[0], self._dtypes[1]
+
+    @property
+    def output_shapes(self):
+        return tensor_shape.TensorShape([]), tensor_shape.TensorShape([])
+
+    @property
+    def output_classes(self):
+        return ops.Tensor
 
 
 DALIDataset.__doc__ = DALIDataset.__doc__
