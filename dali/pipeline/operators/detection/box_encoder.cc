79afb88d79cdfae7d3218ce43f6047c3211a6334
Albert Wolant <awolant@nvidia.com>
2018-12-05 09:16:41 +0100

    Fixed box encoder AddArg bug
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/pipeline/operators/detection/box_encoder.cc
+++ dali/pipeline/operators/detection/box_encoder.cc
@@ -165,6 +165,6 @@ encoded box.")code")
     .AddOptionalArg(
         "criteria",
         R"code(Threshold IOU for matching bounding boxes with anchors. Value between 0 and 1. Default is 0.5.)code",
-        0.5f, DALI_FLOAT);
+        0.5f, false);
 
 }  // namespace dali
