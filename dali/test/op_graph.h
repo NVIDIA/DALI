38155a8577c4bde8b3d0918c3d8e5ab39d22971b
Michal Zientkiewicz <michalz@nvidia.com>
2018-12-14 11:12:00 +0100

    Change graph builder interface.
    Now it's harder to build topologically incorrect graphs.
    Single-line sytanx added for adding nodes and specifying inputs.
    Removed (buggy) node outputs.
    Coding style.
    
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/test/op_graph.h
+++ dali/test/op_graph.h
@@ -19,21 +19,23 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <iostream>
+#include <utility>
 
 namespace dali {
 namespace testing {
 
 struct Node;
 
-struct NodeOutput {
-  NodeOutput(Node *n, int index, bool explicit_GPU = false) : node(n), index(index), explicit_GPU(false) {}
+struct Edge {
+  Edge() = default;
+  Edge(Node *n, int index, bool explicit_GPU = false)
+  : node(n), index(index), explicit_GPU(false) {}
 
   Node *node = nullptr;
   int index = 0;
   bool explicit_GPU = false;
 
-  NodeOutput gpu() const {
+  Edge gpu() const {
     auto ret = *this;
     ret.explicit_GPU = true;
     return ret;
@@ -48,7 +50,22 @@ struct Node {
   Node(std::string op_name, std::string node_name, int backend_mask)
   : op_name(std::move(op_name)), node_name(std::move(node_name)), backend_mask(backend_mask) {}
 
-  NodeOutput operator()(int i) { return { this, i }; }
+  template <typename... Inputs>
+  Node &operator()(Inputs&&... inputs) {
+    int junk[] = { (add_in(inputs), 0)... };
+    (void)junk;
+    return *this;
+  }
+
+  void add_in(Edge e) {
+    in.push_back(e);
+  }
+
+  Edge operator[](int i) {
+    if (i >= num_outputs)
+      num_outputs = i+1;
+    return { this, i };
+  }
 
   enum Backend {
     CPU = 1,
@@ -59,56 +76,24 @@ struct Node {
   std::string op_name;
   std::string node_name;
   int backend_mask = 0;
-  std::vector<NodeOutput> inputs;
+  int num_outputs = 0;
+  std::vector<Edge> in;
 };
-
-struct NodePtr {
-  NodePtr(Node *ptr = nullptr) : ptr(ptr) {}  // NOLINT
-  Node &operator*() const { return *ptr; }
-  Node *operator->() const { return ptr; }
-  NodeOutput operator()(int output_index) const { return (*ptr)(output_index); }
-  Node *ptr;
-};
-
-
-struct NodeInputAssignment {
-  Node *ptr;
-};
-
-inline NodeInputAssignment operator,(NodeInputAssignment n, NodeOutput o) {
-  n.ptr->inputs.push_back(o);
-  return n;
-}
-
-inline NodeInputAssignment operator<<(Node &node, NodeOutput o) {
-  node.inputs.push_back(o);
-  return { &node };
-}
-
-template <typename T>
-NodeInputAssignment operator<<(NodePtr ptr, T&& rhs) {
-  return *ptr << std::forward<T>(rhs);
-}
-
-
 
 struct OpDAG {
-  OpDAG() = default;
-  OpDAG(const std::string &op_name, int backend_mask = Node::AnyBackend, int num_inputs = 1, int num_outputs = 1) {
-    auto n = add(op_name);
-    for (int i = 0; i < num_inputs; i++)
-      n << input(i);
-    for (int i = 1; i < num_outputs; i++)
-      output << n(0);
+  Node &add(std::string op_name, std::string node_name, int backend_mask = Node::AnyBackend) {
+    Node node(std::move(op_name), std::move(node_name), backend_mask);
+    auto result = nodes.emplace(node.op_name, std::move(node));
+    assert(result.second && "Duplicate node");
+    return result.first->second;
   }
 
-  NodePtr add(std::string op_name, int backend_mask = Node::AnyBackend) {
+  Node &add(std::string op_name, int backend_mask = Node::AnyBackend) {
     std::string node_name = op_name;
     for (int suffix = 0; nodes.count(node_name); suffix++) {
       node_name = op_name + std::to_string(suffix);
     }
-    Node node(std::move(op_name), std::move(node_name), backend_mask);
-    return &nodes.emplace(node.op_name, std::move(node)).first->second;
+    return add(std::move(op_name), std::move(node_name), backend_mask);
   }
 
   bool check_cycles(
@@ -119,7 +104,7 @@ struct OpDAG {
       return true;
     if (!in_progress.insert(n).second)
       return false;
-    for (auto in : n->inputs)
+    for (auto in : n->in)
       if (!check_cycles(in.node, done, in_progress))
         return false;
     done.insert(n);
@@ -127,21 +112,41 @@ struct OpDAG {
     return true;
   }
 
-  bool validate(bool checkInputReferenced) const {
+  bool validate() const {
     std::unordered_set<const Node *> done;
     std::unordered_set<const Node *> in_progress;
-    if (!check_cycles(&output, done, in_progress))
+    if (!check_cycles(&out, done, in_progress))
       return false;
-    if (checkInputReferenced)
-      return done.count(&input);
-    return true;
+
+    return in.num_outputs == 0 || done.count(&in);
   }
 
   std::unordered_map<std::string, Node> nodes;
-  Node input{"", "__input", Node::AnyBackend}, output{"output", "__output", Node::AnyBackend};
+  Node in{"", "__input", Node::AnyBackend};
+  Node out{"", "__output", Node::AnyBackend};
 };
 
-}  // testing
-}  // dali
+/// @brief Creates a trivial graph with just one node.
+/// @param op_name - name of the operator
+/// @param backend_mask - backends to test
+/// @param num_inputs  - number of inputs, if 0 then it's inferred from number
+///                      of inputs supplied to a function running the graph
+/// @param num_outputs - number of outputs, if 0 then it's inferred from number
+///                      of reference outputs supplied to a function running the graph
+OpDAG SingleOpGraph(const std::string &op_name,
+                    int backend_mask = Node::AnyBackend,
+                    int num_inputs = 0, int num_outputs = 0) {
+  OpDAG dag;
+  auto &n = dag.add(op_name, backend_mask);
+  for (int i = 0; i < num_inputs; i++)
+    n.in.push_back(dag.in[i]);
+  for (int i = 0; i < num_outputs; i++)
+    dag.out(n[i]);
+  return dag;
+}
 
-#endif
+
+}  // namespace testing
+}  // namespace dali
+
+#endif  // DALI_TEST_OP_GRAPH_H_
--- dali/test/op_graph_test.cc
+++ dali/test/op_graph_test.cc
@@ -1,3 +1,17 @@
+// Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <gtest/gtest.h>
 #include <dali/test/op_graph.h>
 
@@ -5,43 +19,32 @@ namespace dali {
 namespace testing {
 
 TEST(OpGraph, ValidDAG1) {
-
   OpDAG dag;
-  auto n1 = dag.add("MyOp1");
-  auto n2 = dag.add("MyOp2");
-  auto n3 = dag.add("MyOp3");
-  dag.output << n3(0);
-  n3 << n1(0), n2(0);
-  n1 << dag.input(0);
-  n2 << dag.input(1);
-  EXPECT_TRUE(dag.validate(true));
+  auto &n1 = dag.add("MyOp1")(dag.in[0]);
+  auto &n2 = dag.add("MyOp2")(dag.in[1]);
+  auto &n3 = dag.add("MyOp3")(n1[0], n2[0]);
+  dag.out(n3[0]);
+  EXPECT_TRUE(dag.validate());
 }
 
 TEST(OpGraph, ValidDAG2) {
-
   OpDAG dag;
-  auto n1 = dag.add("MyOp1");
-  auto n2 = dag.add("MyOp2");
-  auto n3 = dag.add("MyOp3");
-  dag.output << n3(0);
-  n3 << n1(0), n2(0);
-  n1 << dag.input(0);
-  n2 << dag.input(1), n1(0);
-  EXPECT_TRUE(dag.validate(true));
+  auto &n1 = dag.add("MyOp1")(dag.in[0]);
+  auto &n2 = dag.add("MyOp2")(dag.in[1], n1[0]);
+  auto &n3 = dag.add("MyOp3")(n1[0], n2[0]);
+  dag.out(n3[0]);
+  EXPECT_TRUE(dag.validate());
 }
 
 TEST(OpGraph, InvalidDAG) {
-
   OpDAG dag;
-  auto n1 = dag.add("MyOp1");
-  auto n2 = dag.add("MyOp2");
-  auto n3 = dag.add("MyOp3");
-  dag.output << n3(0);
-  n3 << n1(0), n2(0);
-  n1 << dag.input(0);
-  n2 << dag.input(1), n3(0);
-  EXPECT_FALSE(dag.validate(true));
+  auto &n1 = dag.add("MyOp1")(dag.in[0]);
+  auto &n2 = dag.add("MyOp2")(dag.in[1], n1[0]);
+  auto &n3 = dag.add("MyOp3")(n1[0], n2[0]);
+  n2.add_in(n3[0]);
+  dag.out(n3[0]);
+  EXPECT_FALSE(dag.validate());
 }
 
-}  // testing
+}  // namespace testing
 }  // namespace dali
