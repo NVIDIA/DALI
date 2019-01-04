6ae3acb8ab0c37c4e94aa162e59dccfbf95f1dee
Serge Panev <spanev@nvidia.com>
2019-01-04 19:00:43 +0100

    Fix video files dir
    Signed-off-by: Serge Panev <spanev@nvidia.com>

--- qa/L0_videoreader_test/test.sh
+++ qa/L0_videoreader_test/test.sh
@@ -9,7 +9,7 @@ pushd ../..
 
 cd docs/examples/video
 
-mkdir -p videos
+mkdir -p video_files
 
 container_name=prepared.mp4
 
@@ -20,7 +20,7 @@ IFS='.' read -a splitted <<< "$container_name"
 
 for i in {0..4};
 do
-    ffmpeg -ss 00:00:${i}0 -t 00:00:10 -i $container_name -vcodec copy -acodec copy videos/${splitted[0]}_$i.${splitted[1]}
+    ffmpeg -ss 00:00:${i}0 -t 00:00:10 -i $container_name -vcodec copy -acodec copy video_files/${splitted[0]}_$i.${splitted[1]}
 done
 
 test_body() {
