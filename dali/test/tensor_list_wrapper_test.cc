4fc2b72752dbefb48a2a1fb67d956fb00e53586a
szalpal <mszolucha@nvidia.com>
2019-01-04 11:02:39 +0100

    <> to "" in #include
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- dali/test/tensor_list_wrapper_test.cc
+++ dali/test/tensor_list_wrapper_test.cc
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <dali/test/tensor_list_wrapper.h>
 #include <gtest/gtest.h>
-#include <dali/pipeline/data/tensor.h>
+#include "dali/pipeline/data/tensor.h"
+#include "dali/test/tensor_list_wrapper.h"
 
 namespace dali {
 namespace testing {
