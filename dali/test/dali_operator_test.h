da45c1d1f824174cc2452ccaa8369ac0bc12e988
Michał Szołucha <mszolucha@nvidia.com>
2019-01-07 15:14:25 +0100

    catering for review
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/test/dali_operator_test.h
+++ dali/test/dali_operator_test.h
@@ -48,6 +48,77 @@ inline std::string BackendStringName<GPUBackend>() {
   return "gpu";
 }
 
+
+std::unique_ptr<Pipeline> CreatePipeline(size_t batch_size, size_t num_threads) {
+  return std::unique_ptr<Pipeline>(
+          new Pipeline(static_cast<int>(batch_size), static_cast<int>(num_threads), 0));
+}
+
+
+void AddInputToPipeline(Pipeline &pipeline, const TensorListWrapper &input) {
+  assert(input && input.has_cpu());  // External input works only for CPUBackend
+  const std::string input_name = "input";
+  pipeline.AddExternalInput(input_name);
+  pipeline.SetExternalInput(input_name, *input.get<CPUBackend>());
+}
+
+
+void AddOperatorToPipeline(Pipeline &pipeline, const OpSpec &op_spec) {
+  pipeline.AddOperator(op_spec);
+}
+
+
+DeviceWorkspace CreateWorkspace() {
+  DeviceWorkspace ws;
+  return ws;
+}
+
+
+void RunPipeline(Pipeline &pipeline) {
+  pipeline.RunCPU();
+  pipeline.RunGPU();
+}
+
+
+template<typename OutputBackend>
+std::vector<TensorListWrapper>
+GetOutputsFromPipeline(Pipeline &pipeline, const std::string &output_backend) {
+  std::vector<TensorListWrapper> ret;
+  auto workspace = CreateWorkspace();
+  pipeline.Outputs(&workspace);
+  for (int output_idx = 0; output_idx < workspace.NumOutput(); output_idx++) {
+    ret.emplace_back(workspace.template Output<OutputBackend>(output_idx));
+  }
+  return ret;
+}
+
+
+void BuildPipeline(Pipeline &pipeline, const OpSpec &spec) {
+  std::vector<std::pair<std::string, std::string>> vecoutputs_;
+  for (int i = 0; i < spec.NumOutput(); ++i) {
+    vecoutputs_.emplace_back(spec.OutputName(i), spec.OutputDevice(i));
+  }
+  pipeline.Build(vecoutputs_);
+}
+
+
+// TODO(mszolucha): graph of operators
+OpSpec CreateOpSpec(const std::string &operator_name, Arguments operator_arguments, bool has_input,
+                    const std::string &input_backend, const std::string &output_backend) {
+  assert(input_backend == "cpu" || input_backend == "gpu");
+  assert(output_backend == "cpu" || output_backend == "gpu");
+
+  OpSpec opspec = OpSpec(operator_name);
+  for (const auto &arg : operator_arguments) {
+    opspec.AddArg(arg.first.arg_name(), arg.second);
+  }
+  if (has_input) {
+    opspec.AddInput("input", input_backend);
+  }
+  opspec.AddOutput("output", output_backend);
+  return opspec;
+}
+
 }  // namespace detail
 
 
@@ -67,12 +138,13 @@ class DaliOperatorTest : public ::testing::Test, public ::testing::WithParamInte
   void RunTest(const TensorListWrapper &input, TensorListWrapper &output,
                const Arguments &operator_arguments, const Verify &verify) {
     std::string output_backend = detail::BackendStringName<OutputBackend>();
-    ResetPipeline(batch_size_, num_threads_);
+    auto pipeline = detail::CreatePipeline(batch_size_, num_threads_);
     if (input) {
-      AddInputToPipeline(pipeline_.get(), input);
+      detail::AddInputToPipeline(*pipeline, input);
     }
-    auto outputs = RunTestImpl<OutputBackend>(operator_arguments, verify, input ? true : false,
-                                              input.backend(), output_backend);
+    auto outputs = RunTestImpl<OutputBackend>(*pipeline, operator_arguments, verify,
+                                              input ? true : false, input.backend(),
+                                              output_backend);
     assert(outputs.size() == 1); // one input, one output
     verify(input, outputs[0], operator_arguments);
     output = outputs[0];
@@ -100,91 +172,20 @@ class DaliOperatorTest : public ::testing::Test, public ::testing::WithParamInte
 
   template<typename OutputBackend>
   std::vector<TensorListWrapper>
-  RunTestImpl(const Arguments &operator_arguments, const Verify &verify, bool has_inputs,
-              const std::string &input_backend, const std::string &output_backend) {
-    const auto op_spec = CreateOpSpec(GenerateOperatorGraph().get_op_name(), operator_arguments,
-                                      has_inputs, input_backend, output_backend);
-    AddOperatorToPipeline(pipeline_.get(), op_spec);
-    BuildPipeline(pipeline_.get(), op_spec);
-    RunPipeline(pipeline_.get());
-    return GetOutputsFromPipeline<OutputBackend>(pipeline_.get(), output_backend);
+  RunTestImpl(Pipeline &pipeline, const Arguments &operator_arguments, const Verify &verify,
+              bool has_inputs, const std::string &input_backend,
+              const std::string &output_backend) {
+    const auto op_spec = detail::CreateOpSpec(GenerateOperatorGraph().get_op_name(),
+                                              operator_arguments, has_inputs, input_backend,
+                                              output_backend);
+    detail::AddOperatorToPipeline(pipeline, op_spec);
+    detail::BuildPipeline(pipeline, op_spec);
+    detail::RunPipeline(pipeline);
+    return detail::GetOutputsFromPipeline<OutputBackend>(pipeline, output_backend);
   }
 
 
-  void ResetPipeline(size_t batch_size, size_t num_threads) {
-    pipeline_.reset(new Pipeline(static_cast<int>(batch_size), static_cast<int>(num_threads), 0));
-  }
-
-
-  void AddInputToPipeline(Pipeline *pipeline, const TensorListWrapper &input) {
-    assert(input && input.has_cpu());  // External input works only for CPUBackend
-    const std::string input_name = "input";
-    pipeline->AddExternalInput(input_name);
-    pipeline->SetExternalInput(input_name, *input.get<CPUBackend>());
-  }
-
-
-  void AddOperatorToPipeline(Pipeline *pipeline, const OpSpec &op_spec) {
-    pipeline->AddOperator(op_spec);
-  }
-
-
-  DeviceWorkspace CreateWorkspace() const {
-    DeviceWorkspace ws;
-    return ws;
-  }
-
-
-  void RunPipeline(Pipeline *pipeline) {
-    pipeline->RunCPU();
-    pipeline->RunGPU();
-  }
-
-
-  template<typename OutputBackend>
-  std::vector<TensorListWrapper>
-  GetOutputsFromPipeline(Pipeline *pipeline, const std::string &output_backend) {
-    std::vector<TensorListWrapper> ret;
-    auto workspace = CreateWorkspace();
-    pipeline->Outputs(&workspace);
-    for (int output_idx = 0; output_idx < workspace.NumOutput(); output_idx++) {
-      ret.emplace_back(workspace.template Output<OutputBackend>(output_idx));
-    }
-    return ret;
-  }
-
-
-  void BuildPipeline(Pipeline *pipeline, const OpSpec &spec) {
-    std::vector<std::pair<std::string, std::string>> vecoutputs_;
-    for (int i = 0; i < spec.NumOutput(); ++i) {
-      vecoutputs_.emplace_back(spec.OutputName(i), spec.OutputDevice(i));
-    }
-    pipeline->Build(vecoutputs_);
-  }
-
-
-  // TODO(mszolucha): graph of operators
-  OpSpec
-  CreateOpSpec(const std::string &operator_name, Arguments operator_arguments, bool has_input,
-               const std::string &input_backend, const std::string &output_backend) const {
-    assert(input_backend == "cpu" || input_backend == "gpu");
-    assert(output_backend == "cpu" || output_backend == "gpu");
-
-    OpSpec opspec = OpSpec(operator_name);
-    for (const auto &arg : operator_arguments) {
-      opspec.AddArg(arg.first.arg_name(), arg.second);
-    }
-    if (has_input) {
-      opspec.AddInput("input", input_backend);
-    }
-    opspec.AddOutput("output", output_backend);
-    return opspec;
-  }
-
-
-  std::unique_ptr<Pipeline> pipeline_;
-  size_t batch_size_;
-  const size_t num_threads_;
+  const size_t batch_size_, num_threads_;
 };
 
 }  // namespace testing
