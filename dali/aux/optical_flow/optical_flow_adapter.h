10e931ab976df3ba1dc3541a60e512ae7ae4859c
Michał Szołucha <mszolucha@nvidia.com>
2019-01-14 14:42:06 +0100

    first version of of api
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

---
+++ dali/aux/optical_flow/optical_flow_adapter.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_OPTICAL_FLOW_ADAPTER_H
+#define DALI_OPTICAL_FLOW_ADAPTER_H
+
+#include <dali/pipeline/data/backend.h>
+#include "dali/kernels/tensor_view.h"
+
+namespace dali {
+namespace optical_flow {
+
+enum VectorGridSize {
+  UNDEF,
+  SIZE_4,  /// 4x4 grid
+  MAX,
+};
+
+struct OpticalFlowParams {
+  float perf_quality_factor = .0f;  /// 0..1, where 0 is best quality, lowest performance
+  VectorGridSize grid_size = MAX;
+  bool enable_hints = false;
+};
+
+
+class OpticalFlowAdapter {
+ public:
+  virtual void Initialize(OpticalFlowParams params) {}
+
+
+  virtual void CalcOpticalFlow(kernels::TensorView<GPUBackend, uint8_t, 3> reference_image,
+                               kernels::TensorView<GPUBackend, uint8_t, 3> input_image,
+                               kernels::TensorView<GPUBackend, float, 1> output_image,
+                               kernels::TensorView<GPUBackend, float, 1> external_hints = kernels::TensorView<GPUBackend, float, 1>()) = 0;  // NOLINT
+
+
+  virtual ~OpticalFlowAdapter() = default;
+};
+
+}  // namespace optical_flow
+}  // namespace dali
+
+#endif  // DALI_OPTICAL_FLOW_ADAPTER_H
---
+++ dali/aux/optical_flow/optical_flow_adapter_test.cc
@@ -0,0 +1,40 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include "dali/aux/optical_flow/optical_flow_adapter.h"
+
+namespace dali {
+namespace optical_flow {
+using namespace kernels;
+
+class StubOpticalFlow : public OpticalFlowAdapter {
+ public:
+  void CalcOpticalFlow(const TensorView<GPUBackend, uint8_t, 3> reference_image,
+                       const TensorView<GPUBackend, uint8_t, 3> input_image,
+                       TensorView<GPUBackend, float, 1> output_image,
+                       const TensorView<GPUBackend, float, 1> external_hints) override {
+
+  }
+};
+
+TEST(OpticalFlowAdapter, StubApi) {
+  TensorView<GPUBackend, uint8_t, 3> tvref, tvin;
+  TensorView<GPUBackend, float, 1> tvout;
+  std::unique_ptr<OpticalFlowAdapter> of(new StubOpticalFlow());
+  of->CalcOpticalFlow(tvref, tvin, tvout);
+}
+
+}  // namespace optical_flow
+}  // namespace dali
