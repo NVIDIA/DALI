a9ca427d30f26d78eea8df777ed9cc023df359e1
Serge Panev <spanev@me.com>
2019-01-30 11:39:32 +0100

    Update Creating Op doc to new Workspace::Output API (#492)
    Signed-off-by: Serge Panev <spanev@nvidia.com>

--- docs/examples/extend/customdummy/dummy.cc
+++ docs/examples/extend/customdummy/dummy.cc
@@ -4,14 +4,14 @@ namespace other_ns {
 
 template<>
 void Dummy<::dali::CPUBackend>::RunImpl(::dali::SampleWorkspace *ws, const int idx) {
-  auto &input = ws->Input<::dali::CPUBackend>(idx);
-  auto output = ws->Output<::dali::CPUBackend>(idx);
-  output->set_type(input.type());
-  output->ResizeLike(input);
+  const auto &input = ws->Input<::dali::CPUBackend>(idx);
+  auto &output = ws->Output<::dali::CPUBackend>(idx);
+  output.set_type(input.type());
+  output.ResizeLike(input);
 
   ::dali::TypeInfo type = input.type();
   type.Copy<::dali::CPUBackend, ::dali::CPUBackend>(
-      output->raw_mutable_data(),
+      output.raw_mutable_data(),
       input.raw_data(), input.size(), 0);
 }
 
