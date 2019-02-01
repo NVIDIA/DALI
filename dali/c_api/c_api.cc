1f2a164ec4fe5cb82bb8e9693dd6e6463d46099b
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-02-01 17:17:06 +0100

    Add ability to return sparse tensor on CPU for TF DALI op
    - user need to provide sparse list of bool value to tell which
      tensor should be outputted as a sparese
    - this feature is supported only on CPU
    - add example how to use sparse tensors feature
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/c_api/c_api.cc
+++ dali/c_api/c_api.cc
@@ -15,6 +15,7 @@
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 #include "dali/c_api/c_api.h"
 #include "dali/pipeline/pipeline.h"
@@ -64,29 +65,116 @@ void daliOutputRelease(daliPipelineHandle* pipe_handle) {
   pipeline->ReleaseOutputs();
 }
 
-int64_t* daliShapeAt(daliPipelineHandle* pipe_handle, int n) {
+template <typename T>
+static int64_t* daliShapeAtHelper(dali::DeviceWorkspace* ws, int n, int k) {
+  int64_t* c_shape = nullptr;
+  std::vector<dali::Index> shape;
+  const auto &out_tensor_list = ws->Output<T>(n);
+  if (k >= 0) {
+    shape = out_tensor_list.tensor_shape(k);
+  } else {
+    shape = out_tensor_list.tensor_shape(0);
+    shape.insert(shape.begin(), out_tensor_list.ntensor());
+  }
+
+  c_shape = static_cast<int64_t*>(malloc(sizeof(int64_t) * (shape.size() + 1)));
+  c_shape[shape.size()] = 0;
+  memcpy(c_shape, &shape[0], shape.size() * sizeof(int64_t));
+  return c_shape;
+}
+
+static int64_t* daliShapeAtTypedHelper(daliPipelineHandle* pipe_handle, int n, int k) {
   dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
   int64_t* c_shape = nullptr;
   if (ws->OutputIsType<dali::CPUBackend>(n)) {
-    dali::Tensor<dali::CPUBackend> t;
-    t.ShareData(&(ws->Output<dali::CPUBackend>(n)));
-
-    std::vector<dali::Index> shape = t.shape();
-    c_shape = static_cast<int64_t*>(
-      malloc(sizeof(int64_t) * (shape.size() + 1)));
-    c_shape[shape.size()] = 0;
-    memcpy(c_shape, &shape[0], shape.size() * sizeof(int64_t));
+    c_shape = daliShapeAtHelper<dali::CPUBackend>(ws, n, k);
   } else {
-    dali::Tensor<dali::GPUBackend> t;
-    t.ShareData(&(ws->Output<dali::GPUBackend>(n)));
-
-    std::vector<dali::Index> shape = t.shape();
-    c_shape = static_cast<int64_t*>(
-      malloc(sizeof(int64_t) * (shape.size() + 1)));
-    c_shape[shape.size()] = 0;
-    memcpy(c_shape, &shape[0], shape.size() * sizeof(int64_t));
+    c_shape = daliShapeAtHelper<dali::GPUBackend>(ws, n, k);
   }
   return c_shape;
+}
+
+int64_t* daliShapeAtSample(daliPipelineHandle* pipe_handle, int n, int k) {
+  return daliShapeAtTypedHelper(pipe_handle, n, k);
+}
+
+int64_t* daliShapeAt(daliPipelineHandle* pipe_handle, int n) {
+  return daliShapeAtTypedHelper(pipe_handle, n, -1);
+}
+
+template <typename T>
+static size_t daliNumTensorsHelper(dali::DeviceWorkspace* ws, int n) {
+  return ws->Output<T>(n).ntensor();
+}
+
+size_t daliNumTensors(daliPipelineHandle* pipe_handle, int n) {
+  dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
+  size_t num_tensors = 0;
+  if (ws->OutputIsType<dali::CPUBackend>(n)) {
+    num_tensors = daliNumTensorsHelper<dali::CPUBackend>(ws, n);
+  } else {
+    num_tensors = daliNumTensorsHelper<dali::GPUBackend>(ws, n);
+  }
+  return num_tensors;
+}
+
+template <typename T>
+static size_t daliNumElementsHelper(dali::DeviceWorkspace* ws, int n) {
+  return ws->Output<T>(n).GetElementsNumber();
+}
+
+size_t daliNumElements(daliPipelineHandle* pipe_handle, int n) {
+  dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
+  size_t num_elms = 0;
+  if (ws->OutputIsType<dali::CPUBackend>(n)) {
+    num_elms = daliNumElementsHelper<dali::CPUBackend>(ws, n);
+  } else {
+    num_elms = daliNumElementsHelper<dali::GPUBackend>(ws, n);
+  }
+  return num_elms;
+}
+
+template <typename T>
+static size_t daliTensorSizeHelper(dali::DeviceWorkspace* ws, int n) {
+  return ws->Output<T>(n).nbytes();
+}
+
+size_t daliTensorSize(daliPipelineHandle* pipe_handle, int n) {
+  dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
+  size_t size = 0;
+  if (ws->OutputIsType<dali::CPUBackend>(n)) {
+    size = daliTensorSizeHelper<dali::CPUBackend>(ws, n);
+  } else {
+    size = daliTensorSizeHelper<dali::GPUBackend>(ws, n);
+  }
+  return size;
+}
+
+template <typename T>
+static size_t daliMaxDimTensorsHelper(dali::DeviceWorkspace* ws, int n) {
+  const auto &out_tensor_list = ws->Output<T>(n);
+  size_t tensors_num = out_tensor_list.ntensor();
+  size_t max_num_dim = 0;
+  for (size_t i = 0; i < tensors_num; ++i) {
+    size_t num_dim = out_tensor_list.tensor_shape(i).size();
+    // squeze last dimension
+    if (out_tensor_list.tensor_shape(i).back() == 1) {
+      --num_dim;
+    }
+    max_num_dim = std::max(max_num_dim, num_dim);
+  }
+  return max_num_dim;
+}
+
+size_t daliMaxDimTensors(daliPipelineHandle* pipe_handle, int n) {
+  dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
+  size_t max_num_dim = 0;
+  if (ws->OutputIsType<dali::CPUBackend>(n)) {
+    max_num_dim = daliMaxDimTensorsHelper<dali::CPUBackend>(ws, n);
+  } else {
+    max_num_dim = daliMaxDimTensorsHelper<dali::GPUBackend>(ws, n);
+  }
+  return max_num_dim;
 }
 
 unsigned daliGetNumOutput(daliPipelineHandle* pipe_handle) {
@@ -94,17 +182,38 @@ unsigned daliGetNumOutput(daliPipelineHandle* pipe_handle) {
   return ws->NumOutput();
 }
 
+template <typename T>
+static void daliCopyTensorListNToHelper(dali::DeviceWorkspace* ws, void* dst, int n,
+                                        device_type_t dst_type) {
+  dali::CopyToExternalTensor(&(ws->Output<T>(n)), dst, (dali::device_type_t)dst_type);
+}
+
+void daliCopyTensorListNTo(daliPipelineHandle* pipe_handle, void* dst, int n,
+                           device_type_t dst_type) {
+  dali::TimeRange tr("daliCopyTensorNTo", dali::TimeRange::kGreen);
+  dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
+  if (ws->OutputIsType<dali::CPUBackend>(n)) {
+    daliCopyTensorListNToHelper<dali::CPUBackend>(ws, dst, n, dst_type);
+  } else {
+    daliCopyTensorListNToHelper<dali::GPUBackend>(ws, dst, n, dst_type);
+  }
+}
+
+template <typename T>
+static void daliCopyTensorNToHelper(dali::DeviceWorkspace* ws, void* dst, int n,
+                                    device_type_t dst_type) {
+  dali::Tensor<T> t;
+  t.ShareData(&(ws->Output<T>(n)));
+  dali::CopyToExternalTensor(t, dst, (dali::device_type_t)dst_type);
+}
+
 void daliCopyTensorNTo(daliPipelineHandle* pipe_handle, void* dst, int n, device_type_t dst_type) {
   dali::TimeRange tr("daliCopyTensorNTo", dali::TimeRange::kGreen);
   dali::DeviceWorkspace* ws = reinterpret_cast<dali::DeviceWorkspace*>(pipe_handle->ws);
   if (ws->OutputIsType<dali::CPUBackend>(n)) {
-    dali::Tensor<dali::CPUBackend> t;
-    t.ShareData(&(ws->Output<dali::CPUBackend>(n)));
-    dali::CopyToExternalTensor(t, dst, (dali::device_type_t)dst_type);
+    daliCopyTensorNToHelper<dali::CPUBackend>(ws, dst, n, dst_type);
   } else {
-    dali::Tensor<dali::GPUBackend> t;
-    t.ShareData(&(ws->Output<dali::GPUBackend>(n)));
-    dali::CopyToExternalTensor(t, dst, (dali::device_type_t)dst_type);
+    daliCopyTensorNToHelper<dali::GPUBackend>(ws, dst, n, dst_type);
   }
 }
 
