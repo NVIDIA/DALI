f3303a7259196f5699b22db2ef29014942903e79
Michał <7101064+szalpal@users.noreply.github.com>
2019-02-08 16:08:50 +0100

    OpticalFlowAdapter generalization (#505)
    * Generalizing OpticalFlowAdapter
    
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>
    
    * catering for lint
    
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>
    
    * review: changing name of trait
    
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>
    
    * catering for review
    
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>
    
    * catering for review
    
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>
    
    * changing <> to ""
    
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/aux/optical_flow/optical_flow_adapter_test.cc
+++ dali/aux/optical_flow/optical_flow_adapter_test.cc
@@ -15,23 +15,51 @@
 #include <gtest/gtest.h>
 #include <memory>
 #include "dali/aux/optical_flow/optical_flow_stub.h"
+#include "dali/kernels/backend_tags.h"
+#include "dali/util/cuda_utils.h"
 
 namespace dali {
 namespace optical_flow {
 
 using kernels::TensorView;
+using kernels::ComputeCPU;
+using kernels::StorageCPU;
+using kernels::ComputeGPU;
+using kernels::StorageGPU;
 
 static int kTestDataSize = 2;
 
-TEST(OpticalFlowAdapter, StubApi) {
-  std::unique_ptr<float> data(new float[kTestDataSize]);
-  TensorView<detail::Backend, uint8_t, 3> tvref, tvin;
-  TensorView<detail::Backend, float, 3> tvout(data.get(), {1, 1, 2});
+TEST(OpticalFlowAdapter, StubApiCpuBackend) {
+  TensorView<StorageCPU, uint8_t, 3> tvref, tvin;
+  std::vector<float> in_data(kTestDataSize);
+  TensorView<StorageCPU, float, 3> tvout(in_data.data(), {1, 1, 2});
   OpticalFlowParams params;
-  std::unique_ptr<OpticalFlowAdapter> of(new OpticalFlowStub(params));
+  std::unique_ptr<OpticalFlowAdapter<ComputeCPU>> of(new OpticalFlowStub<ComputeCPU>(params));
   of->CalcOpticalFlow(tvref, tvin, tvout);
-  EXPECT_FLOAT_EQ(OpticalFlowStub::kStubValue, *tvout(0, 0, 0));
-  EXPECT_FLOAT_EQ(OpticalFlowStub::kStubValue / 2, *tvout(0, 0, 1));
+  EXPECT_FLOAT_EQ(OpticalFlowStub<ComputeCPU>::kStubValue, *tvout(0, 0, 0));
+  EXPECT_FLOAT_EQ(OpticalFlowStub<ComputeCPU>::kStubValue / 2, *tvout(0, 0, 1));
+}
+
+
+TEST(OpticalFlowAdapter, StubApiGpuBackend) {
+  using StubValueType = std::remove_const<decltype(OpticalFlowStub<ComputeGPU>::kStubValue)>::type;
+  StubValueType *tvout_data;
+  CUDA_CALL(cudaMalloc(reinterpret_cast<void **>(&tvout_data),
+                       kTestDataSize * sizeof(StubValueType)));
+
+  TensorView<StorageGPU, uint8_t, 3> tvref, tvin;
+  TensorView<StorageGPU, float, 3> tvout(tvout_data, {1, 1, 2});
+  OpticalFlowParams params;
+  std::unique_ptr<OpticalFlowAdapter<ComputeGPU>> of(new OpticalFlowStub<ComputeGPU>(params));
+  of->CalcOpticalFlow(tvref, tvin, tvout);
+
+  std::vector<float> host(kTestDataSize);
+  CUDA_CALL(cudaMemcpy(host.data(), tvout.data, kTestDataSize * sizeof(StubValueType),
+                       cudaMemcpyDeviceToHost));
+  EXPECT_FLOAT_EQ(OpticalFlowStub<ComputeGPU>::kStubValue, host[0]);
+  EXPECT_FLOAT_EQ(OpticalFlowStub<ComputeGPU>::kStubValue / 2, host[1]);
+
+  CUDA_CALL(cudaFree(tvout_data));
 }
 
 }  // namespace optical_flow
