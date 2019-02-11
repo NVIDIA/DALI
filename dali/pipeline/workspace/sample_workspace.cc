f257023477d7ffe043c045f809137e32e8f3bbaa
Joaquin Anton <janton@nvidia.com>
2019-02-11 12:56:51 +0100

    Remove duplicated code in workspaces
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/pipeline/workspace/sample_workspace.cc
+++ dali/pipeline/workspace/sample_workspace.cc
@@ -18,50 +18,22 @@ namespace dali {
 
 template <>
 const Tensor<CPUBackend>& SampleWorkspace::Input(int idx) const {
-  DALI_ENFORCE(idx >= 0, "Negative index not supported.");
-  DALI_ENFORCE((size_t)idx < input_index_map_.size(),
-      "Index out of range." + std::to_string(idx) +
-      " not in range [0, " + std::to_string(input_index_map_.size())
-      + ")");
-
-  auto tensor_meta = input_index_map_[idx];
-  DALI_ENFORCE(tensor_meta.first, "Input Tensor with given "
-      "index does not have the calling backend type (CPUBackend)");
-  return *cpu_inputs_[tensor_meta.second];
+  return *CPUInput(idx);
 }
 
 template <>
 const Tensor<GPUBackend>& SampleWorkspace::Input(int idx) const {
-  DALI_ENFORCE(idx >= 0, "Negative index not supported.");
-  DALI_ENFORCE((size_t)idx < input_index_map_.size(),
-      "Index out of range." + std::to_string(idx) +
-      " not in range [0, " + std::to_string(input_index_map_.size())
-      + ")");
-
-  auto tensor_meta = input_index_map_[idx];
-  DALI_ENFORCE(!tensor_meta.first, "Output Tensor with given "
-      "index does not have the calling backend type (GPUBackend)");
-  return *gpu_inputs_[tensor_meta.second];
+  return *GPUInput(idx);
 }
 
 template <>
 Tensor<CPUBackend>& SampleWorkspace::Output(int idx) {
-  DALI_ENFORCE_VALID_INDEX(idx, output_index_map_.size());
-
-  auto tensor_meta = output_index_map_[idx];
-  DALI_ENFORCE(tensor_meta.first, "Output Tensor with given "
-      "index does not have the calling backend type (CPUBackend)");
-  return *cpu_outputs_[tensor_meta.second].get();
+  return *CPUOutput(idx);
 }
 
 template <>
 Tensor<GPUBackend>& SampleWorkspace::Output(int idx) {
-  DALI_ENFORCE_VALID_INDEX(idx, output_index_map_.size());
-
-  auto tensor_meta = output_index_map_[idx];
-  DALI_ENFORCE(!tensor_meta.first, "Output Tensor with given "
-      "index does not have the calling backend type (GPUBackend)");
-  return *gpu_outputs_[tensor_meta.second].get();
+  return *GPUOutput(idx);
 }
 
 }  // namespace dali
--- dali/pipeline/workspace/support_workspace.cc
+++ dali/pipeline/workspace/support_workspace.cc
@@ -18,20 +18,12 @@ namespace dali {
 
 template <>
 const Tensor<CPUBackend>& SupportWorkspace::Input(int idx) const {
-  DALI_ENFORCE_VALID_INDEX(idx, input_index_map_.size());
-  auto tensor_meta = input_index_map_[idx];
-  DALI_ENFORCE(tensor_meta.first, "Input Tensor with given "
-      "index does not have the calling backend type (CPUBackend)");
-  return *cpu_inputs_[tensor_meta.second];
+  return *CPUInput(idx);
 }
 
 template <>
 Tensor<CPUBackend>& SupportWorkspace::Output(int idx) {
-  DALI_ENFORCE_VALID_INDEX(idx, output_index_map_.size());
-  auto tensor_meta = output_index_map_[idx];
-  DALI_ENFORCE(tensor_meta.first, "Output Tensor with given "
-      "index does not have the calling backend type (CPUBackend)");
-  return *cpu_outputs_[tensor_meta.second].get();
+  return *CPUOutput(idx);
 }
 
 }  // namespace dali
