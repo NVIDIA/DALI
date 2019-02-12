fdfd11f21ebb299c672ff115780bd200f53023af
Joaquin Anton <janton@nvidia.com>
2019-02-12 18:43:03 +0100

    Enhance documentation of Crop, and seed argument
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/pipeline/operators/crop/crop.cc
+++ dali/pipeline/operators/crop/crop.cc
@@ -23,7 +23,7 @@
 namespace dali {
 
 DALI_SCHEMA(Crop)
-    .DocStr(R"code(Perform a random crop.)code")
+    .DocStr(R"code(Crops image with a given window dimensions and window position (upper left corner))code")
     .NumInput(1)
     .NumOutput(1)
     .AllowMultipleInputSets()
@@ -39,14 +39,14 @@ with size `(c,c)`)code",
         std::vector<float>{0.f, 0.f})
     .AddOptionalArg(
         "crop_pos_x",
-        R"code(Normalized horizontal position of the crop (0.0 - 1.0).
+        R"code(Normalized (0.0 - 1.0) horizontal position of the cropping window (upper left corner).
 Actual position is calculated as `crop_x = crop_x_norm * (W - crop_W)`,
 where `crop_x_norm` is the normalized position, `W` is the width of the image
 and `crop_W` is the width of the cropping window)code",
         0.5f, true)
     .AddOptionalArg(
         "crop_pos_y",
-        R"code(Normalized vertical position of the crop (0.0 - 1.0).
+        R"code(Normalized (0.0 - 1.0) vertical position of the cropping window (upper left corner).
 Actual position is calculated as `crop_y = crop_y_norm * (H - crop_H)`,
 where `crop_y_norm` is the normalized position, `H` is the height of the image
 and `crop_H` is the height of the cropping window)code",
