2cecb82eed71cea22059473bc50ced96d704ab91
Joaquin Anton <janton@nvidia.com>
2019-02-14 17:52:15 +0100

    [WIP] nvJpegDecoderCrop
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/pipeline/operators/decoder/nvjpeg_decoder_crop.cu
@@ -0,0 +1,48 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dali/pipeline/operators/decoder/nvjpeg_decoder_crop.h"
+
+namespace dali {
+
+DALI_REGISTER_OPERATOR(nvJPEGDecoderCrop, nvJPEGDecoderCrop, Mixed);
+
+DALI_SCHEMA(nvJPEGDecoderCrop)
+  .DocStr(R"code(Partially decode JPEG images using the nvJPEG library and a cropping window.
+Output of the decoder is on the GPU and uses `HWC` ordering.)code")
+  .NumInput(1)
+  .NumOutput(1)
+  .AddOptionalArg(
+    "crop",
+    R"code(Size of the cropped image, specified as a pair `(crop_H, crop_W)`.
+If only a single value `c` is provided, the resulting crop will be square
+with size `(c,c)`)code",
+    std::vector<float>{0.f, 0.f})
+  .AddOptionalArg(
+    "crop_pos_x",
+    R"code(Normalized horizontal position of the crop (0.0 - 1.0).
+Actual position is calculated as `crop_x = crop_x_norm * (W - crop_W)`,
+where `crop_x_norm` is the normalized position, `W` is the width of the image
+and `crop_W` is the width of the cropping window)code",
+    0.5f, true)
+  .AddOptionalArg(
+    "crop_pos_y",
+    R"code(Normalized vertical position of the crop (0.0 - 1.0).
+Actual position is calculated as `crop_y = crop_y_norm * (H - crop_H)`,
+where `crop_y_norm` is the normalized position, `H` is the height of the image
+and `crop_H` is the height of the cropping window)code",
+    0.5f, true)
+  .AddParent("nvJPEGDecoder");
+
+}  // namespace dali
