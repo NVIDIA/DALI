e8da6209f0e4f07b8275be45f704779405aab79e
Joaquin Anton <janton@nvidia.com>
2019-02-15 13:54:31 +0100

    Add tests for nvJPEGDecoderCrop (ROI decoding still not implemented)
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/pipeline/operators/decoder/nvjpeg_decoder_crop.h
+++ dali/pipeline/operators/decoder/nvjpeg_decoder_crop.h
@@ -15,6 +15,7 @@
 #ifndef DALI_PIPELINE_OPERATORS_DECODER_NVJPEG_DECODER_CROP_H_
 #define DALI_PIPELINE_OPERATORS_DECODER_NVJPEG_DECODER_CROP_H_
 
+#include <vector>
 #include "dali/pipeline/operators/decoder/nvjpeg_decoder.h"
 #include "dali/pipeline/operators/crop/crop_attr.h"
 
@@ -24,17 +25,26 @@ class nvJPEGDecoderCrop : public nvJPEGDecoder, protected CropAttr {
  public:
   explicit nvJPEGDecoderCrop(const OpSpec& spec)
     : nvJPEGDecoder(spec)
-    , CropAttr(spec) {
+    , CropAttr(spec)
+    , per_sample_crop_window_generators_(batch_size_) {
+    for (int i = 0; i < batch_size_; i++) {
+      DALI_ENFORCE(crop_height_[i] > 0 && crop_width_[i],
+        "crop window dimensions not provided for sample " + std::to_string(i));
+    }
   }
 
-  ~nvJPEGDecoderCrop() noexcept(false) override {
-  }
-
-  virtual CropWindowGenerator GetCropWindowGenerator(int data_idx) const {
-    return {}; // TODO(janton) : implement this
-  }
+  ~nvJPEGDecoderCrop() noexcept(false) override = default;
 
   DISABLE_COPY_MOVE_ASSIGN(nvJPEGDecoderCrop);
+
+  inline CropWindowGenerator GetCropWindowGenerator(int data_idx) const override {
+    return per_sample_crop_window_generators_[data_idx];
+  }
+
+  void SetupSharedSampleParams(MixedWorkspace *ws) override;
+
+ private:
+  std::vector<CropWindowGenerator> per_sample_crop_window_generators_;
 };
 
 }  // namespace dali
