7d5e915df0ebadbc6df224724ad588f062f696db
Krzysztof Lecki <klecki@nvidia.com>
2019-02-27 12:41:59 +0100

    Test coprime queues

--- dali/pipeline/pipeline.cc
+++ dali/pipeline/pipeline.cc
@@ -306,7 +306,7 @@ void Pipeline::Build(vector<std::pair<string, string>> output_names) {
   executor_.reset(new AsyncSeparatedPipelinedExecutor(
         batch_size_, num_threads_,
         device_id_, bytes_per_sample_hint_,
-        set_affinity_, max_num_stream_, {2, 2, 2}));
+        set_affinity_, max_num_stream_, {11, 4, 3}));
   executor_->Init();
 
   // Creating the graph
