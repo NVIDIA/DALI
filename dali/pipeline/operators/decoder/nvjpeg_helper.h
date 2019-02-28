49eaabe78bd85e956cf1668ce7662121a2e2c61a
Serge Panev <spanev@nvidia.com>
2019-02-28 20:25:41 +0100

    Add single nvJPEG operator with new decoupled API
    Signed-off-by: Serge Panev <spanev@nvidia.com>

---
+++ dali/pipeline/operators/decoder/nvjpeg_helper.h
@@ -0,0 +1,107 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_PIPELINE_OPERATORS_DECODER_NVJPEG_HELPER_H_
+#define DALI_PIPELINE_OPERATORS_DECODER_NVJPEG_HELPER_H_
+
+#include <nvjpeg.h>
+
+#include <string>
+
+#include "dali/common.h"
+#include "dali/error_handling.h"
+
+namespace dali {
+
+#define NVJPEG_CALL(code)                                    \
+  do {                                                       \
+    nvjpegStatus_t status = code;                            \
+    if (status != NVJPEG_STATUS_SUCCESS) {                   \
+      dali::string error = dali::string("NVJPEG error \"") + \
+        std::to_string(static_cast<int>(status)) + "\"";     \
+      DALI_FAIL(error);                                      \
+    }                                                        \
+  } while (0)
+
+#define NVJPEG_CALL_EX(code, extra)                          \
+  do {                                                       \
+    nvjpegStatus_t status = code;                            \
+    string extra_info = extra;                               \
+    if (status != NVJPEG_STATUS_SUCCESS) {                   \
+      dali::string error = dali::string("NVJPEG error \"") + \
+        std::to_string(static_cast<int>(status)) + "\"" +    \
+        " " + extra_info;                                    \
+      DALI_FAIL(error);                                      \
+    }                                                        \
+  } while (0)
+
+struct StateNvJPEG {
+  nvjpegBackend_t nvjpeg_backend;
+  nvjpegBufferPinned_t pinned_buffer;
+  nvjpegJpegState_t decoder_host_state;
+  nvjpegJpegState_t decoder_hybrid_state;
+  nvjpegJpegStream_t jpeg_stream;
+};
+
+struct EncodedImageInfo {
+  bool nvjpeg_support;
+  unsigned int c;
+  nvjpegChromaSubsampling_t subsampling;
+  unsigned int widths[NVJPEG_MAX_COMPONENT];
+  unsigned int heights[NVJPEG_MAX_COMPONENT];
+};
+
+inline nvjpegJpegState_t GetNvjpegState(const StateNvJPEG& state) {
+  switch (state.nvjpeg_backend) {
+    case NVJPEG_BACKEND_HYBRID:
+      return state.decoder_host_state;
+    case NVJPEG_BACKEND_GPU_HYBRID:
+      return state.decoder_hybrid_state;
+    default:
+      DALI_FAIL("Unknown nvjpegBackend_t "
+                + std::to_string(state.nvjpeg_backend));
+  }
+}
+
+inline nvjpegOutputFormat_t GetFormat(DALIImageType type) {
+  switch (type) {
+    case DALI_RGB:
+      return NVJPEG_OUTPUT_RGBI;
+    case DALI_BGR:
+      return NVJPEG_OUTPUT_BGRI;
+    case DALI_GRAY:
+      return NVJPEG_OUTPUT_Y;
+    default:
+      DALI_FAIL("Unknown output format");
+  }
+}
+
+inline bool SupportedSubsampling(const nvjpegChromaSubsampling_t &subsampling) {
+  switch (subsampling) {
+    case NVJPEG_CSS_444:
+    case NVJPEG_CSS_422:
+    case NVJPEG_CSS_420:
+    case NVJPEG_CSS_411:
+    case NVJPEG_CSS_410:
+    case NVJPEG_CSS_GRAY:
+    case NVJPEG_CSS_440:
+      return true;
+    default:
+      return false;
+  }
+}
+
+}  // namespace dali
+
+#endif  // DALI_PIPELINE_OPERATORS_DECODER_NVJPEG_HELPER_H_
