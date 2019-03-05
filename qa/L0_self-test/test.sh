4f60e65610514d35dbdade06d8f2139a4ea13294
Michał Szołucha <mszolucha@nvidia.com>
2019-03-05 16:02:31 +0100

    adding dali_extra support
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- qa/L0_self-test/test.sh
+++ qa/L0_self-test/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -ex
 
+# Fetch test data
+export DALI_EXTRA_PATH=/opt/dali_extra
+git clone --depth=1 https://github.com/NVIDIA/DALI_extra.git ${DALI_EXTRA_PATH}
+
 BINNAME=dali_test.bin
 
 for DIRNAME in \
