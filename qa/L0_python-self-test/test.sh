81efbbdd871ecb2960bd968d8ffb19a4e3fc1ae1
Albert Wolant <awolant@nvidia.com>
2019-03-06 17:51:57 +0100

    Add reason why to comment
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/L0_python-self-test/test.sh
+++ qa/L0_python-self-test/test.sh
@@ -11,6 +11,7 @@ test_body() {
     nosetests --verbose test_pipeline.py
     nosetests --verbose test_plugin_manager.py
 
+    # DISABLED FAILING TESTS DUE TO RANDOM FAILS IN THE CI
     # python test_random_bbcrop.py -i 300
     # python test_RN50_data_pipeline.py -i 10
 
