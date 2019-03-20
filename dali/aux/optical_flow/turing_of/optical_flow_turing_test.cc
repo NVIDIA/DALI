70f28cef1d1a068b65821b21b40ebd9cbd7d7df8
Michał Szołucha <mszolucha@nvidia.com>
2019-03-20 09:31:46 +0100

    lint
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/aux/optical_flow/turing_of/optical_flow_turing_test.cc
+++ dali/aux/optical_flow/turing_of/optical_flow_turing_test.cc
@@ -170,7 +170,8 @@ TEST(OpticalFlowTuringTest, DISABLED_CalcOpticalFlowTest) {
 
     // Output
     auto out_shape = of.GetOutputShape().to_static<3>();
-    auto memout = kernels::memory::alloc_unique<float>(kernels::AllocType::Unified, volume(out_shape));
+    auto memout = kernels::memory::alloc_unique<float>(kernels::AllocType::Unified,
+		                                       volume(out_shape));
     auto tvout = kernels::make_tensor_gpu(memout.get(), out_shape);
 
     of.CalcOpticalFlow(tvref, tvin, tvout);
