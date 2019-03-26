21f10d784f93b87e2cd6b2977fc358a4261395a7
Krzysztof Lecki <klecki@nvidia.com>
2019-03-26 17:15:58 +0100

    Some fixes

--- tools/coverage_sanitizer.sh
+++ tools/coverage_sanitizer.sh
@@ -3,8 +3,9 @@
 # Based on https://clang.llvm.org/docs/SanitizerCoverage.html
 
 mkdir -p build_coverage_sanitizer
-cd build_coverage_sanitizer
+pushd build_coverage_sanitizer
 cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_LMDB=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCOVERAGE_SANITIZER=ON ..
+make -j"$(nproc)"
 
 # Run
 ASAN_OPTIONS=protect_shadow_gap=0:detect_odr_violation=0:coverage=1 ./dali/python/nvidia/dali/test/dali_test.bin
@@ -15,3 +16,5 @@ ASAN_OPTIONS=protect_shadow_gap=0:detect_odr_violation=0:coverage=1 ./dali/pytho
 # Get the coverage-report-server.py from https://github.com/llvm-mirror/llvm/blob/master/tools/sancov/coverage-report-server.py
 # Run the server to analize
 # coverage-report-server.py --symcov dali_test.bin.symcov --srcpath ..
+
+popd
--- tools/coverage_source.sh
+++ tools/coverage_source.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # Based on https://clang.llvm.org/docs/SourceBasedCodeCoverage.html
 
 mkdir -p build_coverage_source
-cd build_coverage_source
-cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_LMDB=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCOVERAGE_SOURCE=ON ..
+pushd build_coverage_source
+# cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_LMDB=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCOVERAGE_SOURCE=ON ..
+make -j$(nproc)
 
 # Run
-LLVM_PROFILE_FILE="dali_test.profraw"  ./dali/python/nvidia/dali/test/dali_test.bin
+LLVM_PROFILE_FILE="dali_test.profraw" ./dali/python/nvidia/dali/test/dali_test.bin
 # Index the raw profile.
 llvm-profdata merge -sparse dali_test.profraw -o dali_test.profdata
 # Create file and function level reports
@@ -15,3 +16,5 @@ llvm-cov report ./dali/python/nvidia/dali/test/dali_test.bin -instr-profile=dali
 llvm-cov report ./dali/python/nvidia/dali/test/dali_test.bin -show-functions -instr-profile=dali_test.profdata -Xdemangler=c++filt .. > cov-report-functions
 # Create a line-oriented coverage report
 llvm-cov show -format html ./dali/python/nvidia/dali/test/dali_test.bin -instr-profile=dali_test.profdata > cov-src.html
+
+popd
