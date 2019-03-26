e66e794333ae3f60bf1898df93fd1a30fa6ae655
Michał <7101064+szalpal@users.noreply.github.com>
2019-03-26 01:56:34 +0100

    l1 fix (#687)
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- qa/L0_self-test/test.sh
+++ qa/L0_self-test/test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 
-# Fetch test data
-export DALI_EXTRA_PATH=/opt/dali_extra
-git clone --depth=1 https://github.com/NVIDIA/DALI_extra.git ${DALI_EXTRA_PATH}
+source ../setup_test.sh
 
 BINNAME=dali_test.bin
 
