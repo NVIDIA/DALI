97184a27cb8d829b96e08bb8715a19a5cc13a6da
Joaquin Anton <j.anton.guirao@gmail.com>
2019-03-29 15:37:57 +0100

    Add different decoder options to test_RN50_data_pipeline.py (#689)
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/test/python/test_RN50_data_pipeline.py
+++ dali/test/python/test_RN50_data_pipeline.py
@@ -22,19 +22,39 @@ import argparse
 import time
 
 class CommonPipeline(Pipeline):
-    def __init__(self, batch_size, num_threads, device_id, prefetch, fp16, nhwc):
+    def __init__(self, data_paths, num_gpus, batch_size, num_threads, device_id, prefetch, fp16, nhwc,
+                 decoder_type, decoder_cache_params, reader_queue_depth):
         super(CommonPipeline, self).__init__(batch_size, num_threads, device_id, prefetch_queue_depth=prefetch)
+        if decoder_type == 'roi':
+            print('Using nvJPEG with ROI decoding')
+            self.decode_gpu = ops.nvJPEGDecoderRandomCrop(device = "mixed", output_type = types.RGB)
+            self.res = ops.Resize(device="gpu", resize_x=224, resize_y=224)
+        elif decoder_type == 'roi_split':
+            print('Using nvJPEG with ROI decoding and split CPU/GPU stages')
+            self.decode_gpu = ops.nvJPEGDecoderRandomCrop(device = "mixed", output_type = types.RGB, split_stages=True)
+            self.res = ops.Resize(device="gpu", resize_x=224, resize_y=224)
+        elif decoder_type == 'cached':
+            assert decoder_cache_params['cache_enabled'] == True
+            cache_size = decoder_cache_params['cache_size']
+            cache_threshold = decoder_cache_params['cache_threshold']
+            cache_type = decoder_cache_params['cache_type']
+            print('Using nvJPEG with cache (size : {} threshold: {}, type: {})'.format(cache_size, cache_threshold, cache_type))
+            self.decode_gpu = ops.nvJPEGDecoder(device = "mixed", output_type = types.RGB,
+                                                cache_size=cache_size, cache_threshold=cache_threshold,
+                                                cache_type=cache_type, cache_debug=False)
+            self.res = ops.RandomResizedCrop(device="gpu", size =(224,224))
+        elif decoder_type == 'split':
+            print('Using nvJPEG with split CPU/GPU stages')
+            self.decode_gpu = ops.nvJPEGDecoder(device = "mixed", output_type = types.RGB, split_stages=True)
+            self.res = ops.RandomResizedCrop(device="gpu", size =(224,224))
+        else:
+            print('Using nvJPEG')
+            self.decode_gpu = ops.nvJPEGDecoder(device = "mixed", output_type = types.RGB)
+            self.res = ops.RandomResizedCrop(device="gpu", size =(224,224))
 
-        self.decode_gpu = ops.nvJPEGDecoder(device = "mixed", output_type = types.RGB)
-        self.res = ops.RandomResizedCrop(device="gpu", size =(224,224))
-        if nhwc:
-            layout = types.NHWC
-        else:
-            layout = types.NCHW
-        if fp16:
-            out_type = types.FLOAT16
-        else:
-            out_type = types.FLOAT
+        layout = types.NHWC if nhwc else types.NCHW
+        out_type = types.FLOAT16 if fp16 else types.FLOAT
+
         self.cmnp = ops.CropMirrorNormalize(device="gpu",
                                             output_dtype=out_type,
                                             output_layout=layout,
@@ -52,50 +72,78 @@ class CommonPipeline(Pipeline):
         return (output, labels)
 
 class MXNetReaderPipeline(CommonPipeline):
-    def __init__(self, batch_size, num_threads, device_id, num_gpus, data_paths, prefetch, fp16, nhwc):
-        super(MXNetReaderPipeline, self).__init__(batch_size, num_threads, device_id, prefetch, fp16, nhwc)
-        self.input = ops.MXNetReader(path = data_paths[0], index_path=data_paths[1], shard_id = device_id, num_shards = num_gpus)
+    def __init__(self, **kwargs):
+        super(MXNetReaderPipeline, self).__init__(**kwargs)
+        cache_enabled = kwargs['decoder_cache_params']['cache_enabled']
+        self.input = ops.MXNetReader(path = kwargs['data_paths'][0],
+                                     index_path = kwargs['data_paths'][1],
+                                     shard_id = kwargs['device_id'],
+                                     num_shards = kwargs['num_gpus'],
+                                     stick_to_shard = cache_enabled,
+                                     #skip_cached_images = cache_enabled,
+                                     prefetch_queue_depth = kwargs['reader_queue_depth'])
 
     def define_graph(self):
         images, labels = self.input(name="Reader")
         return self.base_define_graph(images, labels)
 
 class CaffeReadPipeline(CommonPipeline):
-    def __init__(self, batch_size, num_threads, device_id, num_gpus, data_paths, prefetch, fp16, nhwc):
-        super(CaffeReadPipeline, self).__init__(batch_size, num_threads, device_id, prefetch, fp16, nhwc)
-        self.input = ops.CaffeReader(path = data_paths[0], shard_id = device_id, num_shards = num_gpus)
+    def __init__(self, **kwargs):
+        super(CaffeReadPipeline, self).__init__(**kwargs)
+        cache_enabled = kwargs['decoder_cache_params']['cache_enabled']
+        self.input = ops.CaffeReader(path = kwargs['data_paths'][0],
+                                     shard_id = kwargs['device_id'],
+                                     num_shards = kwargs['num_gpus'],
+                                     stick_to_shard = cache_enabled,
+                                     #skip_cached_images = cache_enabled,
+                                     prefetch_queue_depth = kwargs['reader_queue_depth'])
 
     def define_graph(self):
         images, labels = self.input(name="Reader")
         return self.base_define_graph(images, labels)
 
 class Caffe2ReadPipeline(CommonPipeline):
-    def __init__(self, batch_size, num_threads, device_id, num_gpus, data_paths, prefetch, fp16, nhwc):
-        super(Caffe2ReadPipeline, self).__init__(batch_size, num_threads, device_id, prefetch, fp16, nhwc)
-        self.input = ops.Caffe2Reader(path = data_paths[0], shard_id = device_id, num_shards = num_gpus)
+    def __init__(self, **kwargs):
+        super(Caffe2ReadPipeline, self).__init__(**kwargs)
+        cache_enabled = kwargs['decoder_cache_params']['cache_enabled']
+        self.input = ops.Caffe2Reader(path = kwargs['data_paths'][0],
+                                      shard_id = kwargs['device_id'],
+                                      num_shards = kwargs['num_gpus'],
+                                      stick_to_shard = cache_enabled,
+                                      #skip_cached_images = cache_enabled,
+                                      prefetch_queue_depth = kwargs['reader_queue_depth'])
 
     def define_graph(self):
         images, labels = self.input(name="Reader")
         return self.base_define_graph(images, labels)
 
 class FileReadPipeline(CommonPipeline):
-    def __init__(self, batch_size, num_threads, device_id, num_gpus, data_paths, prefetch, fp16, nhwc):
-        super(FileReadPipeline, self).__init__(batch_size, num_threads, device_id, prefetch, fp16, nhwc)
-        self.input = ops.FileReader(file_root = data_paths[0], shard_id = device_id, num_shards = num_gpus)
+    def __init__(self, **kwargs):
+        super(FileReadPipeline, self).__init__(**kwargs)
+        cache_enabled = kwargs['decoder_cache_params']['cache_enabled']
+        self.input = ops.FileReader(file_root = kwargs['data_paths'][0],
+                                    shard_id = kwargs['device_id'],
+                                    num_shards = kwargs['num_gpus'],
+                                    stick_to_shard = cache_enabled,
+                                    #skip_cached_images = cache_enabled,
+                                    prefetch_queue_depth = kwargs['reader_queue_depth'])
 
     def define_graph(self):
         images, labels = self.input(name="Reader")
         return self.base_define_graph(images, labels)
 
 class TFRecordPipeline(CommonPipeline):
-    def __init__(self, batch_size, num_threads, device_id, num_gpus, data_paths, prefetch, fp16, nhwc):
-        super(TFRecordPipeline, self).__init__(batch_size, num_threads, device_id, prefetch, fp16, nhwc)
-        tfrecord = sorted(glob.glob(data_paths[0]))
-        tfrecord_idx = sorted(glob.glob(data_paths[1]))
+    def __init__(self, **kwargs):
+        super(TFRecordPipeline, self).__init__(**kwargs)
+        tfrecord = sorted(glob.glob(kwargs['data_paths'][0]))
+        tfrecord_idx = sorted(glob.glob(kwargs['data_paths'][1]))
+        cache_enabled = kwargs['decoder_cache_params']['cache_enabled']
         self.input = ops.TFRecordReader(path = tfrecord,
                                         index_path = tfrecord_idx,
-                                        shard_id = device_id,
-                                        num_shards = num_gpus,
+                                        shard_id = kwargs['device_id'],
+                                        num_shards = kwargs['num_gpus'],
+                                        stick_to_shard = cache_enabled,
+                                        #skip_cached_images = cache_enabled,
                                         features = {"image/encoded" : tfrec.FixedLenFeature((), tfrec.string, ""),
                                                     "image/class/label": tfrec.FixedLenFeature([1], tfrec.int64,  -1)
                                         })
@@ -135,6 +183,20 @@ parser.add_argument('--nhwc', action='store_true',
                     help='Use NHWC data instead of default NCHW')
 parser.add_argument('-i', '--iters', default=-1, type=int, metavar='N',
                     help='Number of iterations to run (default: -1 - whole data set)')
+parser.add_argument('--epochs', default=2, type=int, metavar='N',
+                    help='Number of epochs to run')
+parser.add_argument('--decoder_type', default='', type=str, metavar='N',
+                    help='split, roi, roi_split, cached (default: regular nvjpeg)')
+parser.add_argument('--cache_size', default=0, type=int, metavar='N',
+                    help='Cache size (in MB)')
+parser.add_argument('--cache_threshold', default=0, type=int, metavar='N',
+                    help='Cache threshold')
+parser.add_argument('--cache_type', default='none', type=str, metavar='N',
+                    help='Cache type')
+parser.add_argument('--reader_queue_depth', default=1, type=int, metavar='N',
+                    help='prefetch queue depth (default: 1)')
+parser.add_argument('--simulate_N_gpus', default=None, type=int, metavar='N',
+                    help='Used to simulate small shard as it would be in a multi gpu setup with this number of gpus. If provided, each gpu will see a shard size as if we were in a multi gpu setup with this number of gpus')
 args = parser.parse_args()
 
 N = args.gpus             # number of GPUs
@@ -142,8 +204,22 @@ BATCH_SIZE = args.batch   # batch size
 LOG_INTERVAL = args.print_freq
 WORKERS = args.workers
 PREFETCH = args.prefetch
+#PREFETCH = {'cpu_size': 2, 'gpu_size': 2}
 FP16 = args.fp16
 NHWC = args.nhwc
+
+DECODER_TYPE = args.decoder_type
+CACHED_DECODING = DECODER_TYPE == 'cached'
+DECODER_CACHE_PARAMS = {}
+DECODER_CACHE_PARAMS['cache_enabled'] = CACHED_DECODING
+if CACHED_DECODING:
+    DECODER_CACHE_PARAMS['cache_type'] = args.cache_type
+    DECODER_CACHE_PARAMS['cache_size'] = args.cache_size
+    DECODER_CACHE_PARAMS['cache_threshold'] = args.cache_threshold
+READER_QUEUE_DEPTH = args.reader_queue_depth
+SIMULATE_N_GPUS = N if args.simulate_N_gpus == None else args.simulate_N_gpus
+STICK_TO_SHARD = True if CACHED_DECODING else False
+SKIP_CACHED_IMAGES = True if CACHED_DECODING else False
 
 print("GPUs: {}, batch: {}, workers: {}, prefetch depth: {}, loging interval: {}, fp16: {}, NHWC: {}"
       .format(N, BATCH_SIZE, WORKERS, PREFETCH, LOG_INTERVAL, FP16, NHWC))
@@ -172,7 +248,9 @@ for pipe_name in test_data.keys():
     data_set_len = len(test_data[pipe_name])
     for i, data_set in enumerate(test_data[pipe_name]):
         pipes = [pipe_name(batch_size=BATCH_SIZE, num_threads=WORKERS, device_id=n,
-                           num_gpus=N, data_paths=data_set, prefetch=PREFETCH, fp16=FP16, nhwc=NHWC) for n in range(N)]
+                           num_gpus=SIMULATE_N_GPUS, data_paths=data_set, prefetch=PREFETCH, fp16=FP16,
+                           nhwc=NHWC, decoder_type=DECODER_TYPE, decoder_cache_params=DECODER_CACHE_PARAMS,
+                           reader_queue_depth=READER_QUEUE_DEPTH) for n in range(N)]
         [pipe.build() for pipe in pipes]
 
         if args.iters < 0:
@@ -184,8 +262,8 @@ for pipe_name in test_data.keys():
                 iters += 1
             iters_tmp = iters
 
-            iters = iters // N
-            if iters_tmp != iters * N:
+            iters = iters // SIMULATE_N_GPUS
+            if iters_tmp != iters * SIMULATE_N_GPUS:
                 iters += 1
         else:
             iters = args.iters
@@ -193,11 +271,11 @@ for pipe_name in test_data.keys():
         print ("RUN {0}/{1}: {2}".format(i + 1, data_set_len, pipe_name.__name__))
         print (data_set)
         end = time.time()
-        for i in range(2):
+        for i in range(args.epochs):
           if i == 0:
               print("Warm up")
-          elif i == 1:
-              print("Test run")
+          else:
+              print("Test run " + str(i))
           data_time = AverageMeter()
           for j in range(iters):
               for pipe in pipes:
@@ -209,4 +287,3 @@ for pipe_name in test_data.keys():
               end = time.time()
 
         print("OK {0}/{1}: {2}".format(i + 1, data_set_len, pipe_name.__name__))
-
