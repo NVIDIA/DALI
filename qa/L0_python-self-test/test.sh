6fb22499e50321639aee01966c4ae0536b38b99a
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-04-01 13:59:00 +0200

    Enable test for TensorFlow and CUDA 10
    - TensorFlow 1.13.1 supports CUDA 10 tests could be enabled for it
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/L0_python-self-test/test.sh
+++ qa/L0_python-self-test/test.sh
@@ -16,32 +16,29 @@ test_body() {
     python test_RN50_data_pipeline.py -i 10
     python test_RN50_data_fw_iterators.py -i 30 -b 13
 
-    # TensorFlow supports only CUDA 9.0 now
-    if [ "${CUDA_VERSION}" == "90"]; then
-        # DALI TF tests
+    # DALI TF tests
 
-        # Load plugin OK (supported version of TF in the prebuilt plugins)
-        nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadOk
+    # Load plugin OK (supported version of TF in the prebuilt plugins)
+    nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadOk
 
-        # Manually removing the supported plugin so that it fails
-        lib_dir=$(python -c 'import nvidia.dali.sysconfig as sc; print(sc.get_lib_dir())')
-        pushd $lib_dir/plugin
-        mkdir tmp
-        mv *.so tmp
-        popd
-        nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadFail
+    # Manually removing the supported plugin so that it fails
+    lib_dir=$(python -c 'import nvidia.dali.sysconfig as sc; print(sc.get_lib_dir())')
+    pushd $lib_dir/plugin
+    mkdir tmp
+    mv *.so tmp
+    popd
+    nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadFail
 
-        # Installing "current" dali tf (built against installed TF)
-        pip install ../../../nvidia-dali-tf-plugin*.tar.gz
-        nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadOk
+    # Installing "current" dali tf (built against installed TF)
+    pip install ../../../nvidia-dali-tf-plugin*.tar.gz
+    nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadOk
 
-        # Restore plugins (still works)
-        mv $lib_dir/plugin/tmp/* $lib_dir/plugin
-        rm -rf $lib_dir/plugin/tmp
-        nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadOk
+    # Restore plugins (still works)
+    mv $lib_dir/plugin/tmp/* $lib_dir/plugin
+    rm -rf $lib_dir/plugin/tmp
+    nosetests --verbose test_dali_tf_plugin.py:TestDaliTfPluginLoadOk
 
-        # end DALI TF tests
-    fi
+    # end DALI TF tests
 }
 
 source ../../../qa/test_template.sh
