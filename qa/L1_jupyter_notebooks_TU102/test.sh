9b22f664dfb2a6bca6c3d080620bfdcbb5aee906
szal.pal <szal.software@gmail.com>
2019-04-11 13:08:46 +0200

    updating test.sh

--- qa/L1_jupyter_notebooks_TU102/test.sh
+++ qa/L1_jupyter_notebooks_TU102/test.sh
@@ -5,15 +5,10 @@ source ../setup_test.sh
 # used pip packages
 pip_packages="jupyter"
 
-pushd ../..
-
-# attempt to run jupyter on all example notebooks
-mkdir -p idx_files
-
-cd docs/examples
+pushd ../../docs/examples
 
 test_body() {
-    test_files="optical_flow_example.ipynb"
+    test_files=("optical_flow/optical_flow_example.ipynb")
 
     # test code
     echo $test_files | xargs -i jupyter nbconvert \
