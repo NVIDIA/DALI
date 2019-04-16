ddaf63934cbc2161b795fef835117c68c3ee5c4f
Joaquin Anton <janton@nvidia.com>
2019-04-16 11:19:11 +0200

    Add option one_config_only to test template
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/L2_FW_iterators_perf/test.sh
+++ qa/L2_FW_iterators_perf/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages="torchvision mxnet-cu##CUDA_VERSION##"
+pip_packages="tensorflow-gpu torchvision mxnet-cu##CUDA_VERSION##"
+one_config_only=true
 
 pushd ../..
 
@@ -9,16 +10,6 @@ cd dali/test/python
 NUM_GPUS=$(nvidia-smi -L | wc -l)
 
 test_body() {
-    # We install it manually here because we want to limit to one version of TF
-    # depending on the CUDA version. (note: CUDA 10 is only supported in versions > 1.13)
-    if [[ "${CUDA_VERSION}" == "90" ]]; then
-        pip install tensorflow-gpu==1.12.0
-    elif [[ "${CUDA_VERSION}" == "100" ]]; then
-        pip install tensorflow-gpu==1.13.1
-    else
-        echo "Not supported CUDA version"
-        exit 1
-    fi
     python test_RN50_data_fw_iterators.py --gpus ${NUM_GPUS} -b 13 --workers 3 --prefetch 2 --epochs 3
 }
 
