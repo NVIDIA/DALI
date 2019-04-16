54e2c3baf68300a97d9f27093314b2124ef1e975
Albert Wolant <awolant@nvidia.com>
2019-04-16 21:10:51 +0200

    Make eval batch size smaller
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/L3_SSD_convergence/test_pytorch.sh
+++ qa/L3_SSD_convergence/test_pytorch.sh
@@ -18,7 +18,7 @@ NUM_GPUS=$(nvidia-smi -L | wc -l)
 LOG=dali.log
 
 SECONDS=0
-python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} main.py --backbone resnet50 --warmup 300 --bs 64 --data /coco --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.25 2>&1 | tee $LOG
+python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} main.py --backbone resnet50 --warmup 300 --bs 64 --eval-batch-size 8 --data /coco --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.25 2>&1 | tee $LOG
 
 RET=${PIPESTATUS[0]}
 echo "Training ran in $SECONDS seconds"
