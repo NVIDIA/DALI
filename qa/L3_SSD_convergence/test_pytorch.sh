81180449aa10b7564d74cf0b45c3128da62dcd03
Albert Wolant <44801854+awolant@users.noreply.github.com>
2019-04-17 11:22:31 +0200

    Add L3 SSD test (#782)
    * Add L3 SSD test
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>
    Signed-off-by: kychennv <kychen@nvidia.com>

---
+++ qa/L3_SSD_convergence/test_pytorch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+function CLEAN_AND_EXIT {
+    exit $1
+}
+
+cd /opt/dali/docs/examples/pytorch/single_stage_detector/
+
+pip install mlperf_compliance Cython==0.28.4
+pip install pycocotools==2.0.0
+
+NUM_GPUS=$(nvidia-smi -L | wc -l)
+
+LOG=dali.log
+
+SECONDS=0
+python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} main.py --backbone resnet50 --warmup 300 --bs 64 --eval-batch-size 8 --data /coco --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.25 2>&1 | tee $LOG
+
+RET=${PIPESTATUS[0]}
+echo "Training ran in $SECONDS seconds"
+if [[ $RET -ne 0 ]]; then
+    echo "Error in training script."
+    CLEAN_AND_EXIT 2
+fi
+
+CLEAN_AND_EXIT 0
