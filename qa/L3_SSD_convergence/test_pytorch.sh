b961f159fc01daa14f3fc31474f5bb6bbc6bfb27
Albert Wolant <awolant@nvidia.com>
2019-04-19 20:11:49 +0200

    Temporarily disable test
    Signed-off-by: Albert Wolant <awolant@nvidia.com>
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/L3_SSD_convergence/test_pytorch.sh
+++ qa/L3_SSD_convergence/test_pytorch.sh
@@ -8,22 +8,23 @@ function CLEAN_AND_EXIT {
     exit $1
 }
 
-cd /opt/dali/docs/examples/pytorch/single_stage_detector/
+# Temporarily disable test
+# cd /opt/dali/docs/examples/pytorch/single_stage_detector/
 
-pip install --force-reinstall  pycocotools==2.0.0
+# pip install --force-reinstall  pycocotools==2.0.0
 
-NUM_GPUS=$(nvidia-smi -L | wc -l)
+# NUM_GPUS=$(nvidia-smi -L | wc -l)
 
-LOG=dali.log
+# LOG=dali.log
 
-SECONDS=0
-python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} main.py --backbone resnet50 --warmup 300 --bs 64 --eval-batch-size 8 --data /coco --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.25 2>&1 | tee $LOG
+# SECONDS=0
+# python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} main.py --backbone resnet50 --warmup 300 --bs 64 --eval-batch-size 8 --data /coco --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.25 2>&1 | tee $LOG
 
-RET=${PIPESTATUS[0]}
-echo "Training ran in $SECONDS seconds"
-if [[ $RET -ne 0 ]]; then
-    echo "Error in training script."
-    CLEAN_AND_EXIT 2
-fi
+# RET=${PIPESTATUS[0]}
+# echo "Training ran in $SECONDS seconds"
+# if [[ $RET -ne 0 ]]; then
+#     echo "Error in training script."
+#     CLEAN_AND_EXIT 2
+# fi
 
 CLEAN_AND_EXIT 0
