cc9159725741f74ae99a672b973377189871528c
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-04-24 08:55:52 +0200

    Fix test_detection_pipeline for python2
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/test/python/test_detection_pipeline.py
+++ dali/test/python/test_detection_pipeline.py
@@ -273,8 +273,8 @@ def data_paths():
 
 def set_iters(args, dataset_size):
     if args.iters is None:
-        args.iters = ceil(
-            dataset_size / (args.batch_size * args.num_gpus))
+        args.iters = int(ceil(
+            dataset_size / (args.batch_size * args.num_gpus)))
 
 
 def to_array(dali_out):
