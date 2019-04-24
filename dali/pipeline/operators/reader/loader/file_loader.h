650cdb342e26510c8e7ba1179e802a64e70e4110
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2019-04-24 17:18:10 +0200

    Fix `shuffle_after_epoch` option (#812)
    - before that change, global shuffling happened only when
      data reader reached the end of data set, while it should happen when it goes beyond its shard. Now `shuffle_after_epoch` uses
      `stick_to_shard` to achieve that. Still, the user cannot use both
      of that options as `stick_to_shard` is meant to provide the same
      data inside the same shard at each epoch wile in conjunction with
      `shuffle_after_epoch` it will differ
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>
    Signed-off-by: kychennv <kychen@nvidia.com>

--- dali/pipeline/operators/reader/loader/file_loader.h
+++ dali/pipeline/operators/reader/loader/file_loader.h
@@ -56,6 +56,12 @@ class FileLoader : public Loader<CPUBackend, ImageLabelWrapper> {
       shuffle_after_epoch_(shuffle_after_epoch),
       current_index_(0),
       current_epoch_(0) {
+      /*
+       * Imply `stick_to_shard` from  `shuffle_after_epoch`
+       */
+      if (shuffle_after_epoch_) {
+        stick_to_shard_ = true;
+      }
     mmap_reserver = FileStream::FileStreamMappinReserver(
         static_cast<unsigned int>(initial_buffer_fill_));
     copy_read_data_ = !mmap_reserver.CanShareMappedData();
