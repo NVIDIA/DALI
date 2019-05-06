e24360098f2ebc35d403a2e95ac7d95cc9cc2119
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-05-06 10:10:45 +0200

    Fix MXNet L3 and PyTorch L1 and L3 tests
    - fix L3 bash test script for MXNet
    - fix too early usage of world_size in PyTorch RN50 script
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/L3_RN50_convergence/test_mxnet.sh
+++ qa/L3_RN50_convergence/test_mxnet.sh
@@ -18,12 +18,12 @@ mean=`awk 'BEGIN { sum = 0; n = 0 } { sum += $1; n += 1 } END { print sum / n }'
 
 rm tmp2.log tmp3.log
 
-if if [[ $(echo "$best > $threshold" | bc -l) == 0 ]]; then
+if [[ $(echo "$best > $threshold" | bc -l) == 0 ]]; then
     echo "acc = $best; TEST FAILED"
     exit -1
 fi
 
-if if [[ $(echo "$mean > $min_perf" | bc -l) == 0 ]]; then
+if [[ $(echo "$mean > $min_perf" | bc -l) == 0 ]]; then
     echo "perf = $mean; TEST FAILED"
     exit -1
 fi
