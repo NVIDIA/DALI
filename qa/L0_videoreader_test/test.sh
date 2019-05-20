8b62b86a000880134d5d0fdbc711237a550f5c5a
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-05-20 18:25:26 +0200

    Fix out or range read in video file with stride
    - makes L0 video test to use DALI_extra
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/L0_videoreader_test/test.sh
+++ qa/L0_videoreader_test/test.sh
@@ -7,20 +7,21 @@ apt-get install -y wget ffmpeg
 
 pushd ../..
 
+source qa/setup_dali_extra.sh
+
 cd docs/examples/video
 
 mkdir -p video_files
 
-container_name=prepared.mp4
 
-# Download video sample
-wget -q -O ${container_name} https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4
+container_path=${DALI_EXTRA_PATH}/db/optical_flow/sintel_trailer/sintel_trailer.mp4
 
-IFS='.' read -a splitted <<< "$container_name"
+IFS='/' read -a container_name <<< "$container_path"
+IFS='.' read -a splitted <<< "${container_name[-1]}"
 
 for i in {0..4};
 do
-    ffmpeg -ss 00:00:${i}0 -t 00:00:10 -i $container_name -vcodec copy -acodec copy -y video_files/${splitted[0]}_$i.${splitted[1]}
+    ffmpeg -ss 00:00:${i}0 -t 00:00:10 -i $container_path -vcodec copy -acodec copy -y video_files/${splitted[0]}_$i.${splitted[1]} 
 done
 
 test_body() {
