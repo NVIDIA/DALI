1749a5d933db468b2d7682dd90302040ae7523a3
Albert Wolant <awolant@nvidia.com>
2019-05-23 15:43:39 +0200

    Lower test target
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/L1_ssd_training/test.sh
+++ qa/L1_ssd_training/test.sh
@@ -16,7 +16,7 @@ test_body() {
     pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
     popd
 
-    python -m torch.distributed.launch --nproc_per_node=8 main.py --backbone resnet50 --warmup 300 --bs 64 --eval-batch-size 8 --epochs 4 --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.09
+    python -m torch.distributed.launch --nproc_per_node=8 main.py --backbone resnet50 --warmup 300 --bs 64 --eval-batch-size 8 --epochs 4 --data /data/coco/coco-2017/coco2017/ --data_pipeline dali --target 0.085
 }
 
 source ../../../../qa/test_template.sh
