1979bcc4eae0c8c25bc7229bc3e8b05c79fd686b
Joaquin Anton <janton@nvidia.com>
2019-05-24 13:03:19 +0200

    Add NewSlice operator
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/pipeline/operators/crop/new_slice.cc
@@ -0,0 +1,56 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "dali/pipeline/operators/crop/new_slice.h"
+
+namespace dali {
+
+DALI_SCHEMA(NewSlice)
+    .DocStr(
+        R"code(Crop a slice of a defined `size` from an `input` tensor, staring
+at the location specified by `begin`. Inputs must be supplied as 3 Tensors in a
+specific order: `Images` containing image data in NHWC format, `Begin` containing
+the starting pixel coordinates for the `crop` in `(x,y)` format, and `Size` containing
+the pixel dimensions of the `crop` in `(w,h)` format. For both `Begin` and `Size`,
+coordinates must be in the interval `[0.0, 1.0]`. The resulting tensor output of
+Slice operation is a cropped version of the input tensor `Images`.
+**Experimental** Use `Slice` instead)code")
+    .NumInput(3)
+    .NumOutput(1)
+    .AllowSequences(false)
+    .AddOptionalArg(
+      "image_type",
+      R"code(The color space of input and output image)code",
+      DALI_RGB, false);
+
+template <>
+void NewSlice<CPUBackend>::DataDependentSetup(SampleWorkspace *ws, int idx) {
+  const auto &images = ws->Input<CPUBackend>(0);
+  const auto &anchor_tensor = ws->Input<CPUBackend>(1);
+  const auto &slice_shape_tensor = ws->Input<CPUBackend>(2);
+  const auto shape = images.shape();
+  const float* anchor_norm = anchor_tensor.template data<float>();
+  const float* slice_shape_norm = slice_shape_tensor.template data<float>();
+  DataDependentSetup(ws->data_idx(), shape, anchor_norm, slice_shape_norm);
+}
+
+template <>
+void NewSlice<CPUBackend>::RunImpl(SampleWorkspace *ws, int idx) {
+  DataDependentSetup(ws, idx);
+  NewCrop<CPUBackend>::RunImpl(ws, idx);
+}
+
+DALI_REGISTER_OPERATOR(NewSlice, NewSlice<CPUBackend>, CPU);
+
+}  // namespace dali
---
+++ dali/pipeline/operators/crop/new_slice.h
@@ -0,0 +1,76 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_PIPELINE_OPERATORS_CROP_NEW_SLICE_H_
+#define DALI_PIPELINE_OPERATORS_CROP_NEW_SLICE_H_
+
+#include <utility>
+#include <vector>
+#include "dali/core/common.h"
+#include "dali/core/error_handling.h"
+#include "dali/pipeline/operators/common.h"
+#include "dali/pipeline/operators/crop/new_crop.h"
+#include "dali/pipeline/operators/operator.h"
+
+namespace dali {
+
+template <typename Backend>
+class NewSlice : public NewCrop<Backend> {
+ public:
+  explicit inline NewSlice(const OpSpec &spec)
+    : NewCrop<Backend>(spec) {}
+
+ protected:
+  using NewCrop<Backend>::input_type_;
+  using NewCrop<Backend>::output_type_;
+  using NewCrop<Backend>::slice_anchors_;
+  using NewCrop<Backend>::slice_shapes_;
+
+  void RunImpl(Workspace<Backend> *ws, int idx) override;
+
+  void SetupSharedSampleParams(Workspace<Backend> *ws) override {
+    DALI_ENFORCE(ws->NumInput() == 3,
+      "Expected 3 inputs. Received: " + std::to_string(ws->NumInput()));
+    input_type_ = ws->template Input<Backend>(0).type().id();
+    if (output_type_ == DALI_NO_TYPE) {
+      output_type_ = input_type_;
+    }
+  }
+
+private:
+  void DataDependentSetup(Workspace<Backend> *ws, int idx);
+
+  void DataDependentSetup(int data_idx,
+                          const Dims &shape,
+                          const float *anchor_norm,
+                          const float *slice_dims_norm) {
+    auto &anchor = slice_anchors_[data_idx];
+    for (std::size_t d = 0; d < shape.size(); d++) {
+      anchor[d] = anchor_norm[d] * shape[d];
+    }
+
+    // To decrease floating point error, first calculate the bounding box of crop and then
+    // calculate the width and height having left and top coordinates
+    auto &slice_shape = slice_shapes_[data_idx];
+    for (std::size_t d = 0; d < shape.size(); d++) {
+      float slice_end_norm = anchor_norm[d] + slice_dims_norm[d];
+      int64_t slice_end = slice_end_norm * shape[d];
+      slice_shape[d] = slice_end - anchor[d];
+    }
+  }
+};
+
+}  // namespace dali
+
+#endif  // DALI_PIPELINE_OPERATORS_CROP_SLICE_H_
