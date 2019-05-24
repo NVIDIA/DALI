3898a5058018d40673943360506c5e354b971979
Krzysztof Lecki <klecki@nvidia.com>
2019-05-24 12:45:22 +0200

    More improvements

--- qa/setup_dali_extra.sh
+++ qa/setup_dali_extra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch test data
-export DALI_EXTRA_PATH=/opt/dali_extra
+export DALI_EXTRA_PATH=${DALI_EXTRA_PATH:-/opt/dali_extra}
 DALI_EXTRA_URL="https://github.com/NVIDIA/DALI_extra.git"
 if [ ! -d "$DALI_EXTRA_PATH" ] ; then
     git clone --depth=1 "$DALI_EXTRA_URL" "$DALI_EXTRA_PATH"
--- tools/coverage_source.sh
+++ tools/coverage_source.sh
@@ -4,17 +4,20 @@
 
 mkdir -p build_coverage_source
 pushd build_coverage_source
-# cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_LMDB=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCOVERAGE_SOURCE=ON ..
+cmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_LMDB=ON -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE_SOURCE=ON ..
 make -j$(nproc)
 
-# Run
-LLVM_PROFILE_FILE="dali_test.profraw" ./dali/python/nvidia/dali/test/dali_test.bin
+export DALI_EXTRA_PATH=dali_extra
+source ../qa/setup_dali_extra.sh
+
+# Run, and allow it to fail
+LLVM_PROFILE_FILE="dali_test.profraw" ./dali/python/nvidia/dali/test/dali_test.bin || true
 # Index the raw profile.
 llvm-profdata merge -sparse dali_test.profraw -o dali_test.profdata
 # Create file and function level reports
 llvm-cov report ./dali/python/nvidia/dali/test/dali_test.bin -instr-profile=dali_test.profdata > cov-report
-llvm-cov report ./dali/python/nvidia/dali/test/dali_test.bin -show-functions -instr-profile=dali_test.profdata -Xdemangler=c++filt .. > cov-report-functions
+llvm-cov report ./dali/python/nvidia/dali/test/dali_test.bin -show-functions -instr-profile=dali_test.profdata -Xdemangler=llvm-cxxfilt .. > cov-report-functions
 # Create a line-oriented coverage report
-llvm-cov show -format html ./dali/python/nvidia/dali/test/dali_test.bin -instr-profile=dali_test.profdata > cov-src.html
+llvm-cov show -format html ./dali/python/nvidia/dali/test/dali_test.bin -instr-profile=dali_test.profdata -Xdemangler=llvm-cxxfilt > cov-src.html
 
 popd
