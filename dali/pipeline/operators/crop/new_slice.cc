98b96c4724e04d7c91c9e7cfd2bd359bab77762c
Joaquin Anton <janton@nvidia.com>
2019-05-29 09:59:44 +0200

    Update NewSlice documentation
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/pipeline/operators/crop/new_slice.cc
+++ dali/pipeline/operators/crop/new_slice.cc
@@ -18,14 +18,17 @@ namespace dali {
 
 DALI_SCHEMA(NewSlice)
     .DocStr(
-        R"code(Crop a slice of a defined `size` from an `input` tensor, staring
-at the location specified by `begin`. Inputs must be supplied as 3 Tensors in a
-specific order: `Images` containing image data in NHWC format, `Begin` containing
-the starting pixel coordinates for the `crop` in `(x,y)` format, and `Size` containing
-the pixel dimensions of the `crop` in `(w,h)` format. For both `Begin` and `Size`,
-coordinates must be in the interval `[0.0, 1.0]`. The resulting tensor output of
-Slice operation is a cropped version of the input tensor `Images`.
-**Experimental** Use `Slice` instead)code")
+        R"code(Extract a subtensor or `slice` with a given shape and anchor.
+ Inputs must be supplied as 3 separate tensors in a specific order: `data`
+ containing input data, `anchor` containing normalize coordinates for the
+ starting point of the slice (x0, x1, x2, ...), and `shape` containing the normalized
+ dimensions of the slice (s0, s1, s2, ...). Both `anchor` and `shape` coordinates
+ must be in the interval [0.0, 1.0] and should have as many dimensions as the input
+ data. For compatibility with the previous implementation of Slice, `anchor` and
+ `slice` can be specified in format (x, y) and (w, h) respectively for images.
+ This way of specifying the slice arguments is deprecated and shall be removed in
+ future versions of DALI.
+ **Experimental** Use `Slice` instead)code")
     .NumInput(3)
     .NumOutput(1)
     .AllowSequences(false)
