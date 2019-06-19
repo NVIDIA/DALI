27e5be7ceea15171ec90bf21854db3159906e02a
Krzysztof Lecki <klecki@nvidia.com>
2019-06-19 10:52:34 +0200

    Use split decoder

--- qa/L2_RN50_data_perf/test.sh
+++ qa/L2_RN50_data_perf/test.sh
@@ -10,11 +10,11 @@ NUM_GPUS=$(nvidia-smi -L | wc -l)
 
 test_body() {
     # test code
-    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2
-    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 16 --workers 3 --prefetch 11
+    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2 --decoder_type split
+    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 16 --workers 3 --prefetch 11 --decoder_type split
     # fp16 NHWC
-    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2 --fp16 --nhwc
-    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 16 --workers 3 --prefetch 11 --fp16 --nhwc
+    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2 --fp16 --nhwc --decoder_type split
+    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 16 --workers 3 --prefetch 11 --fp16 --nhwc --decoder_type split
 }
 
 source ../../../qa/test_template.sh
