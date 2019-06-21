00ba9085dc2b32e2015e23681f62f943a6d3e0a9
Joaquin Anton <janton@nvidia.com>
2019-06-21 08:38:01 +0200

    Restructure subdirectories in nvjpeg decoder
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/pipeline/operators/decoder/host/fused/host_decoder_random_crop.cc
@@ -0,0 +1,38 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <opencv2/opencv.hpp>
+#include <tuple>
+#include <vector>
+#include <memory>
+#include "dali/core/error_handling.h"
+#include "dali/image/image_factory.h"
+#include "dali/pipeline/operators/decoder/host/fused/host_decoder_random_crop.h"
+#include "dali/pipeline/operators/common.h"
+
+namespace dali {
+
+DALI_REGISTER_OPERATOR(HostDecoderRandomCrop, HostDecoderRandomCrop, CPU);
+
+DALI_SCHEMA(HostDecoderRandomCrop)
+  .DocStr(R"code(Decode images on the host with a random cropping anchor/window.
+When possible, will make use of partial decoding (e.g. libjpeg-turbo).
+When not supported, will decode the whole image and then crop.
+Output of the decoder is in `HWC` ordering.)code")
+  .NumInput(1)
+  .NumOutput(1)
+  .AddParent("HostDecoder")
+  .AddParent("RandomCropAttr");
+
+}  // namespace dali
---
+++ dali/pipeline/operators/decoder/nvjpeg/legacy_api/nvjpeg_decoder.cc
@@ -0,0 +1,45 @@
+// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <string>
+#include "dali/pipeline/operators/decoder/nvjpeg/legacy_api/nvjpeg_decoder.h"
+
+namespace dali {
+
+DALI_REGISTER_OPERATOR(nvJPEGDecoder, nvJPEGDecoder, Mixed);
+
+DALI_SCHEMA(nvJPEGDecoder)
+  .DocStr(R"code(Decode JPEG images using the nvJPEG library.
+Output of the decoder is on the GPU and uses `HWC` ordering.)code")
+  .NumInput(1)
+  .NumOutput(1)
+  .AddOptionalArg("output_type",
+      R"code(The color space of output image.)code",
+      DALI_RGB)
+  .AddOptionalArg("use_batched_decode",
+      R"code(Use nvJPEG's batched decoding API.)code",
+      false)
+  .AddOptionalArg("device_memory_padding",
+      R"code(Padding for nvJPEG's device memory allocations in bytes.
+This parameter helps to avoid reallocation in nvJPEG whenever a bigger image
+is encountered and internal buffer needs to be reallocated to decode it.)code",
+      16*1024*1024)
+  .AddOptionalArg("host_memory_padding",
+      R"code(Padding for nvJPEG's host memory allocations in bytes.
+This parameter helps to avoid reallocation in nvJPEG whenever a bigger image
+is encountered and internal buffer needs to be reallocated to decode it.)code",
+      16*1024*1024)
+  .AddParent("CachedDecoderAttr");
+
+}  // namespace dali
