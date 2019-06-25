4b1793c6954f9bdd9fc7a16f16e6bea1ee8ba1b5
Serge Panev <spanev@nvidia.com>
2019-06-25 15:02:45 +0900

    Add no squeeze option and dynamic shape for MXNet and PyTorch plugins (#988)
    Add no squeeze option and dynamic shape for MXNet and PyTorch plugins
    
    Signed-off-by: Serge Panev <spanev@nvidia.com>

--- qa/L0_FW_iterators/test.sh
+++ qa/L0_FW_iterators/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 # used pip packages
 
-pip_packages="tensorflow-gpu torchvision mxnet-cu##CUDA_VERSION##"
+source ../setup_dali_extra.sh
+
+pip_packages="nose tensorflow-gpu torchvision mxnet-cu##CUDA_VERSION##"
 one_config_only=true
 
 pushd ../..
@@ -12,6 +14,8 @@ NUM_GPUS=$(nvidia-smi -L | wc -l)
 
 test_body() {
     python test_RN50_data_fw_iterators.py --gpus ${NUM_GPUS} -b 13 --workers 3 --prefetch 2 -i 100 --epochs 2
+
+    nosetests --verbose test_fw_iterators_detection.py
 }
 
 source ../../../qa/test_template.sh
