4e8b1bec183f8c2788186aa2c8116e5ff2654483
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-06-25 08:38:59 +0200

    Add path to json in case of error in the COCO reader
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/pipeline/operators/reader/loader/coco_loader.cc
+++ dali/pipeline/operators/reader/loader/coco_loader.cc
@@ -298,7 +298,7 @@ void ParseAnnotationFilesHelper(std::vector<std::string> &annotations_filename,
   for (auto& file_name : annotations_filename) {
     // Loading raw json into the RAM
     std::ifstream f(file_name);
-    DALI_ENFORCE(f, "Could not open JSON annotations file");
+    DALI_ENFORCE(f, "Could not open JSON annotations file: " + file_name);
     f.seekg(0, std::ios::end);
     size_t file_size = f.tellg();
     std::unique_ptr<char, std::function<void(char*)>> buff(new char[file_size],
