86f5fb3ece16888c1d342f193bf602d4305d1a92
Michał Szołucha <mszolucha@nvidia.com>
2019-07-09 15:29:36 +0200

    update
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- include/dali/core/geom/box.h
+++ include/dali/core/geom/box.h
@@ -129,8 +129,8 @@ constexpr DALI_HOST_DEV CoordinateType volume(const Box<ndims, CoordinateType> &
 template<size_t ndims, typename CoordinateType>
 constexpr DALI_HOST_DEV Box<ndims, CoordinateType>
 intersection(const Box<ndims, CoordinateType> &lhs, const Box<ndims, CoordinateType> &rhs) {
-  if (!lhs.overlaps(rhs)) return {};
-  return Box<ndims, CoordinateType>(max(lhs.lo, rhs.lo), min(lhs.hi, rhs.hi));
+  Box<ndims, CoordinateType> tmp = {max(lhs.lo, rhs.lo), min(lhs.hi, rhs.hi)};
+  return any_coord(tmp.hi <= tmp.lo) ? Box<ndims, CoordinateType>() : tmp;
 }
 
 
