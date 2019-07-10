37038df2e57548a6829f0b2fb9e7d1d48ecb3d16
Rafal <Banas.Rafal97@gmail.com>
2019-07-10 16:32:37 +0200

    Fix CUDA version.
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- qa/download_pip_packages.sh
+++ qa/download_pip_packages.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-pip_packages="nose matplotlib jupyter numpy torch tensorflow-gpu mxnet-cu##CUDA_VERSION## torchvision pillow"
-pip_packages=$(echo ${pip_packages} | sed "s/##CUDA_VERSION##/${CUDA_VERSION}/")
+CUDA_VERSION=$(cat /usr/local/cuda/version.txt | sed 's/.*Version \([0-9]\+\)\.\([0-9]\+\).*/\1\2/')
+CUDA_VERSION=${CUDA_VERSION:-90}
 
+pip_packages="nose matplotlib jupyter numpy torch tensorflow-gpu mxnet-cu${CUDA_VERSION} torchvision pillow"
+echo ${CUDA_VERSION}
 mkdir /pip-packages
 pip download $(/opt/dali/qa/setup_packages.py -a -u ${pip_packages} --cuda ${CUDA_VERSION}) -d /pip-packages
 
