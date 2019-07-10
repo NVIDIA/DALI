6cea422473843acee5d09d53f87aece3122dfd5a
Michał Szołucha <mszolucha@nvidia.com>
2019-07-10 12:53:57 +0200

    ready to review
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/kernels/imgproc/colour_manipulation/brightness_contrast.h
+++ dali/kernels/imgproc/colour_manipulation/brightness_contrast.h
@@ -17,6 +17,7 @@
 
 #include "dali/util/ocv.h"
 #include "dali/core/common.h"
+#include "dali/core/geom/box.h"
 #include "dali/core/error_handling.h"
 #include "dali/kernels/kernel.h"
 #include "dali/pipeline/data/types.h"
@@ -24,23 +25,35 @@
 namespace dali {
 namespace kernels {
 
+namespace brightness_contrast {
+
+// TODO(mszolucha): Add Box support -> Roi(const Box<3, int> &box)
 struct Roi {
+  Roi() = default;
+
+
+  Roi(int x, int y, int w, int h) : x(x), y(y), w(w), h(h), use_roi(true) {}
+
+
   int x, y, w, h;
+  bool use_roi = false;
 };
+
+}  // namespace brightness_contrast
 
 template<typename ComputeBackend, typename InputType, typename OutputType>
 class DLL_PUBLIC BrightnessContrast {
  private:
   using StorageBackend = compute_to_storage_t<ComputeBackend>;
+  static constexpr size_t ndims = 3;
 
  public:
   DLL_PUBLIC KernelRequirements
-  Setup(KernelContext &context, const InTensor<StorageBackend, InputType, 3> &image,
-        InputType brightness,
-        InputType contrast, Roi roi = {0, 0, 0, 0}) {
+  Setup(KernelContext &context, const InTensor<StorageBackend, InputType, ndims> &image,
+        InputType brightness, InputType contrast, brightness_contrast::Roi roi = {}) {
     handle_default_roi(roi, image.shape);
     KernelRequirements req;
-    req.output_shapes = {TensorListShape<DynamicDimensions>({roi_to_shape<3>(roi)})};
+    req.output_shapes = {TensorListShape<DynamicDimensions>({roi_to_shape<ndims>(roi)})};
     return req;
   }
 
@@ -48,22 +61,22 @@ class DLL_PUBLIC BrightnessContrast {
   // TODO(mszolucha): CHW layout
   // TODO(mszolucha): first brightness then contrast
   /**
-   * Assumes (for now) HWC memory layout
+   * Assumes HWC memory layout
    *
    * @param out Assumes, that memory is already allocated
    * @param brightness Additive brightness delta. 0 denotes no change
    * @param contrast Multiplicative contrast delta. 1 denotes no change
-   * @param roi When default roi is provided, kernel operates on entire image ("no-roi" case)
+   * @param roi When default or invalid roi is provided,
+   *            kernel operates on entire image ("no-roi" case)
    */
-  DLL_PUBLIC void Run(KernelContext &context, const OutTensor<StorageBackend, OutputType, 3> &out,
-                      const InTensor<StorageBackend, InputType, 3> &in,
-                      InputType brightness,
-                      InputType contrast, Roi roi = {0, 0, 0, 0}) {
+  DLL_PUBLIC void
+  Run(KernelContext &context, const OutTensor<StorageBackend, OutputType, ndims> &out,
+      const InTensor<StorageBackend, InputType, ndims> &in, InputType brightness,
+      InputType contrast, brightness_contrast::Roi roi = {}) {
     handle_default_roi(roi, in.shape);
     auto num_channels = in.shape[2];
     auto image_width = in.shape[1];
     auto ptr = out.data;
-    DALI_ENFORCE(roi.h > 0 && roi.w > 0, "Region of interest can't be empty");
 
     for (int y = roi.y; y < roi.y + roi.h; y++) {
       for (int xc = (roi.x + y * image_width) * num_channels;
@@ -75,18 +88,20 @@ class DLL_PUBLIC BrightnessContrast {
 
 
  private:
-  void handle_default_roi(Roi &roi, const TensorShape<DynamicDimensions> &shape) {
-    if (roi.h == 0 && roi.w == 0) {
+  void
+  handle_default_roi(brightness_contrast::Roi &roi, const TensorShape<DynamicDimensions> &shape) {
+    if (!roi.use_roi) {
       roi.x = 0;
       roi.y = 0;
       roi.h = shape[0];
       roi.w = shape[1];
+      roi.use_roi = true;
     }
   }
 
 
   template<int nchannels>
-  TensorShape<nchannels> roi_to_shape(const Roi &roi) {
+  TensorShape<nchannels> roi_to_shape(const brightness_contrast::Roi &roi) {
     TensorShape<nchannels> sh = {static_cast<int64_t>(roi.h), roi.w, nchannels};
     return sh;
   }
