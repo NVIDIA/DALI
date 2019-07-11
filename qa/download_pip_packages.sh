f0aee0df246303a8204994c8ff2984221b4843d8
Rafal <Banas.Rafal97@gmail.com>
2019-07-11 11:22:54 +0200

    Fix download script.
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- qa/download_pip_packages.sh
+++ qa/download_pip_packages.sh
@@ -6,7 +6,10 @@ CUDA_VERSION=${CUDA_VERSION:-90}
 pip_packages="nose matplotlib jupyter numpy torch tensorflow-gpu mxnet-cu${CUDA_VERSION} torchvision pillow"
 echo ${CUDA_VERSION}
 mkdir /pip-packages
-pip download $(/opt/dali/qa/setup_packages.py -a -u ${pip_packages} --cuda ${CUDA_VERSION}) -d /pip-packages
+to_download=$(/opt/dali/qa/setup_packages.py -a -u ${pip_packages} --cuda ${CUDA_VERSION})
+for p in $( to_download ); do
+    pip download ${p} -d /pip-packages
+done
 
 
 
