6bebd7529a937e9744e90b37306d2920ebb8f197
Michał Zientkiewicz <michalz@nvidia.com>
2019-07-12 16:28:38 +0200

    Rounding Convert and ConvertSat added. (#1068)
    * Rounding Convert and ConvertSat added.
    * Add a Converter helper class to avoid excessive number of `enable_if`s.
    * Add documentation for `Convert[Sat][Norm]` function family.
    * Add ./include to directory list processed by Doxygen.
    
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- include/dali/core/convert.h
+++ include/dali/core/convert.h
@@ -19,6 +19,7 @@
 #include <cstdint>
 #include <limits>
 #include <type_traits>
+#include "dali/core/host_dev.h"
 #ifndef __CUDA_ARCH__
 #include "dali/util/half.hpp"
 #else
@@ -31,11 +32,11 @@ struct const_limits;
 
 // std::numeric_limits are not compatible with CUDA
 template <typename T>
-__host__ __device__ constexpr T max_value() {
+DALI_HOST_DEV constexpr T max_value() {
   return const_limits<std::remove_cv_t<T>>::max;
 }
 template <typename T>
-__host__ __device__ constexpr T min_value() {
+DALI_HOST_DEV constexpr T min_value() {
   return const_limits<std::remove_cv_t<T>>::min;
 }
 
@@ -78,83 +79,83 @@ struct ret_type {  // a placeholder for return type
 };
 
 template <typename T, typename U>
-__host__ __device__ constexpr std::enable_if_t<
+DALI_HOST_DEV constexpr std::enable_if_t<
     needs_clamp<U, T>::value && std::is_signed<U>::value,
     T>
 clamp(U value, ret_type<T>) {
-  return value < min_value<T>() ? min_value<T>() :
-         value > max_value<T>() ? max_value<T>() :
+  return value <= min_value<T>() ? min_value<T>() :
+         value >= max_value<T>() ? max_value<T>() :
          static_cast<T>(value);
 }
 
 template <typename T, typename U>
-__host__ __device__ constexpr std::enable_if_t<
+DALI_HOST_DEV constexpr std::enable_if_t<
     needs_clamp<U, T>::value && std::is_unsigned<U>::value,
     T>
 clamp(U value, ret_type<T>) {
-  return value > max_value<T>() ? max_value<T>() : static_cast<T>(value);
+  return value >= max_value<T>() ? max_value<T>() : static_cast<T>(value);
 }
 
 template <typename T, typename U>
-__host__ __device__ constexpr std::enable_if_t<
+DALI_HOST_DEV constexpr std::enable_if_t<
     !needs_clamp<U, T>::value,
     T>
 clamp(U value, ret_type<T>) { return value; }
 
-__host__ __device__ constexpr int32_t clamp(uint32_t value, ret_type<int32_t>) {
+DALI_HOST_DEV constexpr int32_t clamp(uint32_t value, ret_type<int32_t>) {
   return value & 0x80000000u ? 0x7fffffff : value;
 }
 
-__host__ __device__ constexpr uint32_t clamp(int32_t value, ret_type<uint32_t>) {
+DALI_HOST_DEV constexpr uint32_t clamp(int32_t value, ret_type<uint32_t>) {
   return value < 0 ? 0u : value;
 }
 
-__host__ __device__ constexpr int32_t clamp(int64_t value, ret_type<int32_t>) {
+DALI_HOST_DEV constexpr int32_t clamp(int64_t value, ret_type<int32_t>) {
   return value < static_cast<int64_t>(min_value<int32_t>()) ? min_value<int32_t>() :
          value > static_cast<int64_t>(max_value<int32_t>()) ? max_value<int32_t>() :
          static_cast<int32_t>(value);
 }
 
 template <>
-__host__ __device__ constexpr int32_t clamp(uint64_t value, ret_type<int32_t>) {
+DALI_HOST_DEV constexpr int32_t clamp(uint64_t value, ret_type<int32_t>) {
   return value > static_cast<uint64_t>(max_value<int32_t>()) ? max_value<int32_t>() :
          static_cast<int32_t>(value);
 }
 
 template <>
-__host__ __device__ constexpr uint32_t clamp(int64_t value, ret_type<uint32_t>) {
+DALI_HOST_DEV constexpr uint32_t clamp(int64_t value, ret_type<uint32_t>) {
   return value < 0 ? 0 :
          value > static_cast<int64_t>(max_value<uint32_t>()) ? max_value<uint32_t>() :
          static_cast<uint32_t>(value);
 }
 
 template <>
-__host__ __device__ constexpr uint32_t clamp(uint64_t value, ret_type<uint32_t>) {
+DALI_HOST_DEV constexpr uint32_t clamp(uint64_t value, ret_type<uint32_t>) {
   return value > static_cast<uint64_t>(max_value<uint32_t>()) ? max_value<uint32_t>() :
          static_cast<uint32_t>(value);
 }
 
 template <typename T>
-__host__ __device__ constexpr bool clamp(T value, ret_type<bool>) {
+DALI_HOST_DEV constexpr bool clamp(T value, ret_type<bool>) {
   return static_cast<bool>(value);
 }
 
 #ifndef __CUDA_ARCH__
 template <typename T>
-__host__ __device__ constexpr half_float::half clamp(T value, ret_type<half_float::half>) {
+DALI_HOST_DEV constexpr half_float::half clamp(T value, ret_type<half_float::half>) {
   return static_cast<half_float::half>(value);
 }
 
 template <typename T>
-__host__ __device__ constexpr T clamp(half_float::half value, ret_type<T>) {
+DALI_HOST_DEV constexpr T clamp(half_float::half value, ret_type<T>) {
   return clamp(static_cast<float>(value), ret_type<T>());
 }
 
-__host__ __device__ inline bool clamp(half_float::half value, ret_type<bool>) {
+DALI_HOST_DEV inline bool clamp(half_float::half value, ret_type<bool>) {
   return static_cast<bool>(value);
 }
 
-__host__ __device__ constexpr half_float::half clamp(half_float::half value,
+DALI_HOST_DEV constexpr half_float::half clamp(half_float::half value,
                                                      ret_type<half_float::half>) {
   return value;
 }
@@ -162,81 +163,294 @@ __host__ __device__ constexpr half_float::half clamp(half_float::half value,
 #else
 
 template <typename T>
-__host__ __device__ constexpr float16 clamp(T value, ret_type<float16>) {
+DALI_HOST_DEV constexpr float16 clamp(T value, ret_type<float16>) {
   return static_cast<float16>(value);
 }
 
 // __half does not have a constructor for int64_t, use long long
-__host__ __device__ inline float16 clamp(int64_t value, ret_type<float16>) {
+DALI_HOST_DEV inline float16 clamp(int64_t value, ret_type<float16>) {
   return static_cast<float16>(static_cast<long long int>(value));  // NOLINT
 }
 
 template <typename T>
-__host__ __device__ constexpr T clamp(float16 value, ret_type<T>) {
+DALI_HOST_DEV constexpr T clamp(float16 value, ret_type<T>) {
   return clamp(static_cast<float>(value), ret_type<T>());
 }
 
-__host__ __device__ inline bool clamp(float16 value, ret_type<bool>) {
+DALI_HOST_DEV inline bool clamp(float16 value, ret_type<bool>) {
   return static_cast<bool>(value);
 }
 
-__host__ __device__ constexpr float16 clamp(float16 value, ret_type<float16>) {
+DALI_HOST_DEV constexpr float16 clamp(float16 value, ret_type<float16>) {
   return value;
 }
 
 #endif
 
 template <typename T, typename U>
-__host__ __device__ constexpr T clamp(U value) {
+DALI_HOST_DEV constexpr T clamp(U value) {
   return clamp(value, ret_type<T>());
 }
 
-template <typename Out, typename In>
-__host__ __device__ constexpr Out Convert(In value) {
-  return static_cast<Out>(value);
-}
-
-template <typename Out, typename In>
-__host__ __device__ constexpr Out ConvertSat(In value) {
-  return clamp<Out>(value);
-}
-
-template <typename Out, typename In>
-__host__ __device__ constexpr std::enable_if_t<
-    std::is_floating_point<Out>::value && !std::is_floating_point<In>::value, Out>
-ConvertNorm(In value) {
-  return value * (Out(1) / max_value<In>());
-}
-
-template <typename Out, typename In>
-__host__ __device__ constexpr std::enable_if_t<
-    std::is_floating_point<Out>::value && std::is_floating_point<In>::value, Out>
-ConvertNorm(In value) {
-  return static_cast<Out>(value);
-}
-
-template <typename Out>
-constexpr __device__ __host__ std::enable_if_t<std::is_unsigned<Out>::value, Out>
-ConvertSatNorm(float value) {
+namespace detail {
 #ifdef __CUDA_ARCH__
-  return max_value<Out>() * __saturatef(value);
-#else
-  return max_value<Out>() * (value < 0.0f ? 0.0f : value > 1.0f ? 1.0f : value);
-#endif
-}
 
-template <typename Out>
-constexpr __device__ __host__ std::enable_if_t<
-  std::is_signed<Out>::value && std::is_integral<Out>::value, Out>
-ConvertSatNorm(float value) {
-  return clamp<Out>(value * static_cast<float>(max_value<Out>()));
+__device__ int cuda_round_helper(float f, int) {  // NOLINT
+  return __float2int_rn(f);
 }
+__device__ unsigned cuda_round_helper(float f, unsigned) {  // NOLINT
+  return __float2uint_rn(f);
+}
+__device__ long long  cuda_round_helper(float f, long long) {  // NOLINT
+  return __float2ll_rn(f);
+}
+__device__ unsigned long long cuda_round_helper(float f, unsigned long long) {  // NOLINT
+  return __float2ull_rn(f);
+}
+__device__ int cuda_round_helper(double f, int) {  // NOLINT
+  return __double2int_rn(f);
+}
+__device__ unsigned cuda_round_helper(double f, unsigned) {  // NOLINT
+  return __double2uint_rn(f);
+}
+__device__ long long  cuda_round_helper(double f, long long) {  // NOLINT
+  return __double2ll_rn(f);
+}
+__device__ unsigned long long cuda_round_helper(double f, unsigned long long) {  // NOLINT
+  return __double2ull_rn(f);
+}
+#endif
+
+template <typename Out, typename In,
+  bool OutIsFP = std::is_floating_point<Out>::value,
+  bool InIsFP = std::is_floating_point<In>::value>
+struct ConverterBase;
 
 template <typename Out, typename In>
-__host__ __device__ constexpr std::enable_if_t<
-    !std::is_floating_point<Out>::value && std::is_floating_point<In>::value, Out>
-ConvertNorm(In value) {
-  return ConvertSatNorm<Out>(value);
+struct Converter : ConverterBase<Out, In> {
+  static_assert(std::is_arithmetic<Out>::value && std::is_arithmetic<In>::value,
+    "Default ConverterBase can only be used with arithmetic types. For custom types, "
+    "specialize or overload dali::Convert");
+};
+
+/// Converts between two FP types
+template <typename Out, typename In>
+struct ConverterBase<Out, In, true, true> {
+  DALI_HOST_DEV
+  static constexpr Out Convert(In value) { return value; }
+  DALI_HOST_DEV
+  static constexpr Out ConvertNorm(In value) { return value; }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSat(In value) { return value; }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSatNorm(In value) { return value; }
+};
+
+/// Converts integral to FP type
+template <typename Out, typename In>
+struct ConverterBase<Out, In, true, false> {
+  DALI_HOST_DEV
+  static constexpr Out Convert(In value) { return value; }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSat(In value) { return value; }
+
+  DALI_HOST_DEV
+  static constexpr Out ConvertNorm(In value) { return value * (Out(1) / (max_value<In>())); }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSatNorm(In value) { return value * (Out(1) / (max_value<In>())); }
+};
+
+/// Converts FP to integral type
+template <typename Out, typename In>
+struct ConverterBase<Out, In, false, true> {
+  DALI_HOST_DEV
+  static constexpr Out Convert(In value) {
+#ifdef __CUDA_ARCH__
+  return clamp<Out>(detail::cuda_round_helper(value, Out()));
+#else
+  return clamp<Out>(std::round(value));
+#endif
+  }
+
+  DALI_HOST_DEV
+  static constexpr Out ConvertSat(In value) {
+#ifdef __CUDA_ARCH__
+  return clamp<Out>(detail::cuda_round_helper(value, Out()));
+#else
+  return clamp<Out>(std::round(value));
+#endif
+  }
+
+  DALI_HOST_DEV
+  static constexpr Out ConvertNorm(In value) {
+#ifdef __CUDA_ARCH__
+    return detail::cuda_round_helper(value * max_value<Out>(), Out());
+#else
+    return std::round(value * max_value<Out>());
+#endif
+  }
+
+  DALI_HOST_DEV
+  static constexpr Out ConvertSatNorm(In value) {
+#ifdef __CUDA_ARCH__
+    return std::is_signed<Out>::value
+      ? clamp<Out>(detail::cuda_round_helper(value * max_value<Out>(), Out()))
+      : detail::cuda_round_helper(max_value<Out>() * __saturatef(value), Out());
+#else
+    return clamp<Out>(std::round(value * max_value<Out>()));
+#endif
+  }
+};
+
+/// Converts signed to signed, unsigned to unsigned or unsigned to signed
+template <typename Out, typename In,
+          bool IsOutSigned = std::is_signed<Out>::value,
+          bool IsInSigned = std::is_signed<In>::value>
+struct ConvertIntInt {
+  DALI_HOST_DEV
+  static constexpr Out Convert(In value) { return value; }
+  DALI_HOST_DEV
+  static constexpr Out ConvertNorm(In value) {
+    return Converter<Out, float>::Convert(value * (1.0f * max_value<Out>() / max_value<In>()));
+  }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSat(In value) { return clamp<Out>(value); }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSatNorm(In value) {
+    return ConvertNorm(value);
+  }
+};
+
+/// Converts signed to unsigned integer
+template <typename Out, typename In>
+struct ConvertIntInt<Out, In, false, true> {
+  DALI_HOST_DEV
+  static constexpr Out Convert(In value) { return value; }
+  DALI_HOST_DEV
+  static constexpr Out ConvertNorm(In value) {
+    return Converter<Out, float>::Convert(value * (1.0f * max_value<Out>() / max_value<In>()));
+  }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSat(In value) { return clamp<Out>(value); }
+  DALI_HOST_DEV
+  static constexpr Out ConvertSatNorm(In value) {
+#ifdef __CUDA_ARCH__
+    return detail::cuda_round_helper(
+      __saturatef(value * (1.0f / max_value<In>())) * max_value<Out>());
+#else
+    return value < 0 ? 0 : ConvertNorm(value);
+#endif
+  }
+};
+
+/// Converts between integral types
+template <typename Out, typename In>
+struct ConverterBase<Out, In, false, false> : ConvertIntInt<Out, In> {
+  static_assert(std::is_arithmetic<Out>::value && std::is_arithmetic<In>::value,
+    "Default ConverterBase can only be used with arithmetic types. For custom types, "
+    "specialize or overload dali::Convert");
+};
+
+/// Pass-through conversion
+template <typename T>
+struct Converter<T, T> {
+  static DALI_HOST_DEV
+  constexpr T Convert(T value) { return value; }
+
+  static DALI_HOST_DEV
+  constexpr T ConvertSat(T value) { return value; }
+
+    static DALI_HOST_DEV
+  constexpr T ConvertNorm(T value) { return value; }
+
+  static DALI_HOST_DEV
+  constexpr T ConvertSatNorm(T value) { return value; }
+};
+
+template <typename raw_out, typename raw_in>
+using converter_t = Converter<
+  std::remove_cv_t<raw_out>,
+  std::remove_cv_t<std::remove_reference_t<raw_in>>>;;
+
+}  // namespace detail
+
+/// @brief Converts value to a specified `Out` type, rounding if necessary
+///
+/// Usage:
+/// ```
+///   Convert<uint8_t>(100.2f);   // == 100
+///   Convert<uint8_t>(100.7f);   // == 101
+///   Convert<uint8_t>(-5);       // usage discouraged, typically 250
+///   Convert<uint8_t>(-5.0f);    // undefined
+///   Convert<uint8_t>(100.0.0f); // undefined
+/// ```
+template <typename Out, typename In>
+DALI_HOST_DEV constexpr Out Convert(In value) {
+  return detail::converter_t<Out, In>::Convert(value);
+}
+
+/// @brief Converts value from `In` to `Out` keeping whole (positive) dynamic range
+///
+///   * When converting from signed to unsigned types, negative values produce undefined result
+///   * When converting from floating point to integral types, the value is multiplied by
+///     `max_value<Out>()`. Results are undefined for values where `value * max_value<Out>()`
+///     cannot be represented by `Out`.
+///   * When converting from integral type to floating point, the input value is normalized by
+///     multiplying by reciprocal of `Out` type's maximum positive value.
+///
+/// Usage:
+/// ```
+///   ConvertNorm<uint8_t>(0.5f);               // == 127
+///   ConvertNorm<uint8_t>(0.502);              // == 128
+///   ConvertNorm<int8_t>(-1.0f);               // -127
+///   ConvertNorm<int8_t, int16_t>(256 * 1/3);  // == 0
+///   ConvertNorm<int8_t, int16_t>(256 * 2/3);  // == 1
+///   ConvertNorm<int8_t, int16_t>(0x7fff);     // == 255
+///   ConvertNorm<float, uint8_t>(255);         // == 1.0f
+///   ConvertNorm<uint8_t, int8_t>(-1);         // undefined
+///   ConvertNorm<uint8_t>(1000.0f);            // undefined
+/// ```
+template <typename Out, typename In>
+DALI_HOST_DEV constexpr Out ConvertNorm(In value) {
+  return detail::converter_t<Out, In>::ConvertNorm(value);
+}
+
+/// @brief Converts value to a specified `Out` type, rounding and clamping if necessary
+///
+/// Usage:
+/// ```
+///   ConvertSat<uint8_t>(-1);          // == 0
+///   ConvertSat<uint8_t>(1000);        // == 255
+///   ConvertSat<int8_t>(-1000.0f);     // == -128
+///   ConvertSat<unsigned>(-1000.0f);   // == 0
+/// ```
+template <typename Out, typename In>
+DALI_HOST_DEV constexpr Out ConvertSat(In value) {
+  return detail::converter_t<Out, In>::ConvertSat(value);
+}
+
+/// @brief Converts value from `In` to `Out` keeping whole (positive) dynamic range and clamping
+///
+///   * When converting from signed to unsigned types, negative values produce 0
+///   * When converting from floating point to integral types, the value is multiplied
+///     by `max_value<Out>()` and then clamped to range representable in `Out`.
+///   * When converting from integral type to floating point, the input value is divided
+///     by `Out(max_value<In>())`
+///
+/// Usage:
+/// ```
+///   ConvertSatNorm<uint8_t>(0.5f);              // == 127
+///   ConvertSatNorm<uint8_t>(0.502);             // == 128
+///   ConvertSatNorm<int8_t>(-1.0f);              // == -127
+///   ConvertSatNorm<int8_t, int16_t>(256 * 1/3); // == 0
+///   ConvertSatNorm<int8_t, int16_t>(256 * 2/3); // == 1
+///   ConvertSatNorm<int8_t, int16_t>(0x7fff);    // == 255
+///   ConvertSatNorm<float, uint8_t>(255);        // == 1.0f
+///   ConvertSatNorm<uint8_t, int8_t>(-1);        // == 0
+/// ```
+template <typename Out, typename In>
+DALI_HOST_DEV constexpr Out ConvertSatNorm(In value) {
+  return detail::converter_t<Out, In>::ConvertSatNorm(value);
 }
 
 }  // namespace dali
