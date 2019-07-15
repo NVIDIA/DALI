84a5b5cf99394b6f1f54d61da4925a1abdf74225
Rafal <Banas.Rafal97@gmail.com>
2019-07-15 10:56:10 +0200

    Download opencv.
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- qa/download_pip_packages.sh
+++ qa/download_pip_packages.sh
@@ -3,7 +3,7 @@
 CUDA_VERSION=$(cat /usr/local/cuda/version.txt | sed 's/.*Version \([0-9]\+\)\.\([0-9]\+\).*/\1\2/')
 CUDA_VERSION=${CUDA_VERSION:-90}
 
-pip_packages="nose matplotlib jupyter numpy torch tensorflow-gpu mxnet-cu${CUDA_VERSION} torchvision pillow"
+pip_packages="nose matplotlib jupyter numpy torch tensorflow-gpu mxnet-cu${CUDA_VERSION} torchvision pillow opencv-python"
 echo ${CUDA_VERSION}
 mkdir /pip-packages
 to_download=$(/opt/dali/qa/setup_packages.py -a -u ${pip_packages} --cuda ${CUDA_VERSION})
