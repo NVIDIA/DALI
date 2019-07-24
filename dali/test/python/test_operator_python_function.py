fec4f6a6c18ab1dfc23955b9523f4212b65dbd84
Rafał Banaś <32324320+banasraf@users.noreply.github.com>
2019-07-24 16:50:57 +0200

    Fix python operator with side effects. (#1105)
    Signed-off-by: Rafal Banas.Rafal97@gmail.com
    
    Why we need this PR?
    It fixes a race condition in the python operator with side effects.
    What happened in this PR?
    Explain solution of the problem, new feature added.
    A global mutex was added in python operator implementation to protect the python function
    execution.
    What was changed, added, removed?
    A simple change in python_function.cc
    Was this PR tested? How?
    A test in test_operator_python_function.py was added.
    JIRA TASK: [DALI-837]

--- dali/test/python/test_operator_python_function.py
+++ dali/test/python/test_operator_python_function.py
@@ -354,3 +354,38 @@ def test_sink():
     for file in created_files:
         os.remove(file)
     os.rmdir(SINK_PATH)
+
+
+counter = 0
+def func_with_side_effects(images):
+    global counter
+    counter = counter + 1
+
+    print('Call ' + str(counter))
+
+    return numpy.full_like(images, counter)
+
+def test_func_with_side_effects():
+    pipe_one = PythonOperatorPipeline(
+        BATCH_SIZE, NUM_WORKERS, DEVICE_ID, SEED, images_dir, func_with_side_effects)
+    pipe_two = PythonOperatorPipeline(
+        BATCH_SIZE, NUM_WORKERS, DEVICE_ID, SEED, images_dir, func_with_side_effects)
+
+    pipe_one.build()
+    pipe_two.build()
+
+    global counter
+
+    for it in range(ITERS):
+        counter = 0
+        out_one, = pipe_one.run()
+        out_two, = pipe_two.run()
+
+        print('Iter ' + str(it) + ' Len one ' + str(len(out_one)) + ' len two ' + str(len(out_two)))
+        assert counter == len(out_one) + len(out_two)
+        elems_one = [out_one.at(s)[0][0][0] for s in range(BATCH_SIZE)]
+        elems_one.sort()
+        assert elems_one == [i for i in range(1, BATCH_SIZE + 1)]
+        elems_two = [out_two.at(s)[0][0][0] for s in range(BATCH_SIZE)]
+        elems_two.sort()
+        assert elems_two == [i for i in range(BATCH_SIZE + 1, 2 * BATCH_SIZE + 1)]
