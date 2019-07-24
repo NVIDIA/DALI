ec1fa4388ad75ff5c4ed833fb66055c6fafe04f9
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-07-24 17:10:23 +0200

    Add virtual env and conda test case for DALI TF plugin
    - adds the ability to add a list of prolog-epilog functions run
      for each test variant before required pip packages are installed
    - adds prolog/epilog that activates/deactivates conda and virtual env
      inside DALI TensorFlow plugin test
    - it assumes /virtualenv_${PYTHON_VERSION}/bin/activate and
      conda_py${PYTHON_VERSION}_env (without a dot are present in the system)
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_tensorflow_plugin/test.sh
+++ qa/TL0_tensorflow_plugin/test.sh
@@ -3,6 +3,11 @@
 pip_packages="nose tensorflow-gpu"
 target_dir=./dali/test/python
 
+# populate epilog and prolog with variants to enable/disable conda and virtual env
+# every test will be executed for bellow configs
+prolog=(: enable_conda enable_virtualenv)
+epilog=(: disable_conda disable_virtualenv)
+
 test_body() {
     # Manually removing the supported plugin so that it fails
     lib_dir=$(python -c 'import nvidia.dali.sysconfig as sc; print(sc.get_lib_dir())')
