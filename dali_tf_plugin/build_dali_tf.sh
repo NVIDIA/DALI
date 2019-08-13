cd5584becfc06479c89cfad9c60e0b68070f28f2
Joaquin Anton <janton@nvidia.com>
2019-08-13 16:24:40 +0200

    use CXX to compile tf plugin
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali_tf_plugin/build_dali_tf.sh
+++ dali_tf_plugin/build_dali_tf.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+COMPILER=${CXX:-g++}
 LIB_NAME=${1:-"libdali_tf_current.so"}
 SRCS="daliop.cc"
 INCL_DIRS="-I/usr/local/cuda/include/"
-
-echo $LIB_NAME
-exit 0
 
 DALI_CFLAGS=( $(python ./dali_compile_flags.py --cflags) )
 DALI_LFLAGS=( $(python ./dali_compile_flags.py --lflags) )
@@ -13,4 +11,5 @@ DALI_LFLAGS=( $(python ./dali_compile_flags.py --lflags) )
 TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
 TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
-g++ -std=c++11 -O2 -shared -fPIC ${SRCS} -o ${LIB_NAME} ${INCL_DIRS} ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} ${DALI_CFLAGS[@]} ${DALI_LFLAGS[@]}
+
+$COMPILER -std=c++11 -O2 -shared -fPIC ${SRCS} -o ${LIB_NAME} ${INCL_DIRS} ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} ${DALI_CFLAGS[@]} ${DALI_LFLAGS[@]}
