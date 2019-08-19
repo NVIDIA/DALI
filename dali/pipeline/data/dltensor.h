ba3fca62f7bb5c6a5e2a0cc82b258eee126b7e27
Rafal <Banas.Rafal97@gmail.com>
2019-08-19 15:52:04 +0200

    Fix lint
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- dali/pipeline/data/dltensor.h
+++ dali/pipeline/data/dltensor.h
@@ -17,6 +17,7 @@
 
 #include <memory>
 #include <vector>
+#include <utility>
 #include "third_party/dlpack/include/dlpack/dlpack.h"
 #include "dali/pipeline/data/tensor.h"
 
--- dali/pipeline/data/dltensor_test.cc
+++ dali/pipeline/data/dltensor_test.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include <gtest/gtest.h>
+#include <utility>
 #include "dali/pipeline/data/dltensor.h"
 
 namespace dali {
