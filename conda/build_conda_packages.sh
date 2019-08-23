8fe9a66494afc3d0dfe5bff7a19cfec607b1c2d7
Joaquin Anton <janton@nvidia.com>
2019-08-23 10:35:31 +0200

    No need to depend on conda's cudatoolkit, since we are using /usr/local/cuda
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- conda/build_conda_packages.sh
+++ conda/build_conda_packages.sh
@@ -7,20 +7,10 @@ if [ -z $PYVER ]; then
     echo "PYVER is not set"
     exit 1
 fi
-CONDA_BUILD_OPTIONS="--python=${PYVER}"
 
-CUDA_VERSION=$(cat /usr/local/cuda/version.txt | sed 's/.*Version \([0-9]\+\)\.\([0-9]\+\).*/\1/')
+nvidia-smi
 
-if [ "$CUDA_VERSION" = "9" ]; then
-    echo "CUDA 9 build"
-elif [ "$CUDA_VERSION" = "10" ]; then
-    echo "CUDA 10 build"
-else
-    echo "CUDA ${CUDA_VERSION} not supported (only CUDA 9 and 10 are supported)"
-    exit 1
-fi
-
-CONDA_BUILD_OPTIONS="--python=${PYVER} --exclusive-config-file config/conda_build_config_cuda_${CUDA_VERSION}.yaml"
+CONDA_BUILD_OPTIONS="--python=${PYVER} --exclusive-config-file config/conda_build_config.yaml"
 
 CONDA_PREFIX=${CONDA_PREFIX:-/root/miniconda3}
 
