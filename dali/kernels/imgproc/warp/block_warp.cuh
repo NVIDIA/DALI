bc8d2d8969d4ab365b703f2dfed7f061422d35f2
Michał Zientkiewicz <michalz@nvidia.com>
2019-08-29 15:36:28 +0200

    CPU warp, with tests. (#1159)
    Main functionality
    * CPU warp, with tests.
    
    Minor fixes:
    * Disambiguate naming: ndim -> spatial_ndim
    * Make doxygen look at CUDA files.
    
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/kernels/imgproc/warp/block_warp.cuh
+++ dali/kernels/imgproc/warp/block_warp.cuh
@@ -19,28 +19,12 @@
 #include "dali/kernels/imgproc/sampler.h"
 #include "dali/kernels/imgproc/warp/warp_setup.cuh"
 #include "dali/kernels/imgproc/warp/mapping_traits.h"
+#include "dali/kernels/imgproc/warp/map_coords.h"
 
 namespace dali {
 namespace kernels {
 namespace warp {
 
-template <typename Mapping, std::size_t dim>
-DALI_HOST_DEV
-enable_if_t<is_fp_mapping<Mapping>::value, vec<dim>> map_coords(const Mapping &m, ivec<dim> pos) {
-  // When given floating point coordinates, samplers expect pixel centers to be offset by half.
-  // Since ultimately our destination coordinates are integer, we need to move to pixel-center
-  // frame before applying the mapping and passing the output to a sampler.
-  return m(pos + 0.5f);
-}
-
-template <typename Mapping, std::size_t dim>
-DALI_HOST_DEV
-enable_if_t<!is_fp_mapping<Mapping>::value, ivec<dim>> map_coords(const Mapping &m, ivec<dim> pos) {
-  // When given integer point coordinates, samplers simply uses them as 0-based indices.
-  // If the mapping producces integral coordinates, there's no point in going from indices to
-  // pixel centers and then back to integral coordinates - hence, 0.5 is not added.
-  return m(pos);
-}
 
 template <DALIInterpType interp_type, typename Mapping,
           int ndim, typename OutputType, typename InputType,
