855c07660f9c1b5d2c9b66def1017810c74908d5
Krzysztof Lecki <klecki@nvidia.com>
2019-08-29 14:06:37 +0200

    Add types to DALIDataType that were missing (#1213)
    All common arithmetic types should be deemed worthy to use.
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/types.h
+++ dali/pipeline/data/types.h
@@ -93,31 +93,35 @@ inline Copier GetCopier() {
  * the pipeline can output.
  */
 enum DALIDataType {
-  DALI_NO_TYPE = -1,
-  DALI_UINT8 = 0,
-  DALI_INT16 = 1,
-  DALI_INT32 = 2,
-  DALI_INT64 = 3,
-  DALI_FLOAT16 = 4,
-  DALI_FLOAT = 5,
-  DALI_FLOAT64 = 6,
-  DALI_BOOL = 7,
-  DALI_STRING = 8,
-  DALI_BOOL_VEC = 9,
-  DALI_INT_VEC = 10,
-  DALI_STRING_VEC = 11,
-  DALI_FLOAT_VEC = 12,
+  DALI_NO_TYPE         = -1,
+  DALI_UINT8           =  0,
+  DALI_UINT16          =  1,
+  DALI_UINT32          =  2,
+  DALI_UINT64          =  3,
+  DALI_INT8            =  4,
+  DALI_INT16           =  5,
+  DALI_INT32           =  6,
+  DALI_INT64           =  7,
+  DALI_FLOAT16         =  8,
+  DALI_FLOAT           =  9,
+  DALI_FLOAT64         = 10,
+  DALI_BOOL            = 11,
+  DALI_STRING          = 12,
+  DALI_BOOL_VEC        = 13,
+  DALI_INT_VEC         = 14,
+  DALI_STRING_VEC      = 15,
+  DALI_FLOAT_VEC       = 16,
 #ifdef DALI_BUILD_PROTO3
-  DALI_TF_FEATURE = 13,
-  DALI_TF_FEATURE_VEC = 14,
-  DALI_TF_FEATURE_DICT = 15,
+  DALI_TF_FEATURE      = 17,
+  DALI_TF_FEATURE_VEC  = 18,
+  DALI_TF_FEATURE_DICT = 19,
 #endif  // DALI_BUILD_PROTO3
-  DALI_IMAGE_TYPE = 16,
-  DALI_DATA_TYPE = 17,
-  DALI_INTERP_TYPE = 18,
-  DALI_TENSOR_LAYOUT = 19,
-  DALI_PYTHON_OBJECT = 20,
-  DALI_DATATYPE_END = 1000
+  DALI_IMAGE_TYPE      = 20,
+  DALI_DATA_TYPE       = 21,
+  DALI_INTERP_TYPE     = 22,
+  DALI_TENSOR_LAYOUT   = 23,
+  DALI_PYTHON_OBJECT   = 24,
+  DALI_DATATYPE_END    = 1000
 };
 
 template <DALIDataType id>
@@ -311,27 +315,35 @@ DLL_PUBLIC inline bool IsValidType(const TypeInfo &type) {
 // the type as a string. This does not work for non-fundamental types,
 // as we do not have any mechanism for calling the constructor of the
 // type when the buffer allocates the memory.
-#define DALI_REGISTER_TYPE(Type, dtype)                             \
+#define DALI_REGISTER_TYPE_WITH_NAME(Type, TypeString, dtype)       \
   template <> DLL_PUBLIC string TypeTable::GetTypeName<Type>()      \
-    DALI_TYPENAME_REGISTERER(Type);                                 \
+    DALI_TYPENAME_REGISTERER(TypeString);                           \
   template <> DLL_PUBLIC DALIDataType TypeTable::GetTypeID<Type>()  \
     DALI_TYPEID_REGISTERER(Type, dtype);                            \
   DALI_STATIC_TYPE_MAPPING(Type, dtype);
 
+#define DALI_REGISTER_TYPE(Type, dtype) \
+  DALI_REGISTER_TYPE_WITH_NAME(Type, #Type, dtype)
+
 // Instantiate some basic types
-DALI_REGISTER_TYPE(NoType, DALI_NO_TYPE);
-DALI_REGISTER_TYPE(uint8, DALI_UINT8);
-DALI_REGISTER_TYPE(int16, DALI_INT16);
-DALI_REGISTER_TYPE(int32, DALI_INT32);
-DALI_REGISTER_TYPE(int64, DALI_INT64);
-DALI_REGISTER_TYPE(float16, DALI_FLOAT16);
-DALI_REGISTER_TYPE(float, DALI_FLOAT);
-DALI_REGISTER_TYPE(double, DALI_FLOAT64);
-DALI_REGISTER_TYPE(bool, DALI_BOOL);
-DALI_REGISTER_TYPE(string, DALI_STRING);
-DALI_REGISTER_TYPE(DALIImageType, DALI_IMAGE_TYPE);
-DALI_REGISTER_TYPE(DALIDataType, DALI_DATA_TYPE);
-DALI_REGISTER_TYPE(DALIInterpType, DALI_INTERP_TYPE);
+DALI_REGISTER_TYPE_WITH_NAME(NoType,   "NoType", DALI_NO_TYPE);
+DALI_REGISTER_TYPE_WITH_NAME(uint8_t,  "uint8",  DALI_UINT8);
+DALI_REGISTER_TYPE_WITH_NAME(uint16_t, "uint16", DALI_UINT16);
+DALI_REGISTER_TYPE_WITH_NAME(uint32_t, "uint32", DALI_UINT32);
+DALI_REGISTER_TYPE_WITH_NAME(uint64_t, "uint64", DALI_UINT64);
+DALI_REGISTER_TYPE_WITH_NAME(int8_t,   "int8",   DALI_INT8);
+DALI_REGISTER_TYPE_WITH_NAME(int16_t,  "int16",  DALI_INT16);
+DALI_REGISTER_TYPE_WITH_NAME(int32_t,  "int32",  DALI_INT32);
+DALI_REGISTER_TYPE_WITH_NAME(int64_t,  "int64",  DALI_INT64);
+
+DALI_REGISTER_TYPE(float16,          DALI_FLOAT16);
+DALI_REGISTER_TYPE(float,            DALI_FLOAT);
+DALI_REGISTER_TYPE(double,           DALI_FLOAT64);
+DALI_REGISTER_TYPE(bool,             DALI_BOOL);
+DALI_REGISTER_TYPE(string,           DALI_STRING);
+DALI_REGISTER_TYPE(DALIImageType,    DALI_IMAGE_TYPE);
+DALI_REGISTER_TYPE(DALIDataType,     DALI_DATA_TYPE);
+DALI_REGISTER_TYPE(DALIInterpType,   DALI_INTERP_TYPE);
 DALI_REGISTER_TYPE(DALITensorLayout, DALI_TENSOR_LAYOUT);
 
 #ifdef DALI_BUILD_PROTO3
