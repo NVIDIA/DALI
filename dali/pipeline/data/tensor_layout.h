64c82cf031dd734e69737f3769a29230c9f730dc
Michal Zientkiewicz <michalz@nvidia.com>
2019-09-05 14:23:51 +0200

    Add TensorLayout and accompanying functions for image and video layout analysis.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/pipeline/data/tensor_layout.h
+++ dali/pipeline/data/tensor_layout.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,23 +15,41 @@
 #ifndef DALI_PIPELINE_DATA_TENSOR_LAYOUT_H_
 #define DALI_PIPELINE_DATA_TENSOR_LAYOUT_H_
 
+#include <memory>
 #include <string>
 #include <cstring>
+#include "dali/core/common.h"
 #include "dali/core/small_vector.h"
 
 namespace dali {
 
 struct TensorLayout {
-  enum {
-    Dim_Sample = 'N',
-    Dim_Depth = 'D',
-    Dim_Height = 'H',
-    Dim_Width = 'W',
-    Dim_Channel = 'C',
-    Dim_Frame = 'F',
-    Dim_Frequency = 'Q',
-    Dim_Time = 'T',
-  };
+  TensorLayout() = default;
+
+  TensorLayout(const char *str) {  // NOLINT
+    data_.resize(std::strlen(str) + 1);
+    for (size_t i = 0; i < data_.size(); i++)
+      data_[i] = str[i];
+  }
+
+  TensorLayout(const char *str, size_t n) {  // NOLINT
+    data_.resize(n + 1);
+    for (size_t i = 0; i < n; i++)
+      data_[i] = str[i];
+    data_[n] = 0;
+  }
+
+  template <size_t N>
+  TensorLayout(const char (&s)[N]) {  // NOLINT
+    size_t n = N && s[N-1] == '\0' ? N - 1 : N;
+    data_.resize(n + 1);
+    for (size_t i = 0; i < n; i++)
+      data_[i] = s[i];
+    data_[n] = 0;
+  }
+
+  TensorLayout(const std::string &s) : TensorLayout(s.data(), s.length()) {  // NOLINT
+  }
 
   char &operator[](int d) {
     return data_[d];
@@ -47,6 +65,18 @@ struct TensorLayout {
     return data_.data();
   }
   std::string str() const { return c_str(); }
+
+  int find(char dim_name) const noexcept {
+    for (int i = 0; i < ndim(); i++) {
+      if (data_[i] == dim_name)
+        return i;
+    }
+    return -1;
+  }
+
+  bool contains(char dim_name) const noexcept {
+    return find(dim_name) >= 0;
+  }
 
   int compare(const TensorLayout &tl) const noexcept {
     return std::strcmp(c_str(), tl.c_str());
@@ -73,6 +103,7 @@ struct TensorLayout {
 
   size_t size() const noexcept { return data_.size() - 1; }
   int ndim() const noexcept { return size(); }
+  bool empty() const noexcept { return size() == 0; }
 
   auto begin() { return data_.begin(); }
   auto end() { return data_.end() - 1; }
@@ -86,6 +117,82 @@ struct TensorLayout {
 
   /// @brief Stores the dimension descriptions as a null-terminated string
   SmallVector<char, static_capacity> data_ = { '\0' };
+};
+
+#define DEFINE_TENSOR_LAYOUT_COMPARISON(op)\
+bool operator op (const TensorLayout &tl, const std::string &s) {\
+  return std::strcmp(tl.c_str(), s.c_str()) op 0;\
+}\
+bool operator op (const TensorLayout &tl, const char *s) {\
+  return std::strcmp(tl.c_str(), s) op 0;\
+}\
+bool operator op (const std::string &s, const TensorLayout &tl) {\
+  return std::strcmp(s.c_str(), tl.c_str()) op 0;\
+}\
+bool operator op (const char *s, const TensorLayout &tl) {\
+  return std::strcmp(s, tl.c_str()) op 0;\
+}
+
+DEFINE_TENSOR_LAYOUT_COMPARISON(<)  // NOLINT
+DEFINE_TENSOR_LAYOUT_COMPARISON(>)  // NOLINT
+DEFINE_TENSOR_LAYOUT_COMPARISON(<=)
+DEFINE_TENSOR_LAYOUT_COMPARISON(>=)
+DEFINE_TENSOR_LAYOUT_COMPARISON(==)
+DEFINE_TENSOR_LAYOUT_COMPARISON(!=)
+
+struct LayoutInfo {
+  static int HasSampleDim(const TensorLayout &tl) {
+    // sample dim may only appear at outermost level
+    return !tl.empty() && tl[0] == 'N';
+  }
+  static int DimIndex(const TensorLayout &tl, char dim_symbol) {
+    return tl.find(dim_symbol);
+  }
+};
+
+struct ImageLayoutInfo : LayoutInfo {
+  static int NumSpatialDims(const TensorLayout &tl) {
+    int s = 0;
+    for (int i = 0; i < tl.ndim(); i++) {
+      switch (tl[i]) {
+      case 'D':
+      case 'H':
+      case 'W':
+        s++;
+        break;
+      default:
+        break;
+      }
+    }
+    return s;
+  }
+  static int ChannelDimIndex(const TensorLayout &tl) {
+    return DimIndex(tl, 'C');
+  }
+  static int HasChannel(const TensorLayout &tl) {
+    return ChannelDimIndex(tl) >= 0;
+  }
+  static int Is2D(const TensorLayout &tl) {
+    return NumSpatialDims(tl) == 2;
+  }
+  static int Is3D(const TensorLayout &tl) {
+    return NumSpatialDims(tl) == 3;
+  }
+  static bool IsChannelFirst(const TensorLayout &tl) {
+    return tl[0] == 'C' || (tl[0] == 'N' && tl[1] == 'C');
+  }
+  static bool IsChannelLast(const TensorLayout &tl) {
+    return !tl.empty() && tl[tl.ndim()-1] == 'C';
+  }
+};
+
+struct VideoLayoutInfo : ImageLayoutInfo {
+  static int FrameDim(const TensorLayout &tl) {
+    return DimIndex(tl, 'F');
+  }
+  static bool IsSequence(const TensorLayout &tl) {
+    return tl.contains('F');
+  }
 };
 
 }  // namespace dali
--- dali/pipeline/data/tensor_layout_test.cc
+++ dali/pipeline/data/tensor_layout_test.cc
@@ -17,8 +17,135 @@
 
 namespace dali {
 
+TEST(TensorLayout, Construction) {
+  TensorLayout empty;
+  EXPECT_EQ(empty.ndim(), 0);
+
+  TensorLayout from_literal = "NHWC";
+  EXPECT_EQ(from_literal.ndim(), 4);
+  EXPECT_EQ(from_literal.str(), "NHWC");
+  EXPECT_STREQ(from_literal.c_str(), "NHWC");
+
+  const char *cstr = "CHW";
+  TensorLayout from_cstr = cstr;
+  EXPECT_EQ(from_cstr.ndim(), 3);
+  EXPECT_EQ(from_cstr.str(), "CHW");
+  EXPECT_STREQ(from_cstr.c_str(), "CHW");
+
+  TensorLayout from_str = std::string("asdfg");
+  EXPECT_EQ(from_str.ndim(), 5);
+  EXPECT_EQ(from_str.str(), "asdfg");
+  EXPECT_STREQ(from_str.c_str(), "asdfg");
+}
+
+TEST(TensorLayout, Equality) {
+  EXPECT_TRUE(TensorLayout("NHWC") == TensorLayout("NHWC"));
+  EXPECT_TRUE(TensorLayout("HWC") != TensorLayout("CHW"));
+  EXPECT_TRUE(TensorLayout("asdf") == "asdf");                // NOLINT
+  EXPECT_TRUE(TensorLayout("asdf") != std::string("asdfg"));
+  EXPECT_TRUE(std::string("dsaf") == TensorLayout("dsaf"));
+  EXPECT_TRUE("fadd" != TensorLayout("fads"));                // NOLINT
+
+  EXPECT_FALSE(TensorLayout("NHWC") != TensorLayout("NHWC"));
+  EXPECT_FALSE(TensorLayout("HWC") == TensorLayout("CHW"));
+  EXPECT_FALSE(TensorLayout("asdf") != "asdf");               // NOLINT
+  EXPECT_FALSE(TensorLayout("asdf") == std::string("asdfg"));
+  EXPECT_FALSE(std::string("dsaf") != TensorLayout("dsaf"));
+  EXPECT_FALSE("fadd" == TensorLayout("fads"));               // NOLINT
+}
+
+
+void TestTLCompare(const std::string &s1, const std::string &s2) {
+  static const char err_msg[] =
+    "Tensor layout comparison should yield same result as string comparison";
+
+  EXPECT_EQ((TensorLayout(s1) < TensorLayout(s2)), (s1 < s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) < s2), (s1 < s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) < s2.c_str()), (s1 < s2)) << err_msg;
+  EXPECT_EQ((s1 < TensorLayout(s2)), (s1 < s2)) << err_msg;
+  EXPECT_EQ((s1.c_str() < TensorLayout(s2)), (s1 < s2)) << err_msg;
+
+  EXPECT_EQ((TensorLayout(s1) > TensorLayout(s2)), (s1 > s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) > s2), (s1 > s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) > s2.c_str()), (s1 > s2)) << err_msg;
+  EXPECT_EQ((s1 > TensorLayout(s2)), (s1 > s2)) << err_msg;
+  EXPECT_EQ((s1.c_str() > TensorLayout(s2)), (s1 > s2)) << err_msg;
+
+  EXPECT_EQ((TensorLayout(s1) <= TensorLayout(s2)), (s1 <= s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) <= s2), (s1 <= s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) <= s2.c_str()), (s1 <= s2)) << err_msg;
+  EXPECT_EQ((s1 <= TensorLayout(s2)), (s1 <= s2)) << err_msg;
+  EXPECT_EQ((s1.c_str() <= TensorLayout(s2)), (s1 <= s2)) << err_msg;
+
+  EXPECT_EQ((TensorLayout(s1) >= TensorLayout(s2)), (s1 >= s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) >= s2), (s1 >= s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) >= s2.c_str()), (s1 >= s2)) << err_msg;
+  EXPECT_EQ((s1 >= TensorLayout(s2)), (s1 >= s2)) << err_msg;
+  EXPECT_EQ((s1.c_str() >= TensorLayout(s2)), (s1 >= s2)) << err_msg;
+
+  EXPECT_EQ((TensorLayout(s1) == TensorLayout(s2)), (s1 == s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) == s2), (s1 == s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) == s2.c_str()), (s1 == s2)) << err_msg;
+  EXPECT_EQ((s1 == TensorLayout(s2)), (s1 == s2)) << err_msg;
+  EXPECT_EQ((s1.c_str() == TensorLayout(s2)), (s1 == s2)) << err_msg;
+
+  EXPECT_EQ((TensorLayout(s1) != TensorLayout(s2)), (s1 != s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) != s2), (s1 != s2)) << err_msg;
+  EXPECT_EQ((TensorLayout(s1) != s2.c_str()), (s1 != s2)) << err_msg;
+  EXPECT_EQ((s1 != TensorLayout(s2)), (s1 != s2)) << err_msg;
+  EXPECT_EQ((s1.c_str() != TensorLayout(s2)), (s1 != s2)) << err_msg;
+}
+
 TEST(TensorLayout, Comparison) {
-  std::cerr << sizeof(SmallVector<char, 16>) << std::endl;
+  TestTLCompare("str1", "str2");
+  TestTLCompare("ASDF", "ADSF");
+  TestTLCompare("111", "1111");
+  TestTLCompare("4321", "432");
+  TestTLCompare("bang", "bang");
+}
+
+TEST(TensorLayout, ImageLayout) {
+  TensorLayout nhwc = "NHWC";
+  TensorLayout chw = "CHW";
+  TensorLayout nchw = "NCHW";
+  TensorLayout ncdhw = "NCDHW";
+  TensorLayout ndhwc = "NDHWC";
+  TensorLayout layouts[] = { nhwc, chw, nchw, ncdhw, ndhwc };
+
+  EXPECT_FALSE(ImageLayoutInfo::IsChannelFirst(nhwc));
+  EXPECT_FALSE(ImageLayoutInfo::IsChannelFirst(ndhwc));
+  EXPECT_TRUE(ImageLayoutInfo::IsChannelFirst(nchw));
+  EXPECT_TRUE(ImageLayoutInfo::IsChannelFirst(ncdhw));
+  EXPECT_TRUE(ImageLayoutInfo::IsChannelFirst(chw));
+
+  EXPECT_TRUE(ImageLayoutInfo::IsChannelLast(nhwc));
+  EXPECT_TRUE(ImageLayoutInfo::IsChannelLast(ndhwc));
+  EXPECT_FALSE(ImageLayoutInfo::IsChannelLast(nchw));
+  EXPECT_FALSE(ImageLayoutInfo::IsChannelLast(ncdhw));
+  EXPECT_FALSE(ImageLayoutInfo::IsChannelLast(chw));
+
+  EXPECT_TRUE(LayoutInfo::HasSampleDim(nhwc));
+  EXPECT_FALSE(LayoutInfo::HasSampleDim(chw));
+  EXPECT_TRUE(LayoutInfo::HasSampleDim(nchw));
+  EXPECT_TRUE(LayoutInfo::HasSampleDim(ndhwc));
+  EXPECT_TRUE(LayoutInfo::HasSampleDim(ncdhw));
+
+  EXPECT_EQ(ImageLayoutInfo::NumSpatialDims(nhwc), 2);
+  EXPECT_EQ(ImageLayoutInfo::NumSpatialDims(chw), 2);
+  EXPECT_EQ(ImageLayoutInfo::NumSpatialDims(nchw), 2);
+  EXPECT_EQ(ImageLayoutInfo::NumSpatialDims(ndhwc), 3);
+  EXPECT_EQ(ImageLayoutInfo::NumSpatialDims(ncdhw), 3);
+
+  for (const TensorLayout &tl : layouts) {
+    EXPECT_EQ(ImageLayoutInfo::Is2D(tl), ImageLayoutInfo::NumSpatialDims(tl) == 2);
+    EXPECT_EQ(ImageLayoutInfo::Is3D(tl), ImageLayoutInfo::NumSpatialDims(tl) == 3);
+  }
+}
+
+TEST(TensorLayout, VideoLayout) {
+  EXPECT_TRUE(VideoLayoutInfo::IsSequence("NFCHW"));
+  EXPECT_EQ(VideoLayoutInfo::FrameDim("NFCHW"), 1);
+  EXPECT_FALSE(VideoLayoutInfo::IsSequence("NDCHW"));
 }
 
 }  // namespace dali
