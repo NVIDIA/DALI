68faf5065f2d5bc8175c4ede1c2f2e8180f9d2b1
Rafal <Banas.Rafal97@gmail.com>
2019-09-17 12:02:32 +0200

    Install cupy for the tests
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- qa/TL0_python_self_test_frameworks/test.sh
+++ qa/TL0_python_self_test_frameworks/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages="nose numpy torch mxnet-cu##CUDA_VERSION##"
+pip_packages="nose numpy torch mxnet-cu##CUDA_VERSION## cupy"
 target_dir=./dali/test/python
 
 test_body() {
