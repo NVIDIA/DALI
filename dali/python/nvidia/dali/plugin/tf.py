9feacd48aa1de66abbd7946b3e6d9975adda8607
Albert Wolant <awolant@nvidia.com>
2019-09-17 17:37:45 +0200

    Working random set
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/python/nvidia/dali/plugin/tf.py
+++ dali/python/nvidia/dali/plugin/tf.py
@@ -25,6 +25,10 @@ from tensorflow.python.framework import tensor_shape
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.data.util import structure
 from tensorflow.python.util.tf_export import tf_export
+from tensorflow.python.framework import dtypes
+
+from tensorflow.python.data.util import random_seed
+from tensorflow.python.framework import tensor_spec
 
 _tf_plugins = glob.glob(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libdali_tf*.so'))
 _dali_tf_module = None
@@ -119,128 +123,92 @@ def DALIRawIterator():
     return _dali_tf
 
 
-def _PrepareDALIArgsHelper(pipeline = None, serialized_pipeline = None, sparse = [],
-                     shapes = [], dtypes = [], batch_size = -1, prefetch_queue_depth = 2, **kwargs):
-  """
-  Internal helper that converts user input arguments into TensorFlow operator arguments.
-  """
+# ================================================================================================================================
 
-  if type(prefetch_queue_depth) is dict:
-      exec_separated = True
-      cpu_prefetch_queue_depth = prefetch_queue_depth["cpu_size"]
-      gpu_prefetch_queue_depth = prefetch_queue_depth["gpu_size"]
-  elif type(prefetch_queue_depth) is int:
-      exec_separated = False
-      cpu_prefetch_queue_depth = -1 # dummy: wont' be used
-      gpu_prefetch_queue_depth = prefetch_queue_depth
+# def _PrepareDALIArgsHelper(shapes = [], **kwargs):
+#   prepared_args = {'shapes':shapes}
+#   prepared_args.update(**kwargs)
+#   return prepared_args
 
-  if serialized_pipeline is None:
-    serialized_pipeline = pipeline.serialize()
 
-  # if batch_size is not provided we need to extract if from the shape arg
-  if (not isinstance(shapes, Iterable) or len(shapes) == 0) and batch_size == -1:
-    raise Exception('shapes and batch_size arguments cannot be empty, '
-                    'please provide at leas one shape argument element with the BATCH size or set batch_size')
+# class DALIDatasetV2(dataset_ops.DatasetSource):
+#     def __init__(self, shapes=None, **kwargs):
+#         """Creates a `DALIDataset`.
+#         Args:
+#         pipeline: A`nvidia.dali.Pipeline` defining the augmentation to be performed
+#         batch_size: `int` defining the number of samples in a batch
+#         shapes: A `List` of `tf.TensorShape` with the expected output shapes
+#         dtypes: A `List` of `tf.DType` with the expected output types
+#         devices: A `List` with the indexes of the devices to use
+#         prefetch_queue_depth: `int` with the amount of prefetched batches
+#         num_threads: `int` with the number of reader threads in the pipeline per GPU
+#         """
+#         oldargs = locals().copy()
+#         oldargs.pop('self', None)
+#         oldargs.pop('kwargs', None)
+#         oldargs.pop('__class__', None)
+#         self._dali_args = _PrepareDALIArgsHelper(**oldargs)
+#         self._dali_args.update(**kwargs)
 
-  if len(sparse) > 0 and sparse[0] and batch_size == -1:
-    if isinstance(shapes[0], Iterable) and len(shapes[0]) == 1:
-      shapes[0] = (shapes[0][0], 1)
-    else:
-      shapes[0] = (shapes[0], 1)
+#         # self._shapes = self._dali_args['shapes']
 
-  # shapes and dtypes need to take into account that sparse tensor will produce 3 output tensors
-  new_dtypes = []
-  new_shapes = []
-  for i in range(len(dtypes)):
-    if i < len(sparse) and sparse[i]:
-      # indices type of sparse tensor is tf.int64
-      new_dtypes.append(tf.int64)
-      new_dtypes.append(dtypes[i])
-      # dense shape type of sparse tensor is tf.int64
-      new_dtypes.append(tf.int64)
-      if len(shapes) > i and len(shapes[i]) > 0:
-        new_shapes.append((shapes[i][0], 1))
-        new_shapes.append((shapes[i][0]))
-      else:
-        new_shapes.append(())
-        new_shapes.append(())
-      new_shapes.append(())
-    else:
-      new_dtypes.append(dtypes[i])
-      if len(shapes) > i:
-        new_shapes.append(shapes[i])
-  prepared_args = {'serialized_pipeline':serialized_pipeline , 'shapes':new_shapes, 'sparse':sparse, 'dtypes':dtypes, 'batch_size':batch_size,
-                   'exec_separated':exec_separated, 'gpu_prefetch_queue_depth':gpu_prefetch_queue_depth, 'cpu_prefetch_queue_depth':cpu_prefetch_queue_depth}
-  prepared_args.update(**kwargs)
-  return prepared_args
+#         # types = (dtypes.int32)
+#         # shapes = (self._shapes[0])
+#         # output_classes = (ops.Tensor, ops.Tensor)
+#         # self._structure = structure.convert_legacy_structure(types, shapes, output_classes)
+
+#         # super(DALIDatasetV2, self).__init__()
+#         # Change to this when TF updates
+#         variant_tensor = self._as_variant_tensor
+#         super(DALIDatasetV2, self).__init__(variant_tensor)
+
+#     @property
+#     def _element_structure(self):
+#       return structure.TensorStructure(dtypes.int64, [])
+
+#     def _as_variant_tensor(self):
+#         dali_args = self._dali_args
+#         return _dali_tf_module.dali_dataset(**dali_args)
+
+# class DALIDatasetV1(dataset_ops.DatasetV1Adapter):
+#   """Creates a `DALIDataset`.
+#   Args:
+#   pipeline: A`nvidia.dali.Pipeline` defining the augmentation to be performed
+#   batch_size: `int` defining the number of samples in a batch
+#   shapes: A `List` of `tf.TensorShape` with the expected output shapes
+#   dtypes: A `List` of `tf.DType` with the expected output types
+#   devices: A `List` with the indexes of the devices to use
+#   prefetch_queue_depth: `int` with the amount of prefetched batches
+#   num_threads: `int` with the number of reader threads in the pipeline per GPU
+#   """
+
+#   @functools.wraps(DALIDatasetV2.__init__)
+#   def __init__(self, **kwargs):
+#     wrapped = DALIDatasetV2(**kwargs)
+#     super(DALIDatasetV1, self).__init__(wrapped)
+
+# DALIDataset = DALIDatasetV1
 
 
 class DALIDatasetV2(dataset_ops.DatasetSource):
-    def __init__(self,
-                  pipeline=None,
-                  batch_size=1,
-                  sparse=[],
-                  shapes=None,
-                  dtypes=None,
-                  prefetch_queue_depth=2,
-                  num_threads=1,
-                  **kwargs):
-        """Creates a `DALIDataset`.
-        Args:
-        pipeline: A`nvidia.dali.Pipeline` defining the augmentation to be performed
-        batch_size: `int` defining the number of samples in a batch
-        shapes: A `List` of `tf.TensorShape` with the expected output shapes
-        dtypes: A `List` of `tf.DType` with the expected output types
-        devices: A `List` with the indexes of the devices to use
-        prefetch_queue_depth: `int` with the amount of prefetched batches
-        num_threads: `int` with the number of reader threads in the pipeline per GPU
-        """
-        oldargs = locals().copy()
-        oldargs.pop('self', None)
-        oldargs.pop('kwargs', None)
-        oldargs.pop('__class__', None)
-        self._dali_args = _PrepareDALIArgsHelper(**oldargs)
-        self._dali_args.update(**kwargs)
+  def __init__(self, seed=None):
+    self._seed, self._seed2 = random_seed.get_seed(seed)
+    variant_tensor = _dali_tf_module.dali_dataset(
+      seed=self._seed, seed2=self._seed2)
+    super(DALIDatasetV2, self).__init__(variant_tensor)
 
-        self._dtypes = self._dali_args['dtypes']
-        self._shapes = self._dali_args['shapes']
-
-        types = (self._dtypes[0], self._dtypes[1])
-        shapes = (self._shapes[0], self._shapes[1])
-        output_classes = (ops.Tensor, ops.Tensor)
-        self._structure = structure.convert_legacy_structure(types, shapes, output_classes)
-
-        # super(DALIDatasetV2, self).__init__()
-        # Change to this when TF updates
-        variant_tensor = self._as_variant_tensor
-        super(DALIDatasetV2, self).__init__(variant_tensor)
-
-    @property
-    def _element_structure(self):
-        return self._structure
-
-    def _as_variant_tensor(self):
-        dali_args = self._dali_args
-        return _dali_tf_module.dali_dataset(**dali_args)
+  @property
+  def _element_structure(self):
+    return structure.TensorStructure(dtypes.int64, [])
 
 class DALIDatasetV1(dataset_ops.DatasetV1Adapter):
-  """Creates a `DALIDataset`.
-  Args:
-  pipeline: A`nvidia.dali.Pipeline` defining the augmentation to be performed
-  batch_size: `int` defining the number of samples in a batch
-  shapes: A `List` of `tf.TensorShape` with the expected output shapes
-  dtypes: A `List` of `tf.DType` with the expected output types
-  devices: A `List` with the indexes of the devices to use
-  prefetch_queue_depth: `int` with the amount of prefetched batches
-  num_threads: `int` with the number of reader threads in the pipeline per GPU
-  """
-
   @functools.wraps(DALIDatasetV2.__init__)
   def __init__(self, **kwargs):
     wrapped = DALIDatasetV2(**kwargs)
     super(DALIDatasetV1, self).__init__(wrapped)
 
 DALIDataset = DALIDatasetV1
+
 
 
 DALIIterator.__doc__ = DALIIteratorWrapper.__doc__
--- dali_tf_plugin/dali_dataset_op.cc
+++ dali_tf_plugin/dali_dataset_op.cc
@@ -30,6 +30,7 @@
 #include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/framework/tensor_shape.h"
 #include "tensorflow/core/framework/types.h"
+#include "tensorflow/core/framework/common_shape_fns.h"
 
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/op_kernel.h"
@@ -61,87 +62,37 @@ namespace {
 
 class DALIDatasetOp : public DatasetOpKernel {
  public:
-  using DatasetOpKernel::DatasetOpKernel;
-
-  explicit DALIDatasetOp(OpKernelConstruction* context) : DatasetOpKernel(context) {
-    // bool exec_separated;
-    // int cpu_prefetch_queue_depth;
-    // std::string serialized_pipeline;
-
-    // OP_REQUIRES_OK(context, context->GetAttr("serialized_pipeline",
-    //                                          &serialized_pipeline));
-    // OP_REQUIRES_OK(context, context->GetAttr("shapes", &shapes_));
-    // OP_REQUIRES_OK(context, context->GetAttr("batch_size", &batch_size_));
-    // OP_REQUIRES_OK(context, context->GetAttr("dtypes", &dtypes_));
-    // OP_REQUIRES_OK(context, context->GetAttr("sparse", &sparse_));
-    // OP_REQUIRES_OK(context, context->GetAttr("exec_separated", &exec_separated));
-    // // In exec_separated==false case, gpu_prefetch_queue_depth is the global prefetch_queue_depth_
-    // OP_REQUIRES_OK(context, context->GetAttr("gpu_prefetch_queue_depth", &prefetch_queue_depth_));
-    // OP_REQUIRES_OK(context, context->GetAttr("num_threads", &num_threads_));
-    // OP_REQUIRES_OK(context, context->GetAttr("cpu_prefetch_queue_depth",
-    //                                          &cpu_prefetch_queue_depth));
-
-    // std::vector<int> devices;
-    // OP_REQUIRES_OK(context, context->GetAttr("devices", &devices));
-
-    // pipe_handles_.resize(devices.size());
-
-
-    // for (size_t i = 0; i < devices.size(); i++) {
-    //   daliCreatePipeline(&pipe_handles_[i],
-    //               serialized_pipeline.c_str(),
-    //               serialized_pipeline.length(),
-    //               batch_size_,
-    //               num_threads_,
-    //               devices[i],
-    //               exec_separated,
-    //               prefetch_queue_depth_,
-    //               cpu_prefetch_queue_depth,
-    //               prefetch_queue_depth_);
-
-    // }
-
-    // for (size_t i = 0; i < devices.size(); i++) {
-    //   for (auto& handle : pipe_handles_) {
-    //     if (!exec_separated) {
-    //       daliPrefetchUniform(&handle, prefetch_queue_depth_);
-    //     } else {
-    //       daliPrefetchSeparate(&handle,
-    //                             cpu_prefetch_queue_depth,
-    //                             prefetch_queue_depth_);
-    //     }
-    //   }
-    // }
-  }
-
-  // ~DALIDatasetOp() {
-  //   for (auto& handle : pipe_handles_) {
-  //     daliDeletePipeline(&handle);
-  //   }
-  // }
-
-  // void MakeDataset(OpKernelContext* context, DatasetBase** output) override {
-  //   *output = new Dataset(context, pipe_handles_, shapes_, dtypes_, sparse_);
-  // }
+  explicit DALIDatasetOp(OpKernelConstruction* context) : DatasetOpKernel(context) { }
 
   void MakeDataset(OpKernelContext* context, DatasetBase** output) override {
-    *output = new Dataset(context);
+    int64 seed;
+    OP_REQUIRES_OK(context, ParseScalarArgument<int64>(context, "seed", &seed));
+    int64 seed2;
+    OP_REQUIRES_OK(context, ParseScalarArgument<int64>(context, "seed2", &seed2));
+
+    if (seed == 0 && seed2 == 0) {
+      seed = random::New64();
+      seed2 = random::New64();
+    }
+
+    *output = new Dataset(context, seed, seed2);
   }
 
    private:
     class Dataset : public DatasetBase {
       public:
-        explicit Dataset(OpKernelContext *context) : DatasetBase(DatasetContext(context)) {}
+        explicit Dataset(OpKernelContext *context, int64 seed, int64 seed2) 
+          : DatasetBase(DatasetContext(context)), seed_(seed), seed2_(seed2) {}
 
         std::unique_ptr<IteratorBase> MakeIteratorInternal(
           const string &prefix) const override {
-            return std::unique_ptr<IteratorBase>(new Iterator(
-              {this, strings::StrCat(prefix, "::DALI")}
-            ));
+            return absl::make_unique<Iterator>(
+              Iterator::Params{this, strings::StrCat(prefix, "::DALI")}
+            );
         }
 
         const DataTypeVector &output_dtypes() const override {
-          static auto *const dtypes = new DataTypeVector({DT_INT32});
+          static DataTypeVector* dtypes = new DataTypeVector({DT_INT64});
           return *dtypes;
         }
 
@@ -151,284 +102,90 @@ class DALIDatasetOp : public DatasetOpKernel {
           return *shapes;
         }
 
-        string DebugString() const override { return "DALI::DatasetOp"; }
+        string DebugString() const override { 
+          return strings::StrCat("DALI::DatasetOp(", seed_, ", ", seed2_, ")::Dataset"); }
 
+        int64 Cardinality() const override { return kInfiniteCardinality; }
+\
       protected:
         Status AsGraphDefInternal(
           SerializationContext *context,
           DatasetGraphDefBuilder *b,
           Node **output) const override {
+
+          Node *seed = nullptr;
+          Node *seed2 = nullptr;
+          TF_RETURN_IF_ERROR(b->AddScalar(seed_, &seed));
+          TF_RETURN_IF_ERROR(b->AddScalar(seed2_, &seed2));
+          TF_RETURN_IF_ERROR(b->AddDataset(this, {seed, seed2}, output));
+
           return Status::OK();
         }
 
       private:
+        const int64 seed_;
+        const int64 seed2_;
+
         class Iterator : public DatasetIterator<Dataset> {
           public:
             explicit Iterator(const Params &params)
-              : DatasetIterator<Dataset>(params) {}
+              : DatasetIterator<Dataset>(params),
+                parent_generator_(dataset()->seed_, dataset()->seed2_),
+                generator_(&parent_generator_) {}
 
             Status GetNextInternal(
               IteratorContext *context,
               std::vector<Tensor> *out_tensors,
               bool *end_of_sequence) override {
-                
+                tensorflow::mutex_lock l(mu_);
+                out_tensors->emplace_back(context->allocator({}), DT_INT64, TensorShape({}));
+                out_tensors->back().scalar<int64>()() = 999;
+                *end_of_sequence = false;
+
                 return Status::OK();
               }
+
+          private:
+            random::SingleSampleAdapter<random::PhiloxRandom>::ResultType Random()
+              EXCLUSIVE_LOCKS_REQUIRED(mu_) {
+              
+              num_random_samples_++;
+              auto out = generator_();
+              return out;
+            }
+
+            tensorflow::mutex mu_;
+            random::PhiloxRandom parent_generator_ GUARDED_BY(mu_);
+            random::SingleSampleAdapter<random::PhiloxRandom> generator_ GUARDED_BY(mu_);
+            int64 num_random_samples_ GUARDED_BY(mu_) = 0;
         };
     };
-
-
-//  private:
-//   class Dataset : public DatasetBase {
-//    public:
-//     explicit Dataset(OpKernelContext* ctx,
-//                      const std::vector<daliPipelineHandle>& pipeline_handles,
-//                      const std::vector<TensorShape>& shapes,
-//                      const DataTypeVector& types,
-//                      const std::vector<bool>& sparse)
-//           : DatasetBase(DatasetContext(ctx)),
-//             pipeline_handles_(pipeline_handles),
-//             shapes_{[shapes]() {
-//               std::vector<PartialTensorShape> partial_shapes;
-//               for (size_t i = 0; i < shapes.size(); i++) {
-//                 std::vector<int64> dims(shapes[i].dims(), -1);
-//                 PartialTensorShape s;
-//                 PartialTensorShape::MakePartialShape(dims.data(), dims.size(),
-//                                                      &s);
-//                 partial_shapes.push_back(s);
-//               }
-//               return partial_shapes;
-//             }()},
-//             dtypes_{types},
-//             sparse_{sparse} 
-//             {
-//             }
-
-//     std::unique_ptr<IteratorBase> MakeIteratorInternal(
-//       const string& prefix) const override {
-//       return std::unique_ptr<IteratorBase>(
-//           new Iterator({this, strings::StrCat(prefix, "::DALI")}));
-//     }
-
-//     const DataTypeVector& output_dtypes() const override {
-//       return dtypes_;
-//     }
-
-//     const std::vector<PartialTensorShape>& output_shapes() const override {
-//       return shapes_;
-//     }
-
-//     string DebugString() const override {
-//       return "DALIDatasetOp::Dataset";
-//     }
-
-//    protected:
-//     Status AsGraphDefInternal(SerializationContext* ctx,
-//                               DatasetGraphDefBuilder* b,
-//                               Node** output) const override {
-//       return Status::OK();
-//     }
-
-
-//    private:
-//     const std::vector<daliPipelineHandle>& pipeline_handles_;
-//     const std::vector<PartialTensorShape> shapes_;
-//     const DataTypeVector& dtypes_;
-//     const std::vector<bool>& sparse_;
-
-//     // shareded dataset "tensorflow/core/kernels/data/shard_dataset_op.cc"
-//     class Iterator : public DatasetIterator<Dataset> {
-//      public:
-//       explicit Iterator(const Params& params)
-//           : DatasetIterator<Dataset>(params),
-//             current_idx_{0},
-//             num_pipelines_{dataset()->pipeline_handles_.size()} {}
-
-//       Status GetNextInternal(IteratorContext* ctx,
-//                             std::vector<Tensor>* out_tensors,
-//                             bool* end_of_sequence) override {
-//         // Main logic here: copy pipeline output to `out_tensors`
-//         std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   1 \n";
-//         mutex_lock l(mu_);
-//         auto current_handle = dataset()->pipeline_handles_[current_idx_];
-
-//         TF_DALI_CALL(daliShareOutput(&current_handle));
-
-//         std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   2 \n";
-
-
-//         const auto sparse = dataset()->sparse_;
-//         unsigned additional_sparse_tensors = std::accumulate(sparse.begin(), sparse.end(), 0) * 2;
-//         unsigned dali_num_out = daliGetNumOutput(&current_handle);
-
-//         out_tensors->reserve(dali_num_out + additional_sparse_tensors);
-
-//         std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   3 \n";
-
-//         for (unsigned i = 0, j = 0; i < dali_num_out; ++i, ++j) {
-//           bool should_be_sparse_tensor = i < sparse.size() && sparse[i];
-//           unsigned elms = 0;
-//           unsigned dims = 0;
-//           std::vector<int64> max_dims;
-//           if (!should_be_sparse_tensor) {
-//             // dense output
-//             TensorShape data_output_shape;
-//             AllocatorAttributes attrs;
-//             attrs.set_on_host(false);
-//             attrs.set_gpu_compatible(true);
-//             TF_DALI_CALL(data_output_shape = DaliToShape(AutoCPtr<int64_t>(
-//                                                          daliShapeAt(&current_handle, i))));
-
-//             out_tensors->emplace_back(ctx->allocator(attrs),
-//                        dataset()->dtypes_[i], data_output_shape);
-//           } else {
-//             // sparse output
-//           }
-
-//           auto& out = out_tensors->back();
-//           void* dst = nullptr;
-
-//           switch (dataset()->dtypes_[j]) {
-//             case DT_HALF:
-//                   dst = reinterpret_cast<void*>(out.flat<uint16_t>().data());
-//               break;
-//             case DT_FLOAT:
-//                   dst = reinterpret_cast<void*>(out.flat<float>().data());
-//               break;
-//             case DT_UINT8:
-//                   dst = reinterpret_cast<void*>(out.flat<uint8_t>().data());
-//               break;
-//             case DT_INT16:
-//                   dst = reinterpret_cast<void*>(out.flat<int16_t>().data());
-//               break;
-//             case DT_INT32:
-//                   dst = reinterpret_cast<void*>(out.flat<int32_t>().data());
-//               break;
-//             case DT_INT64:
-//                   dst = reinterpret_cast<void*>(out.flat<int64>().data());
-//               break;
-//             default:
-//                 std::string error = "Unsupported type: " + DataTypeString(dataset()->dtypes_[i]) +
-//                                     "for tensor " + std::to_string(i);
-//                 // propagate error
-//             break;
-//           }
-
-
-//           if (!should_be_sparse_tensor) {
-//             // dense output
-//             // TODO(spanev) change device type to dynamic one
-//             const device_type_t device_type = device_type_t::GPU;
-//             // cudaStream_t stream = ctx->eigen_device<Eigen::GpuDevice>().stream();
-//             cudaStream_t stream = 0;
-//             TF_DALI_CALL(daliCopyTensorNTo(&current_handle, dst, i, device_type, stream));
-//           } else {
-//             // sparse output
-//           }
-//         }
-
-//         current_idx_ = (current_idx_ + 1) % num_pipelines_;
-//         return Status::OK();
-//       }
-
-
-//      protected:
-//       Status SaveInternal(IteratorStateWriter* writer) override {
-//         mutex_lock l(mu_);
-//         TF_RETURN_IF_ERROR(
-//             writer->WriteScalar(full_name("current_idx"), current_idx_));
-
-//         return Status::OK();
-//       }
-
-//       Status RestoreInternal(IteratorContext* ctx,
-//                              IteratorStateReader* reader) override {
-//         int64 current_index;
-//         TF_RETURN_IF_ERROR(
-//             reader->ReadScalar(full_name("current_index"), &current_index));
-//         current_idx_ = static_cast<unsigned int>(current_index);
-
-//         return Status::OK();
-//       }
-
-//       mutex mu_;
-//       unsigned int current_idx_;
-//       const size_t num_pipelines_;
-//     };  // class Iterator
-//   };  // class Dataset
-
-//  protected:
-//   std::vector<daliPipelineHandle> pipe_handles_;
-//   // Do we need to keep the serialized pipeline?
-//   // std::string serialized_pipeline_;
-//   int batch_size_;
-//   std::vector<TensorShape> shapes_;
-//   DataTypeVector dtypes_;
-//   std::vector<bool> sparse_;
-//   int prefetch_queue_depth_;
-//   int num_threads_;
-
-
-
-
-
 };
 
 
 // Regestrations
-REGISTER_OP("DALIDataset")
-  // TODO(spanev): Replace to string array in order to be able to have different pipeline per GPU
-  .Attr("serialized_pipeline: string")
-  .Attr("shapes: list(shape) >= 1")
-  .Attr("num_threads: int = -1")
-  .Attr("device_id: int = -1")
-  .Attr("exec_separated: bool = false")
-  .Attr("dtypes: list({half, float, uint8, int16, int32, int64}) >= 1")
-  .Attr("devices: list(int) >= 1")
-  .Attr("gpu_prefetch_queue_depth: int = 2")
-  .Attr("cpu_prefetch_queue_depth: int = 2")
-  .Attr("sparse: list(bool) = []")
-  .Attr("batch_size: int = -1")
-  .Output("handle: variant")
-  // To prevent replacing DALI op with constant tensor during TF constant folding process
-  .SetIsStateful()
-  .SetShapeFn([](shape_inference::InferenceContext* c) {
-    std::vector<PartialTensorShape> shapes;
-    TF_RETURN_IF_ERROR(c->GetAttr("shapes", &shapes));
-    for (unsigned i = 0; i < shapes.size(); ++i) {
-      if (shapes[i].dims() > 0) {
-        shape_inference::ShapeHandle passed_shape;
-        TF_RETURN_IF_ERROR(
-            c->MakeShapeFromPartialTensorShape(shapes[i], &passed_shape));
-        TF_RETURN_IF_ERROR(
-            c->WithRank(passed_shape, shapes[i].dims(), &passed_shape));
-        c->set_output(i, passed_shape);
-      }
-    }
-    return Status::OK();
-  })
-  .Doc(R"doc(
-DALI Dataset plugin
-Creates a DALI dataset compatible with tf.data.Dataset from a serialized DALI pipeline
-(given in `serialized_pipeline` parameter).
-`shapes` must match the shape of the corresponding DALI Pipeline output tensor shape.
-`dtypes` must match the type of the corresponding DALI Pipeline output tensors type.
-)doc");
+REGISTER_KERNEL_BUILDER(
+  Name("DALIDataset").Device(tensorflow::DEVICE_CPU),
+  DALIDatasetOp);
 
-REGISTER_KERNEL_BUILDER(Name("DALIDataset").Device(tensorflow::DEVICE_CPU),
-                        DALIDatasetOp);
-REGISTER_KERNEL_BUILDER(Name("DALIDatasetOp")
-                            .Device(DEVICE_GPU)
-                            .HostMemory("buffer_size")
-                            .HostMemory("input_dataset")
-                            .HostMemory("handle"),
-                            // .Priority(1),
-DALIDatasetOp);
+REGISTER_OP("DALIDataset")
+    .Input("seed: int64")
+    .Input("seed2: int64")
+    .Output("handle: variant")
+    // .Attr("output_types: list(type) >= 1")
+    // .Attr("output_shapes: list(shape) >= 1")
+    .SetIsStateful()  // TODO(b/123753214): Source dataset ops must be marked
+                      // stateful to inhibit constant folding.
+    .SetShapeFn([](shape_inference::InferenceContext* c) {
+      shape_inference::ShapeHandle unused;
+      // buffer_size, seed, and seed2 should be scalars.
+      TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 0, &unused));
+      TF_RETURN_IF_ERROR(c->WithRank(c->input(1), 0, &unused));
+      return shape_inference::ScalarShape(c);
+    });
 
 }  // namespace
-
 }  // namespace data
-
 }  // namespace tensorflow
 
 // #endif  // TF_MAJOR_VERSION == 1 && TF_MINOR_VERSION >= 12
