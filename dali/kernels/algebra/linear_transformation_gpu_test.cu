2a798230fa8cfe40090f4b7250c5b42eac0ce2b1
Michał Szołucha <mszolucha@nvidia.com>
2019-09-20 19:26:36 +0200

    roi
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/kernels/algebra/linear_transformation_gpu_test.cu
+++ dali/kernels/algebra/linear_transformation_gpu_test.cu
@@ -22,6 +22,7 @@
 #include "dali/kernels/common/copy.h"
 #include "dali/kernels/test/tensor_test_utils.h"
 #include "dali/kernels/test/kernel_test_utils.h"
+#include "dali/kernels/imgproc/color_manipulation/color_manipulation_test_utils.h"
 
 namespace dali {
 namespace kernels {
@@ -84,6 +85,7 @@ class LinearTransformationGpuTest : public ::testing::Test {
   std::vector<TensorShape<kNDims>> out_shapes_ = {{4, 3, kNChannelsOut}};
   mat<kNChannelsOut, kNChannelsIn, float> mat_{{{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}}};
   std::vector<mat<kNChannelsOut, kNChannelsIn, float>> vmat_ = {mat_};
+  std::vector<Roi<2>> rois_ = {{{1, 1}, {2, 2}}};
 
 
   void calc_output() {
@@ -91,7 +93,7 @@ class LinearTransformationGpuTest : public ::testing::Test {
       for (size_t j = 0; j < kNChannelsOut; j++) {
         float res = 0;
         for (size_t k = 0; k < kNChannelsIn; k++) {
-          res += input_host_[i + k] * mat_.at(j, k);
+          res += static_cast<float>(input_host_[i + k]) * mat_.at(j, k);
         }
         ref_output_.push_back(custom_round<Out>(res));
       }
@@ -108,7 +110,7 @@ class LinearTransformationGpuTest : public ::testing::Test {
   }
 };
 
-using TestTypes = std::tuple<uint8_t, int32_t>;
+using TestTypes = std::tuple<uint8_t>;
 /* Cause the line below takes RIDICULOUSLY long time to compile */
 // using TestTypes = std::tuple<uint8_t, int8_t, uint16_t, int16_t, int32_t, float>;
 
@@ -146,8 +148,6 @@ TYPED_TEST(LinearTransformationGpuTest, run_test) {
   TheKernel<TypeParam> kernel;
   KernelContext c;
   InListGPU<typename TypeParam::In, kNDims> in(this->input_device_, this->in_shapes_);
-  OutListGPU<typename TypeParam::Out, kNDims> out(this->output_,
-                                                  TensorListShape<kNDims>(this->out_shapes_));
 
   auto reqs = kernel.Setup(c, in, this->vmat_);
 
@@ -155,13 +155,50 @@ TYPED_TEST(LinearTransformationGpuTest, run_test) {
   sa.Reserve(reqs.scratch_sizes);
   auto scratchpad = sa.GetScratchpad();
   c.scratchpad = &scratchpad;
+
+  OutListGPU<typename TypeParam::Out, kNDims> out(
+          this->output_, reqs.output_shapes[0].template to_static<kNDims>());
+
   kernel.Run(c, out, in, this->vmat_);
   cudaDeviceSynchronize();
 
   auto res = copy<AllocType::Host>(out[0]);
   ASSERT_EQ(static_cast<int>(this->ref_output_.size()), res.first.num_elements());
   for (size_t i = 0; i < this->ref_output_.size(); i++) {
-    EXPECT_EQ(this->ref_output_[i], res.second.get()[i]) << "Failed for index " << i;
+    EXPECT_FLOAT_EQ(this->ref_output_[i], res.second.get()[i]) << "Failed for index " << i;
+  }
+}
+
+
+TYPED_TEST(LinearTransformationGpuTest, run_test_with_roi) {
+  TheKernel<TypeParam> kernel;
+  KernelContext c;
+  InListGPU<typename TypeParam::In, kNDims> in(this->input_device_, this->in_shapes_);
+
+  auto reqs = kernel.Setup(c, in, this->vmat_, this->rois_);
+
+  ScratchpadAllocator sa;
+  sa.Reserve(reqs.scratch_sizes);
+  auto scratchpad = sa.GetScratchpad();
+  c.scratchpad = &scratchpad;
+
+  OutListGPU<typename TypeParam::Out, kNDims> out(
+          this->output_, reqs.output_shapes[0].template to_static<kNDims>());
+
+  kernel.Run(c, out, in, this->vmat_, this->rois_);
+  cudaDeviceSynchronize();
+
+  auto res = copy<AllocType::Host>(out[0]);
+  auto mat = color_manipulation::test::to_mat<kNChannelsOut>(this->ref_output_.data(),
+                                                             this->rois_[0],
+                                                             this->out_shapes_[0][0],
+                                                             this->out_shapes_[0][1]);
+
+  ASSERT_EQ(mat.rows * mat.cols * mat.channels(), res.first.num_elements())
+                        << "Number of elements doesn't match";
+  auto ptr = reinterpret_cast<typename TypeParam::Out *>(mat.data);
+  for (int i = 0; i < res.first.num_elements(); i++) {
+    EXPECT_FLOAT_EQ(ptr[i], res.second.get()[i]) << "Failed at idx: " << i;
   }
 }
 
@@ -187,7 +224,7 @@ TYPED_TEST(LinearTransformationGpuTest, sample_descriptors) {
   OutListGPU<Out, kNDims> out(this->output_, TensorListShape<3>(this->out_shapes_));
 
   auto res = detail::CreateSampleDescriptors
-          <Out, In, kNChannelsOut, kNChannelsIn, kNDims - 1>(out, in, this->vmat_);
+          <Out, In, kNChannelsOut, kNChannelsIn, kNDims - 1>(out, in, this->vmat_, this->rois_);
 
   EXPECT_EQ(this->input_device_, res[0].in);
   EXPECT_EQ(this->output_, res[0].out);
