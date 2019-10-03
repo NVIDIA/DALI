503d148a047d7a2a9e060ef5910936b02159e11f
Joaquin Anton <janton@nvidia.com>
2019-10-03 13:22:38 +0200

    Add complete implementation of BmpImage::PeekShapeImpl, including number of channels
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/image/bmp.cc
+++ dali/image/bmp.cc
@@ -14,8 +14,45 @@
 
 #include "dali/image/bmp.h"
 #include "dali/core/byte_io.h"
+#include "dali/core/format.h"
 
 namespace dali {
+
+namespace  {
+
+enum BmpCompressionType {
+  BMP_COMPRESSION_RGB       = 0,
+  BMP_COMPRESSION_RLE8      = 1,
+  BMP_COMPRESSION_RLE4      = 2,
+  BMP_COMPRESSION_BITFIELDS = 3
+};
+
+// similar logic to what OpenCv does to determine the output number of channels
+int number_of_channels(int bpp, int compression_type) {
+  if (compression_type == BMP_COMPRESSION_RGB) {
+    if (bpp == 1 || bpp == 4 || bpp == 8 || bpp == 24)
+      return 3;
+    else if (bpp == 32)
+      return 4;
+  } else if (compression_type == BMP_COMPRESSION_BITFIELDS) {
+    if (bpp == 16)
+      return 3;
+    else if (bpp == 32)
+      return 4;
+    else
+      return 1;
+  } else if (compression_type == BMP_COMPRESSION_RLE8) {
+    return (bpp == 4) ? 3 : 1;
+  } else if (compression_type == BMP_COMPRESSION_RLE4) {
+    return (bpp == 8) ? 3 : 1;
+  }
+
+  DALI_WARN(make_string(
+    "configuration not supported. bpp:", bpp, "compression_type:", compression_type));
+  return 0;
+}
+
+}  // namespace
 
 // https://en.wikipedia.org/wiki/BMP_file_format#DIB_header_(bitmap_information_header)
 
@@ -26,17 +63,25 @@ Image::Shape BmpImage::PeekShapeImpl(const uint8_t *bmp, size_t length) const {
   DALI_ENFORCE(bmp != nullptr);
 
   uint32_t header_size = ReadValueLE<uint32_t>(bmp + 14);
-  int64_t h = 0, w = 0;
-  // BITMAPCOREHEADER: | 32u header | 16u width | 16u height | ...
+  int64_t h = 0, w = 0, c = 0;
+  int bpp = 0, compression_type = BMP_COMPRESSION_RGB;
   if (length >= 22 && header_size == 12) {
+    // BITMAPCOREHEADER:
+    // | 32u header | 16u width | 16u height | 16u number of color planes | 16u bits per pixel
     w = ReadValueLE<uint16_t>(bmp + 18);
     h = ReadValueLE<uint16_t>(bmp + 20);
-    // BITMAPINFOHEADER and later: | 32u header | 32s width | 32s height | ...
+    bpp = ReadValueLE<uint16_t>(bmp + 24);
   } else if (length >= 26 && header_size >= 40) {
+    // BITMAPINFOHEADER and later:
+    // | 32u header | 32s width | 32s height | 16u number of color planes | 16u bits per pixel
+    // | 32u compression type
     w = ReadValueLE<int32_t>(bmp + 18);
     h = abs(ReadValueLE<int32_t>(bmp + 22));
+    bpp = ReadValueLE<uint16_t>(bmp + 28);
+    compression_type = ReadValueLE<uint32_t>(bmp + 30);
   }
-  return {h, w, 0};  // TODO(mszolucha): fill channels
+  c = number_of_channels(bpp, compression_type);
+  return {h, w, c};
 }
 
 
