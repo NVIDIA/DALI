8ffe4dbccc06aec5d8651e03778d1783e5109a1a
Albert Wolant <44801854+awolant@users.noreply.github.com>
2019-10-03 12:47:16 +0200

    Add tf dataset (#1299)
    Why we need this PR?
    It adds new feature needed to improve DALI TensorFlow support and prepare it for the upcomming TF 2.0
    What happened in this PR?
    Explain solution of the problem, new feature added.
    To support future versions of TensorFlow there is a need to use tf.data.Dataset API rather then the older options of feeding the data to the TensorFlow graph.
    This PR adds initial support for this API in DALI.
    
    What was changed, added, removed?
    Added DALIDatasetOp class and its Python wrappers. Modified how DALI TF plugin is build to include those.
    
    What is most important part that reviewers should focus on?
    How DALI API is used in the context of TensorFlow DatasetOp.
    
    Was this PR tested? How?
    Added python tests for DALIDataset.
    
    Were docs and examples updated, if necessary?
    Yes
    
    JIRA TASK: [DALI-380]
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>
    Signed-off-by: Jianjun Liu <00liujj@163.com>

--- dali_tf_plugin/build_dali_tf.sh
+++ dali_tf_plugin/build_dali_tf.sh
@@ -3,7 +3,7 @@
 COMPILER=${CXX:-g++}
 PYTHON=${PYTHON:-python}
 LIB_NAME=${1:-"libdali_tf_current.so"}
-SRCS="daliop.cc"
+SRCS="daliop.cc dali_dataset_op.cc"
 INCL_DIRS="-I/usr/local/cuda/include/"
 
 DALI_CFLAGS=( $($PYTHON ./dali_compile_flags.py --cflags) )
@@ -13,4 +13,7 @@ TF_CFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.g
 TF_LFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
 
-$COMPILER -std=c++11 -O2 -shared -fPIC ${SRCS} -o ${LIB_NAME} ${INCL_DIRS} ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} ${DALI_CFLAGS[@]} ${DALI_LFLAGS[@]}
+# Note: DNDEBUG flag is needed due to issue with TensorFlow custom ops:
+# https://github.com/tensorflow/tensorflow/issues/17316
+# Do not remove it.
+$COMPILER -std=c++11 -DNDEBUG -O2 -shared -fPIC ${SRCS} -o ${LIB_NAME} ${INCL_DIRS} ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} ${DALI_CFLAGS[@]} ${DALI_LFLAGS[@]}
