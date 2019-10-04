02052f18d8019efea3715fdb0b59df827d0ff4de
Janusz Lisiecki <jlisiecki@nvidia.com>
2019-10-04 14:46:35 +0200

    Enable CUDA 10 compatibility layer for Conda build
    - when DALI Conda build is run for CUDA 10 and the underlying
      driver happens to be < 410 enable CUDA compatibility layer
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- conda/build_conda_packages.sh
+++ conda/build_conda_packages.sh
@@ -10,6 +10,23 @@ fi
 
 nvidia-smi
 
+# If driver version is less than 410 and CUDA version is 10,
+# add /usr/local/cuda/compat to LD_LIBRARY_PATH
+CUDA_VERSION=$(cat /usr/local/cuda/version.txt | sed 's/.*Version \([0-9]\+\)\.\([0-9]\+\).*/\1\2/')
+CUDA_VERSION=${CUDA_VERSION:-90}
+
+NVIDIA_SMI_DRIVER_VERSION=$(nvidia-smi | grep -Po '(?<=Driver Version: )\d+.\d+')
+
+function version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" == "$1"; }
+function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
+function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
+
+version_ge "$CUDA_VERSION" "100" && \
+version_lt "$NVIDIA_SMI_DRIVER_VERSION" "410.0" && \
+export LD_LIBRARY_PATH="/usr/local/cuda/compat:$LD_LIBRARY_PATH"
+echo "LD_LIBRARY_PATH is $LD_LIBRARY_PATH"
+
 # Adding conda-forge channel for dependencies
 conda config --add channels conda-forge
 
