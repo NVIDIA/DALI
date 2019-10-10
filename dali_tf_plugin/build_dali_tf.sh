1cd80e87868aebfda15ab01547f8e014e2f5c721
Joaquin Anton <janton@nvidia.com>
2019-10-10 15:47:53 +0200

    use include_flags instead of cflags
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali_tf_plugin/build_dali_tf.sh
+++ dali_tf_plugin/build_dali_tf.sh
@@ -6,7 +6,7 @@ LIB_NAME=${1:-"libdali_tf_current.so"}
 SRCS="daliop.cc dali_dataset_op.cc"
 INCL_DIRS="-I/usr/local/cuda/include/"
 
-DALI_CFLAGS=( $($PYTHON ./dali_compile_flags.py --cflags) )
+DALI_CFLAGS=( $($PYTHON ./dali_compile_flags.py --include_flags) )
 DALI_LFLAGS=( $($PYTHON ./dali_compile_flags.py --lflags) )
 
 TF_CFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
