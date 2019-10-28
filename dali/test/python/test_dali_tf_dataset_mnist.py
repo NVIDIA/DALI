731db878a82cd0c2118009f3d2264b99b7394fe5
Albert Wolant <awolant@nvidia.com>
2019-10-28 16:22:49 +0100

    Add graph test
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/test/python/test_dali_tf_dataset_mnist.py
+++ dali/test/python/test_dali_tf_dataset_mnist.py
@@ -33,9 +33,18 @@ try:
 except:
     pass
 
+target = 0.8
+batch_size = 32
+dropout = 0.2
+image_size = 28
+labels_size = 10
+hidden_size = 128
+epochs = 5
+iterations = 100
+
 
 class MnistPipeline(Pipeline):
-    def __init__(self, batch_size, num_threads, path, device, device_id = 0, shard_id = 0, num_shards = 1, seed = 0):
+    def __init__(self, num_threads, path, device, device_id = 0, shard_id = 0, num_shards = 1, seed = 0):
         super(MnistPipeline, self).__init__(batch_size, num_threads, device_id, seed)
         self.device = device
         self.reader = ops.FileReader(file_root = path, random_shuffle = True)
@@ -60,42 +69,95 @@ class MnistPipeline(Pipeline):
         return (images, labels)
 
 
-def _get_mnist_dataset(batch_size, path, device = 'cpu', device_id = 0):
-    mnist_pipeline = MnistPipeline(batch_size, 4, path, device, device_id)
+def _get_mnist_dataset(path, device = 'cpu', device_id = 0):
+    mnist_pipeline = MnistPipeline(4, path, device, device_id)
     shapes = [
-        (batch_size, 28, 28), 
+        (batch_size, image_size,image_size), 
         (batch_size)]
     dtypes = [
         tf.float32,
         tf.int32]
 
-    return dali_tf.DALIDataset(
+    daliset = dali_tf.DALIDataset(
         pipeline=mnist_pipeline,
-        batch_size= batch_size,
+        batch_size=batch_size,
         shapes=shapes, 
         dtypes=dtypes,
         num_threads=4,
         device_id=device_id)
+    return daliset.with_options(dataset_options())
 
 
-def _get_train_dataset(batch_size, device = 'cpu', device_id = 0):
-    return _get_mnist_dataset(batch_size, '/data/MNIST/training', device, device_id)
-
-
-def _get_test_dataset(batch_size, device = 'cpu', device_id = 0):
+def _get_train_dataset(device = 'cpu', device_id = 0):
     return _get_mnist_dataset(
-        batch_size, 
-        '/data/MNIST/testing', 
+        '/data/MNIST/training', 
         device, 
-        device_id).take(batch_size)
+        device_id)
+
+
+def _test_graph_single_device(device = 'cpu', device_id = 0):
+    skip_for_incompatible_tf()
+
+    with tf.device('/{0}:{1}'.format(device, device_id)):
+        daliset = _get_train_dataset(device, device_id)
+
+        iterator = tf.compat.v1.data.make_initializable_iterator(daliset)
+        images, labels = iterator.get_next()
+        keep_prob = tf.placeholder(tf.float32)
+
+        images = tf.reshape(images, [batch_size, image_size*image_size])
+        labels = tf.reshape(tf.one_hot(labels, labels_size), [batch_size, labels_size])
+        W_h = tf.Variable(tf.truncated_normal([image_size*image_size, hidden_size], stddev=0.1))
+        b_h = tf.Variable(tf.constant(0.1, shape=[hidden_size]))
+
+        hidden = tf.nn.relu(tf.matmul(images, W_h) + b_h)
+
+        W = tf.Variable(tf.truncated_normal([hidden_size, labels_size], stddev=0.1))
+        b = tf.Variable(tf.constant(0.1, shape=[labels_size]))
+
+        output = tf.matmul(hidden, W) + b
+
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=output))
+
+        train_step = tf.train.AdamOptimizer().minimize(loss)
+        correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    with tf.compat.v1.Session() as sess:
+        sess.run([tf.compat.v1.global_variables_initializer(), iterator.initializer])
+        for i in range(epochs * iterations):
+            sess.run(train_step, feed_dict={keep_prob: dropout})
+
+            if i % iterations == 0:
+                train_accuracy = accuracy.eval(feed_dict={keep_prob: 1.0})
+                print("Step %d, accuracy: %g"%(i, train_accuracy))
+
+        final_accuracy = 0
+        for _ in range(iterations):
+            final_accuracy = final_accuracy + accuracy.eval(feed_dict={keep_prob: 1.0})
+        final_accuracy = final_accuracy / iterations
+
+        assert final_accuracy > target
+
+    
+def test_graph_single_gpu():
+    _test_graph_single_device('gpu', 0)
+
+
+def test_graph_single_other_gpu():
+    _test_graph_single_device('gpu', 1)
+
+
+def test_graph_single_cpu():
+    _test_graph_single_device('cpu', 0)
 
 
 def _keras_model():
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(10, activation='softmax')
+        tf.keras.layers.Flatten(input_shape=(image_size, image_size)),
+        tf.keras.layers.Dense(hidden_size, activation='relu'),
+        tf.keras.layers.Dropout(dropout),
+        tf.keras.layers.Dense(labels_size, activation='softmax')
         ])
     model.compile(
         optimizer='adam',
@@ -105,21 +167,22 @@ def _keras_model():
     return model
 
 
-def _test_keras_single_device(device = 'cpu', device_id = 0, batch_size = 32):
+def _test_keras_single_device(device = 'cpu', device_id = 0):
     skip_for_incompatible_tf()
 
     with tf.device('/{0}:{1}'.format(device, device_id)):
         model = _keras_model()
 
-        train_dataset = _get_train_dataset(batch_size, device, device_id)
+        train_dataset = _get_train_dataset(device, device_id)
 
         model.fit(
             train_dataset,
-            epochs = 5,
-            steps_per_epoch = 100)
+            epochs = epochs,
+            steps_per_epoch = iterations)
 
         assert model.evaluate(
-            _get_test_dataset(batch_size, device, device_id))[1] > 0.9
+           train_dataset,
+           steps = iterations)[1] > target
 
 
 def test_keras_single_gpu():
@@ -138,45 +201,44 @@ def clear_checkpoints():
     remove_directory('/tmp/tensorflow-checkpoints', ignore_errors = True)
 
 
-def _test_estimators_single_device(device = 'cpu', device_id = 0, batch_size = 32):
+def _test_estimators_single_device(device = 'cpu', device_id = 0):
     skip_for_incompatible_tf()
     
     with tf.device('/{0}:{1}'.format(device, device_id)):
         def train_fn():
-            return _get_train_dataset(batch_size, device, device_id).map(
+            return _get_train_dataset(device, device_id).map(
                 lambda features, labels: ({'x':features}, labels))
 
-        feature_columns = [tf.feature_column.numeric_column("x", shape=[28, 28])]
+        feature_columns = [tf.feature_column.numeric_column("x", shape=[image_size, image_size])]
 
         classifier = tf.estimator.DNNClassifier(
             feature_columns = feature_columns,
-            hidden_units = [128],
-            n_classes = 10,
-            dropout = 0.2,
-            activation_fn=tf.nn.relu,
+            hidden_units = [hidden_size],
+            n_classes = labels_size,
+            dropout = dropout,
             model_dir = '/tmp/tensorflow-checkpoints',
             optimizer='Adam')
 
-        classifier.train(input_fn = train_fn, steps = 500)
+        classifier.train(input_fn = train_fn, steps = epochs * iterations)
 
-        def test_fn():
-            return _get_test_dataset(batch_size, device, device_id).map(
-                lambda features, labels: ({'x':features}, labels))
+        assert classifier.evaluate(
+            input_fn=train_fn,
+            steps = iterations)["accuracy"] > target
 
-        assert classifier.evaluate(input_fn=test_fn)["accuracy"] > 0.8
 
 @with_setup(clear_checkpoints, clear_checkpoints)
 def test_estimators_single_gpu():
     _test_estimators_single_device('gpu', 0)
 
+
 @with_setup(clear_checkpoints, clear_checkpoints)
 def test_estimators_single_other_gpu():
     _test_estimators_single_device('gpu', 1)
 
+
 @with_setup(clear_checkpoints, clear_checkpoints)
 def test_estimators_single_cpu():
     _test_estimators_single_device('cpu', 0)
-
 
 
     
