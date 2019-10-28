5fc8a78d913f32d21d98cc67313ffeb4aeb91562
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2019-10-28 13:27:59 +0100

    Fix RN50 MXNet TL3 test (#1424)
    - patches RN50 training script by replacing deprecated
      nvJPEGDeocer with ImageDecoder
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL3_RN50_convergence/test_mxnet.sh
+++ qa/TL3_RN50_convergence/test_mxnet.sh
@@ -5,6 +5,9 @@ min_perf=10000
 
 NUM_GPUS=`nvidia-smi -L | wc -l`
 
+# Fix deprecations, we need to fix that for the older FW containers version
+sed -i "s/nvJPEGDecoder/ImageDecoder/" /opt/mxnet/example/image-classification/common/dali.py
+
 python /opt/mxnet/example/image-classification/train_imagenet_runner -b 208 -n $NUM_GPUS --seed 42 2>&1 | tee dali.log
 
 cat dali.log  | grep -o "Validation-accuracy=0\.[0-9]*" > tmp2.log
