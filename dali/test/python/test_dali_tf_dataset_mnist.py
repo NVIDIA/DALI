74146988eb390805d7fd59b9d62a8a481a0ae6a3
Albert Wolant <awolant@nvidia.com>
2019-10-30 14:08:19 +0100

    Add multi GPU graph test for 1.15
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/test/python/test_dali_tf_dataset_mnist.py
+++ dali/test/python/test_dali_tf_dataset_mnist.py
@@ -47,7 +47,7 @@ try:
 except:
     pass
 
-target = 0.9
+target = 0.85
 batch_size = 32
 dropout = 0.2
 image_size = 28
@@ -84,8 +84,8 @@ class MnistPipeline(Pipeline):
         return (images, labels)
 
 
-def _get_mnist_dataset(path, device='cpu', device_id=0):
-    mnist_pipeline = MnistPipeline(4, path, device, device_id)
+def _get_mnist_dataset(path, device='cpu', device_id=0, shard_id = 0, num_shards = 1):
+    mnist_pipeline = MnistPipeline(4, path, device, device_id, shard_id, num_shards)
     shapes = [
         (batch_size, image_size, image_size),
         (batch_size)]
@@ -103,12 +103,22 @@ def _get_mnist_dataset(path, device='cpu', device_id=0):
     return daliset.with_options(dataset_options())
 
 
-def _get_train_dataset(device='cpu', device_id=0):
+def _get_train_dataset(device='cpu', device_id=0, shard_id = 0, num_shards = 1):
     return _get_mnist_dataset(
         '/data/MNIST/training',
         device,
-        device_id)
+        device_id,
+        shard_id,
+        num_shards)
 
+def _graph_model(images, reuse, is_training):
+    with tf.variable_scope('mnist_net', reuse=reuse):
+        images = tf.contrib.layers.flatten(images)
+        images = tf.layers.dense(images, hidden_size, activation=tf.nn.relu)
+        images = tf.layers.dropout(images, rate=dropout, training=is_training)
+        images = tf.layers.dense(images, labels_size, activation=tf.nn.softmax)
+
+    return images
 
 def _test_graph_single_device(device='cpu', device_id=0):
     skip_for_incompatible_tf()
@@ -118,59 +128,49 @@ def _test_graph_single_device(device='cpu', device_id=0):
 
         iterator = tf.compat.v1.data.make_initializable_iterator(daliset)
         images, labels = iterator.get_next()
-        keep_prob = placeholder(tf.float32)
 
         images = tf.reshape(images, [batch_size, image_size*image_size])
         labels = tf.reshape(
             tf.one_hot(labels, labels_size),
             [batch_size, labels_size])
-        W_h = tf.Variable(truncated_normal(
-            [image_size*image_size, hidden_size], stddev=0.1))
-        b_h = tf.Variable(tf.constant(0.1, shape=[hidden_size]))
+        
+        logits_train = _graph_model(images, reuse=False, is_training=True)
+        logits_test = _graph_model(images, reuse=True, is_training=False)
 
-        hidden = tf.nn.relu(tf.matmul(images, W_h) + b_h)
+        loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits=logits_train, labels=labels))
+        train_step = AdamOptimizer().minimize(loss_op)
 
-        W = tf.Variable(truncated_normal(
-            [hidden_size, labels_size], stddev=0.1))
-        b = tf.Variable(tf.constant(0.1, shape=[labels_size]))
-
-        output = tf.matmul(hidden, W) + b
-
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            labels=labels, logits=output))
-
-        train_step = AdamOptimizer().minimize(loss)
-        correct_prediction = tf.equal(
-            tf.argmax(output, 1), tf.argmax(labels, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        correct_pred = tf.equal(tf.argmax(logits_test, 1), tf.argmax(labels, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     with Session() as sess:
-        sess.run(
-            [tf.compat.v1.global_variables_initializer(), iterator.initializer])
+        sess.run([tf.compat.v1.global_variables_initializer(), iterator.initializer])
         for i in range(epochs * iterations):
-            sess.run(train_step, feed_dict={keep_prob: dropout})
+            sess.run(train_step)
 
             if i % iterations == 0:
-                train_accuracy = accuracy.eval(feed_dict={keep_prob: 1.0})
+                train_accuracy = accuracy.eval()
                 print("Step %d, accuracy: %g" % (i, train_accuracy))
 
         final_accuracy = 0
         for _ in range(iterations):
             final_accuracy = final_accuracy + \
-                accuracy.eval(feed_dict={keep_prob: 1.0})
+                accuracy.eval()
         final_accuracy = final_accuracy / iterations
 
+        print('Final accuracy: ', final_accuracy)
         assert final_accuracy > target
 
-
+@with_setup(tf.reset_default_graph)
 def test_graph_single_gpu():
     _test_graph_single_device('gpu', 0)
 
-
+@with_setup(tf.reset_default_graph)
 def test_graph_single_other_gpu():
     _test_graph_single_device('gpu', 1)
 
-
+@with_setup(tf.reset_default_graph)
 def test_graph_single_cpu():
     _test_graph_single_device('cpu', 0)
 
@@ -307,6 +307,91 @@ def test_estimators_wrapping_keras_single_other_gpu():
 @with_setup(clear_checkpoints, clear_checkpoints)
 def test_estimators_wrapping_keras_single_cpu():
     _test_estimators_wrapping_keras_single_device('cpu', 0)
+
+
+# This function is copied form: https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10_multi_gpu_train.py#L102
+def _average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        # Note that each grad_and_vars looks like the following:
+        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+        grads = []
+        for g, _ in grad_and_vars:
+            # Add 0 dimension to the gradients to represent the tower.
+            expanded_g = tf.expand_dims(g, 0)
+
+            # Append on a 'tower' dimension which we will average over below.
+            grads.append(expanded_g)
+
+        # Average over the 'tower' dimension.
+        grad = tf.concat(grads, 0)
+        grad = tf.reduce_mean(grad, 0)
+
+        # Keep in mind that the Variables are redundant because they are shared
+        # across towers. So .. we will just return the first tower's pointer to
+        # the Variable.
+        v = grad_and_vars[0][1]
+        grad_and_var = (grad, v)
+        average_grads.append(grad_and_var)
+    return average_grads
+
+@with_setup(tf.reset_default_graph)
+def test_graph_multi_gpu():
+    iterator_initializers = []
+
+    with tf.device('/cpu:0'):
+        tower_grads = []
+
+        for i in range(num_available_gpus()):
+            with tf.device('/gpu:{}'.format(i)):
+                daliset = _get_train_dataset(
+                    'gpu', i, i, num_available_gpus())
+
+                iterator = tf.compat.v1.data.make_initializable_iterator(daliset)
+                iterator_initializers.append(iterator.initializer)
+                images, labels = iterator.get_next()
+
+                images = tf.reshape(images, [batch_size, image_size*image_size])
+                labels = tf.reshape(
+                    tf.one_hot(labels, labels_size),
+                    [batch_size, labels_size])
+
+                logits_train = _graph_model(images, reuse=(i != 0), is_training=True)
+                logits_test = _graph_model(images, reuse=True, is_training=False)
+
+                loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+                    logits=logits_train, labels=labels))
+                optimizer = tf.train.AdamOptimizer()
+                grads = optimizer.compute_gradients(loss_op)
+
+                if i == 0:
+                    correct_pred = tf.equal(tf.argmax(logits_test, 1), tf.argmax(labels, 1))
+                    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+                tower_grads.append(grads)
+
+        tower_grads = _average_gradients(tower_grads)
+        train_op = optimizer.apply_gradients(tower_grads)
+
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            sess.run(iterator_initializers)
+
+            for i in range(epochs * iterations):
+                sess.run(train_op)
+                if i % iterations == 0:
+                    train_accuracy = accuracy.eval()
+                    print("Step %d, accuracy: %g" % (i, train_accuracy))
+
+            final_accuracy = 0
+            for _ in range(iterations):
+                final_accuracy = final_accuracy + \
+                    accuracy.eval()
+            final_accuracy = final_accuracy / iterations
+
+            print('Final accuracy: ', final_accuracy)
+            assert final_accuracy > target
 
 
 @with_setup(clear_checkpoints, clear_checkpoints)
