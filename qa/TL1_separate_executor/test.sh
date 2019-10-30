192defa92d3e014c2930241a5560d0811a068c0e
Joaquin Anton <janton@nvidia.com>
2019-10-30 09:15:20 +0100

    Fix other calls to test_RN50_data_fw_iterators.py
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/TL1_separate_executor/test.sh
+++ qa/TL1_separate_executor/test.sh
@@ -9,11 +9,14 @@ do_once() {
 }
 
 test_body() {
-    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --separate_queue --cpu_size 2 --gpu_size 2 --fp16 --nhwc
-
-    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --separate_queue --cpu_size 5 --gpu_size 3 --fp16 --nhwc
-
-    python test_RN50_data_fw_iterators.py --gpus ${NUM_GPUS} -b 13 --workers 3 --separate_queue --cpu_size 3 --gpu_size 2 --iters 32 --epochs 2
+    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --separate_queue \
+        --cpu_size 2 --gpu_size 2 --fp16 --nhwc
+    python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --separate_queue \
+        --cpu_size 5 --gpu_size 3 --fp16 --nhwc
+    for fw in "mxnet" "pytorch" "tf"; do
+        python test_RN50_data_fw_iterators.py --framework ${fw} --gpus ${NUM_GPUS} -b 13 \
+            --workers 3 --separate_queue --cpu_size 3 --gpu_size 2 --iters 32 --epochs 2
+    done
 }
 
 pushd ../..
