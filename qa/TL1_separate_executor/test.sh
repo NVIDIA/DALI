9a1e2438f8dd17a18900f0cfa4408d4edd3a81a2
Joaquin Anton <janton@nvidia.com>
2019-10-31 08:51:57 +0100

    Add paddle to TL1_separate_executor and TL2_FW_iterators_perf
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/TL1_separate_executor/test.sh
+++ qa/TL1_separate_executor/test.sh
@@ -13,7 +13,7 @@ test_body() {
         --cpu_size 2 --gpu_size 2 --fp16 --nhwc
     python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --separate_queue \
         --cpu_size 5 --gpu_size 3 --fp16 --nhwc
-    for fw in "mxnet" "pytorch" "tf"; do
+    for fw in "mxnet" "pytorch" "tf" "paddle"; do
         python test_RN50_data_fw_iterators.py --framework ${fw} --gpus ${NUM_GPUS} -b 13 \
             --workers 3 --separate_queue --cpu_size 3 --gpu_size 2 --iters 32 --epochs 2
     done
--- qa/TL2_FW_iterators_perf/test.sh
+++ qa/TL2_FW_iterators_perf/test.sh
@@ -9,7 +9,7 @@ do_once() {
 }
 
 test_body() {
-    for fw in "mxnet" "pytorch" "tf"; do
+    for fw in "mxnet" "pytorch" "tf" "paddle"; do
         python test_RN50_data_fw_iterators.py --framework ${fw} --gpus ${NUM_GPUS} -b 13 \
             --workers 3 --prefetch 2 --epochs 3
     done
