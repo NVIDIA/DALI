008978882cd0cb49971f4649cd21f5d4bcbafd98
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2019-11-04 12:52:04 +0100

    Fix rounding down of the RN50 PyTorch example epoch length (#1428)
    - fixes rounding down of the RN50 PyTorch example epoch length
      by using math.ceil()
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docs/examples/pytorch/resnet50/main.py
+++ docs/examples/pytorch/resnet50/main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import shutil
 import time
+import math
 
 import torch
 from torch.autograd import Variable
@@ -306,7 +307,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
     for i, data in enumerate(train_loader):
         input = data[0]["data"]
         target = data[0]["label"].squeeze().cuda().long()
-        train_loader_len = int(train_loader._size / args.batch_size)
+        train_loader_len = int(math.ceil(train_loader._size / args.batch_size))
 
         adjust_learning_rate(optimizer, epoch, i, train_loader_len)
 
