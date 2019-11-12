40c382d56dd3758539b5e080be6ea13c7f7efc20
Joaquin Anton <janton@nvidia.com>
2019-11-12 16:59:33 +0100

    Fix typo
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/kernels/signal/fft/fft_cpu.h
+++ dali/kernels/signal/fft/fft_cpu.h
@@ -78,7 +78,7 @@ class DLL_PUBLIC FftImpl {
 
 /**
  * @brief Computes 1-D FFT related transformation from real data to either a complex spectrum
- *   or a transformation of the complex spectrum (power, magnitude, log power)
+ *   or a transformation of the complex spectrum (power, magnitude)
  *
  * Input data can be a 2D or 3D tensor representing an signal (e.g. [channels, time]) or
  * a sequence of frames (e.g. [channels, frames, time]). The kernel can work with other data
