747e15aad3fd26b3c6f4e7dd83d1b9198fbfb587
Yang Zhang <yangzhang@live.com>
2019-11-12 11:37:21 +0800

    Use sintel trailer instead of youtube video
    Signed-off-by: Yang Zhang <yangzhang@live.com>

--- qa/TL1_paddle_tsm/test.sh
+++ qa/TL1_paddle_tsm/test.sh
@@ -5,16 +5,14 @@ target_dir=./docs/examples/paddle/tsm/
 
 do_once() {
   apt-get update
-  apt-get install -y youtube-dl ffmpeg
+  apt-get install -y ffmpeg
 
   mkdir -p demo
-  youtube-dl --quiet --no-warnings -f mp4 -o demo/tmp.mp4 'https://www.youtube.com/watch?v=iU3ByohkPaM'
-  ffmpeg -y -i demo/tmp.mp4 -filter:v scale=-1:300 -ss 0 -t 10 -c:a copy demo/1.mp4
-  rm demo/tmp.mp4
+  ffmpeg -y -i ${DALI_EXTRA_PATH}/db/video/sintel/labelled_videos/1/sintel_trailer-720p_7.mp4 -filter:v scale=534:300 -ss 0 -t 10 -c:a copy demo/7.mp4
 }
 
 test_body() {
-  if python infer.py -k 1 -s 30 demo | grep 'carving_pumpkin'; then
+  if python infer.py -k 1 -s 15 demo | grep 'paragliding'; then
     exit 0
   fi
   exit 3
