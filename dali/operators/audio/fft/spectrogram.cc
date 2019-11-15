3959335863b2edcb4c82cc5a7d5384bfd50911b5
Joaquin Anton <janton@nvidia.com>
2019-11-15 15:00:08 +0100

    [WIP] Spectrogram CPU
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/operators/audio/fft/spectrogram.cc
@@ -0,0 +1,238 @@
+// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include "dali/operators/audio/fft/spectrogram.h"
+#include "dali/kernels/kernel_manager.h"
+#include "dali/kernels/signal/window/extract_windows_cpu.h"
+#include "dali/kernels/signal/fft/fft_cpu.h"
+#include "dali/core/static_switch.h"
+#include "dali/pipeline/data/views.h"
+
+#define SPECTROGRAM_SUPPORTED_NDIMS (1, 2)
+
+namespace dali {
+
+DALI_SCHEMA(Spectrogram)
+  .DocStr(R"code(Produces a spectrogram from a 1D signal (e.g. audio). Input data is expected
+to be single channel (1D shape `(time)`) or multi channel in planar layout (channel, time) 32 bit
+float tensor)code")
+  .NumInput(1)
+  .NumOutput(1)
+  .AddOptionalArg("nfft",
+    R"code(Size of the FFT. The number of bins created in the output is `nfft // 2 + 1`
+(positive part of the spectrum only).)code",
+    -1)
+  .AddOptionalArg("window_length",
+    R"code(Window size (in number of samples))code",
+    512)
+  .AddOptionalArg("window_step",
+    R"code(Step betweeen the STFT windows)code",
+    256)
+  .AddOptionalArg("window_type",
+    R"code(Window function type that is applied to the extracted windows)code",
+    "rectangular")
+  .AddOptionalArg("power",
+    "Exponent of the magnitude of the spectrum. Supported values are 1 for energy and 2 for power)",
+    2);
+
+template <int Dims>
+struct SpectrogramImplCpu : Spectrogram<CPUBackend>::ImplBase {
+  using OutputType = float;
+  using InputType = float;
+
+  static constexpr int InputDims = Dims;
+  using WindowKernel =
+    kernels::signal::window::ExtractWindowsCpu<InputType, InputType, InputDims>;
+
+  static constexpr int WindowsDims = Dims+1;
+  using FftKernel = kernels::signal::fft::Fft1DCpu<OutputType, InputType, WindowsDims>;
+
+  SpectrogramImplCpu(const OpSpec & spec)
+    : nfft_(spec.GetArgument<int>("nfft"))
+    , window_length_(spec.GetArgument<int>("window_length"))
+    , window_step_(spec.GetArgument<int>("window_step"))
+    , power_(spec.GetArgument<int>("power"))
+    , window_type_(spec.GetArgument<std::string>("window_type")) {}
+
+  bool SetupImpl(std::vector<OutputDesc> &output_desc, const workspace_t<CPUBackend> &ws) override;
+  void RunImpl(workspace_t<CPUBackend> &ws) override;
+
+ private:
+  int nfft_ = -1;
+  int window_length_ = -1;
+  int window_step_ = -1;
+  int power_ = -1;
+  std::string window_type_;
+
+  Tensor<CPUBackend> window_fn_;
+
+  kernels::KernelManager kmgr_window_;
+  kernels::signal::window::ExtractWindowsArgs window_args_;
+
+  std::vector<OutputDesc> window_out_desc_;
+  std::vector<Tensor<CPUBackend>> window_out_;
+
+  kernels::KernelManager kmgr_fft_;
+  kernels::signal::fft::FftArgs fft_args_;
+};
+
+namespace {
+  void FillExtractWindowsArgs(kernels::signal::window::ExtractWindowsArgs& args,
+                              int window_length, int window_step, int ndim) {
+    args.window_length = window_length;
+    args.window_step = window_step;
+    args.in_time_axis = ndim - 1;
+    args.out_frame_axis = ndim;  // one extra dimension is created by this kernel
+  }
+
+  void FillFftArgs(kernels::signal::fft::FftArgs& args,
+                   int power, int window_length, int nfft, int ndims) {
+    args.nfft = nfft;
+    DALI_ENFORCE(window_length <= nfft);
+    switch (power) {
+      case 1:
+        args.spectrum_type = kernels::signal::fft::FFT_SPECTRUM_MAGNITUDE;
+        break;
+      case 2:
+        args.spectrum_type = kernels::signal::fft::FFT_SPECTRUM_POWER;
+        break;
+      default:
+        DALI_FAIL(make_string("`power` can be only 1 (energy) or 2 (power), received ", power));
+    }
+
+    // FFT is calculated in the outermost dimension
+    args.transform_axis = ndims - 1;
+  }
+
+}  // namespace
+
+template <int Dims>
+bool SpectrogramImplCpu<Dims>::SetupImpl(std::vector<OutputDesc> &out_desc,
+                                         const workspace_t<CPUBackend> &ws) {
+  const auto &input = ws.InputRef<CPUBackend>(0);
+  auto &output = ws.OutputRef<CPUBackend>(0);
+  kernels::KernelContext ctx;
+  auto in_shape = input.shape();
+  int nsamples = input.size();
+  auto nthreads = ws.HasThreadPool() ? ws.GetThreadPool().size() : 1;
+
+  // Intermediate output buffers
+  window_out_.resize(nthreads);
+
+  // rectangular (TODO: extend and do only once)
+  window_fn_.set_type(TypeInfo::Create<float>());
+  window_fn_.Resize({window_length_});
+  for (int t = 0; t < window_length_; t++) {
+    window_fn_.mutable_data<float>()[t] = 1.0f;
+  }
+
+  kmgr_window_.Initialize<WindowKernel>();
+  kmgr_window_.Resize<WindowKernel>(nthreads, nsamples);
+  FillExtractWindowsArgs(window_args_, window_length_, window_step_, InputDims);
+
+  kmgr_fft_.Initialize<FftKernel>();
+  kmgr_fft_.Resize<FftKernel>(nthreads, nsamples);
+  FillFftArgs(fft_args_, power_, window_length_, nfft_, WindowsDims);
+
+  window_out_desc_.resize(1);
+  window_out_desc_[0].type = TypeInfo::Create<InputType>();
+  window_out_desc_[0].shape.resize(nsamples, WindowsDims);
+
+  out_desc.resize(1);
+  out_desc[0].type = TypeInfo::Create<OutputType>();
+  out_desc[0].shape.resize(nsamples, WindowsDims);
+
+  for (int i = 0; i < nsamples; i++) {
+    auto &windows_req =
+      kmgr_window_.Setup<WindowKernel>(
+        i, ctx,
+        view<const InputType, Dims>(input[i]),
+        view<const float, 1>(window_fn_),
+        window_args_);
+    window_out_desc_[0].shape.set_tensor_shape(i, windows_req.output_shapes[0][0].shape);
+
+    auto windows_shape = windows_req.output_shapes[0][0].template to_static<WindowsDims>();
+    kernels::InTensorCPU<InputType, WindowsDims> dummy_win_view(nullptr, windows_shape);
+    auto &out_req =
+      kmgr_fft_.Setup<FftKernel>(
+        i, ctx,
+        dummy_win_view,
+        fft_args_);
+    out_desc[0].shape.set_tensor_shape(i, out_req.output_shapes[0][0].shape);
+  }
+  return true;
+}
+
+template <int Dims>
+void SpectrogramImplCpu<Dims>::RunImpl(workspace_t<CPUBackend> &ws) {
+  const auto &input = ws.InputRef<CPUBackend>(0);
+  auto &output = ws.OutputRef<CPUBackend>(0);
+  int nsamples = input.size();
+  auto& thread_pool = ws.GetThreadPool();
+
+  for (int i = 0; i < nsamples; i++) {
+    thread_pool.DoWorkWithID(
+      [this, &input, &output, i](int thread_id) {
+        kernels::KernelContext ctx;
+
+        auto &win_out = window_out_[thread_id];
+        win_out.set_type(TypeInfo::Create<InputType>());
+        win_out.Resize(window_out_desc_[0].shape.tensor_shape(i));
+
+        kmgr_window_.Run<WindowKernel>(
+          thread_id, i, ctx,
+          view<InputType, WindowsDims>(win_out),
+          view<const InputType, InputDims>(input[i]),
+          view<const float, 1>(window_fn_),
+          window_args_);
+
+        kmgr_fft_.Run<FftKernel>(
+          thread_id, i, ctx,
+          view<OutputType, WindowsDims>(output[i]),
+          view<const InputType, WindowsDims>(win_out),
+          fft_args_);
+    });
+  }
+
+  thread_pool.WaitForWork();
+}
+
+template <>
+Spectrogram<CPUBackend>::Spectrogram(const OpSpec &spec)
+    : Operator<CPUBackend>(spec)
+    , spec__(spec) {}
+
+template <>
+bool Spectrogram<CPUBackend>::SetupImpl(std::vector<OutputDesc> &output_desc,
+                                        const workspace_t<CPUBackend> &ws) {
+  const auto &input = ws.InputRef<CPUBackend>(0);
+  auto in_shape = input.shape();
+  VALUE_SWITCH(in_shape.size(), Dims, SPECTROGRAM_SUPPORTED_NDIMS,
+    (impl_ = std::make_unique<SpectrogramImplCpu<Dims>>(spec__);),
+    (DALI_FAIL(make_string("Not supported number of dimensions: ", in_shape.size()))));
+
+  assert(impl_ != nullptr);
+  return impl_->SetupImpl(output_desc, ws);
+}
+
+template <>
+void Spectrogram<CPUBackend>::RunImpl(workspace_t<CPUBackend> &ws) {
+  assert(impl_ != nullptr);
+  impl_->RunImpl(ws);
+}
+
+DALI_REGISTER_OPERATOR(Spectrogram, Spectrogram<CPUBackend>, CPU);
+
+}  // namespace dali
