a9f6e1f641218ebd6b352febdc97f92dcc72384e
Yang Zhang <yangzhang@live.com>
2019-11-15 20:59:06 +0800

    Add copyright headers
    Signed-off-by: Yang Zhang <yangzhang@live.com>

--- dali/python/nvidia/dali/plugin/paddle.py
+++ dali/python/nvidia/dali/plugin/paddle.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserved.
 # Copyright (c) 2017-2019, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
