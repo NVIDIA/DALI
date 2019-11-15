235f1ad853b324c8bf57b4a5beee878f2a06361e
Yang Zhang <yangzhang@live.com>
2019-11-15 20:59:39 +0800

    Remove unused pip package
    Signed-off-by: Yang Zhang <yangzhang@live.com>

--- qa/TL1_paddle_ssd/test.sh
+++ qa/TL1_paddle_ssd/test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages="numpy Cython paddle"
+pip_packages="numpy paddle"
 target_dir=./docs/examples/paddle/ssd/
 
 test_body() {
-    # workaround due to errors while pycocotools is in "pip_packages" above
-    pip install -I pycocotools
     python train.py --check-loss-steps=300 -b 8 /data/coco/coco-2017/coco2017/
 }
 
