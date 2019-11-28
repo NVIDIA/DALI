debe46f8e0a2f7c80b608c6f3f8731eb2f864e24
Joaquin Anton <janton@nvidia.com>
2019-11-28 16:22:51 +0100

    Code review fixes
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/kernels/signal/decibel/decibel_calculator.h
+++ dali/kernels/signal/decibel/decibel_calculator.h
@@ -16,6 +16,7 @@
 #define DALI_KERNELS_SIGNAL_DECIBEL_DECIBEL_CALCULATOR_H_
 
 #include <cmath>
+#include "dali/core/force_inline.h"
 
 namespace dali {
 namespace kernels {
@@ -25,29 +26,29 @@ template <typename T>
 struct DecibelCalculator {
  public:
   explicit DecibelCalculator(T mul = 10.0, T s_ref = 1.0, T min_ratio = 1e-8)
-      : mul_log2_(mul / std::log2(10.0))
+      : mul_log2_(mul * kLog2Factor)
       , inv_s_ref_(s_ref == 1.0 ? 1.0 : 1.0 / s_ref)
       , min_ratio_(min_ratio) {
     assert(min_ratio_ > 0.0);
     assert(inv_s_ref_ > 0.0);
   }
 
+  DALI_FORCEINLINE
   T operator()(T s) const {
     T ratio = s * inv_s_ref_;
     return mul_log2_ * std::log2(std::max(min_ratio_, ratio));
   }
 
  private:
+  static constexpr  T kLog2Factor = 0.3010299956639812;  // std::log10(2.0);
+
   // equivalent multiplier in terms of log2
-  // (e.g. 10.0 when expressing a power ratio, 20.0 for magnitudes)
   T mul_log2_;
 
   // Inverse of the magnitude reference to which we are calculating the ratio against
-  // (i.e `1.0 / s_ref` where s_ref is the reference magnitude or denominator inside the logarithm)
   T inv_s_ref_;
 
   // Cut-off or minimum value for `s/s_ref` ratio
-  // (e.g. 1e-8 means that the decibel output will saturate to a minimum of -80 dB)
   T min_ratio_;
 };
 
