b075ab242df60835c99621bfb6783dde4aeac2f4
Michał Szołucha <mszolucha@nvidia.com>
2019-11-29 18:21:14 +0100

    reivew
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/test/python/test_operator_preemph.py
+++ dali/test/python/test_operator_preemph.py
@@ -53,7 +53,7 @@ class PreemphasisPipeline(Pipeline):
         self.feed_input(self.data, premade_batch)
 
 
-def body(preemph_coeff):
+def check_preemphasis_operator(preemph_coeff):
     eps = 1e-5
     pipeline = PreemphasisPipeline(preemph_coeff=preemph_coeff)
     pipeline.build()
@@ -65,6 +65,6 @@ def body(preemph_coeff):
         assert isclose(a, b, abs_tol=eps)
 
 
-def test_preemphasis():
-    body(0.0)
-    body(0.5)
+def test_preemphasis_operator():
+    for coef in [0.5, 0.0]:
+       yield check_preemphasis_operator, coef
