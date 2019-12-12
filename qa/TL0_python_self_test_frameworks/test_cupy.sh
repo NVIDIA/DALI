e61c304d9f5560fff1be5c821ee140cdab104aef
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2019-12-12 19:22:56 +0100

    Fix problem with TensorFlow and cupy tests (#1568)
    - when tensorflow-gpu v1.15.0 is installed in the conda environment
      some strange dependency leads to `ModuleNotFoundError: No module
      named 'prompt_toolkit.formatted_text'` error
    - according to https://github.com/jupyter/notebook/issues/4050 installing
      `ipykernel<5.0.0 ipython<7.0.0` helps
    - fixes cupy version to 6.6.0 as this the last one supporting python 2.7
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_python_self_test_frameworks/test_cupy.sh
+++ qa/TL0_python_self_test_frameworks/test_cupy.sh
@@ -4,10 +4,7 @@ pip_packages="nose numpy cupy"
 target_dir=./dali/test/python
 
 test_body() {
-    if [[ ${PYTHON_VERSION} != 2.7 ]]
-    then
-        nosetests --verbose -m '(?:^|[\b_\./-])[Tt]est.*cupy' test_dltensor_operator.py
-    fi
+    nosetests --verbose -m '(?:^|[\b_\./-])[Tt]est.*cupy' test_dltensor_operator.py
 }
 
 pushd ../..
