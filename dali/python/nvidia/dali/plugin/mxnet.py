e93169420ee0eea194b3c803f4874fed3689e2db
Krzysztof Lecki <klecki@nvidia.com>
2020-02-06 13:22:24 +0100

    Adjust mxnet DALIClassificationIterator doc
    The description of equivalent DALIGenericIterator
    was wrong.
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/python/nvidia/dali/plugin/mxnet.py
+++ dali/python/nvidia/dali/plugin/mxnet.py
@@ -331,17 +331,16 @@ class DALIClassificationIterator(DALIGenericIterator):
 
     .. code-block:: python
 
-       DALIClassificationIterator(pipelines, size, data_name,
-                                  label_name, data_layout)
+       DALIClassificationIterator(pipelines, size, data_name, label_name, data_layout)
 
     is equivalent to calling
 
     .. code-block:: python
 
        DALIGenericIterator(pipelines,
-                           [data_name, DALIClassificationIterator.DATA_TAG,
-                            label_name, DALIClassificationIterator.LABEL_TAG],
-                           size, data_name, label_name,
+                           [(data_name, DALIClassificationIterator.DATA_TAG),
+                            (label_name, DALIClassificationIterator.LABEL_TAG)],
+                           size,
                            data_layout)
 
     Please keep in mind that NDArrays returned by the iterator are
