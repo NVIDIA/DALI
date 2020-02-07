7ff2344475b765a179523070a645e6b160d9003f
Rafał Banaś <32324320+banasraf@users.noreply.github.com>
2020-02-07 11:30:18 +0100

    Gpu python operator notebook (#1715)
    Add GPU Python Operator notebook. 
    
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

---
+++ qa/TL0_jupyter/test_cupy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# used pip packages
+pip_packages="jupyter numpy matplotlib cupy"
+target_dir=./docs/examples
+
+test_body() {
+    jupyter nbconvert --to notebook --inplace --execute \
+                      --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
+                      --ExecutePreprocessor.timeout=300 custom_operations/gpu_python_operator.ipynb
+}
+
+pushd ../..
+source ./qa/test_template.sh
+popd
