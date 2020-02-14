Fix precision loss in CropWindowGenerator
- CropWindowGenerator computes anchor and shapes as
  a rel_anch * shape, rel_shape * shape. To be more accurate it should
  be rel_anch * shape, (rel_anch + rel_shape) * shape - (rel_anch * shape)
- adds a special case when anchors and shapes are relative. When only one or
  none is then the old code path is used
- adds rounding in place of truncation
- updates tests
- uses doubles in place of floats to improve precision

Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

dali/test/python/test_operator_slice.py
@@ -238,24 +238,25 @@ def slice_func_helper(axes, axis_names, layout, normalized_anchor, normalized_sh
         full_slice_anchor[axis] = slice_anchor[idx]
         full_slice_shape[axis] = slice_shape[idx]
 
+    #std::round has different behaviour than np.round so manually add 0.5 and truncate to int
     if normalized_anchor and normalized_shape:
-        start = [int(np.float32(shape[i]) * np.float32(full_slice_anchor[i]))
+        start = [int(np.float32(shape[i]) * np.float32(full_slice_anchor[i]) + 0.5)
                  for i in range(len(shape))]
-        end = [int(np.float32(shape[i]) * np.float32(full_slice_anchor[i]+full_slice_shape[i]))
+        end = [int(np.float32(shape[i]) * np.float32(full_slice_anchor[i]+full_slice_shape[i]) + 0.5)
                for i in range(len(shape))]
     else:
         if normalized_anchor:
-            start = [int(np.float32(shape[i]) * np.float32(full_slice_anchor[i]))
+            start = [int(np.float32(shape[i]) * np.float32(full_slice_anchor[i]) + 0.5)
                     for i in range(len(shape))]
         else:
-            start = [int(np.float32(full_slice_anchor[i]))
+            start = [int(np.float32(full_slice_anchor[i]) + 0.5)
                     for i in range(len(shape))]
 
         if normalized_shape:
-            end = [start[i] + int(np.float32(shape[i]) * np.float32(full_slice_shape[i]))
+            end = [start[i] + int(np.float32(shape[i]) * np.float32(full_slice_shape[i]) + 0.5)
                 for i in range(len(shape))]
         else:
-            end = [start[i] + int(np.float32(full_slice_shape[i]))
+            end = [start[i] + int(np.float32(full_slice_shape[i]) + 0.5)
                 for i in range(len(shape))]
 
     if len(full_slice_anchor) == 3: