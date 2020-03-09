c8de5f3862a7297371c2a8c79090a34758c28246
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2020-03-09 12:24:51 +0100

    Move RNNT test to Torch specific tests (#1805)
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_python_self_test_frameworks/test_pytorch.sh
+++ qa/TL0_python_self_test_frameworks/test_pytorch.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages="nose numpy torch"
+pip_packages="nose numpy librosa torch"
 target_dir=./dali/test/python
 
 test_body() {
     nosetests --verbose -m '(?:^|[\b_\./-])[Tt]est.*pytorch' test_pytorch_operator.py
     nosetests --verbose -m '(?:^|[\b_\./-])[Tt]est.*pytorch' test_dltensor_operator.py
+    nosetests --verbose test_torch_pipeline_rnnt.py
 }
 
 pushd ../..
