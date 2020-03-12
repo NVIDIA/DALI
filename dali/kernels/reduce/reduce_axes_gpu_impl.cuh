03ff2dc8853d402b7f6ba4060cfe5c8a8e63f263
Michal Zientkiewicz <michalz@nvidia.com>
2020-03-12 17:07:39 +0100

    Initial implementation
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/kernels/reduce/reduce_axes_gpu_impl.cuh
+++ dali/kernels/reduce/reduce_axes_gpu_impl.cuh
@@ -24,72 +24,112 @@ namespace dali {
 namespace kernels {
 
 /**
- * @brief This kernel is used for reducing innermost dimension with small (<32) extent.
+ * @brief This function is used for reducing innermost dimension with small (<32) extent.
  *
- * Limitations: innermost extent up to 64 elements
+ * Limitations: innermost extent up to 63 elements
+ *
+ * @param out per-sample output (partial result)
+ * @param in  input sample (single tensor)
+ * @param
  */
 template <typename Acc, typename In,
           typename Reduction = reductions::sum,
           typename Preprocess = dali::identity>
-__global__ void ReduceInnerSmallKernel(Acc *out, const In *const *in, const int64_t *in_shapes,
-                                       Reduction reduce = {}, Preprocess pp = {}) {
-
-  auto bank_hack = [](int index) {
-    // value of 19 gives no bank conflicts for inner dimensions 1 to 8 and is only really bad for
-    // inner dim = 27 and 47, when it uses only 4 out of 16 banks
-    return (index & 1) + (index >> 4) * 19;
+__device__ void ReduceInnerSmall(Acc *out, const In *in, int64_t n_outer, int n_inner,
+                                 Reduction reduce = {}, Preprocess pp = {}) {
+  // this table contains multipliers to use in shared memory reindexing to best avoid bank
+  // conflicts - the values are result of exhaustive search for particular
+  static __constant__ uint8_t bank_friendly_table[64] = {
+    17, 16, 17, 16, 17, 16, 19, 16,
+    17, 16, 19, 16, 19, 16, 17, 16,
+    17, 16, 17, 16, 19, 16, 19, 16,
+    17, 16, 17, 16, 19, 16, 19, 16,
+    17, 16, 17, 16, 17, 16, 17, 16,
+    17, 16, 17, 16, 17, 16, 17, 16,
+    17, 16, 17, 16, 17, 16, 17, 16,
+    17, 16, 17, 16, 19, 16, 17, 16,
   };
 
-  __shared__ Acc tmp[64*38];  // 38 = 32 * 19/16, see bank_hack
+  __shared__ Acc tmp[64*38];  // 38 = 32 * 19/16, see bank_friendly
 
-  int sample = threadIdx.y;
-  int64_t n_outer = in_shapes[2*samples];
-  int n_inner = in_shapes[2*sample+1];
   int64_t sample_size = n_outer * n_inner;
+
+  int bank_friendly_mul = bank_friendly_table[n_inner];
+  auto bank_friendly = [bank_friendly_mul](int index) {
+    // value of 19 gives no bank conflicts for inner dimensions 1 to 8 and is only really bad for
+    // inner dim = 27 and 47, when it uses only 4 out of 16 banks
+    return (index & 1) + (index >> 4) * bank_friendly_mul;
+  };
 
   int64_t grid_size_x = blockDim.x * gridDim.x;
   int64_t grid_stride = grid_size_x * n_inner;
-  int64_t block_stride = blockDim.x * n_inner;
-  int64_t block_start = blockIdx.x * block_stride;
 
-  for (int64_t base = block_start; base < sample_size; base += block_stride) {
+  for (int64_t outer = blockIdx.x * blockDim.x; outer < n_outer; outer += grid_size_x) {
+    int64_t base = outer * n_inner;
     for (int i = 0; i < n_inner; i++) {
       int ofs = i * blockDim.x + threadIdx.x;
       auto idx = base + ofs;
-      tmp[bank_hack(ofs)] = idx < sample_size ? pp(in[sample][idx]) : R.template neutral<Acc>();
+      tmp[bank_friendly(ofs)] = idx < sample_size ? pp(in[idx]) : reduce.template neutral<Acc>();
     }
     __syncthreads();
-    Acc acc = tmp[bank_hack(threadIdx.x * n_inner)];
-    for (int i = 1; i < n_inner; i++) {
-      int ofs = threadIdx.x * n_inner + i;
-      reduce(acc, tmp[bank_hack(ofs)]);
+    int64_t outer_ofs = outer + threadIdx.x;
+    Acc acc;
+    if (outer_ofs < n_outer) {
+      Acc acc = tmp[bank_friendly(threadIdx.x * n_inner)];
+      for (int i = 1; i < n_inner; i++) {
+        int ofs = threadIdx.x * n_inner + i;
+        reduce(acc, tmp[bank_friendly(ofs)]);
+      }
     }
-    __syncthreads()
-    out[blockIdx.x + gridDim.x * blockIdx.y] = acc;
+    __syncthreads();  // this must be in convergent code
+    if (outer_ofs < n_outer)
+      out[outer_ofs] = acc;
   }
 }
 
 /**
  * @brief This kernel is used for reducing innermost dimension with medium extent.
  *
- * Limitations: innermost extent 32 to ~256 elements
+ * Limitations: innermost extent 32 to ~256 elements due to performance and precision issues
  */
 template <typename Acc, typename In,
           typename Reduction = reductions::sum,
           typename Preprocess = dali::identity>
-__global__ void ReduceInnerMediumKernel(Acc *out, const In *const *in, const int64_t *in_sizes,
-                                       Reduction reduce = {}, Preprocess pp = {}) {
+__device__ void ReduceInnerMedium(Acc *out, const In *in, int64_t n_outer, int n_inner,
+                                  Reduction reduce = {}, Preprocess pp = {}) {
   Acc val = reduce.template neutral<Acc>();
+}
 
+/**
+ * @brief This kernel is used for reducing innermost dimension with large extent.
+ *
+ * Limitations: not recommended for small reduced extents due to performance.
+ */
+template <typename Acc, typename In,
+          typename Reduction = reductions::sum,
+          typename Preprocess = dali::identity>
+__device__ void ReduceInnerLarge(Acc *out, const In *in, int64_t n_outer, int64_t n_inner,
+                                  Reduction reduce = {}, Preprocess pp = {}) {
+  Acc val = reduce.template neutral<Acc>();
+}
+
+template <typename Acc, typename In,
+          typename Reduction = reductions::sum,
+          typename Preprocess = dali::identity>
+__global__ void ReduceInnerKernel(Acc *const *out, const In *const *in, const int64_t *in_shapes,
+                                 Reduction reduce = {}, Preprocess pp = {}) {
   int sample = threadIdx.y;
-  int64_t n_outer = in_shapes[2*samples];
-  int n_inner = in_shapes[2*sample+1];
+  int64_t n_outer = in_shapes[2*sample];
+  int64_t n_inner = in_shapes[2*sample+1];
   int64_t sample_size = n_outer * n_inner;
 
-  int64_t grid_size_x = blockDim.x * gridDim.x;
-  int64_t grid_stride = grid_size_x * n_inner;
-  int64_t block_stride = blockDim.x * n_inner;
-  int64_t block_start = blockIdx.x * block_stride;
+  if (n_inner < 64) {
+    ReduceInnerSmall(&out[sample], in[sample], n_outer, n_inner, reduce, pp);
+  } else if (n_inner < 256) {
+    ReduceInnerMedium(&out[sample], in[sample], n_outer, n_inner, reduce, pp);
+  } else {
+    ReduceInnerLarge(&out[sample], in[sample], n_outer, n_inner, reduce, pp);
+  }
 
 }
 
