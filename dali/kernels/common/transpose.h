1393d2bb2f8e6f12db1a2518b37dde452ab300fa
Thorsten Kurth <tkurth@nvidia.com>
2020-04-06 17:34:16 -0700

    fixed indentation in transpose.cc

--- dali/kernels/common/transpose.h
+++ dali/kernels/common/transpose.h
@@ -148,7 +148,7 @@ inline SmallVector<std::pair<int, int>, 6> PermutationBlocks(span<const int> per
  * @return Collapsed permutation
  */
 inline std::vector<int> CollapsePermutation(span<const int> perm,
-                                     span<const std::pair<int, int>> perm_blocks) {
+                                            span<const std::pair<int, int>> perm_blocks) {
   std::vector<int> result;
   std::vector<int> removed;
   result.reserve(perm_blocks.size());
@@ -176,8 +176,8 @@ inline std::vector<int> CollapsePermutation(span<const int> perm,
  * @brief Convert permutation blocks [first_idx_in_perm, last_idx_in_perm] to
  *        description of shape blocks: {starting_dimension_idx, length}
  */
-inline SmallVector<std::pair<int, int>, 6> PermToShapeBlocks(span<const int> perm,
-                            span<const std::pair<int, int>> perm_blocks) {
+inline SmallVector<std::pair<int, int>, 6>
+  PermToShapeBlocks(span<const int> perm, span<const std::pair<int, int>> perm_blocks) {
   SmallVector<std::pair<int, int>, 6> shape_blocks;
   shape_blocks.reserve(perm_blocks.size());
   for (auto &range : perm_blocks) {
