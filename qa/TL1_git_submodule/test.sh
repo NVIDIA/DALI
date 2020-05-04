725ccc29689440e11c4b8a5a96c3e3b07536bd92
szalpal <mszolucha@nvidia.com>
2020-05-04 13:13:24 +0200

    test
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- qa/TL1_git_submodule/test.sh
+++ qa/TL1_git_submodule/test.sh
@@ -2,23 +2,27 @@
 
 test_body() {
     mkdir -p /tmp/repo_that_uses_dali
-    cd /tmp/repo_that_uses_dali
+    pushd /tmp/repo_that_uses_dali
     git init
-    git submodule add -b dali_as_submodule https://github.com/AlexBula/DALI
-    cd DALI
+    git submodule add https://github.com/NVIDIA/DALI
+    pushd DALI
     git submodule sync --recursive
     git submodule update --init --recursive
-    cd -
+    popd
+    touch main_stub.cc
     echo "
 cmake_minimum_required(VERSION 3.13)
 project(UsingDali)
 
 add_subdirectory(DALI)
+
+add_library(test_lib SHARED main_stub.cc)
+target_link_libraries(test_lib PUBLIC DALI::dali DALI::dali_core DALI::dali_kernels DALI::dali_operators)
 " > CMakeLists.txt
     mkdir build
-    cd build
+    pushd build
     cmake ..
-    make 
+    make -j"$(grep ^processor /proc/cpuinfo | wc -l)"
 }
 
 pushd ../..
