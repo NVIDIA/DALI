8a7f781bc74e350738c42b4c81124dc58583b405
Rafał Banaś <32324320+banasraf@users.noreply.github.com>
2020-05-06 17:21:47 +0200

    Use LinearTransformation kernel in ColorTwist GPU Op (#1918)
    Merge Hsv and ColorTwist operators. 
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- dali/operators/image/color/color_twist.cc
+++ dali/operators/image/color/color_twist.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,14 +13,50 @@
 // limitations under the License.
 
 #include "dali/operators/image/color/color_twist.h"
-#include "dali/image/transform.h"
+#include "dali/kernels/imgproc/pointwise/linear_transformation_cpu.h"
 
 namespace dali {
+namespace {
+
+template <typename Out, typename In>
+using TheKernel = kernels::LinearTransformationCpu<Out, In, 3, 3, 3>;
+
+}  // namespace
+
+DALI_SCHEMA(Hsv)
+    .DocStr(R"code(This operator performs HSV manipulation.
+To change hue, saturation and/or value of the image, pass corresponding coefficients.
+Keep in mind, that `hue` has additive delta argument,
+while for `saturation` and `value` they are multiplicative.
+
+This operator accepts RGB color space as an input.
+
+For performance reasons, the operation is approximated by a linear transform in RGB space.
+The color vector is projected along the neutral (gray) axis,
+rotated (according to hue delta) and scaled according to value and saturation multiplers,
+and then restored to original color space.
+)code")
+    .NumInput(1)
+    .NumOutput(1)
+    .AddOptionalArg(color::kHue,
+                    R"code(Set additive change of hue. 0 denotes no-op)code",
+                    0.0f, true)
+    .AddOptionalArg(color::kSaturation,
+                    R"code(Set multiplicative change of saturation. 1 denotes no-op)code",
+                    1.0f, true)
+    .AddOptionalArg(color::kValue,
+                    R"code(Set multiplicative change of value. 1 denotes no-op)code",
+                    1.0f, true)
+    .AddOptionalArg(color::kOutputType, "Output data type; if not set, the input type is used.",
+                    DALI_UINT8)
+    .InputLayout(0, "HWC");
 
 DALI_SCHEMA(ColorTransformBase)
     .DocStr(R"code(Base Schema for color transformations operators.)code")
     .AddOptionalArg("image_type",
-        R"code(The color space of input and output image)code", DALI_RGB);
+        R"code(The color space of input and output image)code", DALI_RGB)
+    .AddOptionalArg(color::kOutputType, "Output data type; if not set, the input type is used.",
+                    DALI_UINT8);
 
 DALI_SCHEMA(Brightness)
     .DocStr(R"code(Changes the brightness of an image)code")
@@ -100,43 +136,57 @@ Values >= 0 are accepted. For example:
     .AddParent("ColorTransformBase")
     .InputLayout(0, "HWC");
 
-template <>
-void ColorTwistBase<CPUBackend>::RunImpl(SampleWorkspace &ws) {
-  const auto &input = ws.Input<CPUBackend>(0);
-  auto &output = ws.Output<CPUBackend>(0);
-  const auto &input_shape = input.shape();
 
-  CheckParam(input, "Color augmentation");
+DALI_REGISTER_OPERATOR(Hsv, ColorTwistCpu, CPU)
+DALI_REGISTER_OPERATOR(Brightness, ColorTwistCpu, CPU);
+DALI_REGISTER_OPERATOR(Contrast, ColorTwistCpu, CPU);
+DALI_REGISTER_OPERATOR(Hue, ColorTwistCpu, CPU);
+DALI_REGISTER_OPERATOR(Saturation, ColorTwistCpu, CPU);
+DALI_REGISTER_OPERATOR(ColorTwist, ColorTwistCpu, CPU);
 
-  const auto H = input_shape[0];
-  const auto W = input_shape[1];
-  const auto C = input_shape[2];
-
-  output.ResizeLike(input);
-  output.SetLayout(InputLayout(ws, 0));
-
-  auto pImgInp = input.template data<uint8>();
-  auto pImgOut = output.template mutable_data<uint8>();
-
-  if (!augments_.empty()) {
-    float matrix[nDim][nDim];
-    float *m = reinterpret_cast<float *>(matrix);
-    IdentityMatrix(m);
-    for (size_t j = 0; j < augments_.size(); ++j) {
-      augments_[j]->Prepare(ws.data_idx(), spec_, &ws);
-      (*augments_[j])(m);
-    }
-
-    MakeColorTransformation(pImgInp, H, W, C, m, pImgOut);
-  } else {
-    memcpy(pImgOut, pImgInp, H * W * C);
-  }
+bool ColorTwistCpu::SetupImpl(std::vector<OutputDesc> &output_desc, const HostWorkspace &ws) {
+  const auto &input = ws.template InputRef<CPUBackend>(0);
+  output_desc.resize(1);
+  DetermineTransformation(ws);
+  TYPE_SWITCH(input.type().id(), type2id, InputType, (uint8_t, int16_t, int32_t, float, float16), (
+      TYPE_SWITCH(output_type_, type2id, OutputType, (uint8_t, int16_t, int32_t, float, float16), (
+          {
+              using Kernel = TheKernel<OutputType, InputType>;
+              kernel_manager_.Initialize<Kernel>();
+              auto shapes = CallSetup<Kernel, InputType>(input);
+              TypeInfo type;
+              type.SetType<OutputType>(output_type_);
+              output_desc[0] = {shapes, type};
+          }
+      ), DALI_FAIL(make_string("Unsupported output type: ", output_type_)))  // NOLINT
+  ), DALI_FAIL(make_string("Unsupported input type: ", input.type().id())))  // NOLINT
+  return true;
 }
 
-DALI_REGISTER_OPERATOR(Brightness, BrightnessAdjust<CPUBackend>, CPU);
-DALI_REGISTER_OPERATOR(Contrast, ContrastAdjust<CPUBackend>, CPU);
-DALI_REGISTER_OPERATOR(Hue, HueAdjust<CPUBackend>, CPU);
-DALI_REGISTER_OPERATOR(Saturation, SaturationAdjust<CPUBackend>, CPU);
-DALI_REGISTER_OPERATOR(ColorTwist, ColorTwistAdjust<CPUBackend>, CPU);
+
+void ColorTwistCpu::RunImpl(workspace_t<CPUBackend> &ws) {
+  const auto &input = ws.template InputRef<CPUBackend>(0);
+  auto &output = ws.template OutputRef<CPUBackend>(0);
+  output.SetLayout(InputLayout(ws, 0));
+  auto &tp = ws.GetThreadPool();
+  TYPE_SWITCH(input.type().id(), type2id, InputType, (uint8_t, int16_t, int32_t, float, float16), (
+      TYPE_SWITCH(output_type_, type2id, OutputType, (uint8_t, int16_t, int32_t, float, float16), (
+          {
+              using Kernel = TheKernel<OutputType, InputType>;
+              for (int i = 0; i < input.shape().num_samples(); i++) {
+                tp.DoWorkWithID([&, i](int thread_id) {
+                  kernels::KernelContext ctx;
+                  auto tvin = view<const InputType, 3>(input[i]);
+                  auto tvout = view<OutputType, 3>(output[i]);
+                  kernel_manager_.Run<Kernel>(ws.thread_idx(), i, ctx, tvout, tvin,
+                                              tmatrices_[i], toffsets_[i]);
+                });
+              }
+          }
+      ), DALI_FAIL("Unsupported output type"))  // NOLINT
+  ), DALI_FAIL("Unsupported input type"))  // NOLINT
+  tp.WaitForWork();
+}
+
 
 }  // namespace dali
