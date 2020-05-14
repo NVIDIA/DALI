81f07380cc1a33037b87ef09909eb06fa6bef524
Joaquin Anton <janton@nvidia.com>
2020-05-14 09:11:18 +0200

    Add dummy package
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/python/dummy/make_nvidia_dali_dummy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o xtrace
+set -e
+
+mkdir -p build
+cd build
+
+CUDA_VERSION=$(echo $(ls /usr/local/cuda/lib64/libcudart.so*)  | sed 's/.*\.\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)/\1.\2/')
+
+cmake .. \
+      -DCUDA_VERSION:STRING="${CUDA_VERSION}" \
+      -DDALI_BUILD_FLAVOR=${NVIDIA_DALI_BUILD_FLAVOR} \
+      -DTIMESTAMP=${DALI_TIMESTAMP} \
+      -DGIT_SHA=${GIT_SHA}
+
+python setup.py bdist_wheel
+cp dist/*.whl /nvidia_dali_dummy_wheel
