34e12163422ef98eb0c0e54968b4774201ea42f5
Joaquin Anton <janton@nvidia.com>
2020-06-24 15:23:37 +0200

    Use slice kernel for Pad operator
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/operators/generic/pad.h
+++ dali/operators/generic/pad.h
@@ -78,17 +78,6 @@ class Pad : public Operator<Backend> {
   template <typename Args>
   std::vector<Args>& FillArgs(TensorListShape<> in_shape, TensorLayout in_layout) {
     int nsamples = in_shape.num_samples();
-    if (!kernel_sample_args_.has_value()) {
-      kernel_sample_args_ = std::vector<Args>();
-    }
-
-    auto &kernel_sample_args = any_cast<std::vector<Args>&>(kernel_sample_args_);
-    kernel_sample_args.clear();
-    kernel_sample_args.reserve(nsamples);
-    for (int i = 0; i < nsamples; i++) {
-      kernel_sample_args.emplace_back(in_shape[i]);
-    }
-
     int ndim = in_shape.sample_dim();
 
     for (auto axis : axes_) {
@@ -140,13 +129,26 @@ class Pad : public Operator<Backend> {
       }
     }
 
+    if (!kernel_sample_args_.has_value()) {
+      kernel_sample_args_ = std::vector<Args>();
+    }
+
+    auto &kernel_sample_args = any_cast<std::vector<Args>&>(kernel_sample_args_);
+    kernel_sample_args.clear();
+    kernel_sample_args.resize(nsamples);
+
     for (int sample_idx = 0; sample_idx < nsamples; sample_idx++) {
       auto &sample_args = kernel_sample_args[sample_idx];
       const auto &sample_shape = in_shape[sample_idx];
-      sample_args.padding_val = fill_value_;
+      for (int d = 0; d < sample_args.anchor.size(); d++) {
+        sample_args.anchor[d] = 0;
+        sample_args.shape[d] = sample_shape[d];
+      }
+      sample_args.fill_values.push_back(fill_value_);
+
       for (int i = 0; i < naxes; i++) {
         auto axis = axes_[i];
-        auto &extent = sample_args.padded_shape[axis];
+        auto &extent = sample_args.shape[axis];
         // Adjust padded extent only if it is bigger than the sample's extent
         // That is, we are not cropping the image
         if (padded_shape[axis] > extent)
