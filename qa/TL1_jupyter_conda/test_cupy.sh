cc0f5b52c83daa2cd0360cda4c3601e0a18ae6c2
Janusz Lisiecki <jlisiecki@nvidia.com>
2020-06-24 16:31:17 +0200

    Fix TL1_jupyter_conda test
    - changes introduced by https://github.com/NVIDIA/DALI/pull/1997
      were not applied to conda based test
    - ExternalSource jupyter example is extended by GPU case and
      requires cupy and imageio to run, this PR fixes this
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_jupyter_conda/test_cupy.sh
+++ qa/TL1_jupyter_conda/test_cupy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages="jupyter numpy matplotlib cupy"
+pip_packages="jupyter numpy matplotlib cupy imageio"
 target_dir=./docs/examples
 
 # populate epilog and prolog with variants to enable/disable conda
@@ -9,9 +9,15 @@ prolog=(enable_conda)
 epilog=(disable_conda)
 
 test_body() {
-    jupyter nbconvert --to notebook --inplace --execute \
-                      --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
-                      --ExecutePreprocessor.timeout=300 custom_operations/gpu_python_operator.ipynb
+    test_files=(
+        "custom_operations/gpu_python_operator.ipynb"
+        "general/data_loading/external_input.ipynb"
+    )
+    for f in ${test_files[@]}; do
+        jupyter nbconvert --to notebook --inplace --execute \
+                        --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
+                        --ExecutePreprocessor.timeout=300 $f;
+    done
 }
 
 pushd ../..
