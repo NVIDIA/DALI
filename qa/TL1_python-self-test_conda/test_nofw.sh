ab48f7bb8502e6b69647dba994253f62e5a911d5
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2020-07-27 10:15:39 +0200

    Remove pinning of numba version as librosa 0.8.0 has been released (#2151)
    - as librosa 0.8.0 has been released it reflects the most recent changes in numba API so DALI no longer needs to pin its version to  <= 0.49
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_python-self-test_conda/test_nofw.sh
+++ qa/TL1_python-self-test_conda/test_nofw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-# lock numba version as 0.50 changed module location and librosa hasn't catched up in 7.2 yet
-pip_packages="nose numpy>=1.17 opencv-python pillow librosa numba<=0.49"
+pip_packages="nose numpy>=1.17 opencv-python pillow librosa"
 target_dir=./dali/test/python
 
 # test_body definition is in separate file so it can be used without setup
