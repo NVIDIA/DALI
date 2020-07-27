7258bed0f6502b55db4311d278e753b37c4b92f6
Joaquin Anton <janton@nvidia.com>
2020-07-27 10:18:59 +0200

    Extract repeated code to a templated function. Improve error msg about number of inputs
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/pipeline/executor/executor.h
+++ dali/pipeline/executor/executor.h
@@ -339,6 +339,17 @@ class DLL_PUBLIC Executor : public ExecutorBase, public WorkspacePolicy, public 
   std::mutex gpu_memory_stats_mutex_;
 
  private:
+  template <typename InputRef>
+  static bool SetDefaultLayoutIfNeeded(InputRef &in, const OpSchema &schema, int in_idx) {
+    if (!in.GetLayout().empty())
+      return false;
+    auto default_layout = schema.GetInputLayout(in_idx, in.shape().sample_dim(), in.GetLayout());
+    if (default_layout.empty())
+      return false;
+    in.SetLayout(default_layout);
+    return true;
+  }
+
   template <typename Workspace>
   void RunHelper(OpNode &op_node, Workspace &ws) {
     auto &output_desc = op_node.output_desc;
@@ -348,23 +359,14 @@ class DLL_PUBLIC Executor : public ExecutorBase, public WorkspacePolicy, public 
     const auto &schema = spec.GetSchema();
     SmallVector<int, 16> empty_layout_in_idxs;
     for (int i = 0; i < spec.NumRegularInput(); i++) {
+      bool had_empty_layout = false;
       if (ws.template InputIsType<CPUBackend>(i)) {
-        auto &in = ws.template InputRef<CPUBackend>(i);
-        bool empty_in_layout = in.GetLayout().empty();
-        auto layout = schema.GetInputLayout(i, in.shape().sample_dim(), in.GetLayout());
-        if (!empty_in_layout || layout.empty())
-          continue;
-        in.SetLayout(layout);
-        empty_layout_in_idxs.push_back(i);
+        had_empty_layout = SetDefaultLayoutIfNeeded(ws.template InputRef<CPUBackend>(i), schema, i);
       } else {
-        auto &in = ws.template InputRef<GPUBackend>(i);
-        bool empty_in_layout = in.GetLayout().empty();
-        auto layout = schema.GetInputLayout(i, in.shape().sample_dim(), in.GetLayout());
-        if (!empty_in_layout || layout.empty())
-          continue;
-        in.SetLayout(layout);
-        empty_layout_in_idxs.push_back(i);
+        had_empty_layout = SetDefaultLayoutIfNeeded(ws.template InputRef<GPUBackend>(i), schema, i);
       }
+      if (had_empty_layout)
+        empty_layout_in_idxs.push_back(i);
     }
 
     if (op.Setup(output_desc, ws)) {
