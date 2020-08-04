81ac59c482299942554d8b93e9ae5c0268f0b823
szalpal <mszolucha@nvidia.com>
2020-08-04 16:43:22 +0200

    Utility for linking DALI deps from the wheel to custom C++ lib
    Signed-off-by: szalpal <mszolucha@nvidia.com>

---
+++ qa/TL1_custom_cpp_lib/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+test_body() {
+    mkdir -p /tmp/custom_cpp_lib
+    git clone https://github.com/NVIDIA/DALI /tmp/dali
+    pushd /tmp/custom_cpp_lib
+    echo "
+#include <dali/c_api.h>
+int main() {
+  daliInitialize();
+  daliInitOperators();
+}
+" > main_stub.cc
+    echo "
+cmake_minimum_required(VERSION 3.13)
+project(CustomCppLib)
+include(/tmp/dali/tools/find_dali.cmake)
+add_library(test_lib SHARED main_stub.cc)
+find_dali(DALI_INCLUDE_DIR DALI_LIB_DIR DALI_LIBRARIES)
+target_include_directories(test_lib ${DALI_INCLUDE_DIR})
+target_link_directories(test_lib ${DALI_LIB_DIR})
+target_link_libraries(test_lib ${DALI_LIBRARIES})
+" > CMakeLists.txt
+    mkdir build
+    pushd build
+    cmake -D CUDA_TARGET_ARCHS=60 -D CMAKE_BUILD_TYPE=Debug ..
+    make -j"$(grep ^processor /proc/cpuinfo | wc -l)"
+    popd
+    popd
+}
+
+pushd ../..
+source ./qa/test_template.sh
+popd
---
+++ tools/find_dali.cmake
@@ -0,0 +1,53 @@
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##################################################################
+# Utility function, useful when building custom C++ library where
+# DALI is used as a shared library. Assumes, that DALI wheel is
+# installed within the system. Acquires paths to DALI libraries
+# and dependencies introduced by the wheel. On the event, that
+# multiple DALI wheels are installed, picks the most recent one.
+#
+# Usage:
+#
+# find_dali(DALI_INCLUDE_DIR DALI_LIB_DIR DALI_LIBRARIES)
+#
+# target_include_directories(my_target ${DALI_INCLUDE_DIR})
+# target_link_directories(my_target ${DALI_LIB_DIR})
+# target_link_libraries(my_target ${DALI_LIBRARIES})
+##################################################################
+function(find_dali DALI_INCLUDE_DIR DALI_LIB_DIR DALI_LIBRARIES)
+    execute_process(
+            COMMAND python3 -c "import nvidia.dali.sysconfig as dali_sc; print(dali_sc.get_include_dir(), end='')"
+            OUTPUT_VARIABLE DALI_INCLUDE_DIR_LOC
+            RESULT_VARIABLE DALI_INCLUDE_DIR_RESULT
+    )
+
+    execute_process(
+            COMMAND python3 -c "import nvidia.dali.sysconfig as dali_sc; print(dali_sc.get_lib_dir(), end='')"
+            OUTPUT_VARIABLE DALI_LIB_DIR_LOC
+            RESULT_VARIABLE DALI_LIB_DIR_RESULT
+    )
+
+    if (${DALI_INCLUDE_DIR_RESULT} EQUAL "1" OR ${DALI_LIB_DIR_RESULT} EQUAL "1")
+        message(FATAL_ERROR "Error acquiring DALI. Verify, that you have DALI whl installed")
+    endif ()
+
+    set(${DALI_INCLUDE_DIR} ${DALI_INCLUDE_DIR_LOC} PARENT_SCOPE)
+    set(${DALI_LIB_DIR} ${DALI_LIB_DIR_LOC} PARENT_SCOPE)
+    set(${DALI_LIBRARIES} dali dali_operators PARENT_SCOPE)
+    message(STATUS "DALI libraries DIR: " ${DALI_LIB_DIR})
+    message(STATUS "DALI include DIR: " ${DALI_INCLUDE_DIR})
+    message(STATUS "DALI linked libraries: " ${DALI_LIBRARIES})
+endfunction()
