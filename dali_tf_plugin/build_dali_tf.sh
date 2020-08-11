7b1f7840d1469da689315af17a145e5c993bedc3
Joaquin Anton <janton@nvidia.com>
2020-08-11 16:18:16 +0200

    Remove DALI_LFLAGS DALI_CFLAGS from build_dali_tf.sh
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali_tf_plugin/build_dali_tf.sh
+++ dali_tf_plugin/build_dali_tf.sh
@@ -4,16 +4,12 @@ COMPILER=${CXX:-g++}
 PYTHON=${PYTHON:-python}
 LIB_NAME=${1:-"libdali_tf_current.so"}
 SRCS="daliop.cc dali_dataset_op.cc"
-INCL_DIRS="-I/usr/local/cuda/include/"
-
-DALI_CFLAGS=( $($PYTHON ./dali_compile_flags.py --include_flags) )
-DALI_LFLAGS=( $($PYTHON ./dali_compile_flags.py --lflags) )
+INCL_DIRS="-I/usr/local/cuda/include/ -I../include"
 
 TF_CFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
 TF_LFLAGS=( $($PYTHON -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
-
 # Note: DNDEBUG flag is needed due to issue with TensorFlow custom ops:
 # https://github.com/tensorflow/tensorflow/issues/17316
 # Do not remove it.
-$COMPILER -std=c++11 -DNDEBUG -O2 -shared -fPIC ${SRCS} -o ${LIB_NAME} ${INCL_DIRS} ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} ${DALI_CFLAGS[@]} ${DALI_LFLAGS[@]}
+$COMPILER -std=c++11 -DNDEBUG -O2 -shared -fPIC ${SRCS} -o ${LIB_NAME} ${INCL_DIRS} ${TF_CFLAGS[@]} ${TF_LFLAGS[@]}
