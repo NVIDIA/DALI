d99ae44d96ed68c8eb56eb3ae67795651ca225ad
Joaquin Anton <janton@nvidia.com>
2020-10-08 11:27:15 +0200

    Use CMake prebuilt distribution
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/TL1_nodeps_build/test.sh
+++ qa/TL1_nodeps_build/test.sh
@@ -3,19 +3,10 @@
 pip_packages=""
 
 do_once() {
-  apt-get update
-  apt-get -y install unzip wget build-essential
-  CMAKE_VERSION=3.13                                                             && \
-  CMAKE_BUILD=3.13.5                                                             && \
-  pushd /tmp                                                                     && \
-  wget -nv https://cmake.org/files/v${CMAKE_VERSION}/cmake-${CMAKE_BUILD}.tar.gz && \
-  tar -xf cmake-${CMAKE_BUILD}.tar.gz                                            && \
-  pushd cmake-${CMAKE_BUILD}                                                     && \
-  ./bootstrap --parallel=$(grep ^processor /proc/cpuinfo | wc -l)                && \
-  make -j"$(grep ^processor /proc/cpuinfo | wc -l)" install                      && \
-  rm -rf /tmp/cmake-${CMAKE_BUILD}                                               && \
-  popd                                                                           && \
-  popd
+  apt-get update && apt-get -y install wget wget
+  wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh
+  bash cmake-3.18.4-Linux-x86_64.sh --skip-license
+  rm cmake-3.18.4-Linux-x86_64.sh
 }
 
 test_body() {
