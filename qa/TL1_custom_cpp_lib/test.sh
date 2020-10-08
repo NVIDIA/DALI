b4f92b811b589f38797a50b1f38fb4f67f26aa7d
szalpal <mszolucha@nvidia.com>
2020-10-08 11:48:16 +0200

    review
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- qa/TL1_custom_cpp_lib/test.sh
+++ qa/TL1_custom_cpp_lib/test.sh
@@ -3,16 +3,10 @@
 export THIS_PATH=`pwd`
 
 do_once() {
-  apt-get update
-  apt-get -y install unzip wget build-essential
-  CMAKE_VERSION=3.13                                                             && \
-  CMAKE_BUILD=3.13.5                                                             && \
-  wget -nv https://cmake.org/files/v${CMAKE_VERSION}/cmake-${CMAKE_BUILD}.tar.gz && \
-  tar -xf cmake-${CMAKE_BUILD}.tar.gz                                            && \
-  cd cmake-${CMAKE_BUILD}                                                        && \
-  ./bootstrap --parallel=$(grep ^processor /proc/cpuinfo | wc -l)                && \
-  make -j"$(grep ^processor /proc/cpuinfo | wc -l)" install                      && \
-  rm -rf /cmake-${CMAKE_BUILD}
+  apt-get update && apt-get -y install wget
+  wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh
+  bash cmake-3.18.4-Linux-x86_64.sh --skip-license
+  rm cmake-3.18.4-Linux-x86_64.sh
 }
 
 test_body() {
