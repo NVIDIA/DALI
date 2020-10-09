09cabb146737ec4250f7e58f2bfd8577da0c3bff
Michał Szołucha <mszolucha@nvidia.com>
2020-10-09 20:39:54 +0200

    review
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- dali/operators/random/uniform.cc
+++ dali/operators/random/uniform.cc
@@ -56,8 +56,8 @@ DALI_REGISTER_OPERATOR(Uniform, Uniform, CPU);
 DALI_SCHEMA(Uniform)
   .DocStr(R"code(Produces random numbers following an uniform distribution.
 
-Both continuous and discrete uniform distributions can be defined by defining
-a continuous ``range`` or a discrete ``set`` of values, respectively.
+Both continuous and discrete uniform distributions can be defined by providing
+a continuous ``range`` or a discrete list of ``values``, respectively.
   
 )code")
   .NumInput(0)
@@ -65,12 +65,12 @@ a continuous ``range`` or a discrete ``set`` of values, respectively.
   .AddOptionalArg("range",
     R"code(Range (``[a, b)``) of produced random numbers.
 
-This argument is mutually exclusive with ``values``
+This argument is mutually exclusive with ``values``.
 )code", std::vector<float>({-1, 1}))
   .AddOptionalArg("values",
-    R"code(The discrete set of values from which the random numbers are picked.
+    R"code(The discrete list of values from which the random numbers are picked.
 
-This argument is mutually exclusive with ``range``
+This argument is mutually exclusive with ``range``.
 )code", std::vector<float>({}))
   .AddOptionalArg("shape",
     R"code(Shape of the samples.)code", std::vector<int>{1});
--- dali/test/python/test_operator_uniform.py
+++ dali/test/python/test_operator_uniform.py
@@ -57,6 +57,7 @@ def test_uniform_discrete():
     lo = -100
     hi = 100
     test_set = (hi - lo) * np.random.random_sample(10) + lo  # 10 elems from [-100, 100] range
+    test_set = test_set.astype('float32')
     with pipe:
         pipe.set_outputs(dali.fn.uniform(values=test_set.tolist(), shape=[1e6]))
     pipe.build()
@@ -65,6 +66,6 @@ def test_uniform_discrete():
     _, pv = st.kstest(rvs=possibly_uniform_distribution, cdf='uniform')
     assert pv < 1e-8, "`possibly_uniform_distribution` is not an uniform distribution"
     for val in possibly_uniform_distribution:
-        assert any(math.isclose(val, t, rel_tol=1e-6) for t in test_set), \
+        assert val in test_set, \
             "Value returned from the op is outside of requested discrete set"
 
