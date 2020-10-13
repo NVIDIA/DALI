96eacec6e2344d4b9c934500d745cb0f4abc0c45
Janusz Lisiecki <jlisiecki@nvidia.com>
2020-10-13 11:39:22 +0200

    More tests
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/test/python/test_operator_uniform.py
+++ dali/test/python/test_operator_uniform.py
@@ -53,6 +53,25 @@ def test_uniform_continuous():
             (test_range[0] < test_range[1]).all(), \
                 "Value returned from the op is outside of requested range"
 
+def test_uniform_continuous_next_after():
+    pipe = dali.pipeline.Pipeline(1, 1, 0)
+    lo = -100
+    hi = 100
+    test_range = (hi - lo) * np.random.random_sample(2) + lo  # 2 elems from [-100, 100] range
+    test_range.sort()
+    test_range = test_range.astype('float32')
+    with pipe:
+        test_range[1] = np.nextafter(test_range[0], test_range[1])
+        pipe.set_outputs(dali.fn.uniform(range=test_range.tolist(), shape=[1e6]))
+    pipe.build()
+    oo = pipe.run()
+    possibly_uniform_distribution = oo[0].as_array()[0]
+    _, pv = st.kstest(rvs=possibly_uniform_distribution, cdf='uniform')
+    assert pv < 1e-8, "`possibly_uniform_distribution` is not an uniform distribution"
+    assert (test_range[0] <= possibly_uniform_distribution).all() and \
+            (test_range[0] < test_range[1]).all(), \
+                "Value returned from the op is outside of requested range"
+
 def test_uniform_discrete():
     pipe = dali.pipeline.Pipeline(1, 1, 0)
     lo = -100
