2cdee9ac6c8723d30c60f59acfb579ee4c5c28a1
Joaquin Anton <janton@nvidia.com>
2020-10-20 12:43:36 +0200

    Add SelectMasks operator (CPU)
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/operators/segmentation/select_masks.h
@@ -0,0 +1,67 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef DALI_OPERATORS_SEGMENTATION_SELECT_MASKS_H_
+#define DALI_OPERATORS_SEGMENTATION_SELECT_MASKS_H_
+
+#include <vector>
+#include <unordered_map>
+#include "dali/core/common.h"
+#include "dali/core/span.h"
+#include "dali/core/tensor_shape.h"
+#include "dali/pipeline/operator/operator.h"
+
+namespace dali {
+
+
+class SelectMasksCPU : public Operator<CPUBackend> {
+ public:
+  explicit SelectMasksCPU(const OpSpec &spec)
+      : Operator<CPUBackend>(spec), reindex_masks_(spec.GetArgument<bool>("reindex_masks")) {}
+
+  ~SelectMasksCPU() override = default;
+  DISABLE_COPY_MOVE_ASSIGN(SelectMasksCPU);
+
+ protected:
+  bool CanInferOutputs() const override {
+    return true;
+  }
+
+  bool SetupImpl(std::vector<OutputDesc> &output_desc, const workspace_t<CPUBackend> &ws) override;
+  void RunImpl(workspace_t<CPUBackend> &ws) override;
+
+  struct MaskMeta {
+    int mask_id = -1;
+    int new_mask_id = -1;
+    int start_coord = -1;
+    int end_coord = -1;
+  };
+
+  struct SampleMeta {
+    span<const int> selected_masks;
+    std::unordered_map<int, MaskMeta> masks_meta;
+    void clear() {
+      selected_masks = {};
+      masks_meta.clear();
+    }
+  };
+  std::vector<SampleMeta> samples_meta_;
+
+  bool reindex_masks_;
+};
+
+}  // namespace dali
+
+#endif  // DALI_OPERATORS_SEGMENTATION_SELECT_MASKS_H_
