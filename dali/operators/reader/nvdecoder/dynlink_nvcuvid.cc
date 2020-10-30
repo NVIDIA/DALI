6d94300f60c9d03d8fc44a40163aa77e969ef419
Janusz Lisiecki <jlisiecki@nvidia.com>
2020-10-30 17:42:00 +0100

    Remove CUDA headers and generate stubs in runtime
    - removes files that should not be in the repo due to license
    - adds a step that generates dynlink_cuda during the build based
      on the available headers
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/operators/reader/nvdecoder/dynlink_nvcuvid.cc
+++ dali/operators/reader/nvdecoder/dynlink_nvcuvid.cc
@@ -1,16 +1,20 @@
-/*
- * Copyright 1993-2017 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
+// Copyright (c) 2017-2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
 #include <stdio.h>
+#include <mutex>
 
 #include "dali/operators/reader/nvdecoder/dynlink_nvcuvid.h"
 
