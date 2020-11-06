f9e5857b8ef10b66d2cb87cc679b78f449471e3f
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2020-11-06 11:11:39 +0100

    Make custom nvJEPG allocator return a relevant allocation status (#2438)
    - fixes lack of returning a proper allocation status in nvJPEG custom allocator. So far it always returns 0.
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/operators/decoder/nvjpeg/decoupled_api/nvjpeg_memory.cc
+++ dali/operators/decoder/nvjpeg/decoupled_api/nvjpeg_memory.cc
@@ -204,13 +204,21 @@ void DeleteAllBuffers(std::thread::id thread_id) {
 }
 
 static int DeviceNew(void **ptr, size_t size) {
+  if (size == 0) {
+    *ptr = nullptr;
+    return cudaSuccess;
+  }
   *ptr = GetBuffer(std::this_thread::get_id(), AllocType::GPU, size);
-  return 0;
+  return *ptr != nullptr ? cudaSuccess : cudaErrorMemoryAllocation;
 }
 
 static int PinnedNew(void **ptr, size_t size, unsigned int flags) {
+  if (size == 0) {
+    *ptr = nullptr;
+    return cudaSuccess;
+  }
   *ptr = GetBuffer(std::this_thread::get_id(), AllocType::Pinned, size);
-  return 0;
+  return *ptr != nullptr ? cudaSuccess : cudaErrorMemoryAllocation;
 }
 
 nvjpegDevAllocator_t GetDeviceAllocator() {
