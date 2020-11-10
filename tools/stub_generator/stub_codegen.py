7e78f5f34315ec3f49c220be904707344513f14a
Janusz Lisiecki <jlisiecki@nvidia.com>
2020-11-10 10:22:40 +0100

    Review fixes
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- tools/stub_generator/stub_codegen.py
+++ tools/stub_generator/stub_codegen.py
@@ -25,23 +25,23 @@ import clang.cindex
 
 
 def main():
-  parser = argparse.ArgumentParser(
-      description='Generate dynamic loading stubs for CUDA and HIP APIs.')
-  parser.add_argument('--unique_prefix', default="", type=str,
-                help='Unique prefix for used in the stub')
-  parser.add_argument(
-      'input', nargs='?', type=argparse.FileType('r'))
-  parser.add_argument(
-      'output', nargs='?', type=argparse.FileType('w'))
-  parser.add_argument(
-      'header', nargs='?', type=str, default=None)
-  parser.add_argument(
-      'extra_args', nargs='*', type=str, default=None)
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Generate dynamic loading stubs for CUDA and HIP APIs.')
+    parser.add_argument('--unique_prefix', default="", type=str,
+                        help='Unique prefix for used in the stub')
+    parser.add_argument(
+        'input', nargs='?', type=argparse.FileType('r'))
+    parser.add_argument(
+        'output', nargs='?', type=argparse.FileType('w'))
+    parser.add_argument(
+        'header', nargs='?', type=str, default=None)
+    parser.add_argument(
+        'extra_args', nargs='*', type=str, default=None)
+    args = parser.parse_args()
 
-  config = json.load(args.input)
+    config = json.load(args.input)
 
-  function_impl = """
+    function_impl = """
 {return_type} %s {1}NotFound({2}) {{
   return {not_found_error};
 }}
@@ -54,7 +54,7 @@ def main():
   return func_ptr({3});
 }}\n""" % ('CUDAAPI', 'CUDAAPI')
 
-  prolog = """
+    prolog = """
 typedef void *tLoadSymbol(const char *name);
 
 static tLoadSymbol *load_symbol_func;
@@ -63,57 +63,59 @@ void {0}SetSymbolLoader(tLoadSymbol *loader_func) {{
   load_symbol_func = loader_func;
 }}\n"""
 
-  index = clang.cindex.Index.create()
-  header = args.header
-  extra_args = args.extra_args
+    index = clang.cindex.Index.create()
+    header = args.header
+    extra_args = args.extra_args
 
-  translation_unit = index.parse(header, args=extra_args)
+    translation_unit = index.parse(header, args=extra_args)
 
-  for diag in translation_unit.diagnostics:
-    if diag.severity in [diag.Warning, diag.Fatal]:
-      raise Exception(str(diag))
+    for diag in translation_unit.diagnostics:
+        if diag.severity in [diag.Warning, diag.Fatal]:
+            raise Exception(str(diag))
 
-  for extra_i in config['extra_include']:
-    args.output.write('#include {}\n'.format(extra_i))
-  args.output.write(prolog.format(args.unique_prefix))
+    for extra_i in config['extra_include']:
+        args.output.write('#include {}\n'.format(extra_i))
+    args.output.write(prolog.format(args.unique_prefix))
 
-  all_definition = set()
-  all_declaration = set()
+    all_definition = set()
+    all_declaration = set()
 
-  for cursor in translation_unit.cursor.get_children():
-      if cursor.is_definition():
-        all_definition.add(cursor.spelling)
+    for cursor in translation_unit.cursor.get_children():
+        if cursor.is_definition():
+            all_definition.add(cursor.spelling)
 
-      if cursor.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-        all_declaration.add(cursor.spelling)
+        if cursor.kind == clang.cindex.CursorKind.FUNCTION_DECL:
+            all_declaration.add(cursor.spelling)
 
-  for cursor in translation_unit.cursor.get_children():
-    if cursor.kind != clang.cindex.CursorKind.FUNCTION_DECL:
-      continue
+    for cursor in translation_unit.cursor.get_children():
+        if cursor.kind != clang.cindex.CursorKind.FUNCTION_DECL:
+            continue
 
-    # make sure that we deal  only with functions with no definition
-    if cursor.spelling not in config['functions'] or cursor.spelling in all_definition or \
-       cursor.spelling not in all_declaration:
-      continue
+        # make sure that we deal  only with functions with no definition
+        if cursor.spelling not in config['functions'] or cursor.spelling in all_definition or \
+           cursor.spelling not in all_declaration:
+            continue
 
-    # make sure that we deal with every function only once
-    all_declaration.remove(cursor.spelling)
+        # make sure that we deal with every function only once
+        all_declaration.remove(cursor.spelling)
 
-    with open(cursor.location.file.name, 'r', encoding='latin-1') as file:
-      start = cursor.extent.start.offset
-      end = cursor.extent.end.offset
-      declaration = file.read()[start:end]
+        with open(cursor.location.file.name, 'r', encoding='latin-1') as file:
+            start = cursor.extent.start.offset
+            end = cursor.extent.end.offset
+            declaration = file.read()[start:end]
 
-    arg_types = [arg.type.spelling for arg in cursor.get_arguments()]
-    arg_names = [arg.spelling for arg in cursor.get_arguments()]
-    return_type = config['functions'][cursor.spelling].get('return_type', config['return_type'])
-    not_found_error = config['functions'][cursor.spelling].get('not_found_error', config['not_found_error'])
-    implementation = function_impl.format(declaration, cursor.spelling, ', '.join(arg_types),
-                                          ', '.join(arg_names), return_type=return_type,
-                                          not_found_error=not_found_error,)
+        arg_types = [arg.type.spelling for arg in cursor.get_arguments()]
+        arg_names = [arg.spelling for arg in cursor.get_arguments()]
+        return_type = config['functions'][cursor.spelling].get(
+            'return_type', config['return_type'])
+        not_found_error = config['functions'][cursor.spelling].get(
+            'not_found_error', config['not_found_error'])
+        implementation = function_impl.format(declaration, cursor.spelling, ', '.join(arg_types),
+                                              ', '.join(arg_names), return_type=return_type,
+                                              not_found_error=not_found_error,)
 
-    args.output.write(implementation)
+        args.output.write(implementation)
 
 
 if __name__ == '__main__':
-  main()
+    main()
