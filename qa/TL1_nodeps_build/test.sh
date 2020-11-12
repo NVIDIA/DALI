a3bcc547ab3da7adb4c7fd60b1db05ea5fcdda59
Janusz Lisiecki <jlisiecki@nvidia.com>
2020-11-12 11:47:49 +0100

    Fix build with all options off, relax libclang required version
    - DALI doesn't have to pin libclang version, any can be used
    - cuda.h cannot be found in the build configuration with all options
      set to off
    - fix TL1_nodeps_build test
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_nodeps_build/test.sh
+++ qa/TL1_nodeps_build/test.sh
@@ -3,10 +3,13 @@
 pip_packages=""
 
 do_once() {
-  apt-get update && apt-get -y install wget wget
-  wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh
-  bash cmake-3.18.4-Linux-x86_64.sh --skip-license --prefix=/
-  rm cmake-3.18.4-Linux-x86_64.sh
+    apt-get update && apt-get -y install wget wget
+    wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh
+    bash cmake-3.18.4-Linux-x86_64.sh --skip-license --prefix=/
+    rm cmake-3.18.4-Linux-x86_64.sh
+    # for stub generation
+    pip install clang
+    pip isntall libclang
 }
 
 test_body() {
