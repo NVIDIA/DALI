7e919105b859d7b5e3a28723ed6635866622fe3d
Janusz Lisiecki <jlisiecki@nvidia.com>
2020-11-24 18:50:09 +0100

    Fix CPU only mode in C API
    - remove stream creation for the CPU only mode in C API
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_cpu_only/test.sh
+++ qa/TL0_cpu_only/test.sh
@@ -9,6 +9,30 @@ test_body() {
   export LD_LIBRARY_PATH=""
   export PATH=${PATH/cuda/}
   nosetests --verbose test_dali_cpu_only.py
+
+  for BINNAME in \
+    "dali_core_test.bin" \
+    "dali_kernel_test.bin" \
+    "dali_test.bin" \
+    "dali_operator_test.bin"
+  do
+    for DIRNAME in \
+      "../../build/dali/python/nvidia/dali" \
+      "$(python -c 'import os; from nvidia import dali; print(os.path.dirname(dali.__file__))' 2>/dev/null || echo '')"
+    do
+        if [ -x "$DIRNAME/test/$BINNAME" ]; then
+            FULLPATH="$DIRNAME/test/$BINNAME"
+            break
+        fi
+    done
+
+    if [[ -z "$FULLPATH" ]]; then
+        echo "ERROR: $BINNAME not found"
+        exit 1
+    fi
+
+    "$FULLPATH" --gtest_filter="*CpuOnlyTest*"
+  done
 }
 
 pushd ../..
