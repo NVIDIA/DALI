22d20b9247691b005f5647f4888cd4ff50fe7aaa
Paweł <pawel.anikiel@gmail.com>
2021-01-13 17:25:07 +0100

    add random tests
    Signed-off-by: Paweł <pawel.anikiel@gmail.com>

--- dali/test/python/test_operator_gridmask.py
+++ dali/test/python/test_operator_gridmask.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -33,50 +33,81 @@ def get_pipeline(batch_size, tile, ratio, angle):
     pipe.set_outputs(grided, decoded)
   return pipe
 
-def get_mask(w, h, tile, ratio, angle):
-  black = round(tile * ratio)
-  diag = math.sqrt(w**2 + h**2)
+def get_random_pipeline(batch_size):
+  pipe = Pipeline(batch_size, 4, None)
+  with pipe:
+    input, _ = fn.file_reader(file_root=img_dir)
+    decoded = fn.image_decoder(input, device='cpu', output_type=types.RGB)
+    tile = fn.cast(fn.uniform(range=(50, 200), shape=[1]), dtype=types.INT32)
+    ratio = fn.uniform(range=(0.3, 0.7), shape=[1])
+    angle = fn.uniform(range=(-math.pi, math.pi), shape=[1])
+    grided = fn.grid_mask(decoded, device='cpu', tile=tile, ratio=ratio, angle=angle)
+    pipe.set_outputs(grided, decoded, tile, ratio, angle)
+  return pipe
+
+def get_mask(w, h, tile, ratio, angle, d):
+  black = int(round(tile * ratio))
+  diag = math.sqrt(w**2 + h**2) + 1
   nrep = int(math.ceil(diag / tile))
 
-  angle_deg = angle * 180 / math.pi
-  R = cv2.getRotationMatrix2D((tile * nrep, tile * nrep), angle_deg, 1)
-  R[0,2] -= tile * nrep
-  R[1,2] -= tile * nrep
-
   mask = np.ones((tile, tile))
-  mask[0:black,0:black] = 0
+  mask[:black, :black] = 0
   mask = np.tile(mask, (2 * nrep, 2 * nrep))
-  mask = cv2.warpAffine(mask, R, (w, h))
 
-  return mask
+  p = tile * nrep
+  c = math.cos(angle)
+  s = math.sin(angle)
+  R = np.array([
+    [ c, s, -p * c - p * s + p],
+    [-s, c, -p * c + p * s + p]])
+  mask = cv2.warpAffine(mask, R, (2 * p, 2 * p))
+
+  # shrink or expand the rotated squares
+  ker = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+  if d == 1:
+    mask = cv2.erode(mask, ker)
+  elif d == -1:
+    mask = cv2.dilate(mask, ker)
+
+  return mask[p:p+h, p:p+w]
 
 
 def check(result, input, tile, ratio, angle):
   result = np.uint8(result)
   input = np.uint8(input)
-  mask = get_mask(result.shape[1], result.shape[0], tile, ratio, angle)
-  ker = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+  w = result.shape[1]
+  h = result.shape[0]
 
   # inside of squares should be black
-  mask2 = np.uint8(1 - cv2.dilate(mask, ker))
-  result2 = cv2.bitwise_and(result, result, mask=mask2)
+  mask = np.uint8(1 - get_mask(w, h, tile, ratio, angle, -1))
+  result2 = cv2.bitwise_and(result, result, mask=mask)
   assert not np.any(result2)
 
   # outside of squares should be same as input
-  mask2 = np.uint8(cv2.erode(mask, ker))
-  result2 = cv2.bitwise_and(result, result, mask=mask2)
-  input2 = cv2.bitwise_and(input, input, mask=mask2)
+  mask = np.uint8(get_mask(w, h, tile, ratio, angle, 1))
+  result2 = cv2.bitwise_and(result, result, mask=mask)
+  input2 = cv2.bitwise_and(input, input, mask=mask)
   assert np.all(result2 == input2)
 
-def check_grid_mask(batch_size, tile, ratio, angle):
-  pipe = get_pipeline(batch_size, tile, ratio, angle)
-  pipe.build()
-  results, inputs = pipe.run()
-  for i in range(batch_size):
-    check(results[i], inputs[i], tile, ratio, angle)
-
 def test_cpu_vs_cv():
+  batch_size = 4
   for tile in [40, 100, 200]:
     for ratio in [0.2, 0.5, 0.8]:
       for angle in [0.0, 0.34, -0.62]:
-        yield check_grid_mask, 4, tile, ratio, angle
+        pipe = get_pipeline(batch_size, tile, ratio, angle)
+        pipe.build()
+        results, inputs = pipe.run()
+        for i in range(batch_size):
+          yield check, results[i], inputs[i], tile, ratio, angle
+
+def test_cpu_vs_cv_random():
+  batch_size = 4
+  pipe = get_random_pipeline(batch_size)
+  pipe.build()
+  for _ in range(16):
+    results, inputs, tiles, ratios, angles = pipe.run()
+    for i in range(batch_size):
+      tile = np.int32(tiles[i])[0]
+      ratio = np.float32(ratios[i])[0]
+      angle = np.float32(angles[i])[0]
+      yield check, results[i], inputs[i], tile, ratio, angle
