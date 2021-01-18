9becd973ce1e165db90f426fed1eb1a094cae002
Albert Wolant <44801854+awolant@users.noreply.github.com>
2021-01-18 17:59:45 +0100

    TF dataset tests rework (#2539)
    * TF dataset tests rework for multi GPU dataset
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/test/python/test_dali_tf_dataset.py
+++ dali/test/python/test_dali_tf_dataset.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,139 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import nvidia.dali as dali
 import tensorflow as tf
-from tensorflow.python.client import device_lib
-import nvidia.dali.plugin.tf as dali_tf
-import os
 import numpy as np
 from nvidia.dali.pipeline import Pipeline
-import nvidia.dali.ops as ops
-import nvidia.dali.types as types
-from distutils.version import StrictVersion
-from test_utils_tensorflow import *
-
-from nose import SkipTest
+import nvidia.dali.fn as fn
+import nvidia.dali.plugin.tf as dali_tf
 from nose.tools import raises
 
-try:
-    tf.compat.v1.disable_eager_execution()
-except:
-    pass
-
-test_data_root = os.environ['DALI_EXTRA_PATH']
-file_root = os.path.join(test_data_root, 'db', 'coco_dummy', 'images')
-annotations_file = os.path.join(test_data_root, 'db', 'coco_dummy', 'instances.json')
-
-
-class TestPipeline(Pipeline):
-    def __init__(self, batch_size, num_threads, device, device_id = 0, shard_id = 0, num_shards = 1, seed = 0):
-        super(TestPipeline, self).__init__(batch_size, num_threads, device_id, seed)
-        self.device = device
-        self.input = ops.COCOReader(
-            file_root = file_root,
-            annotations_file = annotations_file,
-            shard_id = shard_id,
-            num_shards = num_shards,
-            ratio=False,
-            image_ids=True)
-        self.decode = ops.ImageDecoder(
-            device = 'mixed' if device == 'gpu' else 'cpu',
-            output_type = types.RGB)
-        self.resize = ops.Resize(
-            device = device,
-            resize_x = 224,
-            resize_y = 224,
-            interp_type = types.INTERP_LINEAR)
-        self.cmn = ops.CropMirrorNormalize(
-            device = device,
-            dtype = types.FLOAT,
-            mean = [128., 128., 128.],
-            std = [1., 1., 1.])
-        self.cast = ops.Cast(
-            device = device,
-            dtype = types.INT16)
-        self.reshape = ops.Reshape(device = device, shape = [1, 1])
-
-
-    def define_graph(self):
-        inputs, _, _, im_ids = self.input(name="Reader")
-        images = self.decode(inputs)
-        images = self.resize(images)
-        output = self.cmn(
-            images)
-        if self.device == 'gpu':
-            im_ids = im_ids.gpu()
-        im_ids_16 = self.cast(im_ids)
-        reshaped = self.reshape(im_ids)
-
-        return (output, reshaped, im_ids_16)
-
-
-def setup():
-    skip_for_incompatible_tf()
-
-
-def _test_tf_dataset(device, device_id = 0):
-    batch_size = 12
-    num_threads = 4
-    iterations = 10
-
-    dataset_pipeline = TestPipeline(batch_size, num_threads, device, device_id)
-    shapes = (
-        (batch_size, 3, 224, 224),
-        (batch_size, 1, 1),
-        (batch_size, 1))
-    dtypes = (
-        tf.float32,
-        tf.int32,
-        tf.int16)
-
-    dataset_results = []
-    with tf.device('/{0}:{1}'.format(device, device_id)):
-        daliset = dali_tf.DALIDataset(
-            pipeline=dataset_pipeline,
-            batch_size=batch_size,
-            output_shapes=shapes,
-            dtypes=dtypes,
-            num_threads=num_threads,
-            device_id=device_id)
-
-        iterator = tf.compat.v1.data.make_initializable_iterator(daliset)
-        next_element = iterator.get_next()
-
-    with tf.compat.v1.Session() as sess:
-        sess.run([tf.compat.v1.global_variables_initializer(), iterator.initializer])
-        for _ in range(iterations):
-            dataset_results.append(sess.run(next_element))
-
-    standalone_pipeline = TestPipeline(batch_size, num_threads, device, device_id = 0)
-    standalone_pipeline.build()
-    standalone_results = []
-    for _ in range(iterations):
-        if device == 'gpu':
-            standalone_results.append(
-                tuple(result.as_cpu().as_array() for result in standalone_pipeline.run()))
-        else:
-            standalone_results.append(
-                tuple(result.as_array() for result in standalone_pipeline.run()))
-    for dataset_result, standalone_result in zip(dataset_results, standalone_results):
-        for dataset_out, standalone_out in zip(dataset_result, standalone_result):
-            assert np.array_equal(dataset_out, standalone_out)
-
-
-
-def test_tf_dataset_gpu():
-    _test_tf_dataset('gpu')
-
-
-# This test should be private (name starts with _) as it is called separately in L1
-def _test_tf_dataset_other_gpu():
-    _test_tf_dataset('gpu', 1)
-
-
-def test_tf_dataset_cpu():
-    _test_tf_dataset('cpu')
+from test_utils_tensorflow import get_pipeline
 
 
 @raises(Exception)
@@ -152,7 +27,7 @@ def test_different_num_shapes_dtypes():
     batch_size = 12
     num_threads = 4
 
-    dataset_pipeline = TestPipeline(batch_size, num_threads, 'cpu')
+    dataset_pipeline = get_pipeline(batch_size, num_threads, 'cpu')
     shapes = (
         (batch_size, 3, 224, 224),
         (batch_size, 1, 1),
@@ -170,96 +45,21 @@ def test_different_num_shapes_dtypes():
             num_threads=num_threads)
 
 
-def _test_tf_dataset_multigpu():
-    num_devices = num_available_gpus()
-    batch_size = 8
-    num_threads = 4
-
-    shapes = (
-        (batch_size, 3, 224, 224),
-        (batch_size, 1, 1),
-        (batch_size, 1))
-    dtypes = (
-        tf.float32,
-        tf.int32,
-        tf.int16)
-
-    dataset_results = []
-    initializers = [tf.compat.v1.global_variables_initializer()]
-    ops_to_run = []
-
-    for device_id in range(num_devices):
-        with tf.device('/gpu:{0}'.format(device_id)):
-            dataset_pipeline = TestPipeline(batch_size, num_threads, 'gpu', device_id, device_id, num_devices)
-            daliset = dali_tf.DALIDataset(
-                pipeline=dataset_pipeline,
-                batch_size=batch_size,
-                output_shapes=shapes,
-                output_dtypes=dtypes,
-                num_threads=num_threads,
-                device_id=device_id)
-
-            iterator = tf.compat.v1.data.make_initializable_iterator(daliset)
-            initializers.append(iterator.initializer)
-
-            ops_to_run.append(iterator.get_next())
-
-
-    standalone_pipeline = TestPipeline(
-        batch_size, num_threads, device = 'gpu', device_id = 0)
-    standalone_pipeline.build()
-
-    dataset_size = standalone_pipeline.epoch_size("Reader")
-    iterations = dataset_size // batch_size
-
-    with tf.compat.v1.Session() as sess:
-        sess.run(initializers)
-        for _ in range(iterations):
-            dataset_results.append(sess.run(ops_to_run))
-
-    standalone_results = []
-    for _ in range(iterations):
-        standalone_results.append(
-            tuple(result.as_cpu().as_array() for result in standalone_pipeline.run()))
-
-    assert len(dataset_results) == iterations
-    for it in range(iterations):
-        assert len(dataset_results[it]) == num_devices
-        for device_id in range(num_devices):
-            batch_id = iterations - ((it + device_id * (iterations // num_devices)) % iterations) - 1
-            it_id = iterations - it - 1
-            assert np.array_equal(
-                standalone_results[it_id][0],
-                dataset_results[batch_id][device_id][0])
-            assert np.array_equal(
-                standalone_results[it_id][1],
-                dataset_results[batch_id][device_id][1])
-            assert np.array_equal(
-                standalone_results[it_id][2],
-                dataset_results[batch_id][device_id][2])
-
-
-class PythonOperatorPipeline(Pipeline):
-    def __init__(self):
-        super(PythonOperatorPipeline, self).__init__(1, 1, 0, 0)
-        self.python_op = ops.PythonFunction(function=lambda: np.zeros((3, 3, 3)))
-
-    def define_graph(self):
-        return self.python_op()
-
-
 @raises(RuntimeError)
-def test_python_operator_error():
-    dataset_pipeline = PythonOperatorPipeline()
+def test_python_operator_not_allowed_in_tf_dataset_error():
+    pipeline = Pipeline(1, 1, 0, 0)
+    with pipeline:
+        output = fn.python_function(function=lambda: np.zeros((3, 3, 3)))
+        pipeline.set_outputs(output)
+
     shapes = ((1, 3, 3, 3))
     dtypes = (tf.float32)
 
     with tf.device('/cpu:0'):
         daliset = dali_tf.DALIDataset(
-            pipeline=dataset_pipeline,
+            pipeline=pipeline,
             batch_size=1,
             output_shapes=shapes,
             output_dtypes=dtypes,
             num_threads=1,
             device_id=0)
-
---
+++ dali/test/python/test_dali_tf_dataset_mnist_graph.py
@@ -0,0 +1,156 @@
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from test_dali_tf_dataset_mnist import *
+from nose.tools import raises
+
+tf.compat.v1.disable_eager_execution()
+
+def test_keras_single_gpu():
+    run_keras_single_device('gpu', 0)
+
+
+def test_keras_single_other_gpu():
+    run_keras_single_device('gpu', 1)
+
+
+def test_keras_single_cpu():
+    run_keras_single_device('cpu', 0)
+
+
+@raises(Exception)
+def test_keras_wrong_placement_gpu():
+    with tf.device('cpu:0'):
+        model = keras_model()
+        train_dataset = get_dataset('gpu', 0)
+
+    model.fit(
+        train_dataset,
+        epochs=EPOCHS,
+        steps_per_epoch=ITERATIONS)
+
+
+@raises(Exception)
+def test_keras_wrong_placement_cpu():
+    with tf.device('gpu:0'):
+        model = keras_model()
+        train_dataset = get_dataset('cpu', 0)
+
+    model.fit(
+        train_dataset,
+        epochs=EPOCHS,
+        steps_per_epoch=ITERATIONS)
+
+
+@with_setup(tf.compat.v1.reset_default_graph)
+def test_graph_single_gpu():
+    run_graph_single_device('gpu', 0)
+
+
+@with_setup(tf.compat.v1.reset_default_graph)
+def test_graph_single_cpu():
+    run_graph_single_device('cpu', 0)
+
+
+@with_setup(tf.compat.v1.reset_default_graph)
+def test_graph_single_other_gpu():
+    run_graph_single_device('gpu', 1)
+
+
+# This function is copied form: https://github.com/tensorflow/models/blob/master/tutorials/image/cifar10/cifar10_multi_gpu_train.py#L102
+def average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        # Note that each grad_and_vars looks like the following:
+        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+        grads = []
+        for g, _ in grad_and_vars:
+            # Add 0 dimension to the gradients to represent the tower.
+            expanded_g = tf_v1.expand_dims(g, 0)
+
+            # Append on a 'tower' dimension which we will average over below.
+            grads.append(expanded_g)
+
+        # Average over the 'tower' dimension.
+        grad = tf_v1.concat(grads, 0)
+        grad = tf_v1.reduce_mean(grad, 0)
+
+        # Keep in mind that the Variables are redundant because they are shared
+        # across towers. So .. we will just return the first tower's pointer to
+        # the Variable.
+        v = grad_and_vars[0][1]
+        grad_and_var = (grad, v)
+        average_grads.append(grad_and_var)
+    return average_grads
+
+
+@with_setup(tf_v1.reset_default_graph)
+def test_graph_multi_gpu():
+    iterator_initializers = []
+
+    with tf.device('/cpu:0'):
+        tower_grads = []
+
+        for i in range(num_available_gpus()):
+            with tf.device('/gpu:{}'.format(i)):
+                daliset = get_dataset(
+                    'gpu', i, i, num_available_gpus())
+
+                iterator = tf_v1.data.make_initializable_iterator(daliset)
+                iterator_initializers.append(iterator.initializer)
+                images, labels = iterator.get_next()
+
+                images = tf_v1.reshape(
+                    images, [BATCH_SIZE, IMAGE_SIZE*IMAGE_SIZE])
+                labels = tf_v1.reshape(
+                    tf_v1.one_hot(labels, NUM_CLASSES),
+                    [BATCH_SIZE, NUM_CLASSES])
+
+                logits_train = graph_model(
+                    images, reuse=(i != 0), is_training=True)
+                logits_test = graph_model(
+                    images, reuse=True, is_training=False)
+
+                loss_op = tf_v1.reduce_mean(tf_v1.nn.softmax_cross_entropy_with_logits(
+                    logits=logits_train, labels=labels))
+                optimizer = tf_v1.train.AdamOptimizer()
+                grads = optimizer.compute_gradients(loss_op)
+
+                if i == 0:
+                    correct_pred = tf_v1.equal(
+                        tf_v1.argmax(logits_test, 1), tf_v1.argmax(labels, 1))
+                    accuracy = tf_v1.reduce_mean(
+                        tf_v1.cast(correct_pred, tf_v1.float32))
+
+                tower_grads.append(grads)
+
+        tower_grads = average_gradients(tower_grads)
+        train_step = optimizer.apply_gradients(tower_grads)
+
+    train_graph(iterator_initializers, train_step, accuracy)
+
+
+@with_setup(clear_checkpoints, clear_checkpoints)
+def test_estimators_single_gpu():
+    run_estimators_single_device('gpu', 0)
+
+
+@with_setup(clear_checkpoints, clear_checkpoints)
+def test_estimators_single_other_gpu():
+    run_estimators_single_device('gpu', 1)
+
+
+@with_setup(clear_checkpoints, clear_checkpoints)
+def test_estimators_single_cpu():
+    run_estimators_single_device('cpu', 0)
