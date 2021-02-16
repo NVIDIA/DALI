66c3a0e0f3685333dae752caaba2433c411ae806
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-02-16 14:11:53 +0100

    Fix `.gpu` usage detection and error for CPU only pipelines
    - when the user asks to transfer the data node to GPU by calling
      the `.gpu` method there was no proper error handling in the
      graph building stage for CPU only pipeline. This change adds
      proper validation for such a case
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/test/python/test_dali_cpu_only.py
+++ dali/test/python/test_dali_cpu_only.py
@@ -45,6 +45,17 @@ def get_data():
     out = [np.random.randint(0, 255, size = test_data_shape, dtype = np.uint8) for _ in range(batch_size)]
     return out
 
+def test_move_to_device():
+    test_data_shape = [1, 3, 0, 4]
+    def get_data():
+        out = [np.empty(test_data_shape, dtype = np.uint8) for _ in range(batch_size)]
+        return out
+
+    pipe = Pipeline(batch_size=batch_size, num_threads=3, device_id=None)
+    outs = fn.external_source(source = get_data)
+    pipe.set_outputs(outs.gpu())
+    assert_raises(RuntimeError, pipe.build)
+
 def check_bad_device(device_id):
     test_data_shape = [1, 3, 0, 4]
     def get_data():
