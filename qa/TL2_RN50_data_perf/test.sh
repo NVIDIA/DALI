1d9efe5dafa2f661f5dd351e8b284dbbe5824969
Krzysztof Lecki <klecki@nvidia.com>
2021-03-03 15:54:49 +0100

    Make tests shorter a bit
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- qa/TL2_RN50_data_perf/test.sh
+++ qa/TL2_RN50_data_perf/test.sh
@@ -15,9 +15,9 @@ test_body() {
     python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2 --fp16 --nhwc
     python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 16 --workers 3 --prefetch 11 --fp16 --nhwc
     # Paralell ExternalSource:
-    python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2 --worker_init fork --benchmark_iters 1500 --gpus ${NUM_GPUS} --test_pipes parallel
-    python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2 --worker_init spawn --benchmark_iters 1500 --gpus ${NUM_GPUS} --test_pipes parallel
-    python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2  --benchmark_iters 1500 --gpus ${NUM_GPUS} --test_pipes file_reader
+    python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2 --worker_init fork --benchmark_iters 1000 --gpus ${NUM_GPUS} --test_pipes parallel
+    python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2 --worker_init spawn --benchmark_iters 1000 --gpus ${NUM_GPUS} --test_pipes parallel
+    python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2  --benchmark_iters 1000 --gpus ${NUM_GPUS} --test_pipes file_reader
     python ./test_RN50_external_source_parallel_data.py /data/imagenet/train-jpeg/ --workers 6 --py_workers 6 --epochs 3 --batch_size 256 --reader_queue_depth 2 --benchmark_iters 500 --gpus ${NUM_GPUS} --test_pipes scalar
 }
 
