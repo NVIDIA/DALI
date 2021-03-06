3a9c6a8cb2ae3c142260d9a6c4f1b71ead7e5b00
Albert Wolant <awolant@nvidia.com>
2021-03-06 11:39:37 +0100

    Disable extra GPUs in Paddle SSD test
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL1_paddle_ssd/test.sh
+++ qa/TL1_paddle_ssd/test.sh
@@ -5,6 +5,9 @@ pip_packages="paddlepaddle-gpu"
 target_dir=./docs/examples/use_cases/paddle/ssd/
 
 test_body() {
+    # This test is incompatible with multi-gpu environments.
+    export CUDA_VISIBLE_DEVICES=0
+    
     python train.py --check-loss-steps=300 -b 8 /data/coco/coco-2017/coco2017/
 }
 
