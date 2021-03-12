0cd0f1a464781b3bc0592331d4106450e91186fc
Rafal Maj <rmaj@nvidia.com>
2021-03-12 10:47:02 +0100

    src_dims arg added to reshape
    Signed-off-by: Rafal Maj <rmaj@nvidia.com>

--- dali/operators/generic/reshape.h
+++ dali/operators/generic/reshape.h
@@ -72,7 +72,10 @@ class Reshape : public Operator<Backend> {
   std::vector<float> rel_uniform_shape_;
   bool use_layout_ = false;
   bool use_rel_shape_ = false;
+<<<<<<< HEAD
   bool use_src_dims_ = false;
+=======
+>>>>>>> 32f9b2aa... src_dims arg added to reshape
   int wildcard_dim_ = -1;
   DALIDataType output_type_id_ = DALI_NO_TYPE;
 
