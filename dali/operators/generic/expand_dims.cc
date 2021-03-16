c5d5d12060c4c12255f1c199f4e9320223fe0743
Rafal Maj <rmaj@nvidia.com>
2021-03-16 11:29:44 +0100

    Add documentation
    Signed-off-by: Rafal Maj <rmaj@nvidia.com>

--- dali/operators/generic/expand_dims.cc
+++ dali/operators/generic/expand_dims.cc
@@ -26,16 +26,21 @@
 namespace dali {
 
 DALI_SCHEMA(ExpandDims)
-  .DocStr("Insert new dimension[s] of extent 1 and inserts new entries in "
-    "the layout (new_axis_names) at these indices in the layout.") // TODO CAŁA SCHEMA
-  .NumInput(1, 1)
+  .DocStr(R"code(Insert new dimension[s] of extent 1 and inserts new entries in "
+    "the layout (new_axis_names) at these indices in the layout.)code")
+  .NumInput(1, 2)
   .NumOutput(1)
   .InputDox(0, "data", "TensorList", "Data to be expanded")
+  .InputDox(1, "shape_input", "1D TensorList of integers", "Same as ``shape`` keyword argument")
   .PassThrough({{0, 0}})
   .AllowSequences()
   .SupportVolumetric()
-  .AddOptionalArg<int>("axes", "", std::vector<int>(), true)
-  .AddOptionalArg("new_axis_names", "", TensorLayout(""));
+  .AddOptionalArg<int>("axes", R"code(Indices where to put new dimensions of size 1.)code",
+    std::vector<int>(), true)
+  .AddOptionalArg("new_axis_names", R"code(Names of new dimensions in data layout.
+  
+Size of ``new_axis_names`` should be equal to ``axe``s size. 
+If argument won't be provided new dimensions will have layout '?')code", TensorLayout(""));
 
 template <typename Backend>
 ExpandDims<Backend>::ExpandDims(const OpSpec &spec)
@@ -82,23 +87,23 @@ void ExpandDims<Backend>::GenerateSrcDims(const Workspace &ws) {
     make_string("Layout for data has size ",
     in_layout.size(), " but data has ", ndim, " dimensions."));
 
-  std::string out_layout;
+  TensorLayout out_layout;
   size_t axes_ind = 0;
   this->src_dims_.clear();
   for (int d = 0; d < ndim; d++) {
     if (axes_[axes_ind] == d) {
       this->src_dims_.push_back(-1);
-      out_layout.push_back(new_axis_names_.empty() ? '?' : new_axis_names_[axes_ind]);
+      out_layout += new_axis_names_.empty() ? '?' : new_axis_names_[axes_ind];
       axes_ind++;
     }
-
-    out_layout.push_back(in_layout.empty() ? '?' : in_layout[d]);
+    out_layout += in_layout.empty() ? '?' : in_layout[d];
     this->src_dims_.push_back(d);
   }
+
   while (axes_ind < axes_.size()) {
     DALI_ENFORCE(axes_[axes_ind] <= ndim, make_string("ERROR"));
     this->src_dims_.push_back(-1);
-    out_layout.push_back(new_axis_names_.empty() ? '?' : new_axis_names_[axes_ind]);
+    out_layout += new_axis_names_.empty() ? '?' : new_axis_names_[axes_ind];
     ndim++;
     axes_ind++;
   }
