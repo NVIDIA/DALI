7d1d8c7b542a6dd3642c9225ace4b36148e85227
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-03-18 10:21:16 +0100

    To remove
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/setup_dali_extra.sh
+++ qa/setup_dali_extra.sh
@@ -2,11 +2,11 @@
 
 # Fetch test data
 export DALI_EXTRA_PATH=${DALI_EXTRA_PATH:-/opt/dali_extra}
-export DALI_EXTRA_URL=${DALI_EXTRA_URL:-"https://github.com/NVIDIA/DALI_extra.git"}
+export DALI_EXTRA_URL=https://github.com/JanuszL/DALI_extra
 
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
 DALI_EXTRA_VERSION_PATH="${DIR}/../DALI_EXTRA_VERSION"
-read -r DALI_EXTRA_VERSION < ${DALI_EXTRA_VERSION_PATH}
+DALI_EXTRA_VERSION=add_more_sampling_samples
 echo "Using DALI_EXTRA_VERSION = ${DALI_EXTRA_VERSION}"
 if [ ! -d "$DALI_EXTRA_PATH" ] ; then
     git clone "$DALI_EXTRA_URL" "$DALI_EXTRA_PATH"
