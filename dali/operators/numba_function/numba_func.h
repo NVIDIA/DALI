3c949aef3b81744f53f9106b566f93f145b667b3
Rafal Maj <rmaj@nvidia.com>
2021-03-18 08:52:20 +0100

    First version
    Signed-off-by: Rafal Maj <rmaj@nvidia.com>

---
+++ dali/operators/numba_function/numba_func.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_OPERATORS_GENERIC_NUMBA_FUNC_H_
+#define DALI_OPERATORS_GENERIC_NUMBA_FUNC_H_
+
+#include <string>
+#include <vector>
+
+#include "dali/core/tensor_view.h"
+#include "dali/pipeline/operator/operator.h"
+
+namespace dali {
+
+template <typename Backend>
+class NumbaFunc : public Operator<Backend> {
+ public:
+  using Base = Operator<Backend>;
+  using Workspace = workspace_t<Backend>;
+
+  explicit NumbaFunc(const OpSpec &spec_);
+
+protected:
+  bool CanInferOutputs() const override {
+    return true;
+  }
+
+  bool SetupImpl(std::vector<OutputDesc> &output_desc, const Workspace &ws) override {
+    const auto &in = ws.template InputRef<Backend>(0);
+    output_desc.resize(1);
+    output_desc[0] = {in.shape(), in.type()};
+    return true;
+  }
+
+  void RunImpl(Workspace &ws) override;
+};
+
+}  // namespace dali
+
+#endif  // DALI_OPERATORS_GENERIC_NUMBA_FUNC_H_
