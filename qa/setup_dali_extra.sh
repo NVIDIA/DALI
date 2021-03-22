5786d490d2669bb670e5c2d009c0e7cea76c4902
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-03-22 12:06:14 +0100

    Update DALI_EXTRA
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/setup_dali_extra.sh
+++ qa/setup_dali_extra.sh
@@ -2,11 +2,11 @@
 
 # Fetch test data
 export DALI_EXTRA_PATH=${DALI_EXTRA_PATH:-/opt/dali_extra}
-export DALI_EXTRA_URL=https://github.com/JanuszL/DALI_extra
+export DALI_EXTRA_URL=${DALI_EXTRA_URL:-"https://github.com/NVIDIA/DALI_extra.git"}
 
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
 DALI_EXTRA_VERSION_PATH="${DIR}/../DALI_EXTRA_VERSION"
-DALI_EXTRA_VERSION=add_more_sampling_samples
+read -r DALI_EXTRA_VERSION < ${DALI_EXTRA_VERSION_PATH}
 echo "Using DALI_EXTRA_VERSION = ${DALI_EXTRA_VERSION}"
 if [ ! -d "$DALI_EXTRA_PATH" ] ; then
     git clone "$DALI_EXTRA_URL" "$DALI_EXTRA_PATH"
