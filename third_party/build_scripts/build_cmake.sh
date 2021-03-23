08d98cc948767c3a5aca7f1dcd993a4ac36fba07
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-03-23 10:13:56 +0100

    Move build scripts
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- third_party/build_scripts/build_cmake.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-# CMake
-pushd third_party/CMake
-./bootstrap --parallel=$(grep ^processor /proc/cpuinfo | wc -l)
-make -j"$(grep ^processor /proc/cpuinfo | wc -l)"
-make install
-popd
--- third_party/build_scripts/build_lmdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-# LMDB
-pushd third_party/lmdb/libraries/liblmdb/
-patch -p3 < ${ROOT_DIR}/patches/Makefile-lmdb.patch
-    CFLAGS="-fPIC" CXXFLAGS="-fPIC" CC=${CC_COMP} CXX=${CXX_COMP} prefix=${INSTALL_PREFIX} \
-make -j"$(grep ^processor /proc/cpuinfo | wc -l)"
-make install
-popd
