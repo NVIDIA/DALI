224ca111d3707af442905970c3a2fa0fae8e88d6
Joaquin Anton <janton@nvidia.com>
2021-03-30 19:19:33 +0200

    Add JpegCompressionDistortion CPU and GPU operators
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/operators/image/distortion/jpeg_compression_distortion_op_cpu.cc
@@ -0,0 +1,83 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include "dali/operators/image/distortion/jpeg_compression_distortion_op.h"
+
+namespace dali {
+
+DALI_SCHEMA(JpegCompressionDistortion)
+    .DocStr(R"code(Produces JPEG-like distortion in RGB images.
+
+The level of degradation of the image can be controlled with the ``quality`` argument,
+)code")
+    .NumInput(1)
+    .InputLayout(0, "HWC")
+    .NumOutput(1)
+    .AddOptionalArg("quality",
+        R"code(JPEG compression quality from 1 (lowest quality) to 99 (highest quality).
+
+Any values outside the range 1-99 will be clamped.)code",
+                    95, true);
+
+class JpegCompressionDistortionCPU : public JpegCompressionDistortion<CPUBackend> {
+ public:
+  explicit JpegCompressionDistortionCPU(const OpSpec &spec) : JpegCompressionDistortion(spec) {}
+
+  using Operator<CPUBackend>::RunImpl;
+  ~JpegCompressionDistortionCPU() override = default;
+  DISABLE_COPY_MOVE_ASSIGN(JpegCompressionDistortionCPU);
+
+ protected:
+  void RunImpl(workspace_t<CPUBackend> &ws) override;
+
+ private:
+  struct ThreadCtx {
+    std::vector<uint8_t> encoded;
+    cv::Mat tmp;
+  };
+  std::vector<ThreadCtx> thread_ctx_;
+};
+
+void JpegCompressionDistortionCPU::RunImpl(workspace_t<CPUBackend> &ws) {
+  const auto &input = ws.InputRef<CPUBackend>(0);
+  auto &output = ws.OutputRef<CPUBackend>(0);
+  auto in_shape = input.shape();
+  int nsamples = input.size();
+  auto& thread_pool = ws.GetThreadPool();
+  auto in_view = view<const uint8_t>(input);
+  auto out_view = view<uint8_t>(output);
+
+  thread_ctx_.resize(thread_pool.NumThreads());
+
+  for (int sample_idx = 0; sample_idx < nsamples; sample_idx++) {
+    thread_pool.AddWork(
+      [&, sample_idx, quality = quality_arg_[sample_idx].data[0]](int thread_id) {
+        auto &ctx = thread_ctx_[thread_id];
+        auto sh = in_shape.tensor_shape_span(sample_idx);
+        cv::Mat in_mat(sh[0], sh[1], CV_8UC3, (void*) in_view[sample_idx].data);  // NOLINT
+        cv::Mat out_mat(sh[0], sh[1], CV_8UC3, (void*) out_view[sample_idx].data);  // NOLINT
+        cv::cvtColor(in_mat, ctx.tmp, cv::COLOR_RGB2BGR);
+        cv::imencode(".jpg", ctx.tmp, ctx.encoded, {cv::IMWRITE_JPEG_QUALITY, quality});
+        cv::imdecode(ctx.encoded, cv::IMREAD_COLOR, &ctx.tmp);
+        cv::cvtColor(ctx.tmp, out_mat, cv::COLOR_BGR2RGB);
+      }, in_shape.tensor_size(sample_idx));
+  }
+  thread_pool.RunAll();
+}
+
+DALI_REGISTER_OPERATOR(JpegCompressionDistortion, JpegCompressionDistortionCPU, CPU);
+
+}  // namespace dali
---
+++ dali/operators/image/distortion/jpeg_compression_distortion_op_gpu.cu
@@ -0,0 +1,73 @@
+// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+#include "dali/operators/image/distortion/jpeg_compression_distortion_op.h"
+#include "dali/kernels/imgproc/jpeg/jpeg_distortion_gpu_kernel.h"
+#include "dali/kernels/kernel_manager.h"
+
+namespace dali {
+
+class JpegCompressionDistortionGPU : public JpegCompressionDistortion<GPUBackend> {
+ public:
+  explicit JpegCompressionDistortionGPU(const OpSpec &spec)
+    : JpegCompressionDistortion(spec) {
+      kmgr_.Initialize<JpegDistortionKernel>();
+      kmgr_.Resize<JpegDistortionKernel>(1, 1);
+  }
+
+  using Operator<GPUBackend>::RunImpl;
+  ~JpegCompressionDistortionGPU() override = default;
+  DISABLE_COPY_MOVE_ASSIGN(JpegCompressionDistortionGPU);
+
+ protected:
+  bool SetupImpl(std::vector<OutputDesc> &output_desc,
+                 const workspace_t<GPUBackend> &ws) override;
+  void RunImpl(workspace_t<GPUBackend> &ws) override;
+
+ private:
+  using JpegDistortionKernel = kernels::jpeg::JpegCompressionDistortionGPU<true, true>;
+  kernels::KernelManager kmgr_;
+  std::vector<int> quality_;
+};
+
+bool JpegCompressionDistortionGPU::SetupImpl(std::vector<OutputDesc> &output_desc,
+                                             const workspace_t<GPUBackend> &ws) {
+  const auto &input = ws.InputRef<GPUBackend>(0);
+  auto in_view = view<const uint8_t, 3>(input);
+  kernels::KernelContext ctx;
+  auto req = kmgr_.Setup<JpegDistortionKernel>(0, ctx, in_view.shape);
+  output_desc = {{req.output_shapes[0], input.type()}};
+  return true;
+}
+
+void JpegCompressionDistortionGPU::RunImpl(workspace_t<GPUBackend> &ws) {
+  const auto &input = ws.InputRef<GPUBackend>(0);
+  auto &output = ws.OutputRef<GPUBackend>(0);
+  auto in_view = view<const uint8_t, 3>(input);
+  auto out_view = view<uint8_t, 3>(output);
+  int nsamples = in_view.shape.size();
+
+  quality_.resize(nsamples);
+  for (int i = 0; i < nsamples; i++) {
+    quality_[i] = quality_arg_[i].data[0];
+  }
+  kernels::KernelContext ctx;
+  ctx.gpu.stream = ws.stream();
+  kmgr_.Run<JpegDistortionKernel>(0, 0, ctx, out_view, in_view, make_cspan(quality_));
+}
+
+DALI_REGISTER_OPERATOR(JpegCompressionDistortion, JpegCompressionDistortionGPU, GPU);
+
+}  // namespace dali
