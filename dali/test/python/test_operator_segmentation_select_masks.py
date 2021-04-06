dd223fb66d451057f7fa5f61273720e3cf249429
Krzysztof Lecki <klecki@nvidia.com>
2021-04-06 18:04:47 +0200

    Add missing licenses to several Python test files
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/test/python/test_operator_segmentation_select_masks.py
+++ dali/test/python/test_operator_segmentation_select_masks.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import nvidia.dali as dali
 import nvidia.dali.fn as fn
@@ -93,7 +107,7 @@ def test_select_masks_wrong_mask_ids():
 
 def test_select_masks_wrong_mask_meta_dim():
     def test_data():
-        # Expects 3 integers, not 4 
+        # Expects 3 integers, not 4
         polygons = [np.array([[0, 0, 2, -1], [1, 3, 5, -1], [2, 6, 8, -1]], dtype=np.int32)]
         vertices = [np.array(np.random.rand(9, 2), dtype=np.float32)]
         mask_ids = [np.array([0], dtype=np.int32)]
