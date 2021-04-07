1c9f5be60614a13e8a1898647fad62fa4afff13f
Krzysztof Lecki <klecki@nvidia.com>
2021-04-07 14:15:27 +0200

    Split Python tests into core and operators
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- qa/TL0_python-self-test-core/test_body.sh
+++ qa/TL0_python-self-test-core/test_body.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 test_nose() {
-    for test_script in $(ls test_operator_*.py test_pipeline*.py test_pool.py test_external_source_dali.py test_external_source_numpy.py test_external_source_parallel.py test_external_source_parallel_shared_batch.py test_functional_api.py test_backend_impl.py); do
+    for test_script in $(ls test_pipeline*.py test_functional_api.py test_backend_impl.py \
+      test_operator_readers_*.py test_operator_decoders_*.py \
+      test_external_source_dali.py test_external_source_numpy.py \
+      test_pool.py test_external_source_parallel.py test_external_source_parallel_shared_batch.py); do
         nosetests --verbose --attr '!slow' ${test_script}
     done
 }
