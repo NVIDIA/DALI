f8198dcc235fe5b1f4e3c23b187022f3dd9218d9
Krzysztof Lecki <klecki@nvidia.com>
2021-04-07 14:07:39 +0200

    Adjust the filenames of decoder test files and update licenses (#2844)
    Make decoder tests follow the same naming as readers.
    
    Add missing licenses to several Python test files.
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/test/python/test_operator_roi_random_crop.py
+++ dali/test/python/test_operator_roi_random_crop.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 import nvidia.dali as dali
 import nvidia.dali.fn as fn
@@ -38,7 +52,7 @@ def check_roi_random_crop(ndim=2, max_batch_size=16,
         in_shape = dali.fn.shapes(shape_like_in, dtype=types.INT32)
 
         crop_shape =  [(crop_min_extent + crop_max_extent) // 2] * ndim if random.choice([True, False]) \
-            else  fn.random.uniform(range=(crop_min_extent, crop_max_extent + 1), 
+            else  fn.random.uniform(range=(crop_min_extent, crop_max_extent + 1),
                                     shape=(ndim,), dtype=types.INT32, device='cpu')
 
         if random.choice([True, False]):
