4aca66779dd665b6d4bce23e70516eb87163ec36
Michał Zientkiewicz <mzient@gmail.com>
2021-04-13 14:51:49 +0200

    Add cross-device functionality to async pool.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/core/mm/pinned_pool_test.cc
+++ dali/core/mm/pinned_pool_test.cc
@@ -30,8 +30,7 @@ namespace test {
 TEST(MMPinnedAlloc, StageCopy) {
   test_pinned_resource upstream;
   {
-    CUDAStream stream;
-    stream = CUDAStream::Create(true);
+    CUDAStream stream = CUDAStream::Create(true);
     stream_view sv(stream);
     async_pool_base<memory_kind::pinned> pool(&upstream);
     std::mt19937_64 rng;
@@ -47,8 +46,8 @@ TEST(MMPinnedAlloc, StageCopy) {
     void *mem2 = pool.allocate_async(N, sv);
     EXPECT_EQ(mem1, mem2);
     CUDA_CALL(cudaMemcpyAsync(copy_back.data(), dev_buf, N, cudaMemcpyDeviceToHost, stream));
-    CUDA_CALL(cudaStreamSynchronize(stream));
     pool.deallocate_async(mem1, N, sv);
+    CUDA_CALL(cudaStreamSynchronize(stream));
     EXPECT_EQ(pattern, copy_back);
   }
   upstream.check_leaks();
@@ -66,6 +65,11 @@ TEST(MMPinnedAlloc, SyncAndSteal) {
     void *mem1 = pool.allocate_async(N, sv1);
     CUDA_CALL(cudaMemsetAsync(mem1, 0, N, s1));
     pool.deallocate_async(mem1, N, sv1);
+    // We've requested a large chunk (16MiB) of memory - that memory is not going
+    // to be readily available, but the pool is configured with "avoid upstream" option
+    // and therefore will wait for the pending deallocations to complete - this is still
+    // lighter than calling cudaMallocHost, which would implicitly synchronize all devices,
+    // not just some streams.
     void *mem2 = pool.allocate_async(N, sv2);
     auto e = cudaStreamQuery(s1);
     EXPECT_NE(e, cudaErrorNotReady) << "Syncrhonization should have occurred";
@@ -77,6 +81,73 @@ TEST(MMPinnedAlloc, SyncAndSteal) {
   }
   upstream.check_leaks();
 }
+
+TEST(MMPinnedAlloc, SyncCrossDevice) {
+  test_pinned_resource upstream;
+  int ndev = 0;
+  CUDA_CALL(cudaGetDeviceCount(&ndev));
+  if (ndev < 2) {
+    GTEST_SKIP() << "This test requires at least 2 CUDA devices.";
+  } else {
+    CUDAStream s1, s2;
+    DeviceGuard dg(0);
+    s1 = CUDAStream::Create(true);
+    cudaSetDevice(1);
+    s2 = CUDAStream::Create(true);
+    cudaSetDevice(0);
+    stream_view sv1(s1), sv2(s2);
+    const int N = 1<<24;
+    async_pool_base<memory_kind::pinned> pool(&upstream, true);
+    void *mem1 = pool.allocate_async(N, sv1);
+    CUDA_CALL(cudaMemsetAsync(mem1, 0, N, s1));
+    pool.deallocate_async(mem1, N, sv1);
+    // We've requested a large chunk (16MiB) of memory - that memory is not going
+    // to be readily available, but the pool is configured with "avoid upstream" option
+    // and therefore will wait for the pending deallocations to complete - this is still
+    // lighter than calling cudaMallocHost, which would implicitly synchronize all devices,
+    // not just some streams.
+    void *mem2 = pool.allocate_async(N, sv2);
+    auto e = cudaStreamQuery(s1);
+    EXPECT_NE(e, cudaErrorNotReady) << "Syncrhonization should have occurred";
+    if (e != cudaErrorNotReady) {
+      CUDA_CALL(cudaGetLastError());
+    }
+    EXPECT_EQ(mem1, mem2) << "Memory should have been stolen from the stream1 after it's finished";
+    pool.deallocate_async(mem2, N, sv2);
+  }
+  upstream.check_leaks();
+}
+
+TEST(MMPinnedAlloc, FreeOnAnotherDevice) {
+  test_pinned_resource upstream;
+  int ndev = 0;
+  CUDA_CALL(cudaGetDeviceCount(&ndev));
+  if (ndev < 2) {
+    GTEST_SKIP() << "This test requires at least 2 CUDA devices.";
+  } else {
+    CUDAStream s1, s2;
+    DeviceGuard dg(0);
+    s1 = CUDAStream::Create(true);
+    cudaSetDevice(1);
+    s2 = CUDAStream::Create(true);
+    cudaSetDevice(0);
+    stream_view sv1(s1), sv2(s2);
+    const int N = 1<<24;
+    async_pool_base<memory_kind::pinned> pool(&upstream, true);
+    void *mem1 = pool.allocate_async(N, sv1);
+    CUDA_CALL(cudaMemsetAsync(mem1, 0, N, s1));
+    cudaStreamSynchronize(s1);
+    // don't set device - it should be inferred from the stream
+    pool.deallocate_async(mem1, N, sv2);
+    // now set the device and allocate
+    cudaSetDevice(1);
+    void *mem2 = pool.allocate_async(N, sv2);
+    EXPECT_EQ(mem1, mem2) << "Memory should have been moved to stream2 on another device.";
+    pool.deallocate_async(mem2, N, sv2);
+  }
+  upstream.check_leaks();
+}
+
 
 }  // namespace test
 }  // namespace mm
