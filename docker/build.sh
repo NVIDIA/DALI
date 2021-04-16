e86e8f181fc5374dfe7af0c539ad1c50bd63c016
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-04-16 11:12:20 +0200

    Enable CUDA 11.3 based builds
    - fixes conda build
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ a build environment
 
 To change build configuration please export appropriate env variables (for exact meaning please check the README):
 PYVER=[default 3.6, required only by Run image]
-CUDA_VERSION=[default 11.2, accepts also 10.0, 11.0 and 11.1]
+CUDA_VERSION=[default 11.3, accepts also 10.0, 11.0 and 11.1]
 NVIDIA_BUILD_ID=[default 12345]
 CREATE_WHL=[default YES]
 CREATE_RUNNER=[default NO]
@@ -40,14 +40,14 @@ shift $((OPTIND - 1))
 export ARCH=${ARCH:-x86_64}
 export PYVER=${PYVER:-3.6}
 export PYV=${PYVER/./}
-export CUDA_VERSION=${CUDA_VERSION:-11.2}
+export CUDA_VERSION=${CUDA_VERSION:-11.3}
 export CUDA_VER=${CUDA_VERSION//./}
 
 if [ "${CUDA_VERSION%%\.*}" ]
 then
-  if [ $CUDA_VER != "100" ] && [ $CUDA_VER != "110" ] && [ $CUDA_VER != "111" ] && [ $CUDA_VER != "112" ]
+  if [ $CUDA_VER != "100" ] && [ $CUDA_VER != "110" ] && [ $CUDA_VER != "111" ] && [ $CUDA_VER != "112" ] && [ $CUDA_VER != "113" ]
   then
-      echo "Wrong CUDA_VERSION=$CUDA_VERSION provided. Only 10.0, 11.0, 11.1 and 11.2 are supported"
+      echo "Wrong CUDA_VERSION=$CUDA_VERSION provided. Only 10.0, 11.0, 11.1, 11.2 and 11.3 are supported"
       exit 1
   fi
 else
--- docs/compilation.rst
+++ docs/compilation.rst
@@ -38,8 +38,8 @@ Building Python Wheel
 Change directory (``cd``) into ``docker`` directory and run ``./build.sh``. If needed,
 set the following environment variables:
 
-* | CUDA_VERSION - CUDA toolkit version (10.0, 11.0, 11.1 and 11.2).
-  | The default is ``11.2``. Thanks to CUDA extended compatibility mode, CUDA 11.1 and 11.2 wheels are
+* | CUDA_VERSION - CUDA toolkit version (10.0, 11.0, 11.1, 11.2 and 11.23.
+  | The default is ``11.3``. Thanks to CUDA extended compatibility mode, CUDA 11.1, 11.2 and 11.3 wheels are
     named as CUDA 11.0 because it can work with the CUDA 11.0 R450.x driver family. Please update
     to the latest recommended driver version in that family.
   | If the value of the CUDA_VERSION is prefixed with `.` then any value ``.XX.Y`` can be passed,
