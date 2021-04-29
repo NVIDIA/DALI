d327858227368f6daad4cc2fcffb20c1cc7928f4
Joaquin Anton <janton@nvidia.com>
2021-04-29 14:46:43 +0200

    Use ConvertSat instead of clamp/static_cast
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/kernels/imgproc/color_manipulation/color_space_conversion_impl.h
+++ dali/kernels/imgproc/color_manipulation/color_space_conversion_impl.h
@@ -55,7 +55,7 @@ DALI_HOST_DEV DALI_FORCEINLINE Output rgb_to_y(vec<3, Input> rgb_in) {
 template <>
 DALI_HOST_DEV DALI_FORCEINLINE uint8_t rgb_to_y(vec<3, uint8_t> rgb) {
   constexpr vec3 coeffs(0.257f, 0.504f, 0.098f);
-  return static_cast<uint8_t>(dot(coeffs, rgb) + 16.0f);
+  return ConvertSat<uint8_t>(dot(coeffs, rgb) + 16.0f);
 }
 
 template <typename Output, typename Input>
@@ -69,7 +69,7 @@ template <>
 DALI_HOST_DEV DALI_FORCEINLINE uint8_t rgb_to_cb(vec<3, uint8_t> rgb) {
   constexpr vec3 coeffs(-0.148f, -0.291f, 0.439f);
   if (rgb.x == rgb.y && rgb.x == rgb.z) return 128;
-  return static_cast<uint8_t>(dot(coeffs, rgb) + 128.0f);
+  return ConvertSat<uint8_t>(dot(coeffs, rgb) + 128.0f);
 }
 
 template <typename Output, typename Input>
@@ -83,7 +83,7 @@ template <>
 DALI_HOST_DEV DALI_FORCEINLINE uint8_t rgb_to_cr(vec<3, uint8_t> rgb) {
   constexpr vec3 coeffs(0.439f, -0.368f, -0.071f);
   if (rgb.x == rgb.y && rgb.x == rgb.z) return 128;
-  return static_cast<uint8_t>(dot(coeffs, rgb) + 128.0f);
+  return ConvertSat<uint8_t>(dot(coeffs, rgb) + 128.0f);
 }
 
 // Gray uses the full dynamic range of the type (e.g. 0..255)
@@ -91,47 +91,49 @@ DALI_HOST_DEV DALI_FORCEINLINE uint8_t rgb_to_cr(vec<3, uint8_t> rgb) {
 template <typename Output, typename Input>
 DALI_HOST_DEV DALI_FORCEINLINE Output y_to_gray(Input gray_in) {
   auto gray = detail::norm(gray_in);  // TODO(janton): optimize number of multiplications
-  return ConvertSatNorm<Output>(1.164f * (gray - 0.0625f));
+  constexpr float scale = 1 / (0.257f +  0.504f + 0.098f);
+  return ConvertSatNorm<Output>(scale * (gray - 0.0625f));
 }
 
 template <>
 DALI_HOST_DEV DALI_FORCEINLINE uint8_t y_to_gray(uint8_t gray) {
-  return static_cast<uint8_t>(1.164f * (gray - 16));
+  constexpr float scale = 1 / (0.257f + 0.504f + 0.098f);
+  return ConvertSat<uint8_t>(scale * (gray - 16));
 }
 
 template <typename Output, typename Input>
 DALI_HOST_DEV DALI_FORCEINLINE Output gray_to_y(Input y_in) {
   auto y = detail::norm(y_in);  // TODO(janton): optimize number of multiplications
-  constexpr float scale = 1 / 1.164f;
+  constexpr float scale = 0.257f + 0.504f + 0.098f;
   return ConvertSatNorm<Output>(y * scale + 0.0625f);
 }
 
 template <>
 DALI_HOST_DEV DALI_FORCEINLINE uint8_t gray_to_y(uint8_t y) {
-  constexpr float scale = 1 / 1.164f;
-  return static_cast<uint8_t>(y * scale + 0.0625f);
+  constexpr float scale = 0.257f + 0.504f + 0.098f;
+  return ConvertSat<uint8_t>(y * scale + 0.0625f);
 }
 
 template <typename Output, typename Input>
 DALI_HOST_DEV DALI_FORCEINLINE vec<3, Output> ycbcr_to_rgb(vec<3, Input> ycbcr_in) {
   auto ycbcr = detail::norm(ycbcr_in);  // TODO(janton): optimize number of multiplications
-  auto gray = y_to_gray<float>(ycbcr.x);
+  auto tmp_y = 1.164f * (ycbcr.x - 0.0625f);
   auto tmp_b = ycbcr.y - 0.5f;
   auto tmp_r = ycbcr.z - 0.5f;
-  auto r = ConvertSatNorm<Output>(gray + 1.596f * tmp_r);
-  auto g = ConvertSatNorm<Output>(gray - 0.813f * tmp_r - 0.392f * tmp_b, 0, 255);
-  auto b = ConvertSatNorm<Output>(gray + 2.017f * tmp_b);
+  auto r = ConvertSatNorm<Output>(tmp_y + 1.596f * tmp_r);
+  auto g = ConvertSatNorm<Output>(tmp_y - 0.813f * tmp_r - 0.392f * tmp_b);
+  auto b = ConvertSatNorm<Output>(tmp_y + 2.017f * tmp_b);
   return {r, g, b};
 }
 
 template <>
 DALI_HOST_DEV DALI_FORCEINLINE vec<3, uint8_t> ycbcr_to_rgb(vec<3, uint8_t> ycbcr) {
-  auto gray = y_to_gray<uint8_t>(ycbcr.x);
+  auto tmp_y = 1.164f * (ycbcr.x - 16);
   auto tmp_b = ycbcr.y - 128;
   auto tmp_r = ycbcr.z - 128;
-  auto r = clamp<uint8_t>(gray + 1.596f * tmp_r, 0, 255);
-  auto g = clamp<uint8_t>(gray - 0.813f * tmp_r - 0.392f * tmp_b, 0, 255);
-  auto b = clamp<uint8_t>(gray + 2.017f * tmp_b, 0, 255);
+  auto r = ConvertSat<uint8_t>(tmp_y + 1.596f * tmp_r);
+  auto g = ConvertSat<uint8_t>(tmp_y - 0.813f * tmp_r - 0.392f * tmp_b);
+  auto b = ConvertSat<uint8_t>(tmp_y + 2.017f * tmp_b);
   return {r, g, b};
 }
 
@@ -194,9 +196,9 @@ template <>
 DALI_HOST_DEV DALI_FORCEINLINE vec<3, uint8_t> ycbcr_to_rgb(vec<3, uint8_t> ycbcr) {
   float tmp_b = ycbcr.y - 128;
   float tmp_r = ycbcr.z - 128;
-  auto r = clamp<uint8_t>(ycbcr.x + 1.402f * tmp_r, 0, 255);
-  auto g = clamp<uint8_t>(ycbcr.x - 0.34413629f * tmp_b - 0.71413629f * tmp_r, 0, 255);
-  auto b = clamp<uint8_t>(ycbcr.x + 1.772f * tmp_b, 0, 255);
+  auto r = ConvertSat<uint8_t>(ycbcr.x + 1.402f * tmp_r);
+  auto g = ConvertSat<uint8_t>(ycbcr.x - 0.34413629f * tmp_b - 0.71413629f * tmp_r);
+  auto b = ConvertSat<uint8_t>(ycbcr.x + 1.772f * tmp_b);
   return {r, g, b};
 }
 
