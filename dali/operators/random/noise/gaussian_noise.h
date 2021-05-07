96758b6ace00d80a568071ffe95f0046526a8e9d
Joaquin Anton <janton@nvidia.com>
2021-05-07 13:52:37 +0200

    Add noise generator changes
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/operators/random/noise/gaussian_noise.h
+++ dali/operators/random/noise/gaussian_noise.h
@@ -21,47 +21,49 @@
 #include "dali/operators/random/rng_base_cpu.h"
 
 #define DALI_GAUSSIAN_NOISE_TYPES \
-  (uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t, float)
+  uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t, uint64_t, int64_t, float
 
 namespace dali {
 
 template <typename Backend, typename T>
-class gaussian_noise_impl {
- public:
+struct GaussianNoiseImpl {
   using FloatType =
       typename std::conditional<((std::is_integral<T>::value && sizeof(T) >= 4) || sizeof(T) > 4),
                                 double, float>::type;
-  using Dist = typename std::conditional_t<std::is_same<Backend, GPUBackend>::value,
-                                           curand_normal_dist<FloatType>,
-                                           std::normal_distribution<FloatType>>;
+  using DistType = typename std::conditional_t<std::is_same<Backend, GPUBackend>::value,
+                                               curand_normal_dist<FloatType>,
+                                               std::normal_distribution<FloatType>>;
 
-  DALI_HOST_DEV explicit gaussian_noise_impl(FloatType mean = 0, FloatType stddev = 1)
-      : dist_{mean, stddev} {}
+  DALI_HOST_DEV explicit GaussianNoiseImpl(FloatType mean = 0, FloatType stddev = 1)
+    : dist_{mean, stddev} {}
 
   template <typename Generator>
-  DALI_HOST_DEV inline T operator()(T in_val, Generator& g) {
-    return ConvertSat<T>(in_val + dist_(g));
+  DALI_HOST_DEV FloatType Generate(T input, Generator &st) {
+    (void) input;  // noise generation doesn't depend on the input
+    return dist_(st);
   }
 
- private:
-  Dist dist_;
+  DALI_HOST_DEV void Apply(T& output, T input, FloatType n) {
+    output = ConvertSat<T>(input + n);
+  }
+
+  DistType dist_;
 };
 
 template <typename Backend>
 class GaussianNoise : public RNGBase<Backend, GaussianNoise<Backend>, true> {
  public:
   using BaseImpl = RNGBase<Backend, GaussianNoise<Backend>, true>;
+
   template <typename T>
-  struct Dist {
-    using type = gaussian_noise_impl<Backend, T>;
-  };
+  using Impl = GaussianNoiseImpl<Backend, T>;
 
   explicit GaussianNoise(const OpSpec &spec)
       : BaseImpl(spec),
         mean_("mean", spec),
         stddev_("stddev", spec) {
     if (mean_.IsDefined() || stddev_.IsDefined()) {
-      backend_data_.ReserveDistsData(sizeof(typename Dist<double>::type) * max_batch_size_);
+      backend_data_.ReserveDistsData(sizeof(Impl<double>) * max_batch_size_);
     }
   }
 
@@ -75,22 +77,25 @@ class GaussianNoise : public RNGBase<Backend, GaussianNoise<Backend>, true> {
   }
 
   template <typename T>
-  bool SetupDists(typename Dist<T>::type* dists_data, int nsamples) {
+  bool SetupDists(Impl<T>* dists_data, int nsamples) {
     if (!mean_.IsDefined() && !stddev_.IsDefined()) {
       return false;
     }
     for (int s = 0; s < nsamples; s++) {
-      dists_data[s] = typename Dist<T>::type{mean_[s].data[0], stddev_[s].data[0]};
+      dists_data[s] = Impl<T>(mean_[s].data[0], stddev_[s].data[0]);
     }
     return true;
   }
 
   using BaseImpl::RunImpl;
   void RunImpl(workspace_t<Backend> &ws) override {
-    TYPE_SWITCH(dtype_, type2id, T, DALI_GAUSSIAN_NOISE_TYPES, (
-      using Dist = typename Dist<T>::type;
-      this->template RunImplTyped<T, Dist>(ws);
-    ), DALI_FAIL(make_string("Unsupported data type: ", dtype_)));  // NOLINT
+    TYPE_SWITCH(dtype_, type2id, T, (DALI_GAUSSIAN_NOISE_TYPES), (
+      using ImplT = Impl<T>;
+      BaseImpl::template RunImplTyped<T, ImplT>(ws);
+    ), (  // NOLINT
+      DALI_FAIL(make_string("Data type ", dtype_, " is currently not supported. "
+                            "Supported types are : ", ListTypeNames<DALI_GAUSSIAN_NOISE_TYPES>()));
+    ));  // NOLINT
   }
 
  protected:
