b2f7be0ddbc90b3f0b76a3ce8b5be68d2303748a
Kacper Kluk <kk406184@students.mimuw.edu.pl>
2021-05-23 18:46:18 +0200

    Add yolo TL3 test
    Signed-off-by: Kacper Kluk <kk406184@students.mimuw.edu.pl>

---
+++ qa/TL3_YOLO_convergence/test_tensorflow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+function CLEAN_AND_EXIT {
+    rm dali.log
+    rm output.h5
+    exit $1
+}
+
+cd /opt/dali/docs/examples/use_cases/tensorflow/yolov4/src
+
+python main.py train \
+    /data/coco/coco-2017/coco2017/train2017 \
+    /data/coco/coco-2017/coco2017/annotations/instances_train2017.json \
+    -b 8 -e 12 -s 3000 -o output.h5 \
+    --learning_rate="1e-3" --dali_use_gpu  --multigpu --use_mosaic
+
+python main.py eval \
+    /data/coco/coco-2017/coco2017/val2017/ \
+    /data/coco/coco-2017/coco2017/annotations/instances_val2017.json \
+    -b 1 -s 5000 --weights output.h5 2>&1 | tee dali.log
+
+RET=$(tail -n 1 dali.log | awk '{ exit ($NF < 0.2); }')
+
+CLEAN_AND_EXIT $RET
