e60d5b271ef2d9564b702107555114bc66263e68
Joaquin Anton <janton@nvidia.com>
2021-05-24 21:31:13 +0200

    Enable SaltAndPepper GPU variable batch size tests (#2976)
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/test/python/test_dali_variable_batch_size.py
+++ dali/test/python/test_dali_variable_batch_size.py
@@ -288,7 +288,7 @@ random_ops = [
     (fn.random_resized_crop, {'size': 69}),
     (fn.noise.gaussian, {}),
     (fn.noise.shot, {}),
-    (fn.noise.salt_and_pepper, {'devices': ['cpu']}),  # TODO(janton): Enalble GPU once salt_and_pepper supports it.
+    (fn.noise.salt_and_pepper, {}),
 ]
 
 def test_random_ops():
