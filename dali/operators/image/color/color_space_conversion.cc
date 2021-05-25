790e19328daf406fd9ffd03bb4318745c9e8186b
Joaquin Anton <janton@nvidia.com>
2021-05-25 14:29:39 +0200

    Use thread_pool in ColorSpaceConversion
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/operators/image/color/color_space_conversion.cc
+++ dali/operators/image/color/color_space_conversion.cc
@@ -39,17 +39,22 @@ void ColorSpaceConversion<CPUBackend>::RunImpl(HostWorkspace &ws) {
   const auto &out_sh = out_view.shape;
   int nsamples = in_sh.num_samples();
   int ndim = in_sh.sample_dim();
+  auto& thread_pool = ws.GetThreadPool();
   for (int i = 0; i < nsamples; i++) {
-    auto in_sample_sh = in_sh.tensor_shape_span(i);
-    // flatten any leading dimensions together with the height
-    int height = volume(in_sample_sh.begin(), in_sample_sh.end() - 2);
-    int width  = in_sample_sh[ndim - 2];
-    auto cv_in =
-        CreateMatFromPtr(height, width, GetOpenCvChannelType(in_nchannels_), in_view[i].data);
-    auto cv_out =
-        CreateMatFromPtr(height, width, GetOpenCvChannelType(out_nchannels_), out_view[i].data);
-    OpenCvColorConversion(input_type_, cv_in, output_type_, cv_out);
+    thread_pool.AddWork(
+      [&, i](int thread_id) {
+        auto in_sample_sh = in_sh.tensor_shape_span(i);
+        // flatten any leading dimensions together with the height
+        int height = volume(in_sample_sh.begin(), in_sample_sh.end() - 2);
+        int width  = in_sample_sh[ndim - 2];
+        auto cv_in =
+          CreateMatFromPtr(height, width, GetOpenCvChannelType(in_nchannels_), in_view[i].data);
+        auto cv_out =
+          CreateMatFromPtr(height, width, GetOpenCvChannelType(out_nchannels_), out_view[i].data);
+        OpenCvColorConversion(input_type_, cv_in, output_type_, cv_out);
+      }, in_sh.tensor_size(i));
   }
+  thread_pool.RunAll();
 }
 
 DALI_REGISTER_OPERATOR(ColorSpaceConversion, ColorSpaceConversion<CPUBackend>, CPU);
