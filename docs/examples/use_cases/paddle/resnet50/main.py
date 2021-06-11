4fc5b63c1b2822c94322fca5fb5e4ab1ae37359f
Janusz Lisiecki <jlisiecki@nvidia.com>
2021-06-11 12:37:26 +0200

    review fixes
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docs/examples/use_cases/paddle/resnet50/main.py
+++ docs/examples/use_cases/paddle/resnet50/main.py
@@ -48,14 +48,14 @@ def create_dali_pipeline(batch_size, num_threads, device_id, data_dir, crop, siz
         preallocate_height_hint = 6430 if decoder_device == 'mixed' else 0
         if is_training:
             images = fn.decoders.image_random_crop(images,
-                                                device=decoder_device, output_type=types.RGB,
-                                                device_memory_padding=device_memory_padding,
-                                                host_memory_padding=host_memory_padding,
-                                                preallocate_width_hint=preallocate_width_hint,
-                                                preallocate_height_hint=preallocate_height_hint,
-                                                random_aspect_ratio=[0.8, 1.25],
-                                                random_area=[0.1, 1.0],
-                                                num_attempts=100)
+                                                   device=decoder_device, output_type=types.RGB,
+                                                   device_memory_padding=device_memory_padding,
+                                                   host_memory_padding=host_memory_padding,
+                                                   preallocate_width_hint=preallocate_width_hint,
+                                                   preallocate_height_hint=preallocate_height_hint,
+                                                   random_aspect_ratio=[0.8, 1.25],
+                                                   random_area=[0.1, 1.0],
+                                                   num_attempts=100)
             images = fn.resize(images,
                                device=dali_device,
                                resize_x=crop,
