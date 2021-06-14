751504d835e9a481456a088353d3c58aec98849a
Awesom E. Dev <devnull@nvidia.com>
2021-06-14 03:03:54 -0700

    little rewording

--- dali/operators/reader/numpy_reader_op.cc
+++ dali/operators/reader/numpy_reader_op.cc
@@ -125,7 +125,7 @@ This argument is mutually exclusive with ``file_list``.)", nullptr)
   .AddOptionalArg("register_buffers",
       R"code(Applies **only** to the ``gpu`` backend type.
 .. warning:
-    This argument is temporarily disabled and left for the backward compatibility.
+    This argument is temporarily disabled and left for backward compatibility.
     It will be reenabled in the future releases.
 If true, the device I/O buffers will be registered with cuFile. It is not recommended if the sample
 sizes vary a lot.)code", true)
