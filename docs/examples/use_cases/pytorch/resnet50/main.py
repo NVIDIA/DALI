2c11fe6356a2a72447fc7952ea2cd6336b335837
Lu Qiu <luqiujob@gmail.com>
2021-06-15 09:48:26 -0700

    Remove the validation step and improve code

--- docs/examples/use_cases/pytorch/resnet50/main.py
+++ docs/examples/use_cases/pytorch/resnet50/main.py
@@ -33,60 +33,30 @@ def parse():
                         help='model architecture: ' +
                         ' | '.join(model_names) +
                         ' (default: resnet18)')
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--threads', default=4, type=int, metavar='N',
+                        help='number of data loading threads (default: 4)')
     parser.add_argument('--epochs', default=90, type=int, metavar='N',
                         help='number of total epochs to run')
     parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                         help='manual epoch number (useful on restarts)')
     parser.add_argument('-b', '--batch-size', default=256, type=int,
                         metavar='N', help='mini-batch size per process (default: 256)')
-    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                        metavar='LR', help='Initial learning rate.  Will be scaled by <global batch size>/256: args.lr = args.lr*float(args.batch_size*args.world_size)/256.  A warmup schedule will also be applied over the first 5 epochs.')
-    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                        help='momentum')
-    parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                        metavar='W', help='weight decay (default: 1e-4)')
     parser.add_argument('--print-freq', '-p', default=10, type=int,
                         metavar='N', help='print frequency (default: 10)')
-    parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                        help='path to latest checkpoint (default: none)')
-    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                        help='evaluate model on validation set')
-    parser.add_argument('--pretrained', dest='pretrained', action='store_true',
-                        help='use pre-trained model')
-
     parser.add_argument('--dali_cpu', action='store_true',
                         help='Runs CPU based version of DALI pipeline.')
-    parser.add_argument('--prof', default=-1, type=int,
-                        help='Only run 10 iterations for profiling.')
-    parser.add_argument('--deterministic', action='store_true')
 
     parser.add_argument("--local_rank", default=0, type=int)
-    parser.add_argument('--sync_bn', action='store_true',
-                        help='enabling apex sync BN.')
 
-    parser.add_argument('--opt-level', type=str, default=None)
-    parser.add_argument('--keep-batchnorm-fp32', type=str, default=None)
-    parser.add_argument('--loss-scale', type=str, default=None)
-    parser.add_argument('--channels-last', type=bool, default=False)
     parser.add_argument('-t', '--test', action='store_true',
                         help='Launch test mode with preset arguments')
     args = parser.parse_args()
     return args
 
-# item() is a recent addition, so this helps with backward compatibility.
-def to_python_float(t):
-    if hasattr(t, 'item'):
-        return t.item()
-    else:
-        return t[0]
-
-
 @pipeline_def
 def create_dali_pipeline(data_dir, crop, size, shard_id, num_shards, dali_cpu=False, is_training=True):
-    # Read jpegs and labels from the hard drive
-    # this line is enough
+    # TODO(lu) is it possible to reduce the logics for images processing
+    # to better test the read throughput
     images, labels = fn.readers.file(file_root=data_dir,
                                      shard_id=shard_id,
                                      num_shards=num_shards,
@@ -97,7 +67,7 @@ def create_dali_pipeline(data_dir, crop, size, shard_id, num_shards, dali_cpu=Fa
     decoder_device = 'cpu' if dali_cpu else 'mixed'
     device_memory_padding = 211025920 if decoder_device == 'mixed' else 0
     host_memory_padding = 140544512 if decoder_device == 'mixed' else 0
-    print('dali device is {}, decoder device is {}'.format(dali_device, decoder_device))
+    print('Dali device is {}, decoder device is {}'.format(dali_device, decoder_device))
     if is_training:
         images = fn.decoders.image_random_crop(images,
                                                device=decoder_device, output_type=types.RGB,
@@ -123,11 +93,6 @@ def create_dali_pipeline(data_dir, crop, size, shard_id, num_shards, dali_cpu=Fa
                            interp_type=types.INTERP_TRIANGULAR)
         mirror = False
 
-    # TODO(lu) what's that, can i change it to CPU or remove it
-    # it supports CPU/GPU
-    # TODO(lu) see if i can directly use cpu()
-    # or i don't need to use images.gpu() but use images directly
-    # or add device=dali_device and use images as input directly from https://github.com/NVIDIA/DALI/blob/main/dali/test/python/test_pipeline.py
     images = fn.crop_mirror_normalize(images,
                                       device=dali_device,
                                       dtype=types.FLOAT,
@@ -137,8 +102,7 @@ def create_dali_pipeline(data_dir, crop, size, shard_id, num_shards, dali_cpu=Fa
                                       std=[0.229 * 255,0.224 * 255,0.225 * 255],
                                       mirror=mirror)
     if dali_device == 'gpu':
-        labels = labels.cpu()
-    # TODO(lu) is this needed? PyTorch expects labels as INT64 labels = fn.cast(labels, dtype=types.INT64) from https://github.com/NVIDIA/DALI/blob/main/docs/examples/frameworks/pytorch/pytorch-lightning.ipynb  
+        labels = labels.gpu()
     return images, labels
 
 
@@ -153,34 +117,29 @@ def main():
     if 'WORLD_SIZE' in os.environ:
         args.distributed = int(os.environ['WORLD_SIZE']) > 1
 
-    args.world_size = 1
-    print('batch_size is {}, num_threads is {}, shard id is {}, world size is {}'.format(args.batch_size, args.workers, args.local_rank, int(os.environ['WORLD_SIZE'])))
-    print('rank is {}'.format(os.environ['RANK']))
-    # print('local rank is'.format(os.environ['LOCAL_RANK']))
-    # Arena doesn't has local rank concept
-    args.local_rank = int(os.environ['RANK'])
-    shard_id = args.local_rank
     if 'RANK' in os.environ:
-        shard_id = int(os.environ['RANK'])
+        # Arena pytorch distributed mode will give each node a unique RANK
+        # but Arena doesn't have LOCAL_RANK concept, the script has to deal with multi-thread/multi-process itself
+        # TODO(lu) use workers multi-thread reading for now, consider launching multi-process in each node.
+        args.local_rank = int(os.environ['RANK'])
+    shard_id=args.local_rank
+
+    args.world_size = 1
     if args.distributed:
-        # backend='gloo', device_ids must be None or an empty list for CPU modules
-        # rank = args.local_rank, world_size=args.world_size these two variables may be set through distributed.launch
-        print('start to init process group')
+        print('Inits distributed process group with gloo backend')
+        # Distributed information will be passed in through environment variable WORLD_SIZE and RANK
         torch.distributed.init_process_group(backend='gloo',
                                              init_method='env://')
-        print('inited process group')
+        print('Process group inited')
         args.world_size = torch.distributed.get_world_size()
-        print('the updated word size is {}'.format(args.world_size))
 
-    args.total_batch_size = args.world_size * args.batch_size
-
-    # Data loading code
+    print('Parameters: world_size[{}], rank[{}], batch_size[{}], data_reader_threads[{}], '
+          'shard_id[{}], num_shards[{}]'.format(args.world_size, os.environ['RANK'], args.batch_size,
+                                                args.threads, shard_id, args.world_size))
     if len(args.data) == 1:
         traindir = os.path.join(args.data[0], 'train')
-        valdir = os.path.join(args.data[0], 'val')
     else:
         traindir = args.data[0]
-        valdir= args.data[1]
 
     if(args.arch == "inception_v3"):
         raise RuntimeError("Currently, inception_v3 is not supported by this example.")
@@ -190,13 +149,8 @@ def main():
         crop_size = 224
         val_size = 256
 
-    # remove the device_id=args.local_rank to let dali uses CPU only device id and not calling libcuda.so
-    # TODO(lu) why node 1 shard id is also 0 local rank is also 0
-    print('batch_size is {}, num_threads is {}, shard id is {}, num shards is {}'.format(args.batch_size, args.workers, args.local_rank, args.world_size))
-    # sharding the dataset into multiple parts/shards, each GPU/CPU get its own shard to process, from https://github.com/NVIDIA/DALI/blob/main/docs/examples/general/multigpu.ipynb
-    # TODO(lu) is sharding similar in CPU world
     pipe = create_dali_pipeline(batch_size=args.batch_size,
-                                num_threads=args.workers,
+                                num_threads=args.threads,
                                 seed=12 + args.local_rank,
                                 data_dir=traindir,
                                 crop=crop_size,
@@ -207,27 +161,8 @@ def main():
                                 is_training=True,
                                 device_id=-99999)
     pipe.build()
-    # DALI iterator for classification tasks for PyTorch. It returns 2 outputs (data and label) in the form of PyTorch’s Tensor.
-    # TODO(lu) can i remove it? run the pipe directly?
+
     train_loader = DALIClassificationIterator(pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL)
-
-    pipe = create_dali_pipeline(batch_size=args.batch_size,
-                                num_threads=args.workers,
-                                seed=12 + args.local_rank,
-                                data_dir=valdir,
-                                crop=crop_size,
-                                size=val_size,
-                                dali_cpu=args.dali_cpu,
-                                shard_id=shard_id,
-                                num_shards=args.world_size,
-                                is_training=False,
-                                device_id=-99999)
-    pipe.build()
-    val_loader = DALIClassificationIterator(pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL)
-
-    if args.evaluate:
-        validate(val_loader)
-        return
 
     total_time = AverageMeter()
     for epoch in range(args.start_epoch, args.epochs):
@@ -237,21 +172,16 @@ def main():
         if args.test:
             break
 
-        # evaluate on validation set
-        validate(val_loader)
-
         train_loader.reset()
-        val_loader.reset()
 
 def train(train_loader, epoch):
     batch_time = AverageMeter()
-
+    train_loader_len = int(math.ceil(train_loader._size / args.batch_size))
     end = time.time()
 
     for i, data in enumerate(train_loader):
         input = data[0]["data"]
         target = data[0]["label"].squeeze(-1).long()
-        train_loader_len = int(math.ceil(train_loader._size / args.batch_size))
 
         if i%args.print_freq == 0:
             batch_time.update((time.time() - end)/args.print_freq)
@@ -266,31 +196,8 @@ def train(train_loader, epoch):
                        args.world_size*args.batch_size/batch_time.avg,
                        batch_time=batch_time))
 
+    print('Train loader size is {}, batch number {}, total time is {}, batch num {}, batch time average is {}'.format(train_loader._size, train_loader_len, batch_time.sum, batch_time.count, batch_time.avg))
     return batch_time.avg
-
-def validate(val_loader):
-    batch_time = AverageMeter()
-
-    end = time.time()
-
-    for i, data in enumerate(val_loader):
-        input = data[0]["data"]
-        target = data[0]["label"].squeeze(-1).long()
-        val_loader_len = int(val_loader._size / args.batch_size)
-
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        # TODO:  Change timings to mirror train().
-        if args.local_rank == 0 and i % args.print_freq == 0:
-            print('Test: [{0}/{1}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Speed {2:.3f} ({3:.3f})'.format(
-                   i, val_loader_len,
-                   args.world_size * args.batch_size / batch_time.val,
-                   args.world_size * args.batch_size / batch_time.avg,
-                   batch_time=batch_time))
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
