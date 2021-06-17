bb1ef50e0ddb21b3c227bcd8e15ee3572a3a5e34
Michał Zientkiewicz <mzient@gmail.com>
2021-06-17 18:21:48 +0200

    Map will set access on the same location where the physical allocation took place.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- include/dali/core/mm/cu_vm.h
+++ include/dali/core/mm/cu_vm.h
@@ -61,13 +61,40 @@ class CUMemAddressRange : public UniqueHandle<CUAddressRange, CUMemAddressRange>
  public:
   DALI_INHERIT_UNIQUE_HANDLE(CUAddressRange, CUMemAddressRange);
 
+  /**
+   * @brief Starting address of the VA reservation.
+   */
   CUdeviceptr ptr() const noexcept {
       return handle_.ptr();
   }
+
+  /**
+   * @brief Actual size, in bytes, of the VA reservation.
+   */
   size_t size() const noexcept {
       return handle_.size();
   }
 
+  /**
+   * @brief Reserves a virtual address range without physical backing storage
+   *
+   * This function is a wrapper around cuMemAddressReserve.
+   *
+   * The virtual address range reserved by this function is not allocated - however,
+   * it is guaranteed that physical allocations can be mapped to addresses from this range
+   * and that no other allocation functions (like cudaMalloc) will return addresses from this range.
+   *
+   * The addresses can be made usable by mapping phycial storage to them with cuvm::Map or
+   * cuMemMap + cuMemSetAccess.
+   *
+   * @param size      Size, in bytes - should be a multiple of GetAddressGranularity - if it's not
+   *                  it is rounded up to the next multiple
+   * @param alignment Mostly ignored
+   * @param start     The requested starting address - a hint for a starting dddress of the range.
+   *                  The driver will attempt to allocate the address range at the address given,
+   *                  but this is just a hint and the function will map a different range if the
+   *                  requested location is not available.
+   */
   static CUMemAddressRange Reserve(size_t size, size_t alignment = 0, CUdeviceptr start = 0) {
     CUdeviceptr ptr = 0;
     size_t grain = GetAddressGranularity();
@@ -106,19 +133,46 @@ struct CUMemAllocation : std::pair<CUmemGenericAllocationHandle, size_t> {
 
 /**
  * @brief Manages a backing storage block for a virtual address range
+ *
+ * This is a handle to a block of physical memory. This memory doesn't have a virtual address
+ * until mapped to an address range with cuvm::Map / cuMemMap
  */
 class CUMem : public UniqueHandle<CUMemAllocation, CUMem> {
  public:
   DALI_INHERIT_UNIQUE_HANDLE(CUMemAllocation, CUMem);
 
+  /**
+   * @brief Driver handle to the physical memory block.
+   */
   CUmemGenericAllocationHandle handle() const noexcept { return handle_.first; }
 
+  /**
+   * @brief Actual size, in bytes, of the allocation.
+   */
   size_t size() const noexcept { return handle_.second; }
 
+  /**
+   * @brief Allocates `size` bytes of physical storage with specified properties.
+   *
+   * The memory must be mapped to a virtual address range before being accessible.
+   * @see cuvm::Map
+   *
+   * @param size      Size, in bytes
+   * @param device_id The identifier of the device on which the memory is going to be located.
+   */
   static CUMem Create(size_t size, int device_id = -1) {
     return Create(size, DeviceMemProp(device_id));
   }
 
+  /**
+   * @brief Allocates `size` bytes of physical storage with specified properties.
+   *
+   * The memory must be mapped to a virtual address range before being accessible.
+   * @see cuvm::Map
+   *
+   * @param size Size, in bytes
+   * @param prop Allocation properties - indicate memory type and location
+   */
   static CUMem Create(size_t size, const CUmemAllocationProp &prop) {
     size = align_up(size, GetAddressGranularity());
     CUmemGenericAllocationHandle handle;
@@ -126,29 +180,35 @@ class CUMem : public UniqueHandle<CUMemAllocation, CUMem> {
     return CUMem({ handle, size });
   }
 
-
   static void DestroyHandle(CUMemAllocation handle) {
     CUDA_DTOR_CALL(cuMemRelease(handle.first));
   }
 };
 
+/**
+ * @brief Maps physical memory to a virtual address range and sets R/W access to it
+ *
+ * @param virt_addr The beginning of the virtual address range to map
+ * @param mem       The physical allocation handle
+ * @param size      The size, in bytes, of the range to map. Typically, this is the entire physical
+ *                  allocation size.
+ */
 template <typename T = void>
-T *Map(CUdeviceptr virt_addr, CUmemGenericAllocationHandle mem, size_t size, int device_id = -1) {
+T *Map(CUdeviceptr virt_addr, CUmemGenericAllocationHandle mem, size_t size) {
   CUDA_CALL(cuMemMap(virt_addr, size, 0, mem, 0));
 
-  if (device_id < 0)
-    CUDA_CALL(cudaGetDevice(&device_id));
   CUmemAccessDesc access = {};
-  access.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
-  access.location.id = device_id;
+  CUmemAllocationProp prop = {};
+  CUDA_CALL(cuMemGetAllocationPropertiesFromHandle(&prop, mem));
+  access.location = prop.location;
   access.flags = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
   CUDA_CALL(cuMemSetAccess(virt_addr, size, &access, 1));
   return reinterpret_cast<T*>(virt_addr);
 }
 
 template <typename T = void>
-T *Map(CUdeviceptr virt_addr, CUMemAllocation mem, int device_id = -1) {
-  return Map<T>(virt_addr, mem.handle(), mem.size(), device_id);
+T *Map(CUdeviceptr virt_addr, CUMemAllocation mem) {
+  return Map<T>(virt_addr, mem.handle(), mem.size());
 }
 
 inline void Unmap(const void *ptr, size_t size) {
