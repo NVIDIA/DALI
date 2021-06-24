d92581d9b6fdd2e22d0b337284b441ff3cfe410b
Joaquin Anton <janton@nvidia.com>
2021-06-24 17:47:59 +0200

    Bugfix
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/kernels/slice/slice_flip_normalize_permute_pad_cpu.h
+++ dali/kernels/slice/slice_flip_normalize_permute_pad_cpu.h
@@ -254,7 +254,7 @@ void SliceFlipNormalizePermutePadKernel(
   // Parallelize
   std::array<int, Dims> split_factor;
   int nblocks = split_shape(split_factor, args.out_shape, min_blk_sz,
-                            req_nblocks > 0 ? exec_engine.NumThreads() * 8 : req_nblocks,
+                            req_nblocks > 0 ? req_nblocks : exec_engine.NumThreads() * 8,
                             args.channel_dim);
   if (nblocks == 1) {
     exec_engine.AddWork([=](int) {
