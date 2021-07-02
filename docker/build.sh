089139e43f7f63f8d510201213ee9e04e253abc3
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2021-07-02 11:30:05 +0200

    Move to CUDA 11.4 build (#3109)
    * Move to CUDA 11.4 build
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,7 @@ a build environment
 
 To change build configuration please export appropriate env variables (for exact meaning please check the README):
 PYVER=[default 3.6, required only by Run image]
-CUDA_VERSION=[default 11.3, accepts also 10.0, 11.0 and 11.1]
+CUDA_VERSION=[default 11.4, accepts also 10.2, 11.0 and 11.1]
 NVIDIA_BUILD_ID=[default 12345]
 CREATE_WHL=[default YES]
 CREATE_RUNNER=[default NO]
@@ -40,12 +40,12 @@ shift $((OPTIND - 1))
 export ARCH=${ARCH:-x86_64}
 export PYVER=${PYVER:-3.6}
 export PYV=${PYVER/./}
-export CUDA_VERSION=${CUDA_VERSION:-11.3}
+export CUDA_VERSION=${CUDA_VERSION:-11.4}
 export CUDA_VER=${CUDA_VERSION//./}
 
 if [ "${CUDA_VERSION%%\.*}" ]
 then
-  if [ $CUDA_VER != "100" ] && [ $CUDA_VER != "110" ] && [ $CUDA_VER != "111" ] && [ $CUDA_VER != "112" ] && [ $CUDA_VER != "113" ]
+  if [ $CUDA_VER != "100" ] && [ $CUDA_VER != "110" ] && [ $CUDA_VER != "111" ] && [ $CUDA_VER != "112" ] && [ $CUDA_VER != "113" ] && [ $CUDA_VER != "114" ]
   then
       echo "Wrong CUDA_VERSION=$CUDA_VERSION provided. Only 10.0, 11.0, 11.1, 11.2 and 11.3 are supported"
       exit 1
