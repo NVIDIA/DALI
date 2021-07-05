31e69b2308e40651579ec0a2dfd4a7ab5aa33de0
Albert Wolant <awolant@nvidia.com>
2021-07-05 21:28:18 +0200

    Add native COCO support
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL3_EfficientDet_convergence/test_tensorflow.sh
+++ qa/TL3_EfficientDet_convergence/test_tensorflow.sh
@@ -15,21 +15,11 @@ python -m pip install -r requirements.txt
 
 
 python train.py \
-    --train_file_pattern 'train/*.tfrecord' \
-    --train_batch_size 32 \
-    --epochs 12 \
-    --train_steps 2000 \
-    --pipeline dali_gpu \
-    --multi_gpu \
-    --seed 1234 \
-    --hparams 'label_map: "coco" num_classes: 91' \
-
-python eval.py \
-    --eval_file_pattern 'val/*.tfrecord' \
-    --eval_steps 3000 \
-    --pipeline dali_gpu \
-    --hparams 'label_map: "coco" num_classes: 91' \
-    --weights output.h5 2>&1 | tee eval.log \
+ --epochs 2 --multi_gpu --seed 1234 --pipeline dali_gpu --input coco \
+ --images_path /data/coco/coco-2017/coco2017/train2017 \
+ --annotations_path /data/coco/coco-2017/coco2017/annotations/instances_train2017.json \
+ --train_batch_size 6 --train_steps 3000 --eval_after_training --eval_during_training \
+ --eval_steps 2000 --eval_freq 1
 
 RET=$(tail -n 1 eval.log | awk '{ exit ($NF < 0.2); }')
 
