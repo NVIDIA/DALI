3436594360c897462cb219e606defe953e17f29e
Albert Wolant <awolant@nvidia.com>
2021-07-06 09:53:03 +0200

    Adjust QA test
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL3_EfficientDet_convergence/test_tensorflow.sh
+++ qa/TL3_EfficientDet_convergence/test_tensorflow.sh
@@ -15,11 +15,11 @@ python -m pip install -r requirements.txt
 
 
 python train.py \
- --epochs 2 --multi_gpu --seed 1234 --pipeline dali_gpu --input coco \
+ --epochs 1 --multi_gpu --seed 1234 --pipeline dali_gpu --input coco \
  --images_path /data/coco/coco-2017/coco2017/train2017 \
  --annotations_path /data/coco/coco-2017/coco2017/annotations/instances_train2017.json \
- --train_batch_size 6 --train_steps 3000 --eval_after_training --eval_during_training \
- --eval_steps 2000 --eval_freq 1
+ --train_batch_size 4 --train_steps 4000 --eval_after_training --eval_during_training \
+ --eval_steps 1000 --eval_freq 1
 
 RET=$(tail -n 1 eval.log | awk '{ exit ($NF < 0.2); }')
 
