6f3e2313e75728a4129374798b507d96aa98a3fe
Krzysztof Lecki <klecki@nvidia.com>
2021-07-19 21:48:42 +0200

    Adjust test scripts and section header for webadataset notebook (#3162)
    Fix CI scripts.
    Move `### Sharding` section header in the notebook
    to separate cell as it didn't render correctly
    in the docs.
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- qa/TL0_jupyter/test_pytorch.sh
+++ qa/TL0_jupyter/test_pytorch.sh
@@ -4,12 +4,15 @@ pip_packages="pillow jupyter numpy matplotlib torch torchvision webdataset"
 target_dir=./docs/examples
 
 test_body() {
-    jupyter nbconvert --to notebook --inplace --execute \
-                      --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
-                      --ExecutePreprocessor.timeout=300 custom_operations/python_operator.ipynb
-    jupyter nbconvert --to notebook --inplace --execute \
-                      --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
-                      --ExecutePreprocessor.timeout=300 use_cases/webdataset-externalsource.ipynb
+    test_files=(
+        "custom_operations/python_operator.ipynb"
+        "use_cases/webdataset-externalsource.ipynb"
+    )
+    for f in ${test_files[@]}; do
+        jupyter nbconvert --to notebook --inplace --execute \
+                        --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
+                        --ExecutePreprocessor.timeout=300 $f;
+    done
 }
 
 pushd ../..
