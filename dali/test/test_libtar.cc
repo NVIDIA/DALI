18c395694addf0e550d36fe31a1063221417c9b0
Bartłomiej Cieślar <bcieslar2001@gmail.com>
2021-07-22 13:35:13 +0200

    Added bingins for libtar
    Signed-off-by: Bartłomiej Cieślar <bcieslar2001@gmail.com>

---
+++ dali/test/test_libtar.cc
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include <libtar.h>
+
+#include <cstdlib>
+#include <string>
+#include <fcntl.h>
+#include <iostream>
+
+#include "dali/operators/reader/loader/filesystem.h"
+
+namespace dali {
+namespace testing {
+
+TEST(LibTar, OpenClose) {
+  std::string filepath(dali::filesystem::join_path(
+      std::getenv("DALI_EXTRA_PATH"),
+      "db/webdataset/MNIST/devel-0.tar"
+  ));
+
+  TAR* archive;
+  ASSERT_EQ(tar_open(&archive, filepath.c_str(), NULL, O_RDONLY, 0, TAR_GNU), 0);
+  ASSERT_EQ(tar_close(archive), 0);
+}
+
+} // namespace testing
+} // namespace dali 
