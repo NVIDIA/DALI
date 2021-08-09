4b4c4f58a91f67a7f10a677ab246a859f77202ba
Bartłomiej Cieślar <bcieslar2001@gmail.com>
2021-08-09 13:22:51 +0200

    Implementation skeleton for wds loader
    Signed-off-by: Bartłomiej Cieślar <bcieslar2001@gmail.com>

--- tools/wds2idx.py
+++ tools/wds2idx.py
@@ -17,10 +17,9 @@
 # under the License.
 
 import os
-import sys
 import time
 import argparse
-import subprocess
+import tarfile
 
 class IndexCreator():
     """Reads `Webdataset` data format, and creates index file
@@ -44,10 +43,16 @@ class IndexCreator():
     def __init__(self, uri, idx_path):
         self.uri = uri
         self.idx_path = idx_path
+        self.farchive = tarfile.TarFile(self.uri)
         self.fidx = open(self.idx_path, 'w')
 
     def open(self):
         """Opens the archive and index files and sets their read heads to 0."""
+        if self.farchive.closed:
+            self.farchive = tarfile.TarFile(self.uri)
+        else:
+            self.farchive.fileobj.seek(0)
+
         if self.fidx.closed:
             self.fidx = open(self.idx_path, 'w')
         else:
@@ -55,6 +60,8 @@ class IndexCreator():
 
     def close(self):
         """Closes the archive and index files."""
+        if not self.farchive.closed:
+            self.farchive.close()
         if not self.fidx.closed:
             self.fidx.close()
 
@@ -63,88 +70,29 @@ class IndexCreator():
         self.close()
         self.open()
 
-    @staticmethod
-    def split_name(filepath): # translated from the matching function in c++
-        """Splits the webdataset into the basename and the extension
-        """
-        base_name_pos = filepath.rfind('\\') + 1
-        dot_pos = filepath.find('.', base_name_pos + 1)
-        if dot_pos == -1:
-            return filepath, ""
-        return filepath[:dot_pos], filepath[dot_pos + 1:]
-
-
     def create_index(self):
         """Creates the index file from open record file
         """
         self.reset()
 
+        # Has to parse the archive first because needs the number of files in the archive
         pre_time = time.time()
+        data = []
         counter = 0
         report_step = 100000
-
-        print(f"time: {time.time() - pre_time:.2f} count: {counter} stage: collect")
-        tar_blocks_proc = subprocess.Popen(["tar", "--list", "--block-num", "--file", self.uri], stdout=subprocess.PIPE)
-        tar_types_sizes_proc = subprocess.Popen(["tar", "--verbose", "--list", "--file", self.uri], stdout=subprocess.PIPE)
-
-        tar_blocks = tar_blocks_proc.communicate()[0].split(b'\n')
-        tar_types_sizes = tar_types_sizes_proc.communicate()[0].split(b'\n')
-
-        last_blocks_line = None
-        for blocks_line in reversed(tar_blocks):
-            if not not blocks_line:
-                last_blocks_line = blocks_line
-                break
-
-        print(str(b'\n'.join(tar_blocks), "ascii"))
-
-        total_size = int(last_blocks_line[last_blocks_line.find(b'block') + 6 : last_blocks_line.find(b':')]) * 512
-
-        tar_data = zip(tar_blocks, tar_types_sizes)
-        tar_data = filter(lambda line: not not line[0] and not not line[1], tar_data)
-
-        # Aggregate extensions in samples
-        data = []
-        last_skipped = 0
-        last_basename = None
-        for blocks_line, types_sizes_line in tar_data:
+        for member in iter(self.farchive):
             if counter % report_step == 0:
-                cur_time = time.time()
-                print(f"time: {cur_time - pre_time:.2f} count: {counter} stage: collect")
+                  cur_time = time.time()
+                  print(f"time: {cur_time - pre_time:.2f} count: {counter} stage: collect")
+            data.append((member.name, member.offset))
             counter += 1
 
-            offset = int(blocks_line[blocks_line.find(b'block') + 6 : blocks_line.find(b':')]) * 512
-            name = str(blocks_line[blocks_line.find(b':') + 2:], 'ascii')
-            entry_type = types_sizes_line[0:1]
-
-            if entry_type != b'-' or name.startswith('.'):
-                continue
-
-            # Extracting size
-            size = types_sizes_line[:-len(name)]
-            size = size[:size.rfind(b'-') - 8] # "... <size> 20yy-mm-...."
-            size = int(size[size.rfind(b' '):])
-
-            basename, extension = IndexCreator.split_name(name)
-            
-            if last_basename != basename:
-                data.append((offset, [(extension, size)]))
-                last_basename = basename
-            else:
-                data[-1][1].append((extension, size))
-
-        if not data:
-            raise ValueError("Webdataset Tar File empty")
-
-        # Then construct the index file out of it
-        self.fidx.write(f"{total_size} {len(data)}\n")
-        for offset, extensions_sizes in data:
+        self.fidx.write(f"{len(data)}\n")
+        for name, offset in data:
             if counter % report_step == 0:
-                cur_time = time.time()
-                print(f"time: {cur_time - pre_time:.2f} count: {counter} stage: index")
-            self.fidx.write(f"""{offset} {' '.join(
-                map(lambda ext_size: str(ext_size[0]) + ' ' + str(ext_size[1]), extensions_sizes)
-            )}\n""")
+                  cur_time = time.time()
+                  print(f"time: {cur_time - pre_time:.2f} count: {counter} stage: index")
+            self.fidx.write(f"{offset} {name}\n")
             counter += 1
         
         cur_time = time.time()
