NVIDIA/DALI

commit 9e2ea4dd3ad9c8470de07c3faf1a66804cb3bfbc

Fix memory leak in backend_impl.cc caused by PyObject_GetAttr (#3233)
- not increasing reference count to the result of PyObject_GetAttr
  causes leaking of the python object. This PR moves to the usage of
  `getattr` to handle this automatically

Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

dali/python/backend_impl.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2017-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -56,15 +56,11 @@ void __backend_impl_force_tls_align_fun(void) {}
 using namespace pybind11::literals; // NOLINT
 
 static void* ctypes_void_ptr(const py::object& object) {
-  PyObject *p_ptr = object.ptr();
-  if (!PyObject_HasAttr(p_ptr, PyUnicode_FromString("value"))) {
+  auto ptr_as_int = getattr(object, "value", py::none());
+  if (ptr_as_int.is_none()) {
     return nullptr;
   }
-  PyObject *ptr_as_int = PyObject_GetAttr(p_ptr, PyUnicode_FromString("value"));
-  if (ptr_as_int == Py_None) {
-    return nullptr;
-  }
-  void *ptr = PyLong_AsVoidPtr(ptr_as_int);
+  void *ptr = PyLong_AsVoidPtr(ptr_as_int.ptr());
   return ptr;
 }
 
@@ -199,15 +195,10 @@ void FillTensorFromDlPack(py::capsule capsule, SourceDataType<SrcBackend> *batch
 template <typename TensorType>
 void FillTensorFromCudaArray(const py::object object, TensorType *batch, int device_id,
                              string layout) {
-  PyObject *p_ptr = object.ptr();
-  if (!PyObject_HasAttr(p_ptr, PyUnicode_FromString("__cuda_array_interface__"))) {
-    DALI_FAIL("Provided object doesn't support cuda array interface protocol")
-  }
-  PyObject *ptr_intf = PyObject_GetAttr(p_ptr, PyUnicode_FromString("__cuda_array_interface__"));
-  if (ptr_intf == Py_None) {
+  py::dict cu_a_interface = getattr(object, "__cuda_array_interface__", py::none());
+  if (cu_a_interface.is_none()) {
     DALI_FAIL("Provided object doesn't support cuda array interface protocol.")
   }
-  py::dict cu_a_interface = py::reinterpret_borrow<py::dict>(ptr_intf);
 
   DALI_ENFORCE(cu_a_interface.contains("typestr") &&
                 // see detail::PyUnicode_Check_Permissive implementation

commit 0b35a00cfb1e649ac84c0951233e43360ed8f853

Fix an assigment of py::none() to py::dict in backend_impl.cc
- fixes problem introduced in https://github.com/NVIDIA/DALI/pull/3233
  when there is no "__cuda_array_interface__" in the object used
  to create TensorGPU, None is returned and assigned to Dict what
  fails. Fix the problem by assigning the result to a generic object,
  and when it is not None casting it to Dict

Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

dali/python/backend_impl.cc
@@ -195,10 +195,11 @@ void FillTensorFromDlPack(py::capsule capsule, SourceDataType<SrcBackend> *batch
 template <typename TensorType>
 void FillTensorFromCudaArray(const py::object object, TensorType *batch, int device_id,
                              string layout) {
-  py::dict cu_a_interface = getattr(object, "__cuda_array_interface__", py::none());
-  if (cu_a_interface.is_none()) {
+  auto cu_a_interface_val = getattr(object, "__cuda_array_interface__", py::none());
+  if (cu_a_interface_val.is_none()) {
     DALI_FAIL("Provided object doesn't support cuda array interface protocol.")
   }
+  py::dict cu_a_interface = py::cast<py::dict>(cu_a_interface_val);
 
   DALI_ENFORCE(cu_a_interface.contains("typestr") &&
                 // see detail::PyUnicode_Check_Permissive implementation