6c9b25b82683fb51a1a9666e8b1a48b735de4386
Krzysztof Lecki <klecki@nvidia.com>
2021-08-12 15:59:10 +0200

    Fix running Python core tests (#3249)
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- qa/TL0_python-self-test-core/test_body.sh
+++ qa/TL0_python-self-test-core/test_body.sh
@@ -4,7 +4,7 @@ test_nose() {
     for test_script in $(ls test_pipeline*.py \
                             test_functional_api.py \
                             test_backend_impl.py \
-                            test_dali_variable_batch_size.py
+                            test_dali_variable_batch_size.py \
                             test_external_source_impl_utils.py); do
         nosetests --verbose --attr '!slow,!pytorch,!mxnet,!cupy' ${test_script}
     done
