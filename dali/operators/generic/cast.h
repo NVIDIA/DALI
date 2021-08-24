fabdf1b076133c5c6fffa1693928b347bd875411
Krzysztof Lecki <klecki@nvidia.com>
2021-08-24 14:09:25 +0200

    Lint
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/operators/generic/cast.h
+++ dali/operators/generic/cast.h
@@ -32,7 +32,7 @@ struct CastSampleDesc {
 
 #define CAST_ALLOWED_TYPES                                                                         \
   (bool, uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t, int64_t, float16, float, \
-   double)
+  double)
 
 template <typename Backend>
 class Cast : public Operator<Backend> {
