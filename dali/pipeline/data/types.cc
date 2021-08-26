cf3b21913098d217aa4a0b260c6766ff3100814c
Michał Zientkiewicz <mzient@gmail.com>
2021-08-26 15:30:13 +0200

    Add include to types.cc where it's still needed.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/pipeline/data/types.cc
+++ dali/pipeline/data/types.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2017-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ const auto &_type_info_##Id = TypeTable::GetTypeID<Type>()
 
 #include "dali/pipeline/data/backend.h"
 #include "dali/core/per_stream_pool.h"
+#include "dali/kernels/common/scatter_gather.h"
 
 namespace dali {
 
