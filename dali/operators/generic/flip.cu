cc80b7f47b73ddab739f557f34755bf694746596
Krzysztof Lecki <klecki@nvidia.com>
2021-08-26 18:55:17 +0200

    Remove access to underlying contiguous TL buffer in Flip op (#3280)
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/operators/generic/flip.cu
+++ dali/operators/generic/flip.cu
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2019-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "dali/operators/generic/flip.h"
 #include <cuda_runtime_api.h>
 #include <vector>
 #include "dali/kernels/imgproc/flip_gpu.cuh"
+#include "dali/operators/generic/flip.h"
 #include "dali/operators/generic/flip_util.h"
+#include "dali/pipeline/data/views.h"
 
 namespace dali {
 
@@ -29,13 +30,13 @@ void RunKernel(TensorList<GPUBackend> &output, const TensorList<GPUBackend> &inp
   DALI_TYPE_SWITCH(
       input.type().id(), DType,
       auto in_shape = TransformShapes(input.shape(), input.GetLayout());
-      kernels::InListGPU<DType, flip_ndim> in_view(input.data<DType>(), in_shape);
+      auto in_view = reshape<flip_ndim>(view<const DType>(input), in_shape);
       kernels::KernelContext ctx;
       ctx.gpu.stream = stream;
       kernels::FlipGPU<DType> kernel;
       auto reqs = kernel.Setup(ctx, in_view);
-      kernels::OutListGPU<DType, flip_ndim> out_view(output.mutable_data<DType>(),
-                                             reqs.output_shapes[0].to_static<flip_ndim>());
+      auto out_shape = reqs.output_shapes[0].to_static<flip_ndim>();
+      auto out_view = reshape<flip_ndim>(view<DType>(output), out_shape);
       kernel.Run(ctx, out_view, in_view, depthwise, vertical, horizontal);
   )
 }
