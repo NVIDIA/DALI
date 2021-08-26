ce421311e4a5c1139542e160b21a1ff295135c5f
Michał Zientkiewicz <mzient@gmail.com>
2021-08-26 15:26:22 +0200

    Remove the inclusion of scatter_gather.h from types.h
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/pipeline/data/types.h
+++ dali/pipeline/data/types.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2018, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2017-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +25,6 @@
 #include <typeinfo>
 #include <unordered_map>
 #include <vector>
-#include "dali/kernels/common/scatter_gather.h"
 #include "dali/core/common.h"
 #include "dali/core/spinlock.h"
 #include "dali/core/float16.h"
