10964054d28e784085deb96f34b2575ce7aff5ab
Krzysztof Lecki <klecki@nvidia.com>
2021-09-06 13:28:11 +0200

    Tf dataset device mismatch
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/test/python/test_dali_tf_dataset_mnist_graph.py
+++ dali/test/python/test_dali_tf_dataset_mnist_graph.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 from test_dali_tf_dataset_mnist import *
-from nose.tools import raises
+from nose_utils import raises
 
 tf.compat.v1.disable_eager_execution()
 
@@ -29,7 +29,7 @@ def test_keras_single_cpu():
     run_keras_single_device('cpu', 0)
 
 
-@raises(Exception)
+@raises(Exception, "TF device and DALI device mismatch. TF*: CPU, DALI*: GPU for output*")
 def test_keras_wrong_placement_gpu():
     with tf.device('cpu:0'):
         model = keras_model()
@@ -41,7 +41,7 @@ def test_keras_wrong_placement_gpu():
         steps_per_epoch=ITERATIONS)
 
 
-@raises(Exception)
+@raises(Exception, "TF device and DALI device mismatch. TF*: GPU, DALI*: CPU for output*")
 def test_keras_wrong_placement_cpu():
     with tf.device('gpu:0'):
         model = keras_model()
