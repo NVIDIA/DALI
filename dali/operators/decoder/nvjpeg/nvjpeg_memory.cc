899e80e8d7bf676e450114c71f1d6dea045a07fd
Michał Zientkiewicz <mzient@gmail.com>
2021-09-07 09:43:50 +0200

    Restore nvjpeg_memory.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/operators/decoder/nvjpeg/nvjpeg_memory.cc
+++ dali/operators/decoder/nvjpeg/nvjpeg_memory.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,49 +24,51 @@
 #include <utility>
 #include <vector>
 #include <memory>
+#include <unordered_map>
 #include "dali/core/cuda_error.h"
-#include "dali/kernels/alloc.h"
+#include "dali/core/mm/memory.h"
+#include "dali/core/mm/memory_kind.h"
+#include "dali/core/mm/malloc_resource.h"
 
 namespace dali {
-
-using kernels::memory::KernelUniquePtr;
-using kernels::AllocType;
-using kernels::memory::alloc_unique;
-using kernels::memory::Allocate;
 
 namespace nvjpeg_memory {
 
 struct AllocInfo {
-  AllocType alloc_type;
+  mm::memory_kind_id kind;
+  mm::Deleter deleter;
   size_t size;
   std::thread::id thread_id;
 };
 
-std::map<void*, AllocInfo> alloc_info_;
+std::unordered_map<void*, AllocInfo> alloc_info_;
 std::shared_timed_mutex alloc_info_mutex_;
 
 struct Deleter {
-  kernels::memory::Deleter deleter;
-  explicit Deleter(AllocType alloc_type) : deleter(kernels::memory::GetDeleter(alloc_type)) {}
-
-  inline void operator()(void *p) {
-    deleter(p);
-    std::unique_lock<std::shared_timed_mutex> lock(alloc_info_mutex_);
-    alloc_info_.erase(p);
+  inline void operator()(void *p) const {
+    AllocInfo ai;
+    {
+      std::lock_guard<std::shared_timed_mutex> lock(alloc_info_mutex_);
+      auto it = alloc_info_.find(p);
+      assert(it != alloc_info_.end());
+      ai = std::move(it->second);
+      alloc_info_.erase(it);
+    }
+    ai.deleter(p);
   }
 };
 
 using unique_ptr_t = std::unique_ptr<char, Deleter>;
 
 struct Buffer {
-  Buffer(unique_ptr_t unq_ptr, AllocType type, size_t sz)
-      : ptr(std::move(unq_ptr)), alloc_type(type), size(sz) {}
+  Buffer(unique_ptr_t unq_ptr, mm::memory_kind_id kind, size_t sz)
+      : ptr(std::move(unq_ptr)), kind(kind), size(sz) {}
   unique_ptr_t ptr;
-  AllocType alloc_type;
+  mm::memory_kind_id kind;
   size_t size;
 };
 
-using MemoryPool = std::array<std::vector<Buffer>, static_cast<size_t>(AllocType::Count)>;
+using MemoryPool = std::array<std::vector<Buffer>, static_cast<size_t>(mm::memory_kind_id::count)>;
 using BufferPool = std::map<std::thread::id, MemoryPool>;
 
 BufferPool buffer_pool_;
@@ -77,7 +79,7 @@ struct MemoryStats {
   size_t biggest_alloc = 0;
 };
 
-std::array<MemoryStats, static_cast<size_t>(AllocType::Count)> mem_stats_;
+std::array<MemoryStats, static_cast<size_t>(mm::memory_kind_id::count)> mem_stats_;
 std::mutex mem_stats_mutex_;
 std::atomic<bool> mem_stats_enabled_ = {true};
 
@@ -85,10 +87,11 @@ void SetEnableMemStats(bool enabled) {
   mem_stats_enabled_ = enabled;
 }
 
-void AddMemStats(AllocType alloc_type, size_t size) {
+template <typename MemoryKind>
+void AddMemStats(size_t size) {
   if (mem_stats_enabled_) {
     std::lock_guard<std::mutex> lock(mem_stats_mutex_);
-    auto &stats = mem_stats_[static_cast<size_t>(alloc_type)];
+    auto &stats = mem_stats_[static_cast<size_t>(mm::kind2id_v<MemoryKind>)];
     stats.nallocs++;
     if (size > stats.biggest_alloc)
       stats.biggest_alloc = size;
@@ -105,31 +108,46 @@ void PrintMemStats() {
     std::ostream& out = log_filename ? log_file : std::cout;
     out << std::dec;  // Don't want numbers printed as hex
     out << "#################### NVJPEG STATS ####################" << std::endl;
-    auto &device_mem_stats = mem_stats_[static_cast<size_t>(AllocType::GPU)];
+    auto &device_mem_stats = mem_stats_[static_cast<size_t>(mm::memory_kind_id::device)];
     out << "Device memory: " << device_mem_stats.nallocs
         << " allocations, largest = " << device_mem_stats.biggest_alloc << " bytes\n";
-    auto &pinned_mem_stats = mem_stats_[static_cast<size_t>(AllocType::Pinned)];
+    auto &pinned_mem_stats = mem_stats_[static_cast<size_t>(mm::memory_kind_id::pinned)];
     out << "Host (pinned) memory: " << pinned_mem_stats.nallocs
         << " allocations, largest = " << pinned_mem_stats.biggest_alloc << " bytes\n";
     out << "################## END NVJPEG STATS ##################" << std::endl;
   }
 }
 
-unique_ptr_t Allocate(std::thread::id thread_id, AllocType alloc_type, size_t size) {
-  auto ptr = kernels::memory::alloc_unique<char>(alloc_type, size);
+template <typename MemoryKind>
+unique_ptr_t Allocate(mm::memory_resource<MemoryKind> *mr, std::thread::id thread_id, size_t size) {
+  auto ptr = mm::alloc_raw_unique<char>(mr, size);
   std::unique_lock<std::shared_timed_mutex> lock(alloc_info_mutex_);
-  alloc_info_[ptr.get()] = {alloc_type, size, thread_id};
-  return {ptr.release(), Deleter(alloc_type)};
+  AllocInfo &ai = alloc_info_[ptr.get()];  // create entry first (before moving out the deleter)
+  ai = { mm::memory_kind_id::device, std::move(ptr.get_deleter()), size, thread_id};
+  return {ptr.release(), {}};
 }
 
-void* GetBuffer(std::thread::id thread_id, AllocType alloc_type, size_t size) {
+template <typename MemoryKind>
+unique_ptr_t Allocate(std::thread::id thread_id, size_t size) {
+  return Allocate(mm::GetDefaultResource<MemoryKind>(), thread_id, size);
+}
+
+template <>
+unique_ptr_t Allocate<mm::memory_kind::device>(std::thread::id thread_id, size_t size) {
+  // use plain cudaMalloc
+  return Allocate(&mm::cuda_malloc_memory_resource::instance(), thread_id, size);
+}
+
+template <typename MemoryKind>
+void* GetBuffer(std::thread::id thread_id, size_t size) {
   std::shared_lock<std::shared_timed_mutex> lock(buffer_pool_mutex_);
+  mm::memory_kind_id kind = mm::kind2id_v<MemoryKind>;
   auto it = buffer_pool_.find(thread_id);
   auto end_it =  buffer_pool_.end();
   lock.unlock();
   // only if pool exits for given thread_id search it, otherwise just allocate
   if (it != end_it) {
-    auto &buffers = it->second[static_cast<size_t>(alloc_type)];
+    auto &buffers = it->second[static_cast<size_t>(kind)];
     auto best_fit = buffers.end();
     auto smallest = buffers.end();
     for (auto it = buffers.begin(); it != buffers.end(); ++it) {
@@ -152,8 +170,8 @@ void* GetBuffer(std::thread::id thread_id, AllocType alloc_type, size_t size) {
     }
   }
   // Couldn't find a preallocated buffer, proceed to allocate
-  AddMemStats(alloc_type, size);
-  return Allocate(thread_id, alloc_type, size).release();
+  AddMemStats<MemoryKind>(size);
+  return Allocate<MemoryKind>(thread_id, size).release();
 }
 
 static int ReturnBufferToPool(void *raw_ptr) {
@@ -162,7 +180,7 @@ static int ReturnBufferToPool(void *raw_ptr) {
   assert(info_it != alloc_info_.end());
   auto info = info_it->second;
   info_lock.unlock();
-  std::unique_ptr<char, Deleter> ptr(reinterpret_cast<char*>(raw_ptr), Deleter(info.alloc_type));
+  std::unique_ptr<char, Deleter> ptr(reinterpret_cast<char*>(raw_ptr), {});
   std::shared_lock<std::shared_timed_mutex> lock(buffer_pool_mutex_);
   auto it = buffer_pool_.find(info.thread_id);
   auto end_it =  buffer_pool_.end();
@@ -175,19 +193,21 @@ static int ReturnBufferToPool(void *raw_ptr) {
     std::unique_lock<std::shared_timed_mutex> lock(buffer_pool_mutex_);
     pool = &(buffer_pool_[info.thread_id]);
   }
-  auto &buffers = (*pool)[static_cast<size_t>(info.alloc_type)];
-  buffers.emplace_back(std::move(ptr), info.alloc_type, info.size);
+  auto &buffers = (*pool)[static_cast<size_t>(info.kind)];
+  buffers.emplace_back(std::move(ptr), info.kind, info.size);
   return 0;
 }
 
-void AddBuffer(std::thread::id thread_id, AllocType alloc_type, size_t size) {
+template <typename MemoryKind>
+void AddBuffer(std::thread::id thread_id, size_t size) {
   std::unique_lock<std::shared_timed_mutex> lock(buffer_pool_mutex_);
+  auto kind = mm::kind2id_v<MemoryKind>;
   auto& thread_buffer_pool = buffer_pool_[thread_id];
   lock.unlock();
 
-  auto &buffers = thread_buffer_pool[static_cast<size_t>(alloc_type)];
-  buffers.emplace_back(Allocate(thread_id, alloc_type, size), alloc_type, size);
-  AddMemStats(alloc_type, size);
+  auto &buffers = thread_buffer_pool[static_cast<size_t>(kind)];
+  buffers.emplace_back(Allocate<MemoryKind>(thread_id, size), kind, size);
+  AddMemStats<MemoryKind>(size);
 }
 
 void DeleteAllBuffers(std::thread::id thread_id) {
@@ -203,6 +223,9 @@ void DeleteAllBuffers(std::thread::id thread_id) {
     buffer.clear();
 }
 
+template void AddBuffer<mm::memory_kind::device>(std::thread::id thread_id, size_t size);
+template void AddBuffer<mm::memory_kind::pinned>(std::thread::id thread_id, size_t size);
+
 static int DeviceNew(void **ptr, size_t size) {
   if (size == 0) {
     *ptr = nullptr;
@@ -210,7 +233,7 @@ static int DeviceNew(void **ptr, size_t size) {
   }
   // this function should not throw, but return a proper result
   try {
-    *ptr = GetBuffer(std::this_thread::get_id(), AllocType::GPU, size);
+    *ptr = GetBuffer<mm::memory_kind::device>(std::this_thread::get_id(), size);
     return *ptr != nullptr ? cudaSuccess : cudaErrorMemoryAllocation;
   } catch (const std::bad_alloc &) {
     *ptr = nullptr;
@@ -230,7 +253,7 @@ static int PinnedNew(void **ptr, size_t size, unsigned int flags) {
   }
   // this function should not throw, but return a proper result
   try {
-    *ptr = GetBuffer(std::this_thread::get_id(), AllocType::Pinned, size);
+    *ptr = GetBuffer<mm::memory_kind::pinned>(std::this_thread::get_id(), size);
     return *ptr != nullptr ? cudaSuccess : cudaErrorMemoryAllocation;
   } catch (const std::bad_alloc &) {
     *ptr = nullptr;
