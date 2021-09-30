df5fff6cf7aa0f2889d3e995880fe68f64fa7c2d
Albert Wolant <awolant@nvidia.com>
2021-09-30 16:56:15 +0200

    Fix CI
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/operators/reader/loader/video/video_test_base.cc
+++ dali/operators/reader/loader/video/video_test_base.cc
@@ -30,6 +30,10 @@ std::vector<std::vector<cv::Mat>> VideoTestBase::cfr_frames_;
 std::vector<std::vector<cv::Mat>> VideoTestBase::vfr_frames_;
 
 void VideoTestBase::SetUpTestSuite() {
+  if (cfr_frames_.size() > 0) {
+    // This setup is called for each test fixture, but we need it only once
+    return;
+  }
   std::vector<std::string> cfr_frames_paths{
       testing::dali_extra_path() + "/db/video/cfr/frames_1/",
       testing::dali_extra_path() + "/db/video/cfr/frames_2/"};
