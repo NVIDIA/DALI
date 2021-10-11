b6af8a15f868901e906644afa8fa890622cbfb32
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2021-10-11 18:16:33 +0200

    Enable usage of sanitizers in tests (#3377)
    * Enable usage of sanitizers in tests
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/test_template.sh
+++ qa/test_template.sh
@@ -45,12 +45,53 @@ epilog=${epilog-:}
 # get the number of elements in `prolog` array
 numer_of_prolog_elms=${#prolog[@]}
 
+# turn on sanitizers
+enable_sanitizer() {
+    # supress leaks that are false positive or not related to DALI
+    export LSAN_OPTIONS=suppressions=$topdir/qa/leak.sup
+    export ASAN_OPTIONS=symbolize=1:protect_shadow_gap=0:log_path=sanitizer.log:start_deactivated=true:allocator_may_return_null=1::detect_leaks=1
+    export ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer)
+    # avoid python false positives
+    export PYTHONMALLOC=malloc
+}
+
+# turn off sanitizer to avoid breaking any non-related system built-ins
+disable_sanitizer() {
+    export ASAN_OPTIONS=start_deactivated=true:detect_leaks=0
+    unset ASAN_SYMBOLIZER_PATH
+    unset PYTHONMALLOC
+}
+
 # Wrap the test_body in a subshell, where we can safely execute it with `set -e`
 # and turn it off in current shell to intercept the error code
+# when sanitizers are on, do set +e to run all the test no matter what the result is
+# and collect as much of sanitizers output as possible
 test_body_wrapper() {(
-    set -e
+    if [ -n "$DALI_ENABLE_SANITIZERS" ]; then
+        set +e
+        enable_sanitizer
+    else
+        set -e
+    fi
+
     test_body
+
+    if [ -n "$DALI_ENABLE_SANITIZERS" ]; then
+        disable_sanitizer
+    fi
 )}
+
+process_sanitizers_logs() {
+    find $topdir -iname "sanitizer.log.*" -print0 | xargs -0 -I file cat file > $topdir/sanitizer.log
+    if [ -e $topdir/sanitizer.log ]; then
+        cat $topdir/sanitizer.log
+        grep -q ERROR $topdir/sanitizer.log || true
+        # ToDo - enable when the suppression file is completed
+        # grep -q ERROR $topdir/sanitizer.log && exit 1 || true
+    fi
+    # rm so the consequitive test won't reread the same logs over and over
+    find $topdir -iname "sanitizer.log.*" -delete
+}
 
 # get extra index url for given packages
 extra_indices=$($topdir/qa/setup_packages.py -u $pip_packages --cuda ${CUDA_VERSION} -e)
@@ -93,7 +134,8 @@ do
         test_body_wrapper
         RV=$?
         set -e
-        if [ $RV -gt 0 ] ; then
+        # if sanitizers are enabled run test until the end so we have as much data as possible
+        if [ $RV -gt 0 ] && [ -z "$DALI_ENABLE_SANITIZERS" ]; then
             mkdir -p $topdir/core_artifacts
             cp core* $topdir/core_artifacts || true
             exit ${RV}
@@ -106,3 +148,7 @@ do
         ${epilog[variant]}
     done
 done
+
+if [ -n "$DALI_ENABLE_SANITIZERS" ]; then
+    process_sanitizers_logs
+fi
