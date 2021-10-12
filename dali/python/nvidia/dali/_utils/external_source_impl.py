3d72b74fc9b5d004752a5dd2865a9e1b59539886
Kamil Tokarski <ktokarski@nvidia.com>
2021-10-12 21:46:18 +0200

    Fix SampleInfo usage in tf and tests
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- dali/python/nvidia/dali/_utils/external_source_impl.py
+++ dali/python/nvidia/dali/_utils/external_source_impl.py
@@ -333,7 +333,7 @@ def get_batch_iterable_from_callback(source_desc):
 def get_sample_iterable_from_callback(source_desc, batch_size):
     """Transform sample callback accepting one argument into an Iterable
     """
-    first = source_desc.source(types.SampleInfo(0, 0, 0))
+    first = source_desc.source(types.SampleInfo(0, 0, 0, 0))
     dtype, shape = _inspect_data(first, False)
 
     class CallableSampleIterator:
@@ -355,7 +355,7 @@ def get_sample_iterable_from_callback(source_desc, batch_size):
                 result = CallableSampleIterator.first_value
                 CallableSampleIterator.first_value = None
             else:
-                idx = types.SampleInfo(self.idx_in_epoch, self.idx_in_batch, self.iteration)
+                idx = types.SampleInfo(self.idx_in_epoch, self.idx_in_batch, self.iteration, 0)
                 result = self.source(idx)
             self.idx_in_epoch += 1
             self.idx_in_batch += 1
