d60738fafa78faf4e3a5f142c21da12b7fb487ab
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2021-10-21 16:52:51 +0200

    Enable sanitizers in regular CI runs (#3422)
    * Enable sanitizers in regular CI runs
    
    - adds a suppression list that should disable all detected
      and not DALI originated errors
    - makes all leaks to fail the test job
    - turns off `fast_unwind_on_malloc` to make the stack trace
      more accurate (but slower) and allow more accurate suppressions,
      when the frame pointer is not preserved (python binary)
    - shortens tests TF based L0 test and excludes numpy_reader test when sanitizers are on
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_python-self-test-readers-decoders/test_body.sh
+++ qa/TL0_python-self-test-readers-decoders/test_body.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -e
 
 test_nose() {
+    # numpy seems to be extremly slow with sanitizers to dissable it
+    if [ -n "$DALI_ENABLE_SANITIZERS" ]; then
+        FILTER_PATTERN="test_operator_readers_numpy.py"
+    else
+        FILTER_PATTERN="#"
+    fi
+
     for test_script in $(ls test_operator_readers_*.py test_operator_decoders_*.py \
       test_external_source_dali.py test_external_source_numpy.py \
       test_external_source_parallel_garbage_collection_order.py \
       test_external_source_parallel_custom_serialization.py \
-      test_pool.py test_external_source_parallel.py test_external_source_parallel_shared_batch.py); do
+      test_pool.py test_external_source_parallel.py test_external_source_parallel_shared_batch.py \
+      | sed "/$FILTER_PATTERN/d"); do
         nosetests --verbose --attr '!slow' ${test_script}
     done
 }
