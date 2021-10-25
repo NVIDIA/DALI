bb797f06d129855c2362104b3782942d93057343
Krzysztof Lecki <klecki@nvidia.com>
2021-10-25 15:40:09 +0200

    Update custom op example for newer CMake
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- docs/examples/custom_operations/custom_operator/customdummy/dummy.cu
+++ docs/examples/custom_operations/custom_operator/customdummy/dummy.cu
@@ -5,9 +5,9 @@ namespace other_ns {
 
 template<>
 void Dummy<::dali::GPUBackend>::RunImpl(::dali::DeviceWorkspace &ws) {
-  const auto &input = ws.Input<::dali::GPUBackend>(0);
+  const auto &input = ws.InputRef<::dali::GPUBackend>(0);
   const auto &shape = input.shape();
-  auto &output = ws.Output<::dali::GPUBackend>(0);
+  auto &output = ws.OutputRef<::dali::GPUBackend>(0);
   for (int sample_idx = 0; sample_idx < shape.num_samples(); sample_idx++) {
     CUDA_CALL(cudaMemcpyAsync(
             output.raw_mutable_tensor(sample_idx),
