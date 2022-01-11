a3965c7b33d24ef05fb4c1bd6e7897b65d1b74f9
Krystian Sztenderski <56883920+ksztenderski@users.noreply.github.com>
2022-01-11 10:09:52 +0100

    TensorList shape (#3591)
    Add shape to TensorListCPU/GPU and DataNodeDebug
    
    Signed-off-by: ksztenderski <ksztenderski@nvidia.com>

--- dali/test/python/test_backend_impl.py
+++ dali/test/python/test_backend_impl.py
@@ -142,3 +142,13 @@ def test_tensor_cpu_squeeze():
              (0, (1, 5, 1), "ABC", "BC"),
              (None, (3, 5, 1), "ABC", "AB")]:
         yield check_squeeze, shape, dim, in_layout, expected_out_layout
+
+
+def test_tensorlist_shape():
+    shapes = [(3, 4, 5, 6), (1, 8, 7, 6, 5), (1,), (1, 1)]
+    for shape in shapes:
+        arr = np.empty(shape)
+        tl = TensorListCPU(arr)
+        tl_gpu = tl._as_gpu()
+        assert tl.shape() == [shape[1:]] * shape[0]
+        assert tl_gpu.shape() == [shape[1:]] * shape[0]
