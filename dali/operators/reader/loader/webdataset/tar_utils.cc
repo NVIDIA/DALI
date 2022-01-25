a9315d467d5c5ad2c5ea90215d32f8e736de3607
Janusz Lisiecki <jlisiecki@nvidia.com>
2022-01-26 00:41:49 +0100

    Add support for tar pax POSIX.1-2001 WebDataset
    - adds support for tar in pax POSIX.1-2001 in WebDataset
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/operators/reader/loader/webdataset/tar_utils.cc
+++ dali/operators/reader/loader/webdataset/tar_utils.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -240,7 +240,7 @@ inline void TarArchive::ParseHeader() {
     } else if (TH_ISFIFO(ToTarHandle(handle_))) {
       filetype_ = ENTRY_FIFO;
     } else {
-      DALI_FAIL("Entry type in a tar archive not recognized");
+      filetype_ = ENTRY_UNKNOWN;
     }
   }
   readoffset_ = 0;
