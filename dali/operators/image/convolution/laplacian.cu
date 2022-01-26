c83234d45581d200083696fe569e984bce0ca070
Kamil Tokarski <ktokarski@nvidia.com>
2022-01-26 14:55:52 +0100

    Split compilation into separate units
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- dali/operators/image/convolution/laplacian.cu
+++ dali/operators/image/convolution/laplacian.cu
@@ -12,19 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <functional>
 #include <memory>
-#include <string>
-#include <utility>
 #include <vector>
 
-#include "dali/core/span.h"
 #include "dali/core/static_switch.h"
-#include "dali/kernels/imgproc/convolution/laplacian_gpu.cuh"
-#include "dali/kernels/imgproc/convolution/laplacian_windows.h"
-#include "dali/kernels/kernel_manager.h"
 #include "dali/operators/image/convolution/laplacian.h"
-#include "dali/pipeline/data/views.h"
+#include "dali/operators/image/convolution/laplacian_gpu.h"
 #include "dali/pipeline/operator/common.h"
 
 
@@ -34,113 +27,48 @@ using namespace convolution_utils;  // NOLINT
 
 namespace laplacian {
 
-template <typename Out, typename In, int axes, bool has_channels, bool is_sequence>
-class LaplacianOpGpu : public OpImplBase<GPUBackend> {
- public:
-  using Kernel = kernels::LaplacianGpu<Out, In, float, axes, has_channels, is_sequence>;
-  static constexpr int ndim = Kernel::ndim;
+extern template op_impl_uptr GetLaplacianGpuImpl<uint8_t, uint8_t>(const OpSpec&,
+                                                                   const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, uint8_t>(const OpSpec&,
+                                                                 const DimDesc& dim_desc);
 
-  explicit LaplacianOpGpu(const OpSpec& spec, const DimDesc& dim_desc)
-      : spec_{spec}, args{spec}, dim_desc_{dim_desc}, lap_windows_{maxWindowSize} {
-    kmgr_.Resize<Kernel>(1, 1);
-  }
+extern template op_impl_uptr GetLaplacianGpuImpl<int8_t, int8_t>(const OpSpec&,
+                                                                 const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, int8_t>(const OpSpec&,
+                                                                const DimDesc& dim_desc);
 
-  bool SetupImpl(std::vector<OutputDesc>& output_desc, const workspace_t<GPUBackend>& ws) override {
-    ctx_.gpu.stream = ws.stream();
+extern template op_impl_uptr GetLaplacianGpuImpl<uint16_t, uint16_t>(const OpSpec&,
+                                                                     const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, uint16_t>(const OpSpec&,
+                                                                  const DimDesc& dim_desc);
 
-    const auto& input = ws.template Input<GPUBackend>(0);
-    auto processed_shape = input.shape();
-    int nsamples = processed_shape.num_samples();
-    // If we are sequence-like, make sure that all sequence elements are compressed to first dim
-    if (is_sequence) {
-      processed_shape = collapse_dims(processed_shape, {{0, dim_desc_.usable_axes_start}});
-    }
+extern template op_impl_uptr GetLaplacianGpuImpl<int16_t, int16_t>(const OpSpec&,
+                                                                   const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, int16_t>(const OpSpec&,
+                                                                 const DimDesc& dim_desc);
 
-    output_desc.resize(1);
-    output_desc[0].type = type2id<Out>::value;
-    // The shape of data stays untouched
-    output_desc[0].shape = input.shape();
+extern template op_impl_uptr GetLaplacianGpuImpl<uint32_t, uint32_t>(const OpSpec&,
+                                                                     const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, uint32_t>(const OpSpec&,
+                                                                  const DimDesc& dim_desc);
 
-    args.ObtainLaplacianArgs(spec_, ws, nsamples);
+extern template op_impl_uptr GetLaplacianGpuImpl<int32_t, int32_t>(const OpSpec&,
+                                                                   const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, int32_t>(const OpSpec&,
+                                                                 const DimDesc& dim_desc);
 
-    for (int i = 0; i < axes; i++) {
-      for (int j = 0; j < axes; j++) {
-        window_sizes_[i][j].resize(nsamples);
-        windows_[i][j].resize(nsamples);
-      }
-      scales_[i].resize(nsamples);
-      scale_spans_[i] = make_span(scales_[i]);
-    }
+extern template op_impl_uptr GetLaplacianGpuImpl<uint64_t, uint64_t>(const OpSpec&,
+                                                                     const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, uint64_t>(const OpSpec&,
+                                                                  const DimDesc& dim_desc);
 
-    std::array<bool, axes> has_smoothing = uniform_array<axes>(false);
-    for (int sample_idx = 0; sample_idx < nsamples; sample_idx++) {
-      const auto& window_sizes = args.GetWindowSizes(sample_idx);
-      const auto& scales = args.GetScales(sample_idx);
-      for (int i = 0; i < axes; i++) {
-        for (int j = 0; j < axes; j++) {
-          if (i != j && window_sizes[i][j] != 1) {
-            has_smoothing[i] = true;
-          }
-          window_sizes_[i][j].set_tensor_shape(sample_idx, {window_sizes[i][j]});
-          auto window_size = window_sizes[i][j];
-          const auto& window = i == j ? lap_windows_.GetDerivWindow(window_size) :
-                                        lap_windows_.GetSmoothingWindow(window_size);
-          windows_[i][j].data[sample_idx] = window.data;
-          windows_[i][j].shape.set_tensor_shape(sample_idx, window.shape);
-        }
-        scales_[i][sample_idx] = scales[i];
-      }
-    }
-    for (int i = 0; i < axes; i++) {
-      if (!has_smoothing[i]) {
-        for (int j = 0; j < axes; j++) {
-          if (i != j) {
-            window_sizes_[i][j].resize(0);
-            windows_[i][j].resize(0);
-          }
-        }
-      }
-    }
+extern template op_impl_uptr GetLaplacianGpuImpl<int64_t, int64_t>(const OpSpec&,
+                                                                   const DimDesc& dim_desc);
+extern template op_impl_uptr GetLaplacianGpuImpl<float, int64_t>(const OpSpec&,
+                                                                 const DimDesc& dim_desc);
 
-    auto& req = kmgr_.Setup<Kernel>(0, ctx_, processed_shape.to_static<ndim>(), window_sizes_);
-    return true;
-  }
-
-  void RunImpl(workspace_t<GPUBackend>& ws) override {
-    const auto& input = ws.template Input<GPUBackend>(0);
-    auto& output = ws.template Output<GPUBackend>(0);
-    output.SetLayout(input.GetLayout());
-
-    auto processed_shape = input.shape();
-    // If we are sequence-like, make sure that all sequence elements are compressed to first dim
-    if (is_sequence) {
-      processed_shape = collapse_dims(processed_shape, {{0, dim_desc_.usable_axes_start}});
-    }
-
-    auto static_shape = processed_shape.to_static<ndim>();
-    auto in_view_dyn = view<const In>(input);
-    auto out_view_dyn = view<Out>(output);
-    auto in_view = reshape<ndim>(in_view_dyn, static_shape);
-    auto out_view = reshape<ndim>(out_view_dyn, static_shape);
-
-    kmgr_.Run<Kernel>(0, 0, ctx_, out_view, in_view, windows_, scale_spans_);
-  }
-
- private:
-  const OpSpec& spec_;
-  LaplacianArgs<axes> args;
-  DimDesc dim_desc_;
-  kernels::LaplacianWindows<float> lap_windows_;
-
-  kernels::KernelManager kmgr_;
-  kernels::KernelContext ctx_;
-
-  std::array<std::array<TensorListShape<1>, axes>, axes> window_sizes_;
-  std::array<std::vector<float>, axes> scales_;
-  std::array<span<const float>, axes> scale_spans_;
-  std::array<std::array<TensorListView<StorageCPU, const float, 1>, axes>, axes> windows_;
-};
-
+extern template op_impl_uptr GetLaplacianGpuImpl<float, float>(const OpSpec&,
+                                                               const DimDesc& dim_desc);
 
 }  // namespace laplacian
 
@@ -159,19 +87,11 @@ bool Laplacian<GPUBackend>::SetupImpl(std::vector<OutputDesc>& output_desc,
     impl_dim_desc_ = dim_desc;
 
     TYPE_SWITCH(input.type(), type2id, In, LAPLACIAN_GPU_SUPPORTED_TYPES, (
-      VALUE_SWITCH(dim_desc.usable_axes_count, Axes, LAPLACIAN_SUPPORTED_AXES, (
-        BOOL_SWITCH(dim_desc.is_channel_last(), HasChannels, (
-          BOOL_SWITCH(dim_desc.is_sequence(), IsSeq, (
-            if (dtype == input.type()) {
-              using LaplacianSame = laplacian::LaplacianOpGpu<In, In, Axes, HasChannels, IsSeq>;
-              impl_ = std::make_unique<LaplacianSame>(spec_, dim_desc);
-            } else {
-              using LaplacianFloat = laplacian::LaplacianOpGpu<float, In, Axes, HasChannels, IsSeq>;
-              impl_ = std::make_unique<LaplacianFloat>(spec_, dim_desc);
-            }
-          ));  // NOLINT
-        ));  // NOLINT
-      ), DALI_FAIL("Axis count out of supported range."));  // NOLINT
+      if (dtype == input.type()) {
+        impl_ = laplacian::GetLaplacianGpuImpl<In, In>(spec_, dim_desc);
+      } else {
+        impl_ = laplacian::GetLaplacianGpuImpl<float, In>(spec_, dim_desc);
+      }
     ), DALI_FAIL(make_string("Unsupported data type: ", input.type())));  // NOLINT
   }
 
