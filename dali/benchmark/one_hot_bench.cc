4d706f0f17bb638ac37ff2a2ab5434c4fdad3799
Maksymilian Grochowski <mg417883@students.mimuw.edu.pl>
2022-01-27 12:12:23 +0100

    added test large number of classes, fixed linter
    Signed-off-by: Maksymilian Grochowski <mg417883@students.mimuw.edu.pl>

--- dali/benchmark/one_hot_bench.cc
+++ dali/benchmark/one_hot_bench.cc
@@ -13,31 +13,37 @@
 // limitations under the License.
 
 #include <benchmark/benchmark.h>
-#include "dali/benchmark/operator_bench.h"
 #include "dali/benchmark/dali_bench.h"
+#include "dali/benchmark/operator_bench.h"
 
 namespace dali {
+
+static void OneHotGPUAlternateAxis(benchmark::internal::Benchmark *b, int batch_size, int h, int w,
+                                   int num_classes) {
+  int axis = -1;
+  b->Args({batch_size, h, w, axis, num_classes});
+  axis = 1;
+  b->Args({batch_size, h, w, axis, num_classes});
+}
 
 static void OneHotGPUArgs(benchmark::internal::Benchmark *b) {
   for (int batch_size = 256; batch_size >= 1; batch_size /= 2) {
     int h = 64, w = h;
-    int axis = -1;
-    int numClasses = 500;
-    b->Args({batch_size, h, w, axis, numClasses});
-    axis = 1;
-    b->Args({batch_size, h, w, axis, numClasses});
-    for (h = 32; h >= 8 ; h /= 2) {
+    int num_classes = 500;
+    OneHotGPUAlternateAxis(b, batch_size, h, w, num_classes);
+    for (h = 32; h >= 8; h /= 2) {
       w = h;
-      axis = -1;
-      numClasses = 1000;      
-      b->Args({batch_size, h, w, axis, numClasses});
-      axis = 1;
-      b->Args({batch_size, h, w, axis, numClasses});
+      num_classes = 1000;
+      OneHotGPUAlternateAxis(b, batch_size, h, w, num_classes);
     }
+    h = 500;
+    w = 500;
+    num_classes = 5;
+    OneHotGPUAlternateAxis(b, batch_size, h, w, num_classes);
   }
 }
 
-BENCHMARK_DEFINE_F(OperatorBench, OneHotGPU)(benchmark::State& st) {
+BENCHMARK_DEFINE_F(OperatorBench, OneHotGPU)(benchmark::State &st) {
   int batch_size = st.range(0);
   int H = st.range(1);
   int W = st.range(2);
@@ -46,18 +52,19 @@ BENCHMARK_DEFINE_F(OperatorBench, OneHotGPU)(benchmark::State& st) {
 
   this->RunGPU<uint8_t>(st,
                         OpSpec("OneHot")
-                          .AddArg("max_batch_size", batch_size)
-                          .AddArg("num_threads", 1)
-                          .AddArg("device", "gpu")
-                          .AddArg("output_type", DALI_RGB)
-                          .AddArg("axis", axis)
-                          .AddArg("num_classes", num_classes),
+                            .AddArg("max_batch_size", batch_size)
+                            .AddArg("num_threads", 1)
+                            .AddArg("device", "gpu")
+                            .AddArg("output_type", DALI_RGB)
+                            .AddArg("axis", axis)
+                            .AddArg("num_classes", num_classes),
                         batch_size, H, W);
 }
 
-BENCHMARK_REGISTER_F(OperatorBench, OneHotGPU)->Iterations(1000)
-->Unit(benchmark::kMicrosecond)
-->UseRealTime()
-->Apply(OneHotGPUArgs);
+BENCHMARK_REGISTER_F(OperatorBench, OneHotGPU)
+    ->Iterations(1000)
+    ->Unit(benchmark::kMicrosecond)
+    ->UseRealTime()
+    ->Apply(OneHotGPUArgs);
 
 }  // namespace dali
