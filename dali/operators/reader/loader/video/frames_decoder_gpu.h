e802a6f90e0428e83c613093cd33f9d7a165d5eb
Albert Wolant <awolant@nvidia.com>
2022-02-07 18:51:18 +0100

    Update for new FFMPEG
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/operators/reader/loader/video/frames_decoder_gpu.h
+++ dali/operators/reader/loader/video/frames_decoder_gpu.h
@@ -15,6 +15,10 @@
 #ifndef DALI_OPERATORS_READER_LOADER_VIDEO_FRAMES_DECODER_GPU_H_
 #define DALI_OPERATORS_READER_LOADER_VIDEO_FRAMES_DECODER_GPU_H_
 
+extern "C" {
+#include <libavcodec/bsf.h>
+}
+
 #include "dali/operators/reader/loader/video/frames_decoder.h"
 
 #include <string>
