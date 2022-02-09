45028d148e5bea8ec1dc2f2ff3f7e6bcbe043a32
Albert Wolant <awolant@nvidia.com>
2022-02-09 21:44:42 +0100

    Fix segfault in packet destruction
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/operators/reader/loader/video/frames_decoder.h
+++ dali/operators/reader/loader/video/frames_decoder.h
@@ -48,10 +48,14 @@ struct AvState {
 
   ~AvState() {
     sws_freeContext(sws_ctx_);
-    av_packet_unref(packet_);
-    av_packet_free(&packet_);
-    av_frame_unref(frame_);
-    av_frame_free(&frame_);
+    if (packet_ != nullptr) {
+      av_packet_unref(packet_);
+      av_packet_free(&packet_);
+    }
+    if (frame_ != nullptr) {
+      av_frame_unref(frame_);
+      av_frame_free(&frame_);  
+    }
     avcodec_free_context(&codec_ctx_);
     avformat_close_input(&ctx_);
     avformat_free_context(ctx_);
