2b0224f4b99ab9fc843f529ef99de38640fa0d44
Janusz Lisiecki <jlisiecki@nvidia.com>
2022-02-14 12:06:16 +0100

    Fix assert glob in test_external_source_parallel_pytorch.py
    - when the check if it is safe to fork DALI process was adjusted
      to detect if the driver has been initialized the error message
      pattern was not adjusted in test_external_source_parallel_pytorch.py:test_pytorch_cuda_context
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/test/python/test_external_source_parallel_pytorch.py
+++ dali/test/python/test_external_source_parallel_pytorch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ class ExtCallbackTorch(ExtCallback):
 
 
 @raises(RuntimeError, "Error*starting Python worker threads for*parallel External Source*"
-                      "Cannot fork*CUDA context already bound*"
+                      "Cannot fork*CUDA has been initialized*"
                       "*start_py_workers*fork*spawn*")
 def test_pytorch_cuda_context():
     # Create a dummy torch CUDA tensor so we acquire CUDA context
