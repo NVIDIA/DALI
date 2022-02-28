13cf69ea6f3b8e268a84aa2b4193bb1acaf956dd
Michal Zientkiewicz <michalz@nvidia.com>
2022-02-28 17:38:20 +0100

    Add cross-device test for PyTorch. Fix MXNet parallel external source test.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/test/python/test_external_source_pytorch_gpu.py
+++ dali/test/python/test_external_source_pytorch_gpu.py
@@ -43,3 +43,20 @@ def test_external_source_callback_torch_stream():
 
             for i in range(10):
                 check_output(pipe.run(), [np.array([attempt * 100 + (i + 1) * 10 + 1.5], dtype=np.float32)])
+
+def _test_cross_device(src, dst):
+    import nvidia.dali.fn as fn
+    pipe = Pipeline(1, 3, dst)
+    data = torch.tensor([[1,2,3,4],[5,6,7,8]], dtype=torch.float32).cuda(device=dst)
+    with pipe:
+        pipe.set_outputs(fn.external_source([data], batch=False, device='gpu', cycle=True))
+
+    pipe.build()
+    for _ in range(10):
+        pipe.run()
+
+def test_cross_device():
+    if torch.cuda.device_count() > 1:
+        for src in [0,1]:
+            for dst in [0,1]:
+                yield _test_cross_device, src, dst
