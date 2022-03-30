40b7b64d5e730edf624fc2705739915c5dd90896
Janusz Lisiecki <jlisiecki@nvidia.com>
2022-03-30 13:52:37 +0200

    Add a TL0 test that runs on more than 1 GPU
    - adds a test case that runs more than 1 GPU (usually 2)
      to test cross-device functionalities
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

---
+++ qa/TL0_multigpu/test_cupy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+# used pip packages
+pip_packages="nose numpy cupy"
+target_dir=./dali/test/python
+
+# test_body definition is in separate file so it can be used without setup
+source test_body.sh
+
+test_body() {
+  test_cupy
+}
+
+pushd ../..
+source ./qa/test_template.sh
+popd
---
+++ qa/TL0_multigpu/test_nofw.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+# used pip packages
+pip_packages="nose"
+target_dir=./dali/test/python
+
+# test_body definition is in separate file so it can be used without setup
+source test_body.sh
+
+test_body() {
+  test_no_fw
+}
+
+pushd ../..
+source ./qa/test_template.sh
+popd
