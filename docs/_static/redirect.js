cfebde7028f9109dac44e92e1229caa4cba5483e
Krzysztof Lecki <klecki@nvidia.com>
2022-04-05 20:46:32 +0200

    Redirect doesn't work
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- docs/_static/redirect.js
+++ docs/_static/redirect.js
@@ -1,18 +1,21 @@
-(function () {
-  if (window.location.pathname.endsWith("supported_ops.html")) {
-    /*
-    * Best practice for extracting hashes:
-    * https://stackoverflow.com/a/10076097/151365
-    */
-    var hash = window.location.hash.substring(1);
-    if (hash) {
-        /*
-        * Best practice for javascript redirects:
-        * https://stackoverflow.com/a/506004/151365
-        */
-        var current = window.location.href;
-        target = current.replace("supported_ops.html", "operations" + hash + ".html")
-        window.location.replace(target);
-    }
-  }
-})();
+// TODO(klecki): doesn't work, how to include it?
+// (function () {
+//   console.log(window.location.pathname);
+//   if (window.location.pathname.endsWith("supported_ops.html")) {
+//     console.log("An I here?");
+//     /*
+//     * Best practice for extracting hashes:
+//     * https://stackoverflow.com/a/10076097/151365
+//     */
+//     var hash = window.location.hash.substring(1);
+//     if (hash) {
+//         /*
+//         * Best practice for javascript redirects:
+//         * https://stackoverflow.com/a/506004/151365
+//         */
+//         var current = window.location.href;
+//         target = current.replace("supported_ops.html", "operations" + hash + ".html")
+//         window.location.replace(target);
+//     }
+//   }
+// })();
