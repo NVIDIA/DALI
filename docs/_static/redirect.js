e8584c7ac22411759a4563b68756e62f13e5d839
Krzysztof Lecki <klecki@nvidia.com>
2022-04-07 13:25:44 +0200

    Cleanup js
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- docs/_static/redirect.js
+++ docs/_static/redirect.js
@@ -4,12 +4,11 @@
  * and redirects to the new location of `operations/nvidia.dali.fn.operator_name.html`
  */
 function redirect_legacy_op_link() {
-  console.log(window.location.pathname);
   if (window.location.pathname.endsWith("supported_ops.html")) {
     var hash = window.location.hash.substring(1);
     if (hash) {
         var current = window.location.href;
-        target = current.replace("supported_ops.html", "operations/" + hash + ".html")
+        var target = current.replace("supported_ops.html", "operations/" + hash + ".html")
         window.location.replace(target);
     }
   }
