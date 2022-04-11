db3a60e1ff22ef44787883fe05c5450ddb1db038
Rafal <Banas.Rafal97@gmail.com>
2022-04-11 18:22:29 +0200

    add test
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- dali/pipeline/util/thread_pool_test.cc
+++ dali/pipeline/util/thread_pool_test.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 #include "dali/pipeline/util/thread_pool.h"
+#include "dali/pipeline/util/worker_thread.h"
 #include <gtest/gtest.h>
 #include <atomic>
 
@@ -65,6 +66,11 @@ TEST(ThreadPool, AddWorkWithPriority) {
 
   tp.RunAll();
   ASSERT_EQ(((1+1) << 3) + 1, count);
+}
+
+TEST(WorkerThread, Destructing) {
+  WorkerThread wt(0, false);
+  // check destruction of a running worker thread
 }
 
 }  // namespace test
