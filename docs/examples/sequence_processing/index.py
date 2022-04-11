b8d5d5b60ac6426aa16fe0db3c5d64d7f22ba78d
Janusz Lisiecki <jlisiecki@nvidia.com>
2022-04-11 16:53:41 +0200

    Review fixes
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docs/examples/sequence_processing/index.py
+++ docs/examples/sequence_processing/index.py
@@ -6,11 +6,11 @@ doc(title="Video Processing",
         doc_entry("video/video_reader_label_example.ipynb",
                   op_reference('fn.readers.video', "Tutorial describing how to use video reader to output frames with labels")),
         doc_entry("video/video_file_list_outputs.ipynb",
-                  op_reference('fn.readers.video', "Tutorial describing how output to frames with \
-                                lables assigned to dedicated ranges of frame numbers/timestamps")),
+                  op_reference('fn.readers.video', "Tutorial describing how to output frames with \
+                                labels assigned to dedicated ranges of frame numbers/timestamps")),
         doc_entry("sequence_reader_simple_example.ipynb",
-                  op_reference('fn.readers.sequence', "Tutorial describing how to read sequence of video frames stores as separete files")),
+                  op_reference('fn.readers.sequence', "Tutorial describing how to read sequence of video frames stored as separate files"),
         doc_entry("optical_flow_example.ipynb",
-                  [op_reference('fn.readers.video', "Tutorial describing how read video and calculate optical flow"),
-                   op_reference('fn.optical_flow', "Tutorial describing how to calculate optical flow from a sequence of frames")]),
+                  [op_reference('fn.readers.video', "Tutorial describing how to calculate optical flow from video inputs"),
+                   op_reference('fn.optical_flow', "Tutorial describing how to calculate optical flow from sequence inputs")]),
     ])
