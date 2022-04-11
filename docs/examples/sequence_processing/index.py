c62e4e6a20d35243eb4980ae37b1eb266e5cc878
Janusz Lisiecki <jlisiecki@nvidia.com>
2022-04-11 14:53:10 +0200

    Add operator cross-referencing to video processing tutorials
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docs/examples/sequence_processing/index.py
+++ docs/examples/sequence_processing/index.py
@@ -1,9 +1,16 @@
 doc(title="Video Processing",
     underline_char="=",
     entries=[
-        "video/video_reader_simple_example.ipynb",
-        "video/video_reader_label_example.ipynb",
-        "video/video_file_list_outputs.ipynb",
-        "sequence_reader_simple_example.ipynb",
-        "optical_flow_example.ipynb",
+        doc_entry("video/video_reader_simple_example.ipynb",
+                  op_reference('fn.readers.video', "Tutorial describing how to use video reader")),
+        doc_entry("video/video_reader_label_example.ipynb",
+                  op_reference('fn.readers.video', "Tutorial describing how to use video reader to output frames with labels")),
+        doc_entry("video/video_file_list_outputs.ipynb",
+                  op_reference('fn.readers.video', "Tutorial describing how output to frames with \
+                                lables assigned to dedicated ranges of frame numbers/timestamps")),
+        doc_entry("sequence_reader_simple_example.ipynb",
+                  op_reference('fn.readers.sequence', "Tutorial describing how to read sequence of video frames stores as separete files")),
+        doc_entry("optical_flow_example.ipynb",
+                  [op_reference('fn.readers.video', "Tutorial describing how read video and calculate optical flow"),
+                   op_reference('fn.optical_flow', "Tutorial describing how to calculate optical flow from a sequence of frames")]),
     ])
