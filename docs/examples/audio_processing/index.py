1bf6621f702b195dd8085e39543604a44a70b3b6
Joaquin Anton <janton@nvidia.com>
2022-04-11 13:16:20 +0200

    Add operator cross-referencing to audio processing tutorials
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- docs/examples/audio_processing/index.py
+++ docs/examples/audio_processing/index.py
@@ -1,6 +1,15 @@
 doc(title="Audio Processing",
     underline_char="=",
     entries=[
-        "audio_decoder.ipynb",
-        "spectrogram.ipynb",
+        doc_entry(
+            "audio_decoder.ipynb",
+            op_reference("fn.decoders.audio", "Audio decoder tutorial")),
+        doc_entry(
+            "spectrogram.ipynb",
+            [
+                op_reference("fn.spectrogram", "Audio spectrogram tutorial"),
+                op_reference("fn.mel_filter_bank", "Audio spectrogram tutorial"),
+                op_reference("fn.to_decibels", "Audio spectrogram tutorial"),
+                op_reference("fn.mfcc", "Audio spectrogram tutorial"),
+            ])
     ])
