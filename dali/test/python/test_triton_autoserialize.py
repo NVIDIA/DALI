ea1381a0557ffad567120a9e614105ac917a3786
szalpal <mszolucha@nvidia.com>
2022-04-20 00:22:36 +0200

    fix ci
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- dali/test/python/test_triton_autoserialize.py
+++ dali/test/python/test_triton_autoserialize.py
@@ -13,28 +13,28 @@
 # limitations under the License.
 
 from nvidia.dali._utils.autoserialize import invoke_autoserialize
-from ..nose_utils import raises
+from nose_utils import raises
 
 serialized_filename = "/tmp/some_custom_name"
 
 
 def test_direct_import():
-    from . import decorated_function
+    from autoserialize_test import decorated_function
     invoke_autoserialize(decorated_function, serialized_filename)
 
 
 def test_indirect_import():
-    from . import imports_decorated_function
+    from autoserialize_test import imports_decorated_function
     invoke_autoserialize(imports_decorated_function, serialized_filename)
 
 
 @raises(RuntimeError, glob="Precisely one autoserialize function must exist in the module.*")
 def test_double_decorated_functions():
-    from . import double_decorated_functions
+    from autoserialize_test import double_decorated_functions
     invoke_autoserialize(double_decorated_functions, serialized_filename)
 
 
 @raises(TypeError, glob="Only `@pipeline_def` can be decorated with `@triton.autoserialize`.")
 def test_improper_decorated_function():
-    from . import improper_decorated_function
+    from autoserialize_test import improper_decorated_function
     invoke_autoserialize(improper_decorated_function, serialized_filename)
