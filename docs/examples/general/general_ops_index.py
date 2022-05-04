d7173210b650600732a47f12576c56a3419d52e0
Albert Wolant <awolant@nvidia.com>
2022-05-04 15:30:53 +0200

    Add tutorials references to ops docs
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- docs/examples/general/general_ops_index.py
+++ docs/examples/general/general_ops_index.py
@@ -2,11 +2,26 @@ doc(title="General Purpose",
     underline_char="=",
     entries=[
         "expressions/index.py",
-        doc_entry("reductions.ipynb",
-                  op_reference('fn.reductions', "Tutorial describing how to use reductions")),
-        "tensor_join.ipynb",
-        "reinterpret.ipynb",
-        "normalize.ipynb",
-        "../math/geometric_transforms.ipynb",
-        "erase.ipynb",
+        doc_entry(
+            "reductions.ipynb",
+            op_reference('fn.reductions', "Tutorial describing how to use reductions")),    
+        doc_entry(
+            "tensor_join.ipynb",
+            op_reference('fn.cat', "Tutorial describing tensor joining")),
+        doc_entry(
+            "reinterpret.ipynb",
+            op_reference('fn.reshape', "Tutorial describing tensor reshaping")
+        ),
+        doc_entry(
+            "normalize.ipynb",
+            op_reference('fn.normalize', "Tutorial describing tensor normalization")),
+        doc_entry(
+            "../math/geometric_transforms.ipynb",
+            [
+                op_reference('fn.transforms', "Tutorial describing tensor geometric transformations"),
+                op_reference('fn.warp_affine', "Tutorial describing tensor geometric transformations"),
+                op_reference('fn.coord_transform', "Tutorial describing tensor geometric transformations")]),
+        doc_entry(
+            "erase.ipynb",
+            op_reference('fn.erase', "Tutorial describing tensor erasing"))
     ])
