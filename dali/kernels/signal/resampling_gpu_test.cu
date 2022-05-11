d88e78b3332cebdeb966c21e93c25b56a70ab988
Joaquin Anton <janton@nvidia.com>
2022-05-11 19:39:24 +0200

    Add benchmark
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/kernels/signal/resampling_gpu_test.cu
+++ dali/kernels/signal/resampling_gpu_test.cu
@@ -17,6 +17,7 @@
 #include <numeric>
 #include "dali/kernels/signal/resampling_gpu.h"
 #include "dali/kernels/signal/resampling_test.h"
+#include "dali/core/cuda_event.h"
 
 namespace dali {
 namespace kernels {
@@ -46,6 +47,44 @@ class ResamplingGPUTest : public ResamplingTest {
 
     CUDA_CALL(cudaStreamSynchronize(ctx.gpu.stream));
   }
+
+  void RunPerfTest(int batch_size, int nchannels, int n_iters = 1000) {
+    std::vector<float> in_rates_v(batch_size, 22050.0f);
+    auto in_rates = make_cspan(in_rates_v);
+    std::vector<float> out_rates_v(batch_size, 16000.0f);
+    auto out_rates = make_cspan(out_rates_v);
+
+    this->PrepareData(batch_size, nchannels, in_rates, out_rates);
+
+    ResamplerGPU<float> R;
+    R.Initialize(16);
+
+    KernelContext ctx;
+    ctx.gpu.stream = 0;
+
+    CUDAEvent start = CUDAEvent::CreateWithFlags(0);
+    CUDAEvent end = CUDAEvent::CreateWithFlags(0);
+    double avg_time = 0;
+    int64_t in_elems = ttl_in_.cpu().shape.num_elements();
+    int64_t in_bytes = in_elems * sizeof(float);
+    std::cout << "Resampling GPU Perf test.\n"
+              << "\nInput contains " << in_elems << " floats.\n";
+
+    auto req = R.Setup(ctx, ttl_in_.gpu(), in_rates, out_rates);
+    ASSERT_EQ(ttl_out_.cpu().shape, req.output_shapes[0]);
+
+    for (int i = 0; i < n_iters; ++i) {
+      CUDA_CALL(cudaEventRecord(start));
+      R.Run(ctx, ttl_out_.gpu(), ttl_in_.gpu(), in_rates, out_rates);
+      CUDA_CALL(cudaEventRecord(end));
+      CUDA_CALL(cudaDeviceSynchronize());
+      float time;
+      CUDA_CALL(cudaEventElapsedTime(&time, start, end));
+
+      avg_time += time;
+    }
+    std::cout << "Processed " << in_bytes / avg_time << " bytes/sec" << std::endl;
+  }
 };
 
 TEST_F(ResamplingGPUTest, SingleChannel) {
@@ -58,6 +97,10 @@ TEST_F(ResamplingGPUTest, TwoChannel) {
 
 TEST_F(ResamplingGPUTest, EightChannel) {
   this->RunTest(3, 8);
+}
+
+TEST_F(ResamplingGPUTest, PerfTest) {
+  this->RunPerfTest(64, 1, 1000);
 }
 
 }  // namespace test
