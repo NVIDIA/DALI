177fced2242c94cbdc593ba1efa37cb177b435f4
Joaquin Anton <janton@nvidia.com>
2022-05-12 15:10:04 +0200

    Update benchmark
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/kernels/signal/resampling_gpu_test.cu
+++ dali/kernels/signal/resampling_gpu_test.cu
@@ -33,6 +33,8 @@ class ResamplingGPUTest : public ResamplingTest {
 
     KernelContext ctx;
     ctx.gpu.stream = 0;
+    DynamicScratchpad dyn_scratchpad({}, AccessOrder(ctx.gpu.stream));
+    ctx.scratchpad = &dyn_scratchpad;
 
     auto req = R.Setup(ctx, ttl_in_.gpu(), in_rates, out_rates);
     auto outref_sh = ttl_outref_.cpu().shape;
@@ -53,36 +55,44 @@ class ResamplingGPUTest : public ResamplingTest {
     auto in_rates = make_cspan(in_rates_v);
     std::vector<float> out_rates_v(batch_size, 16000.0f);
     auto out_rates = make_cspan(out_rates_v);
+    int nsec = 30;
 
-    this->PrepareData(batch_size, nchannels, in_rates, out_rates);
+    this->PrepareData(batch_size, nchannels, in_rates, out_rates, nsec);
 
     ResamplerGPU<float> R;
     R.Initialize(16);
 
     KernelContext ctx;
     ctx.gpu.stream = 0;
-
-    CUDAEvent start = CUDAEvent::CreateWithFlags(0);
-    CUDAEvent end = CUDAEvent::CreateWithFlags(0);
-    double total_time = 0;
-    int64_t in_elems = ttl_in_.cpu().shape.num_elements();
-    int64_t in_bytes = in_elems * sizeof(float);
-    std::cout << "Resampling GPU Perf test.\n"
-              << "\nInput contains " << in_elems << " floats.\n";
-
     auto req = R.Setup(ctx, ttl_in_.gpu(), in_rates, out_rates);
     ASSERT_EQ(ttl_out_.cpu().shape, req.output_shapes[0]);
 
+    CUDAEvent start = CUDAEvent::CreateWithFlags(0);
+    CUDAEvent end = CUDAEvent::CreateWithFlags(0);
+    double total_time_ms = 0;
+    int64_t in_elems = ttl_in_.cpu().shape.num_elements();
+    int64_t out_elems = ttl_out_.cpu().shape.num_elements();
+    int64_t out_bytes = out_elems * sizeof(float);
+    std::cout << "Resampling GPU Perf test.\n"
+              << "Input contains " << in_elems << " floats.\n"
+              << "Output contains " << out_elems << " floats.\n";
+
     for (int i = 0; i < n_iters; ++i) {
+      CUDA_CALL(cudaDeviceSynchronize());
+
+      DynamicScratchpad dyn_scratchpad({}, AccessOrder(ctx.gpu.stream));
+      ctx.scratchpad = &dyn_scratchpad;
+
       CUDA_CALL(cudaEventRecord(start));
       R.Run(ctx, ttl_out_.gpu(), ttl_in_.gpu(), in_rates, out_rates);
       CUDA_CALL(cudaEventRecord(end));
       CUDA_CALL(cudaDeviceSynchronize());
-      float time;
-      CUDA_CALL(cudaEventElapsedTime(&time, start, end));
-      total_time += time;
+      float time_ms;
+      CUDA_CALL(cudaEventElapsedTime(&time_ms, start, end));
+      total_time_ms += time_ms;
     }
-    std::cout << "Processed " << n_iters * in_bytes / (total_time * 1e6) << " MBs/sec" << std::endl;
+    std::cout << "Processed " << n_iters * out_bytes / (total_time_ms * 1e6) << " GBs/sec"
+              << std::endl;
   }
 };
 
