7b7315b4f7ffc14e980e8a72b539af040e643dae
Kamil Tokarski <ktokarski@nvidia.com>
2022-05-13 16:06:28 +0200

    Revert reordering headers
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- dali/pipeline/data/tensor_vector_test.cc
+++ dali/pipeline/data/tensor_vector_test.cc
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "dali/pipeline/data/tensor_vector.h"
 #include <gtest/gtest.h>
 #include <string>
+
 #include "dali/core/format.h"
 #include "dali/core/tensor_shape.h"
+#include "dali/pipeline/data/tensor_vector.h"
 #include "dali/pipeline/data/views.h"
 #include "dali/test/tensor_test_utils.h"
 
