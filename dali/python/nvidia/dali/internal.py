71cc208acb0dca627896fadefe47028e31819822
Albert Wolant <awolant@nvidia.com>
2022-06-13 13:43:57 +0200

    Fix Python formatting in dali/python/nvidia/dali/internal.py
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/python/nvidia/dali/internal.py
+++ dali/python/nvidia/dali/internal.py
@@ -32,7 +32,8 @@ Parameters
             m = sys.modules[module_name] = types.ModuleType(module_name)
             setattr(root, part, m)
         elif not isinstance(m, types.ModuleType):
-            raise RuntimeError("The module {} already contains an attribute \"{}\", which is not a module, but {}".format(
-                root, part, m))
+            raise RuntimeError(
+                "The module {} already contains an attribute \"{}\", which is not a module, but {}".
+                format(root, part, m))
         root = m
     return root
