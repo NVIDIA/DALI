de3fcc1126b02d0a85b889c9f414080911c6eaca
Joaquin Anton <janton@nvidia.com>
2022-06-21 16:31:31 +0200

    Imgcodec target placeholder
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/imgcodec/format/image_format.h
@@ -0,0 +1,143 @@
+// Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_IMGCODEC_FORMAT_IMAGE_FORMAT_H_
+#define DALI_IMGCODEC_FORMAT_IMAGE_FORMAT_H_
+
+#include <fstream>
+#include <stdexcept>
+#include <vector>
+#include <memory>
+#include "dali/core/span.h"
+#include "dali/core/tensor_shape.h"
+#include "dali/pipeline/data/sample_view.h"
+
+namespace dali {
+namespace imgcodec {
+
+enum ImageFormat : int {
+  Unknown = 0,
+  Jpeg = 1,
+  Png = 2,
+  Bmp = 3,
+  Tiff = 4,
+  Pnm = 5,
+  Jpeg2000 = 6,
+  Webp = 7,
+};
+
+struct ImageInfo {
+  ImageFormat format = ImageFormat::Unknown;
+  TensorShape<> shape;
+  struct {
+    int rotate;
+    bool flip_x, flip_y;
+  } orientation;
+};
+
+enum class InputKind : int {
+  None = 0,
+  // abstract interface that reads data from a custom source
+  ReadInterface = 1,
+  // bitstream loaded into host memory
+  HostMemory = 2,
+  // bitstream loaded into device memory
+  DeviceMemory = 4,
+  // file name
+  Filename = 8
+};
+
+constexpr InputKind operator|(InputKind a, InputKind b) {
+  return static_cast<InputKind>(static_cast<int>(a) | static_cast<int>(b));
+}
+
+constexpr InputKind operator&(InputKind a, InputKind b) {
+  return static_cast<InputKind>(static_cast<int>(a) & static_cast<int>(b));
+}
+
+class EncodedImage {
+ public:
+  virtual void *GetRawData() const = 0;
+  virtual size_t GetSize() const = 0;
+  virtual const char *GetFilename() const = 0;
+  virtual ssize_t Read(void *buf, size_t count) = 0;
+  virtual ssize_t Seek(ssize_t pos) = 0;
+  virtual InputKind GetKind() = 0;
+};
+
+class EncodedImageHostMemory : public EncodedImage {
+ public:
+  EncodedImageHostMemory(void *data, size_t len) : ptr_(data), size_(len) {}
+
+  void *GetRawData() const override {
+    return ptr_;
+  }
+
+  size_t GetSize() const override {
+    return size_;
+  }
+
+  const char *GetFilename() const override {
+    throw std::runtime_error("Not supported interface");
+  }
+
+  ssize_t Read(void *buf, size_t count) override {
+    throw std::runtime_error("Not supported interface");
+  }
+
+  ssize_t Seek(ssize_t pos) override {
+    throw std::runtime_error("Not supported interface");
+  }
+
+  InputKind GetKind() override {
+    return InputKind::HostMemory;
+  }
+
+ private:
+  void *ptr_;
+  size_t size_;
+};
+
+class ImageParser {
+ public:
+  virtual ImageInfo GetInfo(EncodedImage *encoded) const = 0;
+  virtual bool CanParse(EncodedImage *encoded) const = 0;
+};
+
+class ImageParserManager {
+ public:
+  void RegisterParser(std::shared_ptr<ImageParser> parser, int position = -1) {
+    if (position < 0)
+      parsers_.push_back(parser);
+    else
+      parsers_.insert(parsers_.begin() + position, parser);
+  }
+
+  ImageInfo Parse(EncodedImage *encoded) const {
+    for (auto &parser : parsers_) {
+      if (parser->CanParse(encoded)) {
+        return parser->GetInfo(encoded);
+      }
+    }
+    return {};  // TODO(janton): error?
+  }
+
+ private:
+  std::vector<std::shared_ptr<ImageParser>> parsers_;
+};
+
+}  // namespace imgcodec
+}  // namespace dali
+
+#endif  // DALI_IMGCODEC_FORMAT_IMAGE_FORMAT_H_
---
+++ dali/imgcodec/image_codec.h
@@ -0,0 +1,81 @@
+// Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_IMGCODEC_IMAGE_CODEC_H_
+#define DALI_IMGCODEC_IMAGE_CODEC_H_
+
+#include <memory>
+#include <stdexcept>
+#include <vector>
+#include "dali/core/span.h"
+#include "dali/core/tensor_shape.h"
+#include "dali/imgcodec/format/image_format.h"
+#include "dali/pipeline/data/sample_view.h"
+
+namespace dali {
+namespace imgcodec {
+
+struct DecodeParams {
+  bool use_roi;
+  struct {
+    TensorShape<> begin, end;
+  } roi;
+  DALIDataType dtype;
+  DALIImageType format;
+  bool planar;  // not required initially
+};
+
+struct DecodeResult {
+  bool success;
+  std::exception_ptr exception;
+};
+
+struct ImageCodecProperties {
+  // the codec natively supports region-of-interest decoding
+  bool roi_support = false;
+  InputKind supported_input_kinds;
+
+  // if true and the codec fails to decode an image,
+  // an attempt will be made to use other compatible codecs
+  bool fallback = true;
+};
+
+template <typename OutputBackend, typename InputBackend>  // ?
+class ImageCodecInstance {
+ public:
+  virtual bool CanDecode(EncodedImage *in, DecodeParams opts);
+  virtual std::vector<bool> CanDecode(span<EncodedImage *> in, span<DecodeParams> opts);
+  virtual DecodeResult Decode(SampleView<CPUBackend> out, EncodedImage *in, DecodeParams opts);
+  virtual std::vector<DecodeResult> Decode(span<SampleView<CPUBackend>> out,
+                                           span<EncodedImage *> in, span<DecodeParams> opts);
+  virtual DecodeResult Decode(SampleView<GPUBackend> out, EncodedImage *in, DecodeParams opts);
+  virtual std::vector<DecodeResult> Decode(span<SampleView<GPUBackend>> out,
+                                           span<EncodedImage *> in, span<DecodeParams> opts);
+
+  /* the `key` is deliberately opaque and it can be a codec-specific enum */
+  virtual void SetParam(const char *key, any value) = 0;
+  virtual const any &GetParam(const char *key) const = 0;
+};
+
+class ImageCodec {
+ public:
+  virtual ImageCodecProperties GetProperties() const;
+  virtual bool IsSupported(int device_id) const;
+  virtual std::shared_ptr<ImageCodecInstance> Create(int device_id, ThreadPool &tp);
+};
+
+}  // namespace imgcodec
+}  // namespace dali
+
+#endif  // DALI_IMGCODEC_IMAGE_CODEC_H_
