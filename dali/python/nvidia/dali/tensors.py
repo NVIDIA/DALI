6ee3512cb967472bee4b28e7dffd769a6e1feffa
Albert Wolant <awolant@nvidia.com>
2022-06-26 14:26:41 +0200

    Fix pylint
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/python/nvidia/dali/tensors.py
+++ dali/python/nvidia/dali/tensors.py
@@ -13,10 +13,7 @@
 # limitations under the License.
 
 # pylint: disable=no-name-in-module, unused-import
-from nvidia.dali.backend import TensorCPU           # noqa: F401
-from nvidia.dali.backend import TensorListCPU       # noqa: F401
-from nvidia.dali.backend import TensorGPU           # noqa: F401
-from nvidia.dali.backend import TensorListGPU       # noqa: F401
+from nvidia.dali.backend import TensorCPU, TensorListCPU, TensorGPU, TensorListGPU      # noqa: F401
 
 
 def _transfer_to_cpu(data, device):
