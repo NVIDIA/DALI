de50bbc945e51ed7928f0ee71776370a3b2ea440
Szymon Karpiński <skarpinski@nvidia.com>
2022-07-12 14:33:08 +0200

    Basic version of JPEG parser
    Signed-off-by: Szymon Karpiński <skarpinski@nvidia.com>

--- dali/imgcodec/parsers/jpeg.cc
+++ dali/imgcodec/parsers/jpeg.cc
@@ -13,18 +13,61 @@
 // limitations under the License.
 
 #include "dali/imgcodec/parsers/jpeg.h"
+#include "dali/core/byte_io.h"
 
 namespace dali {
 namespace imgcodec {
 
+using jpeg_marker_t = std::array<uint8_t, 2>;
+
+constexpr jpeg_marker_t sos_marker = {0xff, 0xda};
+constexpr jpeg_marker_t soi_marker = {0xff, 0xd8};
+constexpr jpeg_marker_t eoi_marker = {0xff, 0xd9};
+
+bool isValidMarker(const jpeg_marker_t &marker) {
+  return marker[0] == 0xff && marker[1] != 0x00;
+}
+
+bool isSofMarker(const jpeg_marker_t &marker) {
+  if (!isValidMarker(marker) || marker[1] < 0xc0 || marker[1] > 0xcf) return false;
+  return (marker[1] != 0xc4 && marker[1] != 0xc8 && marker[1] != 0xcc);
+}
+
 ImageInfo JpegParser::GetInfo(ImageSource *encoded) const {
-  ImageInfo info;
-  // info.shape = {h, w, c};   // TODO(janton)
+  ImageInfo info{};
+  auto stream = encoded->Open();
+
+  jpeg_marker_t first_marker = stream->ReadOne<jpeg_marker_t>();
+  DALI_ENFORCE(first_marker == soi_marker);
+
+  bool read_shape = false;
+  while (!read_shape) {
+    auto marker = stream->ReadOne<jpeg_marker_t>();
+
+    DALI_ENFORCE(isValidMarker(marker));
+    if (marker == sos_marker) break;
+
+    uint16_t size = ReadValueBE<uint16_t>(*stream);
+    ptrdiff_t next_marker_offset = stream->TellRead() - 2 + size;
+    if (isSofMarker(marker)) {
+      stream->Skip(1);  // Skip the precision field
+      auto height = ReadValueBE<uint16_t>(*stream);
+      auto width = ReadValueBE<uint16_t>(*stream);
+      auto nchannels = stream->ReadOne<uint8_t>();
+      info.shape = {height, width, nchannels};
+      read_shape = true;
+    }
+    stream->SeekRead(next_marker_offset, SEEK_SET);
+  }
+  if (!read_shape)
+    DALI_FAIL("Couldn't read dims of JPEG image.");
   return info;
 }
 
 bool JpegParser::CanParse(ImageSource *encoded) const {
-  return false;  // TODO(janton)
+  jpeg_marker_t first_marker;
+  return (ReadHeader(first_marker.data(), encoded, first_marker.size()) == first_marker.size() &&
+          first_marker == soi_marker);
 }
 
 }  // namespace imgcodec
