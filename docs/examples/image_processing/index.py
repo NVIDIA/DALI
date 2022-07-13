9e5328bc16c6588ce56dcc0f03812f4c4eb657d2
Michal Zientkiewicz <michalz@nvidia.com>
2022-07-13 15:11:34 +0200

    Add references to image_processing/index. Add optional ordering to references.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- docs/examples/image_processing/index.py
+++ docs/examples/image_processing/index.py
@@ -1,13 +1,60 @@
 doc(title="Image Processing",
     underline_char="=",
     entries=[
-        "augmentation_gallery.ipynb",
-        "brightness_contrast_example.ipynb",
-        "color_space_conversion.ipynb",
-        "decoder_examples.ipynb",
-        "hsv_example.ipynb",
-        "interp_types.ipynb",
-        "resize.ipynb",
-        "warp.ipynb",
-        "3d_transforms.ipynb",
+        doc_entry(
+            "augmentation_gallery.ipynb",
+            [
+                op_reference("fn.erase", "Augmentation gallery"),
+                op_reference("fn.water", "Augmentation gallery"),
+                op_reference("fn.sphere", "Augmentation gallery"),
+                op_reference("fn.warp_affine", "Augmentation gallery"),
+                op_reference("fn.jpeg_compression_distortion", "Augmentation gallery"),
+                op_reference("fn.paste", "Augmentation gallery"),
+                op_reference("fn.flip", "Augmentation gallery"),
+                op_reference("fn.rotate", "Augmentation gallery"),
+                op_reference("fn.hsv", "Augmentation gallery"),
+                op_reference("fn.brightness_contrast", "Augmentation gallery")
+            ]
+        ),
+        doc_entry(
+            "brightness_contrast_example.ipynb",
+            op_reference("fn.brightness_contrast", "BrightnessContrast example", 0),
+        ),
+        doc_entry(
+            "color_space_conversion.ipynb",
+            op_reference("fn.color_space_conversion", "Color space conversion tutorial", 0),
+        ),
+        doc_entry(
+            "decoder_examples.ipynb",
+            [
+                op_reference("fn.decoders.image",               "Image decoder examples", 0),
+                op_reference("fn.decoders.image_random_crop",   "Image decoder examples", 0),
+                op_reference("fn.decoders.image_crop",          "Image decoder examples", 0),
+                op_reference("fn.decoders.image_slice",         "Image decoder examples", 0)
+            ]
+        ),
+        doc_entry(
+            "hsv_example.ipynb",
+            op_reference("fn.hsv", "HSV example", 0),
+        ),
+        doc_entry(
+            "interp_types.ipynb",
+            op_reference("fn.resize", "Interpolation methods", 1)
+        ),
+        doc_entry(
+            "resize.ipynb",
+            op_reference("fn.resize", "Resize operator tutorial", 0)
+        ),
+        doc_entry(
+            "warp.ipynb",
+            op_reference("fn.warp_affine", "WarpAffine example", 0)
+        ),
+        doc_entry(
+            "3d_transforms.ipynb",
+            [
+                op_reference("fn.resize", "3D transforms", 3),
+                op_reference("fn.warp_affine", "3D transforms"),
+                op_reference("fn.rotate", "3D transforms")
+            ]
+        )
     ])
