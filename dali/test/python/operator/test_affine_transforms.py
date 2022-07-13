25f222ad391d90c016082a81f81b84c95ad6f9f9
Albert Wolant <awolant@nvidia.com>
2022-07-13 14:06:12 +0200

    Fix affine transform tests
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/test/python/operator/test_affine_transforms.py
+++ dali/test/python/operator/test_affine_transforms.py
@@ -505,8 +505,8 @@ def test_combine_transforms(batch_size=3, num_threads=4, device_id=0):
                                                 batch_size, num_threads, device_id
 
 
-def test_combine_transforms_correct_order(batch_size=3, num_threads=4, device_id=0):
-    pipe = Pipeline(batch_size=batch_size, num_threads=num_threads, device_id=device_id)
+def test_combine_transforms_correct_order():
+    pipe = Pipeline(batch_size=3, num_threads=4, device_id=0)
     with pipe:
         import nvidia.dali.fn.transforms as T
         t1 = T.translation(offset=(1, 2))
