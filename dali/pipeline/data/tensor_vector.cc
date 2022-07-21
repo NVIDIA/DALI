c85495e358260b0d435fade06969b45431fb4adf
Krzysztof Lecki <klecki@nvidia.com>
2022-07-21 12:35:52 +0200

    Early exit
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/tensor_vector.cc
+++ dali/pipeline/data/tensor_vector.cc
@@ -314,14 +314,14 @@ void TensorVector<Backend>::SetSize(int new_size) {
 template <typename Backend>
 void TensorVector<Backend>::set_type(DALIDataType new_type_id) {
   DALI_ENFORCE(new_type_id != DALI_NO_TYPE, "new_type must be valid type.");
+  if (type_.id() == new_type_id)
+    return;
   DALI_ENFORCE(type_.id() == new_type_id || (!has_data() || type_.id() == DALI_NO_TYPE),
                make_string("set_type cannot be used to change the current type - it is not "
                            "allowed to cause allocations. Currently set type: '",
                            type_.id(), "' trying to set: '", new_type_id,
                            "'. You may change the current type using Resize or by"
                            " calling Reset first."));
-  if (type_.id() == new_type_id)
-    return;
   type_ = TypeTable::GetTypeInfo(new_type_id);
   tl_->set_type(new_type_id);
   for (auto t : tensors_) {
