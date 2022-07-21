0e42487f5860df420bdea051e7002e8feb2357cc
Albert Wolant <awolant@nvidia.com>
2022-07-21 19:30:47 +0200

    Fix
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/test/python/external_source/extended/test_impl_utils_frameworks.py
+++ dali/test/python/external_source/extended/test_impl_utils_frameworks.py
@@ -46,38 +46,46 @@ def run_checks(samples_allowed, batches_allowed, samples_disallowed, batches_dis
             external_source_impl.assert_cpu_batch_data_type), sample
 
 
-def non_uniform_tl():
-    def get_samples():
-        return [np.array([42, 42]), np.array([1, 2, 3])]
 
-    @pipeline_def(batch_size=2, num_threads=4, device_id=0)
-    def pipe():
-        return fn.external_source(source=get_samples)
-    p = pipe()
-    p.build()
-    return p.run()[0]
-
-
-def test_regular_containers():
+def test_pytorch_containers():
+    import torch
     samples_cpu = [
-        (test_array, test_array),
-        (tensors.TensorCPU(test_array), test_array)
+        (torch.tensor(test_array), test_array),
     ]
     batches_cpu = [
-        ([test_array], [test_array]),
-        ([test_array] * 4, [test_array] * 4),
-        ([tensors.TensorCPU(test_array)], [test_array]),
-        ([tensors.TensorCPU(test_array)] * 4, [test_array] * 4),
-        (tensors.TensorListCPU(test_array), test_array),
+        ([torch.tensor(test_array)], [test_array]),
+        ([torch.tensor(test_array)] * 4, [test_array] * 4),
     ]
-    yield from run_checks(samples_cpu, batches_cpu, [], [])
+    disallowed_samples = [
+        torch.tensor(test_array).cuda(),
+    ]
+    yield from run_checks(samples_cpu, batches_cpu, disallowed_samples, [])
 
 
-def test_non_uniform_batch():
-    batches_disallowed = [
-        [test_array, np.array([[42, 42]], dtype=np.uint8)],
-        non_uniform_tl()
+def test_mxnet_containers():
+    import mxnet as mx
+
+    samples_cpu = [
+        (mx.nd.array(test_array), test_array),
     ]
-    for b in batches_disallowed:
-        yield raises(ValueError, "Uniform input is required (batch of tensors of equal shapes)")(
-            external_source_impl.batch_to_numpy), b
+    batches_cpu = [
+        ([mx.nd.array(test_array)], [test_array]),
+        ([mx.nd.array(test_array)] * 4, [test_array] * 4),
+    ]
+    disallowed_samples = [
+        mx.nd.array(test_array, ctx=mx.gpu(0))
+    ]
+    yield from run_checks(samples_cpu, batches_cpu, disallowed_samples, [])
+
+
+def test_cupy_containers():
+    import cupy as cp
+    test_array = cp.array([[42, 42], [42, 42]], dtype=cp.uint8)
+    disallowed_samples = [
+        test_array,
+        tensors.TensorGPU(test_array)
+    ]
+    disallowed_batches = [
+        tensors.TensorListGPU(test_array)
+    ]
+    yield from run_checks([], [], disallowed_samples, disallowed_batches)
