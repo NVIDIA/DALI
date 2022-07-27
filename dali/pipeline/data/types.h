895230f84df05282ddd8a95df6314db17defccba
Krzysztof Lecki <klecki@nvidia.com>
2022-07-27 11:26:52 +0200

    Remove changes in test.h
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/pipeline/data/types.h
+++ dali/pipeline/data/types.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2017-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -277,7 +277,6 @@ constexpr bool IsUnsigned(DALIDataType type) {
       return false;
   }
 }
-
 
 template <DALIDataType id>
 struct id2type_helper;
