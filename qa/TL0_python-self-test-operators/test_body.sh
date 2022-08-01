832e33281f87cc5d42bde66c3e8dcd3bd1c12ec3
Albert Wolant <awolant@nvidia.com>
2022-08-01 23:40:51 +0200

    Remove nose2 invokation
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL0_python-self-test-operators/test_body.sh
+++ qa/TL0_python-self-test-operators/test_body.sh
@@ -7,8 +7,6 @@ test_py_with_framework() {
     for test_script in $(ls test_operator_*.py | sed "/$FILTER_PATTERN/d"); do
         ${python_invoke_test} --attr '!slow,!pytorch,!mxnet,!cupy,!numba' ${test_script}
     done
-
-    ${python_new_invoke_test} operator
 }
 
 test_no_fw() {
