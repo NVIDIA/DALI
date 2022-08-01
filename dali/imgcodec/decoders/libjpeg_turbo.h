fa2ecd681eafe8a895a91450f7019a5c7d98f54e
Michał Staniewski <m.staniewzki@gmail.com>
2022-08-01 19:44:19 +0200

    Fix
    Signed-off-by: Michał Staniewski <m.staniewzki@gmail.com>
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/imgcodec/decoders/libjpeg_turbo.h
+++ dali/imgcodec/decoders/libjpeg_turbo.h
@@ -31,6 +31,7 @@ class DLL_PUBLIC LibJpegTurboDecoderInstance : public BatchParallelDecoderImpl {
   LibJpegTurboDecoderInstance(int device_id, ThreadPool *tp)
   : BatchParallelDecoderImpl(device_id, tp) {}
 
+  using BatchParallelDecoderImpl::CanDecode;
   bool CanDecode(ImageSource *in, DecodeParams opts, const ROI &roi) override {
     return opts.format != DALI_YCbCr;  // not supported by libjpeg-turbo
   }
