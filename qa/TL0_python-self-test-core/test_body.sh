b77b618a2b150597cd9b5a6ef5e407881ac1e3f1
Albert Wolant <awolant@nvidia.com>
2022-08-04 22:43:32 +0200

    Fix linter
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL0_python-self-test-core/test_body.sh
+++ qa/TL0_python-self-test-core/test_body.sh
@@ -10,8 +10,6 @@ test_py_with_framework() {
                             test_external_source_impl_utils.py); do
         ${python_invoke_test} --attr '!slow,!pytorch,!mxnet,!cupy' ${test_script}
     done
-
-    # ${python_new_invoke_test} -A '!slow,!pytorch,!mxnet,!cupy' -s core
 }
 
 test_py() {
@@ -23,7 +21,7 @@ test_py() {
 }
 
 test_pytorch() {
-    ${python_invoke_test} --attr '!slow,!pytorch,!mxnet,!cupy' test_dali_variable_batch_size.py
+    ${python_invoke_test} --attr '!slow,pytorch' test_dali_variable_batch_size.py
 }
 
 test_no_fw() {
