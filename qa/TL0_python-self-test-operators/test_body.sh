e6e43c9fcd8140dabe691240b2e0abe1b506cb32
Albert Wolant <44801854+awolant@users.noreply.github.com>
2022-08-04 09:58:21 +0200

    Move operator test files (#4125)
    * Move test files
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/TL0_python-self-test-operators/test_body.sh
+++ qa/TL0_python-self-test-operators/test_body.sh
@@ -1,14 +1,9 @@
 #!/bin/bash -e
 
 test_py_with_framework() {
-    # readers and decoders are tested in the TL0_python-self-test-core
-    FILTER_PATTERN="test_operator_readers_.*\.py\|test_operator_decoders_.*\.py"
-
-    for test_script in $(ls test_operator_*.py | sed "/$FILTER_PATTERN/d"); do
+    for test_script in $(ls operator/test_*.py); do
         ${python_invoke_test} --attr '!slow,!pytorch,!mxnet,!cupy,!numba' ${test_script}
     done
-
-    ${python_new_invoke_test} operator
 }
 
 test_no_fw() {
--- qa/TL1_python-self-test-slow/test.sh
+++ qa/TL1_python-self-test-slow/test.sh
@@ -4,7 +4,7 @@ pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pill
 target_dir=./dali/test/python
 
 test_body() {
-    for test_script in $(ls test_operator_*.py test_pipeline*.py test_functional_api.py test_backend_impl.py); do
+    for test_script in $(ls operator/test_*.py reader/test_*.py decoder/test_*.py test_pipeline*.py test_functional_api.py test_backend_impl.py); do
         ${python_invoke_test} --attr 'slow' ${test_script}
     done
 }
