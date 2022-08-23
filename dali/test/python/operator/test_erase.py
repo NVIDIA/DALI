183fb3e618c3c1ca97dc3dcc439d9c94817e9c1a
Joaquin Anton <janton@nvidia.com>
2022-08-23 11:02:32 +0200

    Add python tests
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/test/python/operator/test_erase.py
+++ dali/test/python/operator/test_erase.py
@@ -31,6 +31,14 @@ class ErasePipeline(Pipeline):
         self.layout = layout
         self.iterator = iterator
         self.inputs = ops.ExternalSource()
+
+        if isinstance(fill_value, RandomDataIterator):
+            self.fill_value_iterator = fill_value
+            self.fill_value_inputs = ops.ExternalSource()
+            fill_value = None
+        else:
+            self.fill_value_iterator = None
+
         self.erase = ops.Erase(device=self.device,
                                anchor=anchor,
                                shape=shape,
@@ -43,12 +51,19 @@ class ErasePipeline(Pipeline):
     def define_graph(self):
         self.data = self.inputs()
         random_data = self.data.gpu() if self.device == 'gpu' else self.data
-        out = self.erase(random_data)
+        if self.fill_value_iterator is not None:
+            self.fill_value_data = self.fill_value_inputs()
+            out = self.erase(random_data, fill_value=self.fill_value_data)
+        else:
+            out = self.erase(random_data)
         return out
 
     def iter_setup(self):
         data = self.iterator.next()
         self.feed_input(self.data, data, layout=self.layout)
+        if self.fill_value_iterator is not None:
+            fill_value_data = self.fill_value_iterator.next()
+            self.feed_input(self.fill_value_data, fill_value_data)
 
 
 def get_axes(layout, axis_names):
@@ -87,7 +102,7 @@ def erase_func(anchor, shape, axis_names, axes, layout, fill_value, image):
     if not axes:
         axes = get_axes(layout, axis_names)
 
-    if not fill_value:
+    if fill_value is None:
         fill_value = 0
 
     roi_starts, roi_ends = get_regions(image.shape, axes, anchor, shape)
@@ -119,31 +134,53 @@ class ErasePythonPipeline(Pipeline):
         self.inputs = ops.ExternalSource()
         self.data_layout = data_layout
 
-        function = partial(erase_func, anchor, shape, axis_names, axes, data_layout, fill_value)
+        if isinstance(fill_value, RandomDataIterator):
+            self.fill_value_iterator = fill_value
+            self.fill_value_inputs = ops.ExternalSource()
+            fill_value = None
+            function = partial(erase_func, anchor, shape, axis_names, axes, data_layout)
+        else:
+            self.fill_value_iterator = None
+            function = partial(erase_func, anchor, shape, axis_names, axes, data_layout, fill_value)
 
         self.erase = ops.PythonFunction(function=function, output_layouts=data_layout)
 
     def define_graph(self):
         self.data = self.inputs()
-        out = self.erase(self.data)
+        if self.fill_value_iterator is not None:
+            self.fill_value_data = self.fill_value_inputs()
+            out = self.erase(self.fill_value_data, self.data)
+        else:
+            out = self.erase(self.data)
         return out
 
     def iter_setup(self):
         data = self.iterator.next()
-        self.feed_input(self.data, data, layout=self.data_layout)
+        self.feed_input(self.data, data)
+        if self.fill_value_iterator is not None:
+            fill_value_data = self.fill_value_iterator.next()
+            self.feed_input(self.fill_value_data, fill_value_data)
 
 
 def check_operator_erase_vs_python(device, batch_size, input_shape,
                                    anchor, shape, axis_names, axes, input_layout, fill_value):
     eii1 = RandomDataIterator(batch_size, shape=input_shape, dtype=np.float32)
     eii2 = RandomDataIterator(batch_size, shape=input_shape, dtype=np.float32)
+
+    fill_value_arg1 = fill_value
+    fill_value_arg2 = fill_value
+    if fill_value == 'random':
+        fill_eii1 = RandomDataIterator(batch_size, shape=input_shape[-1:], dtype=np.float32)
+        fill_eii2 = RandomDataIterator(batch_size, shape=input_shape[-1:], dtype=np.float32)
+        fill_value_arg1 = iter(fill_eii1)
+        fill_value_arg2 = iter(fill_eii2)
+
     compare_pipelines(
         ErasePipeline(device, batch_size, input_layout, iter(eii1), anchor=anchor,
-                      shape=shape, axis_names=axis_names, axes=axes, fill_value=fill_value),
+                      shape=shape, axis_names=axis_names, axes=axes, fill_value=fill_value_arg1),
         ErasePythonPipeline(device, batch_size, input_layout, iter(eii2), anchor=anchor,
-                            shape=shape, axis_names=axis_names, axes=axes, fill_value=fill_value),
+                            shape=shape, axis_names=axis_names, axes=axes, fill_value=fill_value_arg2),
         batch_size=batch_size, N_iterations=3, eps=1e-04, expected_layout=input_layout)
-
 
 def test_operator_erase_vs_python():
     # layout, shape, axis_names, axes, anchor, shape, fill_value
@@ -151,16 +188,19 @@ def test_operator_erase_vs_python():
             ("HWC", (60, 80, 3), "HW", None, (4, 10), (40, 50), None),
             ("HWC", (60, 80, 3), "HW", None, (4, 2, 3, 4), (50, 10, 10, 50), -1),
             ("HWC", (60, 80, 3), "HW", None, (4, 2, 3, 4), (50, 10, 10, 50), (118, 185, 0)),
+            ("HWC", (60, 80, 3), "HW", None, (4, 2, 3, 4), (50, 10, 10, 50), "random"),
             ("HWC", (60, 80, 3), "H", None, (4,), (7,), 0),
             ("HWC", (60, 80, 3), "H", None, (4, 15), (7, 8), 0),
             ("HWC", (60, 80, 3), "W", None, (4,), (7,), 0),
             ("HWC", (60, 80, 3), "W", None, (4, 15), (7, 8), 0),
+            ("HWC", (60, 80, 3), "W", None, (4, 15), (7, 8), "random"),
             ("HWC", (60, 80, 3), None, (0, 1), (4, 10), (40, 50), 0),
             ("HWC", (60, 80, 3), None, (0, 1), (4, 2, 3, 4), (50, 10, 10, 50), 0),
             ("HWC", (60, 80, 3), None, (0,), (4,), (7,), 0),
             ("HWC", (60, 80, 3), None, (0,), (4, 15), (7, 8), 0),
             ("HWC", (60, 80, 3), None, (1,), (4,), (7,), 0),
             ("HWC", (60, 80, 3), None, (1,), (4, 15), (7, 8), 0),
+            ("HWC", (60, 80, 3), None, (1,), (4, 15), (7, 8), "random"),
             ("DHWC", (10, 60, 80, 3), "DHW", None, (2, 4, 15), (3, 7, 8), 0),
             ("HWC", (60, 80, 1), "HW", None, (4, 15), (7, 8), 0),
             ("XYZ", (60, 80, 3), "XY", None, (4, 10), (40, 50), -1), ]
