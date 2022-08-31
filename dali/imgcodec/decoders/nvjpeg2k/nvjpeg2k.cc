682eb460a5f3c07749d6c49749c47d104c65807a
Michal Zientkiewicz <michalz@nvidia.com>
2022-08-31 15:23:06 +0200

    Add bulk parameter passing to decoders and factories.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/imgcodec/decoders/nvjpeg2k/nvjpeg2k.cc
+++ dali/imgcodec/decoders/nvjpeg2k/nvjpeg2k.cc
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <map>
 #include <string>
 #include <vector>
 #include "dali/imgcodec/decoders/nvjpeg2k/nvjpeg2k.h"
@@ -36,11 +37,12 @@ bool check_status(nvjpeg2kStatus_t status, ImageSource *in) {
 }
 }  // namespace
 
-NvJpeg2000DecoderInstance::NvJpeg2000DecoderInstance(int device_id)
-: BatchParallelDecoderImpl(device_id)
+NvJpeg2000DecoderInstance::NvJpeg2000DecoderInstance(
+    int device_id, const std::map<std::string, any> &params)
+: BatchParallelDecoderImpl(device_id, params)
 , nvjpeg2k_dev_alloc_(nvjpeg_memory::GetDeviceAllocatorNvJpeg2k())
 , nvjpeg2k_pin_alloc_(nvjpeg_memory::GetPinnedAllocatorNvJpeg2k()) {
-  // TODO(staniewzki): pass params at construction
+  SetParams(params);
   any num_threads_any = GetParam("nvjpeg2k_num_threads");
   int num_threads = num_threads_any.has_value() ? any_cast<int>(num_threads_any) : 4;
   tp_ = std::make_unique<ThreadPool>(num_threads, device_id, true, "NvJpeg2000DecoderInstance");
--- dali/imgcodec/decoders/opencv_fallback.h
+++ dali/imgcodec/decoders/opencv_fallback.h
@@ -15,7 +15,9 @@
 #ifndef DALI_IMGCODEC_DECODERS_OPENCV_FALLBACK_H_
 #define DALI_IMGCODEC_DECODERS_OPENCV_FALLBACK_H_
 
+#include <map>
 #include <memory>
+#include <string>
 #include "dali/imgcodec/image_decoder_interfaces.h"
 #include "dali/imgcodec/decoders/decoder_parallel_impl.h"
 
@@ -28,7 +30,10 @@ namespace imgcodec {
 class DLL_PUBLIC OpenCVDecoderInstance : public BatchParallelDecoderImpl {
  public:
   using Base = BatchParallelDecoderImpl;
-  explicit OpenCVDecoderInstance(int device_id) : Base(device_id) {}
+  explicit OpenCVDecoderInstance(int device_id, const std::map<std::string, any> &params)
+  : Base(device_id, params) {
+    SetParams(params);
+  }
 
   DecodeResult DecodeImplTask(int thread_idx,
                               SampleView<CPUBackend> out,
@@ -55,8 +60,9 @@ class OpenCVDecoderFactory : public ImageDecoderFactory {
     return device_id < 0;
   }
 
-  std::shared_ptr<ImageDecoderInstance> Create(int device_id) const override {
-    return std::make_shared<OpenCVDecoderInstance>(device_id);
+  std::shared_ptr<ImageDecoderInstance> Create(
+        int device_id, const std::map<std::string, any> &params = {}) const override {
+    return std::make_shared<OpenCVDecoderInstance>(device_id, params);
   }
 };
 
