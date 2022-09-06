8e45c0fbc6f0820c94ad2798cff516fd5e14dc90
Michał Sala <msala@nvidia.com>
2022-09-06 16:13:10 +0200

    Remove unnecessary code from nbjpeg_helper.{h,cc}
    Signed-off-by: Michał Sala <msala@nvidia.com>

--- dali/imgcodec/decoders/nvjpeg/nvjpeg_helper.h
+++ dali/imgcodec/decoders/nvjpeg/nvjpeg_helper.h
@@ -24,16 +24,6 @@
 #include "dali/core/error_handling.h"
 #include "dali/core/cuda_error.h"
 #include "dali/core/format.h"
-#include "dali/util/crop_window.h"
-#include "dali/core/backend_tags.h"
-#include "dali/kernels/common/copy.h"
-#include "dali/image/image_factory.h"
-
-#if WITH_DYNAMIC_CUDA_TOOLKIT_ENABLED
-  bool nvjpegIsSymbolAvailable(const char *name);
-#else
-  #define nvjpegIsSymbolAvailable(T) (true)
-#endif
 
 namespace dali {
 namespace imgcodec {
@@ -98,39 +88,6 @@ class NvjpegError : public std::runtime_error {
   nvjpegStatus_t result_;
 };
 
-// Obtain nvJPEG library version or -1 if it is not available
-int nvjpegGetVersion();
-
-struct StateNvJPEG {
-  nvjpegBackend_t nvjpeg_backend;
-  nvjpegBufferPinned_t pinned_buffer;
-  nvjpegJpegState_t decoder_host_state;
-  nvjpegJpegState_t decoder_hybrid_state;
-  nvjpegJpegStream_t jpeg_stream;
-};
-
-template <typename T>
-struct EncodedImageInfo {
-  bool nvjpeg_support;
-  T c;
-  T widths[NVJPEG_MAX_COMPONENT];
-  T heights[NVJPEG_MAX_COMPONENT];
-  nvjpegChromaSubsampling_t subsampling;
-  CropWindow crop_window;
-};
-
-inline nvjpegJpegState_t GetNvjpegState(const StateNvJPEG& state) {
-  switch (state.nvjpeg_backend) {
-    case NVJPEG_BACKEND_HYBRID:
-      return state.decoder_host_state;
-    case NVJPEG_BACKEND_GPU_HYBRID:
-      return state.decoder_hybrid_state;
-    default:
-      DALI_FAIL("Unknown nvjpegBackend_t "
-                + std::to_string(state.nvjpeg_backend));
-  }
-}
-
 inline nvjpegOutputFormat_t GetFormat(DALIImageType type) {
   switch (type) {
     case DALI_RGB:
@@ -148,76 +105,6 @@ inline nvjpegOutputFormat_t GetFormat(DALIImageType type) {
 }
 
 // TODO(spanev): Replace when it is available in the nvJPEG API
-inline uint8_t GetJpegEncoding(const uint8_t* input, size_t size) {
-  if (input[0] != 0xff || input[1] != 0xd8)
-    return 0;
-  const uint8_t* ptr = input + 2;
-  const uint8_t* end = input + size;
-  uint8_t marker = *ptr;
-  ptr++;
-  while (ptr < end) {
-    do {
-      // We ignore padding/custom metadata
-      while (marker != 0xff && ptr != end) {
-        marker = *ptr;
-        ptr++;
-      }
-      if (ptr == end)
-        return 0;
-      marker = *ptr;
-      ptr++;
-    } while (marker == 0 || marker == 0xff);
-    if (marker >= 0xc0 && marker <= 0xcf) {
-      return marker;
-    } else {
-      // Next segment
-      uint16_t segment_length = (*ptr << 8) + *(ptr+1);
-      ptr += segment_length;
-    }
-  }
-  return 0;
-}
-
-inline bool IsProgressiveJPEG(const uint8_t* raw_jpeg, size_t size) {
-  const uint8_t segment_marker = GetJpegEncoding(raw_jpeg, size);
-  constexpr uint8_t progressive_sof = 0xc2;
-  return segment_marker == progressive_sof;
-}
-
-// Predicate to determine if the image should be decoded with the nvJPEG
-// hybrid Huffman decoder instead of the nvjpeg host Huffman decoder
-template <typename T>
-inline bool ShouldUseHybridHuffman(EncodedImageInfo<T>& info,
-                                   const uint8_t* input,
-                                   size_t size,
-                                   unsigned int threshold) {
-  auto &roi = info.crop_window;
-  unsigned int w = static_cast<unsigned int>(info.widths[0]);
-  unsigned int h = static_cast<unsigned int>(
-    roi ? (roi.anchor[0] + roi.shape[0]) : info.heights[0]);
-  // TODO(spanev): replace it by nvJPEG API function when available in future release
-  // We don't wanna call IsProgressiveJPEG if not needed
-  return h*w > threshold && !IsProgressiveJPEG(input, size);
-}
-
-template <typename StorageType>
-void HostFallback(const uint8_t *data, int size, DALIImageType image_type, uint8_t *output_buffer,
-                  cudaStream_t stream, std::string file_name, CropWindow crop_window,
-                  bool use_fast_idct) {
-  std::unique_ptr<Image> img;
-  try {
-    img = ImageFactory::CreateImage(data, size, image_type);
-    img->SetCropWindow(crop_window);
-    img->SetUseFastIdct(use_fast_idct);
-    img->Decode();
-  } catch (std::exception &e) {
-    DALI_FAIL(e.what() + ". File: " + file_name);
-  }
-  const auto decoded = img->GetImage();
-  const auto shape = img->GetShape();
-  kernels::copy<StorageType, StorageCPU>(output_buffer, decoded.get(), volume(shape), stream);
-}
-
 }  // namespace imgcodec
 
 template <>
