de740156e0d259e09e3566ac7eedd2a472976565
Piotr Rak <prak@nvidia.com>
2022-09-21 13:00:44 +0200

    Add rudimentary cmake package files
    Signed-off-by: Piotr Rak <prak@nvidia.com>

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -35,14 +35,58 @@ if (BUILD_DALI_IMGCODEC)
   list(APPEND DALI_LIBS dali_imgcodec)
 endif()
 
-install(TARGETS ${DALI_LIBS}
-        ARCHIVE DESTINATION lib
-        LIBRARY DESTINATION lib
-        PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ WORLD_READ)
+include(CMakePackageConfigHelpers)
+write_basic_package_version_file(
+  "${CMAKE_CURRENT_BINARY_DIR}/DALI/DALIConfigVersion.cmake"
+  VERSION ${DALI_VERSION}
+  COMPATIBILITY SameMajorVersion
+)
+
+set(DALI_CONFIG_PACKAGE_LOCATION ${CMAKE_INSTALL_LIBDIR}/cmake/DALI CACHE STRING
+    "Installation directory for cmake files, a relative path that will be joined with ${CMAKE_INSTALL_PREFIX} or an absolute path.")
 
 # Note the '/' at the end of first path and not present at the end of the other
-install(DIRECTORY ${CMAKE_SOURCE_DIR}/include/ ${CMAKE_SOURCE_DIR}/dali
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/include/
+        COMPONENT Devel
+        DESTINATION include
+        FILES_MATCHING
+          PATTERN "*.h")
+
+install(DIRECTORY ${CMAKE_SOURCE_DIR}/dali
+        COMPONENT Devel-Private
         DESTINATION include
         FILES_MATCHING
           PATTERN "*_test.h" EXCLUDE
           PATTERN "*.h")
+
+install(TARGETS ${DALI_LIBS}
+        EXPORT DALITargets
+        ARCHIVE DESTINATION lib
+        LIBRARY DESTINATION lib
+        PUBLIC_HEADER DESTINATION ${DALI_INC_DIR}
+        PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ WORLD_READ)
+
+export(EXPORT DALITargets
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/DALI/DALITargets.cmake"
+  NAMESPACE DALI::
+)
+
+configure_file(cmake/DALIConfig.cmake
+  "${CMAKE_CURRENT_BINARY_DIR}/DALI/DALIConfig.cmake"
+  COPYONLY
+)
+
+install(EXPORT DALITargets
+  FILE DALITargets.cmake
+  NAMESPACE DALI::
+  DESTINATION ${DALI_CONFIG_PACKAGE_LOCATION}
+  COMPONENT Devel
+)
+
+install(
+  FILES
+    cmake/DALIConfig.cmake
+    "${CMAKE_CURRENT_BINARY_DIR}/DALI/DALIConfigVersion.cmake"
+  DESTINATION "${DALI_CONFIG_PACKAGE_LOCATION}"
+  COMPONENT Devel
+)
