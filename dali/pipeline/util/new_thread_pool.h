6f7a95e7dd75f07f719d9f210319738ad370acf3
Michał Zientkiewicz <mzient@gmail.com>
2022-09-26 23:52:24 +0200

    NewThreadPool - working
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/pipeline/util/new_thread_pool.h
+++ dali/pipeline/util/new_thread_pool.h
@@ -15,46 +15,213 @@
 #ifndef DALI_PIPELINE_UTIL_THREAD_POOL_H_
 #define DALI_PIPELINE_UTIL_THREAD_POOL_H_
 
+#include <map>
 #include <memory>
 #include <queue>
+#include <sstream>
 #include "dali/core/error_handling.h"
-#include <map>
 #include "dali/core/mm/detail/aux_alloc.h"
 
 namespace dali {
 namespace experimental {
 
-class Job {
+class MultipleErrors : public std::runtime_error {
  public:
-  using priority_t = int64_t;
+  MultipleErrors(std::vector<std::exception_ptr> errors)
+  : runtime_error(""), errors_(std::move(errors)) {
+    compose_message();
+  }
 
-  template <typename Runnable>
-  std::enable_if_t<std::is_convertible_v<Runnable, std::function<void(int)>>
-  AddTask(Runnable &&runnable, priority_t priority = {}) {
-    auto it = tasks.emplace(priority, Task());
-    it->second.func = [this, task = &it->second, f = std::move(runnable)](int tid) {
-      try {
-        f(tid);
-      } catch (...) {
-        task->error = std::current_exception();
-      }
-    };
+  virtual const char *what() {
+    return message_.c_str();
+  }
+
+  const std::vector<std::exception_ptr> &errors() const {
+    return errors_;
   }
 
  private:
+  void compose_message() {
+    std::stringstream ss;
+    ss << "Multiple exceptions:\n";
+    for (const auto &e : errors_) {
+      try {
+        std::rethrow_exception(e);
+      } catch (const std::exception &e) {
+        ss << typeid(e).name() << ": " << e.what() << "\n";
+      } catch (...) {
+        ss << "Unknown exception\n";
+      }
+    }
+    message_ = ss.str();
+  }
+
+  std::vector<std::exception_ptr> errors_;
+  std::string message_;
+};
+
+/**
+ * @brief A collection of tasks, ordered by priority
+ *
+ * Tasks are added to a job first and then the entire work is scheduled as a whole.
+ */
+class Job {
+ public:
+  ~Job() {
+    if (started_) {
+      WaitEx<false>();
+    }
+  }
+
+  using priority_t = int64_t;
+
+  template <typename Runnable>
+  std::enable_if_t<std::is_convertible_v<Runnable, std::function<void(int)>>>
+  AddTask(Runnable &&runnable, priority_t priority = {}) {
+    if (started_)
+      throw std::logic_error("This has already been started - cannot add more tasks to it");
+    auto it = tasks_.emplace(priority, Task());
+    try {
+      it->second.func = [this, task = &it->second, f = std::move(runnable)](int tid) {
+        try {
+          f(tid);
+        } catch (...) {
+          task->error = std::current_exception();
+        }
+        if (--num_pending_tasks_ == 0)
+          cv_.notify_one();
+      };
+    } catch (...) {  // if, for whatever reason, we cannot initialize the task, we should erase it
+      tasks_.erase(it);
+      throw;
+    }
+  }
+
+  template <typename Executor>
+  void Run(Executor &executor, bool wait) {
+    if (started_)
+      throw std::logic_error("This job has already been started.");
+    started_ = true;
+    for (auto &x : tasks_) {
+      executor.AddTask(std::move(x.second.func));
+      num_pending_tasks_++;  // increase after successfully scheduling the task - the value
+                             // may go below 0, but we don't care
+    }
+    if (wait)
+      Wait();
+  }
+
+  void Wait() {
+    if (!started_)
+      throw std::logic_error("This job hasn't been run - cannot wait for it.");
+    WaitEx<true>();
+  }
+
+ private:
+  template <bool rethrow>
+  void WaitEx() noexcept(!rethrow) {
+    {
+      std::unique_lock lock(mtx_);
+      cv_.wait(lock, [&]() { return num_pending_tasks_ == 0; });
+      waited_for_ = true;
+    }
+    if constexpr (rethrow) {
+      std::vector<std::exception_ptr> errors;
+      for (auto &x : tasks_) {
+        if (x.second.error)
+          errors.push_back(std::move(x.second.error));
+      }
+      if (errors.size() == 1)
+        std::rethrow_exception(errors[0]);
+      else if (errors.size() > 1)
+        throw MultipleErrors(std::move(errors));
+    }
+  }
+
+
+  std::mutex mtx_;  // this is a dummy mutex - we could just use atomic_wait on num_pending_tasks_
+  std::condition_variable cv_;
+  std::atomic_int num_pending_tasks_{0};
+  bool started_ = false;
+  bool waited_for_ = false;
+
   struct Task {
     std::function<void(int)> func;
     std::exception_ptr error;
   };
 
+  // This needs to be a container which never invalidates references when inserting new items.
   std::multimap<priority_t, Task, std::greater<priority_t>,
-                mm::detail::object_pool_allocator<std::pair<priority_t, Task>>> tasks;
+                mm::detail::object_pool_allocator<std::pair<priority_t, Task>>> tasks_;
 };
 
 class ThreadPool {
  public:
-  void ExecuteWork(Work &work);
+  using TaskFunc = std::function<void(int)>;
+
+  ThreadPool() = default;
+  explicit ThreadPool(int num_threads) {
+    Init(num_threads);
+  }
+
+  void Init(int num_threads) {
+    std::lock_guard g(mtx_);
+    if (!threads_.empty())
+      throw std::logic_error("The thread pool is already started!");
+    stop_requested_ = false;
+    threads_.reserve(num_threads);
+    for (int i = 0; i < num_threads; i++)
+      threads_.push_back(std::thread(&ThreadPool::Run, this, i));
+  }
+
+  ~ThreadPool() {
+    Stop();
+  }
+
+  void AddTask(TaskFunc f) {
+    std::lock_guard g(mtx_);
+    if (stop_requested_)
+      throw std::logic_error("The thread pool is stopped and no longer accepts new tasks.");
+    tasks_.push(std::move(f));
+    cv_.notify_one();
+  }
+
  private:
+   void Stop() {
+    {
+      std::lock_guard g(mtx_);
+      stop_requested_ = true;
+      cv_.notify_all();
+    }
+
+    for (auto &t : threads_)
+      t.join();
+
+    {
+      std::lock_guard g(mtx_);
+      threads_.clear();
+    }
+  }
+
+  void Run(int index) noexcept {
+    std::unique_lock lock(mtx_);
+    while (!stop_requested_) {
+      cv_.wait(lock, [&]() { return stop_requested_ || !tasks_.empty(); });
+      if (stop_requested_)
+        break;
+      TaskFunc t = std::move(tasks_.front());
+      tasks_.pop();
+      lock.unlock();
+      t(index);
+      lock.lock();
+    }
+  }
+
+  std::mutex mtx_;
+  std::condition_variable cv_;
+  bool stop_requested_ = false;
+  std::queue<TaskFunc> tasks_;
+  std::vector<std::thread> threads_;
 };
 
 }  // namespace experimental
--- dali/pipeline/util/new_thread_pool_test.cc
+++ dali/pipeline/util/new_thread_pool_test.cc
@@ -1,6 +1,71 @@
 #include <gtest/gtest.h>
 #include "new_thread_pool.h"
 
-TEST(NewThreadPool, Work) {
+namespace dali {
+namespace experimental {
 
+struct SerialExecutor {
+  template <typename Runnable>
+  std::enable_if_t<std::is_convertible_v<Runnable, std::function<void(int)>>>
+  AddTask(Runnable &&runnable) {
+    runnable(0);
+  }
+};
+
+TEST(NewThreadPool, RunJobInSeries) {
+  Job job;
+  SerialExecutor tp;
+  int a = 0, b = 0, c = 0;
+  job.AddTask([&](int) {
+    a = 1;
+  });
+  job.AddTask([&](int) {
+    b = 2;
+  });
+  job.AddTask([&](int) {
+    c = 3;
+  });
+  job.Run(tp, true);
+  EXPECT_EQ(a, 1);
+  EXPECT_EQ(b, 2);
+  EXPECT_EQ(c, 3);
 }
+
+TEST(NewThreadPool, RunJobInThreadPool) {
+  Job job;
+  ThreadPool tp(4);
+  int a = 0, b = 0, c = 0;
+  job.AddTask([&](int) {
+    a = 1;
+  });
+  job.AddTask([&](int) {
+    b = 2;
+  });
+  job.AddTask([&](int) {
+    c = 3;
+  });
+  job.Run(tp, true);
+  EXPECT_EQ(a, 1);
+  EXPECT_EQ(b, 2);
+  EXPECT_EQ(c, 3);
+}
+
+
+TEST(NewThreadPool, RethrowMultipleErrors) {
+  Job job;
+  ThreadPool tp(4);
+  job.AddTask([&](int) {
+    throw std::runtime_error("Runtime");
+  });
+  job.AddTask([&](int) {
+    // do nothing
+  });
+  job.AddTask([&](int) {
+    throw std::logic_error("Logic");
+  });
+  EXPECT_THROW(job.Run(tp, true), MultipleErrors);
+}
+
+
+}  // namespace experimental
+}  // namespace dali
