5f55e06271d023b83c9bc92fbc4b12456a1e4b7c
Michał Zientkiewicz <mzient@gmail.com>
2022-09-29 00:13:17 +0200

    Add init/exit callbacks.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/pipeline/util/new_thread_pool_test.cc
+++ dali/pipeline/util/new_thread_pool_test.cc
@@ -1,5 +1,19 @@
+// Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <gtest/gtest.h>
-#include "new_thread_pool.h"
+#include "dali/pipeline/util/new_thread_pool.h"
 
 namespace dali {
 namespace experimental {
@@ -33,7 +47,7 @@ TEST(NewThreadPool, RunJobInSeries) {
 
 TEST(NewThreadPool, RunJobInThreadPool) {
   Job job;
-  ThreadPool tp(4);
+  ThreadPoolBase tp(4);
   int a = 0, b = 0, c = 0;
   job.AddTask([&](int) {
     a = 1;
@@ -53,7 +67,7 @@ TEST(NewThreadPool, RunJobInThreadPool) {
 
 TEST(NewThreadPool, RethrowMultipleErrors) {
   Job job;
-  ThreadPool tp(4);
+  ThreadPoolBase tp(4);
   job.AddTask([&](int) {
     throw std::runtime_error("Runtime");
   });
