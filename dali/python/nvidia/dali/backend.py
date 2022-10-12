85602f7c3d8e0f83ca5271cfe989a13482a7944b
Kamil Tokarski <ktokarski@nvidia.com>
2022-10-12 11:29:22 +0200

    Don't stipulate 1.19 version
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- dali/python/nvidia/dali/backend.py
+++ dali/python/nvidia/dali/backend.py
@@ -52,9 +52,9 @@ if not initialized:
                             "Please update your environment to CUDA version 10.2 or newer.")
 
     if __cuda_version__ < 110:
-        deprecation_warning("DALI 1.19 is the last official release that supports CUDA 10.2. "
-                            "The next release will only support CUDA 11 and CUDA 12. "
-                            "Please update your CUDA toolkit.")
+        deprecation_warning(
+            "DALI plans to drop the support for CUDA 10.2 in the upcoming releases. "
+            "Please update your CUDA toolkit.")
 
     for lib in default_plugins:
         LoadLibrary(os.path.join(os.path.dirname(__file__), lib))
