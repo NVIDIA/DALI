9826e6d9908a2ebcd2e8b84c7ff3e2d86e4b319f
Kamil Tokarski <ktokarski@nvidia.com>
2022-10-24 18:13:55 +0200

    Polishing the docs
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- dali/operators/decoder/inflate/inflate.cc
+++ dali/operators/decoder/inflate/inflate.cc
@@ -21,15 +21,18 @@ namespace dali {
 DALI_SCHEMA(experimental__Inflate)
     .DocStr(R"code(Inflates/decompresses the input using specified decompression algorithm.
 
-The input must be a 1D tensor of bytes (uint8). The shape and type of the decompressed samples
-is required. Each input sample can either be a single compressed chunk or consist of multiple
-compressed chunks that have the same shape when inflated, so that they can be be merged into
-a single tensor where the leftmost extent of the tensor corresponds to the number of the chunks.
+The input must be a 1D tensor of bytes (uint8). Passing the ``shape`` and ``dtype`` of the
+decompressed samples is required.
+
+Each input sample can either be a single compressed chunk or consist of multiple
+compressed chunks that have the same shape and type when inflated, so that they can be
+be merged into a single tensor where the leftmost extent of the tensor corresponds
+to the number of the chunks.
 
 If the sample is comprised of multiple chunks, the ``chunks_offsets`` or ``chunks_sizes``
 must be specified. In that case, the ``shape`` must describe the shape of a single inflated
-(output) chunk. For sequences, the number of chunks will automatically be added as
-the leftmost extent to the output tensors.
+(output) chunk. The number of the chunks will automatically be added as the leftmost extent
+to the output tensors.
 
 For example, the following snippet presents decompression of a video-like sequences.
 Each video sequence was deflated by, first, compressing each frame separately and then
@@ -51,8 +54,8 @@ concatenating compressed frames from the corresponding sequences.::
     .NumOutput(1)
     .AddArg(inflate::shapeArgName, "The shape of the output (inflated) chunk.", DALI_INT_VEC, true)
     .AddOptionalTypeArg(inflate::dTypeArgName, "The output (inflated) data type.", DALI_NO_TYPE)
-    .AddOptionalArg(inflate::offsetArgName, R"code("A list of offsets within the sample describing
-where the consecutive chunks begin.
+    .AddOptionalArg(inflate::offsetArgName, R"code("A list of offsets within the input sample
+describing where the consecutive chunks begin.
 
 If the ``chunks_sizes`` is not specified, it is assumed that the chunks are densely packed
 in the input tensor and the last chunk ends with the sample's end.)code",
@@ -69,8 +72,8 @@ Currently only ``LZ4`` is supported.)code",
                     "LZ4")
     .AddOptionalArg(inflate::layoutArgName, R"code(Layout of the output (inflated) chunk.
 
-If the samples consist of multiple chunks, the ``sequence_extent`` extent
-will be automatically added to the beginning of the specified layout.)code",
+If the samples consist of multiple chunks, additionally, the ``sequence_extent`` extent
+will be added to the beginning of the specified layout.)code",
                     TensorLayout(""))
     .AddOptionalArg(inflate::sequenceLayoutArgName, R"code(The name for the sequence extent.
 
