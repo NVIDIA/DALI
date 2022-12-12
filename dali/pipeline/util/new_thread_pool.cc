6d8cb94652e60459de3df11c4c9064da70db0e5a
Michał Zientkiewicz <mzient@gmail.com>
2022-12-12 12:44:01 +0100

    [WIP]
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/pipeline/util/new_thread_pool.cc
+++ dali/pipeline/util/new_thread_pool.cc
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "dali/pipeline/util/new_thread_pool.h"
-
 namespace dali {
 namespace experimental {
 
