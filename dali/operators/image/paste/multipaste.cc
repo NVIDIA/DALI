77c40eed9bc5787c70caf2da384652644f1ff33c
Michał Zientkiewicz <mzient@gmail.com>
2022-12-16 16:44:26 +0100

    Remove dead code.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/operators/image/paste/multipaste.cc
+++ dali/operators/image/paste/multipaste.cc
@@ -85,13 +85,11 @@ void MultiPasteCPU::RunTyped(Workspace &ws) {
     auto paste_count = in_idx_[i].shape[0];
     memset(out_view[i].data, 0, out_view[i].num_elements() * sizeof(OutputType));
 
-    std::string out_source_info;
-    const char *path_delim = ";";
-
     if (no_intersections_[i]) {
       for (int iter = 0; iter < paste_count; iter++) {
         int from_sample = in_idx_[i].data[iter];
         int to_sample = i;
+
         tp.AddWork(
           [&, i, iter, from_sample, to_sample, in_view, out_view](int thread_id) {
             kernels::KernelContext ctx;
