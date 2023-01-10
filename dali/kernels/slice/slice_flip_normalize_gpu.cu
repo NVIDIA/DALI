b9007c7a38f82279313d74e8e43603a5a91e6aab
Joaquin Anton <janton@nvidia.com>
2023-01-10 15:38:56 +0100

    Add new SliceFlipNormalizeGPU kernel
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

---
+++ dali/kernels/slice/slice_flip_normalize_gpu.cu
@@ -0,0 +1,261 @@
+// Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cuda_runtime.h>
+#include <tuple>
+#include "dali/kernels/common/utils.h"
+#include "dali/kernels/slice/slice_flip_normalize_gpu.h"
+#include "dali/kernels/slice/slice_flip_normalize_gpu_impl.cuh"
+
+namespace dali {
+namespace kernels {
+
+namespace slice_flip_normalize {
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>::~SliceFlipNormalizeGPU() = default;
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+int SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>::GetNumChannels(
+    const TensorListShape<ndim> &sh) {
+  if (sh.num_samples() == 0)
+    return 0;
+  if (channel_dim < 0)
+    return 1;
+  const auto first_sh = sh.tensor_shape_span(0);
+  if (channel_dim >= first_sh.size())
+    throw std::invalid_argument("Not enough dimensions in the shape");
+  int nchannels = first_sh[channel_dim];
+  for (int i = 1; i < sh.num_samples(); i++) {
+    if (nchannels != sh.tensor_shape_span(i)[channel_dim]) {
+      throw std::invalid_argument("All samples should have the same number of channels");
+    }
+  }
+  return nchannels;
+}
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+int SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>::GetOutNumChannels(
+    const TensorListShape<ndim> &sh, const Args &args) {
+  if (sh.num_samples() != args.sample_args.size()) {
+    std::invalid_argument(
+        "Number of samples in the arguments should match the number of samples in the shape");
+  }
+  if (sh.num_samples() == 0)
+    return 0;
+  const auto first_sh = sh.tensor_shape_span(0);
+  int nchannels = 1;
+  if (channel_dim < first_sh.size())
+    nchannels = first_sh[channel_dim];
+  int out_nchannels = std::max(nchannels, static_cast<int>(args.sample_args[0].fill_values.size()));
+  for (int i = 1; i < sh.num_samples(); i++) {
+    if (channel_dim > 0 && nchannels != sh.tensor_shape_span(i)[channel_dim])
+      throw std::invalid_argument("All samples should have the same number of channels");
+    if (args.sample_args[i].fill_values.size() != args.sample_args[0].fill_values.size())
+      throw std::invalid_argument(
+          "All sample arguments should have the same number of fill values");
+  }
+  return out_nchannels;
+}
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+KernelRequirements SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>::Setup(
+    KernelContext &ctx, const TensorListShape<ndim> &sh, const Args &args) {
+  (void) ctx;
+  int nsamples = sh.num_samples();
+  if (nsamples != args.sample_args.size())
+    throw std::invalid_argument("Invalid number of samples in kernel args");
+  out_shape_ = TensorListShape<ndim>(nsamples, ndim);
+  out_shape_orig_ = TensorListShape<ndim>(nsamples, ndim);
+  perm_ = args.perm;
+  if (perm_ == ivec<ndim>{})
+    std::iota(perm_.begin(), perm_.end(), 0);
+  inv_perm_ = inverse_permutation(perm_);
+
+  nchannels_ = GetNumChannels(sh);
+  out_nchannels_ = GetOutNumChannels(sh, args);
+
+  for (int i = 0; i < nsamples; i++) {
+    auto out_sh = sh[i];
+    const auto &sample_args = args.sample_args[i];
+    Fill(out_sh, sample_args.roi.extent());
+    out_sh[channel_dim] = out_nchannels_;
+    out_shape_orig_.set_tensor_shape(i, out_sh);
+    out_sh = permute(out_sh, perm_);
+    out_shape_.set_tensor_shape(i, out_sh);
+  }
+  KernelRequirements req;
+  req.output_shapes = {out_shape_};
+  return req;
+}
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+std::tuple<float *, float *, Out *>
+SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>::SetupParams(KernelContext &ctx,
+                                                                       const Args &args) {
+  int num_samples = args.sample_args.size();
+  float *norm_add_cpu = ctx.scratchpad->AllocatePinned<float>(num_samples * nchannels_);
+  float *norm_mul_cpu = ctx.scratchpad->AllocatePinned<float>(num_samples * nchannels_);
+  Out *fill_values_cpu = ctx.scratchpad->AllocatePinned<Out>(num_samples * out_nchannels_);
+  for (int i = 0; i < num_samples; i++) {
+    const auto &a = args.sample_args[i];
+    auto *norm_add_data = norm_add_cpu + i * nchannels_;
+    auto *norm_mul_data = norm_mul_cpu + i * nchannels_;
+    int mean_sz = a.mean.size();
+    assert(mean_sz == a.inv_stddev.size());
+    int c = 0;
+    for (; c < mean_sz; c++) {
+      norm_add_data[c] = -a.mean[c] * a.inv_stddev[c];
+      norm_mul_data[c] = a.inv_stddev[c];
+    }
+    for (; c < nchannels_; c++) {
+      norm_add_data[c] = 0.0f;
+      norm_mul_data[c] = 1.0f;
+    }
+    auto *fill_values_data = fill_values_cpu + i * out_nchannels_;
+    int fill_values_sz = a.fill_values.size();
+    c = 0;
+    for (; c < fill_values_sz; c++)
+      fill_values_data[c] = ConvertSat<Out>(a.fill_values[c]);
+    for (; c < out_nchannels_; c++)
+      fill_values_data[c] = ConvertSat<Out>(0.0f);
+  }
+
+  float *norm_add_gpu = nullptr, *norm_mul_gpu = nullptr;
+  Out *fill_values_gpu;
+  std::tie(norm_add_gpu, norm_mul_gpu, fill_values_gpu) = ctx.scratchpad->ToContiguousGPU(
+      ctx.gpu.stream, make_span(norm_add_cpu, num_samples * nchannels_),
+      make_span(norm_mul_cpu, num_samples * nchannels_),
+      make_span(fill_values_cpu, num_samples * out_nchannels_));
+
+  return std::tuple<float *, float *, Out *>(norm_add_gpu, norm_mul_gpu, fill_values_gpu);
+}
+
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+void SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>::Run(
+    KernelContext &ctx, const OutListGPU<Out, ndim> &out, const InListGPU<In, ndim> &in,
+    const Args &args) {
+  using Tile = kernels::BlockDesc<spatial_ndim>;
+  using Sample = SampleDesc<Out, In, spatial_ndim>;
+  int nsamples = in.num_samples();
+
+  Sample *samples_cpu = ctx.scratchpad->AllocatePinned<Sample>(nsamples);
+
+  float *norm_add_gpu = nullptr, *norm_mul_gpu = nullptr;
+  Out *fill_values_gpu = nullptr;
+  std::tie(norm_add_gpu, norm_mul_gpu, fill_values_gpu) = SetupParams(ctx, args);
+
+  bool need_pad = false;
+  for (int i = 0; i < nsamples; i++) {
+    auto &sample_args = args.sample_args[i];
+    auto &sample = samples_cpu[i];
+
+    auto in_sh = in.shape[i];
+    auto in_strides_sh = GetStrides(in_sh);
+    vec<spatial_ndim, int> in_size;
+    vec<spatial_ndim, int64_t> in_strides;
+    const vec<spatial_ndim, int> zero{0};
+    Fill(in_size, in_sh);
+    Fill(in_strides, in_strides_sh);
+
+    auto out_data = out.tensor_data(i);
+    auto out_size = static_cast<vec<spatial_ndim, int>>(sample_args.roi.extent());
+    auto out_strides_sh = GetStrides(out_shape_[i]);
+    out_strides_sh = permute(out_strides_sh, inv_perm_);
+    vec<spatial_ndim, int64_t> out_strides;
+    Fill(out_strides, out_strides_sh);
+
+    auto in_data = in.tensor_data(i);
+    auto roi = sample_args.roi;
+    Roi<2> bounds = {zero, out_size};
+    vec<spatial_ndim, int> pad_begin = max(-roi.lo, zero);
+    vec<spatial_ndim, int> pad_end = max(roi.hi - in_size, zero);
+    need_pad |= pad_begin != zero || pad_end != zero;
+
+    // We do flipping by adjusting the input data pointer to point to the last
+    // element in the dimension, changing the sign of the stride in that dimension
+    // and adjusting the ROI accordingly, so that we can index the surface object
+    // as in the non-flipped case.
+    for (int d = 0; d < spatial_ndim; d++) {
+      if (sample_args.flip[d]) {
+        in_data += (roi.hi[d] - 1) * in_strides[d];
+        in_strides[d] = -in_strides[d];
+        bounds.lo[d] = pad_end[d];
+        bounds.hi[d] = out_size[d] - pad_begin[d];
+      } else {
+        in_data += roi.lo[d] * in_strides[d];
+        bounds.lo[d] = pad_begin[d];
+        bounds.hi[d] = out_size[d] - pad_end[d];
+      }
+    }
+
+    sample.in = {in_data, in_size, nchannels_, in_strides, in_strides_sh[channel_dim]};
+    sample.out = {out_data, out_size, out_nchannels_, out_strides, out_strides_sh[channel_dim]};
+    sample.bounds = bounds;
+    sample.norm_add = norm_add_gpu + i * nchannels_;
+    sample.norm_mul = norm_mul_gpu + i * nchannels_;
+    sample.fill_values = fill_values_gpu + i * out_nchannels_;
+  }
+
+  block_setup_.SetDefaultBlockSize({64, 32});
+  block_setup_.SetBlockDim(dim3(32, 16, 1));
+  block_setup_.SetupBlocks(out_shape_orig_, true);
+  auto tiles_cpu = block_setup_.Blocks();
+  auto grid_dim = block_setup_.GridDim();
+  auto block_dim = block_setup_.BlockDim();
+
+  Sample *samples_gpu = nullptr;
+  Tile *tiles_gpu = nullptr;
+  std::tie(samples_gpu, tiles_gpu) =
+      ctx.scratchpad->ToContiguousGPU(ctx.gpu.stream, make_span(samples_cpu, nsamples), tiles_cpu);
+
+  if (spatial_ndim == 2) {
+    if (need_pad) {
+      SliceNormalizeKernel_2D<Out, In>
+          <<<grid_dim, block_dim, 0, ctx.gpu.stream>>>(samples_gpu, tiles_gpu);
+    } else {
+      SliceNormalizeKernel_2D_NoPad<Out, In>
+          <<<grid_dim, block_dim, 0, ctx.gpu.stream>>>(samples_gpu, tiles_gpu);
+    }
+  } else {
+    assert(false);  // TODO(janton): implement
+  }
+  CUDA_CALL(cudaGetLastError());
+}
+
+#define INSTANTIATE_IMPL(OutType, InType, spatial_ndim, channel_dim) \
+  template class SliceFlipNormalizeGPU<OutType, InType, spatial_ndim, channel_dim>
+
+#define INSTANTIATE_IMPL_FOREACH_INTYPE(OutType, spatial_ndim, channel_dim) \
+  INSTANTIATE_IMPL(OutType, uint8_t, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL(OutType, int16_t, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL(OutType, uint16_t, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL(OutType, int32_t, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL(OutType, float, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL(OutType, float16, spatial_ndim, channel_dim);
+
+#define INSTANTIATE_IMPL_FOREACH_OUTTYPE(spatial_ndim, channel_dim) \
+  INSTANTIATE_IMPL_FOREACH_INTYPE(float, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL_FOREACH_INTYPE(float16, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL_FOREACH_INTYPE(uint8_t, spatial_ndim, channel_dim); \
+  INSTANTIATE_IMPL_FOREACH_INTYPE(int8_t, spatial_ndim, channel_dim);
+
+INSTANTIATE_IMPL_FOREACH_OUTTYPE(2, 2);  // HWC
+INSTANTIATE_IMPL_FOREACH_OUTTYPE(2, 0);  // CHW
+
+}  // namespace slice_flip_normalize
+
+}  // namespace kernels
+}  // namespace dali
---
+++ dali/kernels/slice/slice_flip_normalize_gpu_test.cu
@@ -0,0 +1,434 @@
+// Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gtest/gtest.h>
+#include <string>
+#include <vector>
+#include "dali/core/cuda_stream_pool.h"
+#include "dali/core/cuda_event.h"
+#include "dali/kernels/dynamic_scratchpad.h"
+#include "dali/kernels/scratch.h"
+#include "dali/kernels/slice/slice_cpu.h"
+#include "dali/kernels/slice/slice_flip_normalize_gpu.h"
+#include "dali/kernels/slice/slice_flip_normalize_permute_pad_gpu.h"
+#include "dali/kernels/slice/slice_kernel_test.h"
+#include "dali/test/dali_test_config.h"
+#include "dali/util/file.h"
+#include "dali/util/numpy.h"
+
+namespace dali {
+namespace kernels {
+namespace slice_flip_normalize {
+namespace test {
+
+template <typename Out, typename In, int spatial_ndim, int channel_dim>
+class SliceFlipNormalizeGPUTest : public ::testing::Test {
+ public:
+  static constexpr int ndim = spatial_ndim + (channel_dim >= 0);
+  static constexpr int d_dim = spatial_ndim < 3 ? -1 : channel_dim == 0 ? 1 : 0;
+  static constexpr int h_dim = 0 + (channel_dim == 0) + (spatial_ndim == 3);
+  static constexpr int w_dim = h_dim + 1;
+  using Kernel = SliceFlipNormalizeGPU<Out, In, spatial_ndim, channel_dim>;
+
+  void RunKernel(TestTensorList<Out, ndim> &output, TestTensorList<In, ndim> &input,
+                 const typename Kernel::Args &args) {
+    KernelContext ctx;
+    DynamicScratchpad scratchpad;
+    ctx.scratchpad = &scratchpad;
+    ctx.gpu.stream = 0;
+
+    Kernel kernel;
+
+    auto in_gpu = input.gpu();
+    auto req = kernel.Setup(ctx, in_gpu.shape, args);
+
+    auto out_sh = req.output_shapes[0].template to_static<ndim>();
+    output.reshape(out_sh);
+
+    kernel.Run(ctx, output.gpu(), input.gpu(), args);
+
+    CUDA_CALL(cudaStreamSynchronize(0));
+  }
+
+  template <typename T, int ndim>
+  void FeedData(TestTensorList<T, ndim> &data,
+                const std::vector<Tensor<CPUBackend>>& samples) {
+    TensorListShape<ndim> sh;
+    sh.resize(samples.size());
+    int nsamples = samples.size();
+    for (int s = 0; s < nsamples; s++) {
+      sh.set_tensor_shape(s, samples[s].shape());
+    }
+    data.reshape(sh);
+    auto data_cpu = data.cpu();
+    for (int s = 0; s < nsamples; s++) {
+      auto &t = samples[s];
+      std::memcpy(data_cpu.tensor_data(s), t.template data<T>(), volume(t.shape()) * sizeof(T));
+    }
+  }
+
+  void LoadTensor(Tensor<CPUBackend> &tensor, const std::string& path_npy) {
+    auto stream = FileStream::Open(path_npy, false, false);
+    tensor = ::dali::numpy::ReadTensor(stream.get());
+  }
+
+  template <typename T, int ndim>
+  void LoadBatch(TestTensorList<T, ndim> &data, const std::string& dir,
+                 const std::vector<std::string>& filenames) {
+    std::vector<Tensor<CPUBackend>> samples;
+    samples.resize(filenames.size());
+    for (size_t i = 0; i < filenames.size(); i++) {
+      LoadTensor(samples[i], dir + "/" + filenames[i]);
+    }
+    FeedData(data, samples);
+  }
+};
+
+class SliceFlipNormalizeGPUTest_uint8_uint8_2D_HWC
+    : public SliceFlipNormalizeGPUTest<uint8_t, uint8_t, 2, 2> {};
+class SliceFlipNormalizeGPUTest_float_uint8_2D_HWC
+    : public SliceFlipNormalizeGPUTest<float, uint8_t, 2, 2> {};
+
+TEST_F(SliceFlipNormalizeGPUTest_uint8_uint8_2D_HWC, only_crop_1) {
+  auto tc_dir = testing::dali_extra_path() + "/db/test_data/crop_mirror_normalize/only_crop/1";
+
+  TestTensorList<uint8_t, 3> input;
+  TestTensorList<uint8_t, 3> ref;
+  this->LoadBatch(input, tc_dir,  {"input0.npy", "input1.npy", "input2.npy"});
+  this->LoadBatch(ref, tc_dir, {"output0.npy", "output1.npy", "output2.npy"});
+
+  TestTensorList<uint8_t, 3> output;
+  typename Kernel::Args args;
+  args.perm = {0, 1, 2};
+  args.sample_args = {
+    { Roi<2>{{8, 2}, {19, 8}} },
+    { Roi<2>{{2, 22}, {4, 28}} },
+    { Roi<2>{{4, 55}, {5, 100}} }
+  };
+
+  this->RunKernel(output, input, args);
+
+  Check(ref.cpu(), output.cpu(), EqualEps(1e-6));
+}
+
+TEST_F(SliceFlipNormalizeGPUTest_uint8_uint8_2D_HWC, crop_and_mirror) {
+  auto tc_dir =
+      testing::dali_extra_path() + "/db/test_data/crop_mirror_normalize/crop_and_mirror/1";
+
+  TestTensorList<uint8_t, 3> input;
+  TestTensorList<uint8_t, 3> ref;
+  this->LoadBatch(input, tc_dir,  {"input0.npy", "input1.npy", "input2.npy"});
+  this->LoadBatch(ref, tc_dir, {"output0.npy", "output1.npy", "output2.npy"});
+
+  TestTensorList<uint8_t, 3> output;
+  typename Kernel::Args args;
+  args.perm = {0, 1, 2};
+  args.sample_args = {
+    { Roi<2>{{8, 2}, {19, 8}}, {true, false} },
+    { Roi<2>{{2, 22}, {4, 28}}, {true, true} },
+    { Roi<2>{{4, 55}, {5, 100}}, {false, false} }
+  };
+
+  this->RunKernel(output, input, args);
+
+  Check(ref.cpu(), output.cpu(), EqualEps(1e-6));
+}
+
+TEST_F(SliceFlipNormalizeGPUTest_float_uint8_2D_HWC, crop_and_mirror_and_normalize) {
+  auto tc_dir = testing::dali_extra_path() +
+                "/db/test_data/crop_mirror_normalize/crop_and_mirror_and_normalize/1";
+
+  TestTensorList<uint8_t, 3> input;
+  TestTensorList<float, 3> ref;
+  this->LoadBatch(input, tc_dir,  {"input0.npy", "input1.npy", "input2.npy"});
+  this->LoadBatch(ref, tc_dir, {"output0.npy", "output1.npy", "output2.npy"});
+
+  TestTensorList<float, 3> output;
+  typename Kernel::Args args;
+  args.perm = {0, 1, 2};
+
+  args.sample_args = {
+    { Roi<2>{{8, 2}, {19, 8}}, {true, false},
+      {255 * 0.485f, 255 * 0.456f, 255 * 0.406f},
+      {1.0f / (255 * 0.229f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.225f)} },
+    { Roi<2>{{2, 22}, {4, 28}}, {true, true},
+      {255 * 0.455f, 255 * 0.436f, 255 * 0.416f},
+      {1.0f / (255 * 0.225f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.221f)} },
+    { Roi<2>{{4, 55}, {5, 100}}, {false, false},
+      {255 * 0.495f, 255 * 0.466f, 255 * 0.396f},
+      {1.0f / (255 * 0.226f), 1.0f / (255 * 0.229f), 1.0f / (255 * 0.222f)} }
+  };
+
+  this->RunKernel(output, input, args);
+
+  Check(ref.cpu(), output.cpu(), EqualEps(1e-6));
+}
+
+TEST_F(SliceFlipNormalizeGPUTest_float_uint8_2D_HWC, crop_and_mirror_and_normalize_and_transpose) {
+  auto tc_dir = testing::dali_extra_path() +
+                "/db/test_data/crop_mirror_normalize/crop_and_mirror_and_normalize_and_transpose/1";
+
+  TestTensorList<uint8_t, 3> input;
+  TestTensorList<float, 3> ref;
+  this->LoadBatch(input, tc_dir, {"input0.npy", "input1.npy", "input2.npy"});
+  this->LoadBatch(ref, tc_dir, {"output0.npy", "output1.npy", "output2.npy"});
+
+  TestTensorList<float, 3> output;
+  typename Kernel::Args args;
+  args.perm = {2, 0, 1};
+
+  args.sample_args = {
+    { Roi<2>{{8, 2}, {19, 8}}, {true, false},
+      {255 * 0.485f, 255 * 0.456f, 255 * 0.406f},
+      {1.0f / (255 * 0.229f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.225f)} },
+    { Roi<2>{{2, 22}, {4, 28}}, {true, true},
+      {255 * 0.455f, 255 * 0.436f, 255 * 0.416f},
+      {1.0f / (255 * 0.225f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.221f)} },
+    { Roi<2>{{4, 55}, {5, 100}}, {false, false},
+      {255 * 0.495f, 255 * 0.466f, 255 * 0.396f},
+      {1.0f / (255 * 0.226f), 1.0f / (255 * 0.229f), 1.0f / (255 * 0.222f)} }
+  };
+
+  this->RunKernel(output, input, args);
+
+  Check(ref.cpu(), output.cpu(), EqualEps(1e-6));
+}
+
+TEST_F(SliceFlipNormalizeGPUTest_float_uint8_2D_HWC, pad_and_normalize) {
+  auto tc_dir = testing::dali_extra_path() +
+                "/db/test_data/crop_mirror_normalize/pad_and_normalize/1";
+
+  TestTensorList<uint8_t, 3> input;
+  TestTensorList<float, 3> ref;
+  this->LoadBatch(input, tc_dir, {"input0.npy", "input1.npy", "input2.npy"});
+  this->LoadBatch(ref, tc_dir, {"output0.npy", "output1.npy", "output2.npy"});
+
+  TestTensorList<float, 3> output;
+  typename Kernel::Args args;
+  args.perm = {};
+
+  args.sample_args = {
+    { Roi<2>{{0, -2}, {19, 8}}, {false, false},
+      {255 * 0.485f, 255 * 0.456f, 255 * 0.406f},
+      {1.0f / (255 * 0.229f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.225f)},
+      {255.0f, 128.0f, 64.0f, 32.0f}
+    },
+    { Roi<2>{{-4, 0}, {4, 28}}, {false, false},
+      {255 * 0.455f, 255 * 0.436f, 255 * 0.416f},
+      {1.0f / (255 * 0.225f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.221f)},
+      {255.0f + 1, 128.0f + 1, 64.0f + 1, 32.0f + 1}
+    },
+    { Roi<2>{{0, 0}, {5, 120}}, {false, false},
+      {255 * 0.495f, 255 * 0.466f, 255 * 0.396f},
+      {1.0f / (255 * 0.226f), 1.0f / (255 * 0.229f), 1.0f / (255 * 0.222f)},
+      {255.0f + 2, 128.0f + 2, 64.0f + 2, 32.0f + 2}
+    }
+  };
+
+  this->RunKernel(output, input, args);
+
+  Check(ref.cpu(), output.cpu(), EqualEps(1e-6));
+}
+
+TEST_F(SliceFlipNormalizeGPUTest_float_uint8_2D_HWC, pad_and_flip_and_normalize) {
+  auto tc_dir = testing::dali_extra_path() +
+                "/db/test_data/crop_mirror_normalize/pad_and_flip_and_normalize/1";
+
+  TestTensorList<uint8_t, 3> input;
+  TestTensorList<float, 3> ref;
+  this->LoadBatch(input, tc_dir, {"input0.npy", "input1.npy", "input2.npy"});
+  this->LoadBatch(ref, tc_dir, {"output0.npy", "output1.npy", "output2.npy"});
+
+  TestTensorList<float, 3> output;
+  typename Kernel::Args args;
+  args.perm = {};
+
+  args.sample_args = {
+    { Roi<2>{{0, -2}, {19, 8}}, {true, false},
+      {255 * 0.485f, 255 * 0.456f, 255 * 0.406f},
+      {1.0f / (255 * 0.229f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.225f)},
+      {255.0f, 128.0f, 64.0f, 32.0f}
+    },
+    { Roi<2>{{-4, 0}, {4, 28}}, {true, true},
+      {255 * 0.455f, 255 * 0.436f, 255 * 0.416f},
+      {1.0f / (255 * 0.225f), 1.0f / (255 * 0.224f), 1.0f / (255 * 0.221f)},
+      {255.0f + 1, 128.0f + 1, 64.0f + 1, 32.0f + 1}
+    },
+    { Roi<2>{{0, 0}, {5, 120}}, {false, false},
+      {255 * 0.495f, 255 * 0.466f, 255 * 0.396f},
+      {1.0f / (255 * 0.226f), 1.0f / (255 * 0.229f), 1.0f / (255 * 0.222f)},
+      {255.0f + 2, 128.0f + 2, 64.0f + 2, 32.0f + 2}
+    }
+  };
+
+  this->RunKernel(output, input, args);
+
+  Check(ref.cpu(), output.cpu(), EqualEps(1e-6));
+}
+
+TEST(SliceFlipNormalizeGPUTest, Benchmark) {
+  using Kernel = SliceFlipNormalizeGPU<float, uint8_t, 2, 2>;
+  typename Kernel::Args args;
+  Kernel kernel;
+
+  int nsamples = 64;
+  using InType = uint8_t;
+  using OutType = float;
+  TestTensorList<InType, 3> in;
+  TensorListShape<3> in_sh = uniform_list_shape(nsamples, TensorShape<3>{1280, 900, 3});
+  in.reshape(in_sh);
+
+  args.perm = {2, 0, 1};
+  args.sample_args.resize(nsamples);
+  for (int i = 0; i < nsamples; i++) {
+    args.sample_args[i] = {Roi<2>{{400, 300}, {400 + 256, 300 + 256}},
+         {false, false},
+         {255 * 0.495f, 255 * 0.466f, 255 * 0.396f},
+         {1.0f / (255 * 0.226f), 1.0f / (255 * 0.229f), 1.0f / (255 * 0.222f)}};
+  }
+
+  auto bench = [&]() {
+    auto stream = CUDAStreamPool::instance().Get();
+
+    KernelContext ctx;
+    ctx.gpu.stream = stream;
+    DynamicScratchpad scratch;
+    ctx.scratchpad = &scratch;
+
+    TestTensorList<OutType, 3> out;
+    auto req = kernel.Setup(ctx, in_sh, args);
+    out.reshape(req.output_shapes[0].to_static<3>());
+    auto out_view = out.gpu();
+    auto in_view = in.gpu();
+
+    // warm-up
+    CUDAEvent started  = CUDAEvent::CreateWithFlags(0);  // timing enabled
+    CUDAEvent finished = CUDAEvent::CreateWithFlags(0);
+    CUDA_CALL(cudaDeviceSynchronize());
+    kernel.Run(ctx, out_view, in_view, args);
+    int iters = 100;
+    CUDA_CALL(cudaEventRecord(started, ctx.gpu.stream));
+    for (int i = 0; i < iters; i++)
+      kernel.Run(ctx, out_view, in_view, args);
+    CUDA_CALL(cudaEventRecord(finished, ctx.gpu.stream));
+    CUDA_CALL(cudaDeviceSynchronize());
+    float time_ms = 0;
+    CUDA_CALL(cudaEventElapsedTime(&time_ms, started, finished));
+    time_ms /= iters;
+    int64_t data_size =  // note: using out_view.num_elements() twice in purpose (we are not reading
+                         // all input)
+        out_view.num_elements() * sizeof(InType) + out_view.num_elements() * sizeof(OutType);
+    std::cout << data_size * 1e-6 / time_ms << " GB/s" << std::endl;
+  };
+
+  std::cout << "with transpose: ";
+  args.perm = {2, 0, 1};
+  bench();
+  std::cout << "no transpose: ";
+  args.perm = {0, 1, 2};
+  bench();
+}
+
+TEST(SliceFlipNormalizeGPUTest, BenchmarkOld) {
+  using Kernel = SliceFlipNormalizePermutePadGpu<float, uint8_t, 3>;
+  Kernel kernel;
+  std::vector<SliceFlipNormalizePermutePadArgs<3>> args;
+  int nsamples = 64;
+  args.reserve(nsamples);
+  for (int i = 0; i < nsamples; i++) {
+    args.emplace_back(TensorShape<3>{256, 256, 3}, TensorShape<3>{1280, 900, 3});
+    auto &a = args.back();
+    a.anchor[0] = 400;
+    a.anchor[1] = 300;
+    a.flip[0] = a.flip[1] = false;
+    a.permuted_dims[0] = 2;
+    a.permuted_dims[1] = 0;
+    a.permuted_dims[2] = 1;
+    a.mean.resize(3);
+    a.mean[0] = 255 * 0.495f;
+    a.mean[1] = 255 * 0.466f;
+    a.mean[2] = 255 * 0.396f;
+    a.inv_stddev.resize(3);
+    a.inv_stddev[0] = 1.0f / (255 * 0.226f);
+    a.inv_stddev[1] = 1.0f / (255 * 0.229f);
+    a.inv_stddev[2] = 1.0f / (255 * 0.222f);
+    a.fill_values.resize(3, 0.0f);
+    a.channel_dim = 2;
+  }
+
+  using InType = uint8_t;
+  using OutType = float;
+  TensorListShape<3> in_sh = uniform_list_shape(nsamples, TensorShape<3>{1280, 900, 3});
+  TestTensorList<InType, 3> in;
+  in.reshape(in_sh);
+  auto in_view = in.gpu();
+
+  auto bench = [&]() {
+    auto stream = CUDAStreamPool::instance().Get();
+
+    KernelContext ctx;
+    ctx.gpu.stream = stream;
+    DynamicScratchpad scratch;
+    ctx.scratchpad = &scratch;
+
+    TestTensorList<OutType, 3> out;
+    KernelRequirements req = kernel.Setup(ctx, in_view, args);
+    out.reshape(req.output_shapes[0].to_static<3>());
+    auto out_view = out.gpu();
+
+    // warm-up
+    CUDAEvent started  = CUDAEvent::CreateWithFlags(0);  // timing enabled
+    CUDAEvent finished = CUDAEvent::CreateWithFlags(0);
+    CUDA_CALL(cudaDeviceSynchronize());
+    kernel.Run(ctx, out_view, in_view, args);
+    int iters = 100;
+    CUDA_CALL(cudaEventRecord(started, ctx.gpu.stream));
+    for (int i = 0; i < iters; i++)
+      kernel.Run(ctx, out_view, in_view, args);
+    CUDA_CALL(cudaEventRecord(finished, ctx.gpu.stream));
+    CUDA_CALL(cudaDeviceSynchronize());
+    float time_ms = 0;
+    CUDA_CALL(cudaEventElapsedTime(&time_ms, started, finished));
+    time_ms /= iters;
+    int64_t data_size =  // note: using out_view.num_elements() twice in purpose (we are not reading
+                        // all input)
+        out_view.num_elements() * sizeof(InType) + out_view.num_elements() * sizeof(OutType);
+    std::cout << data_size * 1e-6 / time_ms << " GB/s" << std::endl;
+  };
+
+
+  std::cout << "with transpose: ";
+  for (int i = 0; i < nsamples; i++) {
+    auto &a = args[i];
+    a.permuted_dims[0] = 2;
+    a.permuted_dims[1] = 0;
+    a.permuted_dims[2] = 1;
+  }
+  bench();
+
+  std::cout << "no transpose: ";
+  for (int i = 0; i < nsamples; i++) {
+    auto &a = args[i];
+    a.permuted_dims[0] = 0;
+    a.permuted_dims[1] = 1;
+    a.permuted_dims[2] = 2;
+  }
+  bench();
+}
+
+
+}  // namespace test
+}  // namespace slice_flip_normalize
+}  // namespace kernels
+}  // namespace dali
