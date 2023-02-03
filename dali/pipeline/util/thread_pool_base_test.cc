8ea8fcc6035a5c88ff116f6959183f9422102f60
Michal Zientkiewicz <michalz@nvidia.com>
2023-02-03 17:20:58 +0100

    Moving files.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/pipeline/util/thread_pool_base_test.cc
+++ dali/pipeline/util/thread_pool_base_test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,23 +13,24 @@
 // limitations under the License.
 
 #include <gtest/gtest.h>
-#include "dali/pipeline/util/new_thread_pool.h"
+#include "dali/pipeline/util/thread_pool_base.h"
+#include "dali/core/format.h"
 
 namespace dali {
 namespace experimental {
 
 struct SerialExecutor {
   template <typename Runnable>
-  std::enable_if_t<std::is_convertible_v<Runnable, std::function<void(int)>>>
+  std::enable_if_t<std::is_convertible_v<Runnable, std::function<void()>>>
   AddTask(Runnable &&runnable) {
-    runnable(0);
+    runnable();
   }
 };
 
 TEST(NewThreadPool, Scrap) {
   EXPECT_NO_THROW({
     Job job;
-    job.AddTask([](int) {});
+    job.AddTask([]() {});
     job.Scrap();
   });
 }
@@ -37,7 +38,7 @@ TEST(NewThreadPool, Scrap) {
 TEST(NewThreadPool, ErrorNotStarted) {
   try {
     Job job;
-    job.AddTask([](int) {});
+    job.AddTask([]() {});
   } catch (std::logic_error &e) {
     EXPECT_NE(nullptr, strstr(e.what(), "The job is not empty"));
     return;
@@ -50,13 +51,13 @@ TEST(NewThreadPool, RunJobInSeries) {
   Job job;
   SerialExecutor tp;
   int a = 0, b = 0, c = 0;
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     a = 1;
   });
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     b = 2;
   });
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     c = 3;
   });
   job.Run(tp, true);
@@ -69,13 +70,13 @@ TEST(NewThreadPool, RunJobInThreadPool) {
   Job job;
   ThreadPoolBase tp(4);
   int a = 0, b = 0, c = 0;
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     a = 1;
   });
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     b = 2;
   });
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     c = 3;
   });
   job.Run(tp, true);
@@ -88,18 +89,58 @@ TEST(NewThreadPool, RunJobInThreadPool) {
 TEST(NewThreadPool, RethrowMultipleErrors) {
   Job job;
   ThreadPoolBase tp(4);
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     throw std::runtime_error("Runtime");
   });
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     // do nothing
   });
-  job.AddTask([&](int) {
+  job.AddTask([&]() {
     throw std::logic_error("Logic");
   });
   EXPECT_THROW(job.Run(tp, true), MultipleErrors);
 }
 
+template <typename... Args>
+void SyncPrint(Args&& ...args) {
+  static std::mutex mtx;
+  std::lock_guard guard(mtx);
+  std::stringstream ss;
+  print(ss, std::forward<Args>(args)...);
+  auto &&str = ss.str();
+  printf("%s", str.c_str());
+}
+
+TEST(NewThreadPool, Reentrant) {
+  Job job;
+  ThreadPoolBase tp(1);  // must not hang with just one thread
+  std::atomic_int outer{0}, inner{0};
+  for (int i = 0; i < 10; i++) {
+    job.AddTask([&, i]() {
+        outer |= (i << 10);
+    });
+  }
+
+  job.AddTask([&]() {
+    Job innerJob;
+
+    for (int i = 0; i < 10; i++)
+      innerJob.AddTask([&, i]() {
+        inner |= (1 << i);
+      });
+
+    innerJob.Run(tp, false);
+    innerJob.Wait();
+    outer |= (1 << 11);
+  });
+
+  for (int i = 11; i < 20; i++) {
+    job.AddTask([&, i]() {
+        outer |= (1 << i);
+    });
+  }
+  job.Run(tp, true);
+}
 
 }  // namespace experimental
 }  // namespace dali
