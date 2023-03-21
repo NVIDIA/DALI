d014a920b6edeb338032787f1abaeb382096709d
szalpal <mszolucha@nvidia.com>
2023-03-21 10:17:45 +0100

    Naive histogram test
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- docs/examples/custom_operations/custom_operator/naive_histogram/test_naive_histogram.py
+++ docs/examples/custom_operations/custom_operator/naive_histogram/test_naive_histogram.py
@@ -19,12 +19,11 @@ import nvidia.dali.fn as fn
 
 # Load the Custom Operator
 import nvidia.dali.plugin_manager as plugin_manager
-
-plugin_manager.load_library("./build/libnaivehistogram.so")
+plugin_manager.load_library('./build/libnaivehistogram.so')
 
 
 # List test files. This step should be customized.
-dali_extra_path = os.environ["DALI_EXTRA_PATH"]
+dali_extra_path = os.environ['DALI_EXTRA_PATH']
 test_file_list = [
     dali_extra_path + "/db/single/jpeg/100/swan-3584559_640.jpg",
     dali_extra_path + "/db/single/jpeg/113/snail-4368154_1280.jpg",
@@ -39,14 +38,14 @@ test_file_list = [
 @pipeline_def
 def naive_hist_pipe():
     img, _ = fn.readers.file(files=test_file_list)
-    # The naive_histogram accepts single-channels image, thus we conert the image to Grayscale.
-    img = fn.decoders.image(img, device="mixed", output_type=DALIImageType.GRAY)
+    # The naive_histogram accepts single-channels image, thus we convert the image to Grayscale.
+    img = fn.decoders.image(img, device='mixed', output_type=DALIImageType.GRAY)
     img = img.gpu()
     img = fn.naive_histogram(img, n_bins=24)
     return img
 
-
-pipe = naive_hist_pipe(batch_size=2, num_threads=1, device_id=0)
-pipe.build()
-out = pipe.run()
-print(out[0].as_cpu().as_array())
+def test_naive_histogram():
+    pipe = naive_hist_pipe(batch_size=2, num_threads=1, device_id=0)
+    pipe.build()
+    out = pipe.run()
+    print(out[0].as_cpu().as_array())
---
+++ qa/TL1_naive_histogram/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+do_once() {
+  apt-get update && apt-get -y install wget
+  wget https://github.com/Kitware/CMake/releases/download/v3.18.4/cmake-3.18.4-Linux-x86_64.sh
+  bash cmake-*.sh --skip-license --prefix=/usr
+  rm cmake-*.sh
+}
+
+test_body() {
+    pushd $(pwd)/../../docs/examples/custom_operations/custom_operator/naive_histogram
+    (mkdir build && cd build && cmake .. && make -j"$(grep ^processor /proc/cpuinfo | wc -l)")
+    ${python_invoke_test} test_naive_histogram.py
+    popd
+}
+
+pushd ../..
+source ./qa/test_template.sh
+popd
