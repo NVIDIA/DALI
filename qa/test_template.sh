66a4adffa6b9912b8f9b82ddcf03a613fa8bca6d
Michał Zientkiewicz <mzient@gmail.com>
2023-03-22 09:16:34 +0100

    Fix stderr timestamps.
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- qa/test_template.sh
+++ qa/test_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source /opt/dali/qa/test_template_impl.sh | perl -pe 'use POSIX strftime;
+source /opt/dali/qa/test_template_impl.sh 2>&1 | perl -pe 'use POSIX strftime;
                     $|=1;
                     select((select(STDERR), $| = 1)[0]);
                     print strftime "[%Y-%m-%d %H:%M:%S] ", localtime'
