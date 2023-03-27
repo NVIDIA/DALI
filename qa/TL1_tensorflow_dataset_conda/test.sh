ab34e88a2bb68ae2efec6ed945194edfb33488e8
Janusz Lisiecki <jlisiecki@nvidia.com>
2023-03-27 12:58:48 +0200

    Review fix
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_tensorflow_dataset_conda/test.sh
+++ qa/TL1_tensorflow_dataset_conda/test.sh
@@ -1,3 +1,11 @@
-pushd ../TL1_tensorflow_dataset
-bash -e ./test.sh
+#!/bin/bash -e
+source ../TL1_tensorflow_dataset/test_impl.sh
+
+# populate epilog and prolog with variants to enable/disable conda
+# every test will be executed for bellow configs
+prolog=(enable_conda)
+epilog=(disable_conda)
+
+pushd ../..
+source ./qa/test_template.sh
 popd
