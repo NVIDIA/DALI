da78096da66cd3a0a0e750378c29473aaeb7cf51
Albert Wolant <44801854+awolant@users.noreply.github.com>
2023-04-06 10:22:14 +0200

    Update CLANG version (#4768)
    * Fix versions for clang and libclang python packages.
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL1_nodeps_build/test.sh
+++ qa/TL1_nodeps_build/test.sh
@@ -8,8 +8,8 @@ do_once() {
     bash cmake-*.sh --skip-license --prefix=/usr
     rm cmake-*.sh
     # for stub generation
-    pip install clang
-    pip install libclang
+    pip install clang==14.0
+    pip install libclang==14.0.1
 }
 
 test_body() {
