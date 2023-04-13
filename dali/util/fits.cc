235cfc21b1384c5abfb96f9925d7254157bcbfa7
aderylo <a.m.derylo@gmail.com>
2023-04-13 21:55:01 +0200

    Add reading undecoded data to fits utils
    Signed-off-by: aderylo <a.m.derylo@gmail.com>

--- dali/util/fits.cc
+++ dali/util/fits.cc
@@ -68,7 +68,7 @@ int ImgTypeToDatatypeCode(int img_type) {
   }
 }
 
-const TypeInfo &TypeFromFitsDatatypeCode(int datatype) {
+const TypeInfo& TypeFromFitsDatatypeCode(int datatype) {
   switch (datatype) {
     case TSBYTE:
       return TypeTable::GetTypeInfo<int8_t>();
@@ -95,7 +95,7 @@ const TypeInfo &TypeFromFitsDatatypeCode(int datatype) {
   }
 }
 
-void ParseHeader(HeaderData &parsed_header, fitsfile *src) {
+void ParseHeader(HeaderData& parsed_header, fitsfile* src) {
   int32_t hdu_type, img_type, n_dims, status = 0;
 
   FITS_CALL(fits_get_hdu_type(src, &hdu_type, &status));
@@ -118,6 +118,109 @@ void ParseHeader(HeaderData &parsed_header, fitsfile *src) {
   for (auto it = dims.rbegin(); it != dims.rend(); ++it) {
     parsed_header.shape.shape.push_back(static_cast<int64_t>(*it));
   }
+}
+
+int extract_undecoded_data(fitsfile* fptr, std::vector<std::vector<uint8_t>>& raw_data,
+                           std::vector<int64_t>& tile_offset, std::vector<int64_t>& tile_size,
+                           long tiles, int* status) {
+  raw_data.resize(tiles);
+  tile_offset.resize(tiles + 1);
+  tile_size.resize(tiles);
+
+  LONGLONG fpixel[MAX_COMPRESS_DIM], lpixel[MAX_COMPRESS_DIM];
+  for (int i = 0; i < (fptr->Fptr)->zndim; ++i) {
+    fpixel[i] = 1;
+    lpixel[i] = (fptr->Fptr)->znaxis[i];
+  }
+
+  long naxis[MAX_COMPRESS_DIM] = {1, 1, 1, 1, 1, 1};
+  long tiledim[MAX_COMPRESS_DIM] = {1, 1, 1, 1, 1, 1};
+  long tilesize[MAX_COMPRESS_DIM] = {1, 1, 1, 1, 1, 1};
+  long ftile[MAX_COMPRESS_DIM] = {1, 1, 1, 1, 1, 1};
+  long ltile[MAX_COMPRESS_DIM] = {1, 1, 1, 1, 1, 1};
+  long rowdim[MAX_COMPRESS_DIM] = {1, 1, 1, 1, 1, 1};
+  long tfpixel[MAX_COMPRESS_DIM], tlpixel[MAX_COMPRESS_DIM];
+  long offset[MAX_COMPRESS_DIM], thistilesize[MAX_COMPRESS_DIM];
+  long mfpixel[MAX_COMPRESS_DIM], mlpixel[MAX_COMPRESS_DIM];
+  long i5, i4, i3, i2, i1, i0, irow;
+  long ntemp, sum_nelemll;
+  int ndim, size;
+
+  ndim = (fptr->Fptr)->zndim;
+  ntemp = 1;
+  for (int i = 0; i < ndim; ++i) {
+    if (fpixel[i] <= lpixel[i]) {
+      mfpixel[i] = (long)fpixel[i];
+      mlpixel[i] = (long)lpixel[i];
+    } else {
+      mfpixel[i] = (long)lpixel[i];
+      mlpixel[i] = (long)fpixel[i];
+    }
+
+    naxis[i] = (fptr->Fptr)->znaxis[i];
+    tilesize[i] = (fptr->Fptr)->tilesize[i];
+    tiledim[i] = (naxis[i] - 1) / tilesize[i] + 1;
+    ftile[i] = (mfpixel[i] - 1) / tilesize[i] + 1;
+    ltile[i] = minvalue((mlpixel[i] - 1) / tilesize[i] + 1, tiledim[i]);
+    rowdim[i] = ntemp;
+    ntemp *= tiledim[i];
+  }
+
+  size = 0;
+  sum_nelemll = 0;
+  for (i5 = ftile[5]; i5 <= ltile[5]; ++i5) {
+    tfpixel[5] = (i5 - 1) * tilesize[5] + 1;
+    tlpixel[5] = minvalue(tfpixel[5] + tilesize[5] - 1, naxis[5]);
+    thistilesize[5] = tlpixel[5] - tfpixel[5] + 1;
+    offset[5] = (i5 - 1) * rowdim[5];
+    for (i4 = ftile[4]; i4 <= ltile[4]; ++i4) {
+      tfpixel[4] = (i4 - 1) * tilesize[4] + 1;
+      tlpixel[4] = minvalue(tfpixel[4] + tilesize[4] - 1, naxis[4]);
+      thistilesize[4] = thistilesize[5] * (tlpixel[4] - tfpixel[4] + 1);
+      offset[4] = (i4 - 1) * rowdim[4] + offset[5];
+      for (i3 = ftile[3]; i3 <= ltile[3]; ++i3) {
+        tfpixel[3] = (i3 - 1) * tilesize[3] + 1;
+        tlpixel[3] = minvalue(tfpixel[3] + tilesize[3] - 1, naxis[3]);
+        thistilesize[3] = thistilesize[4] * (tlpixel[3] - tfpixel[3] + 1);
+        offset[3] = (i3 - 1) * rowdim[3] + offset[4];
+        for (i2 = ftile[2]; i2 <= ltile[2]; ++i2) {
+          tfpixel[2] = (i2 - 1) * tilesize[2] + 1;
+          tlpixel[2] = minvalue(tfpixel[2] + tilesize[2] - 1, naxis[2]);
+          thistilesize[2] = thistilesize[3] * (tlpixel[2] - tfpixel[2] + 1);
+          offset[2] = (i2 - 1) * rowdim[2] + offset[3];
+          for (i1 = ftile[1]; i1 <= ltile[1]; ++i1) {
+            tfpixel[1] = (i1 - 1) * tilesize[1] + 1;
+            tlpixel[1] = minvalue(tfpixel[1] + tilesize[1] - 1, naxis[1]);
+            thistilesize[1] = thistilesize[2] * (tlpixel[1] - tfpixel[1] + 1);
+            offset[1] = (i1 - 1) * rowdim[1] + offset[2];
+            for (i0 = ftile[0]; i0 <= ltile[0]; ++i0) {
+              tfpixel[0] = (i0 - 1) * tilesize[0] + 1;
+              tlpixel[0] = minvalue(tfpixel[0] + tilesize[0] - 1, naxis[0]);
+              thistilesize[0] = thistilesize[1] * (tlpixel[0] - tfpixel[0] + 1);
+              irow = i0 + offset[1];
+
+              LONGLONG nelemll = 0, noffset = 0;
+              ffgdesll(fptr, (fptr->Fptr)->cn_compressed, irow, &nelemll, &noffset, status);
+
+              tile_offset[size] = sum_nelemll;
+              tile_size[size] = (long)thistilesize[0];
+
+              unsigned char charnull = 0;
+              raw_data[size].resize(nelemll / sizeof(unsigned char));
+              fits_read_col(fptr, TBYTE, (fptr->Fptr)->cn_compressed, irow, 1, (long)nelemll,
+                            &charnull, raw_data[size].data(), nullptr, status);
+
+              ++size;
+              sum_nelemll += nelemll;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  tile_offset[size] = sum_nelemll;
+  return (*status);
 }
 
 DALIDataType HeaderData::type() const {
