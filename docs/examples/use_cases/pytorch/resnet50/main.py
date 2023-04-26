94af2fd3cae5d6d825189310c356bace706069d7
Janusz Lisiecki <jlisiecki@nvidia.com>
2023-04-26 12:59:43 +0200

    Review fixes
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- docs/examples/use_cases/pytorch/resnet50/main.py
+++ docs/examples/use_cases/pytorch/resnet50/main.py
@@ -379,15 +379,14 @@ def main():
                       args.total_batch_size / total_time.avg))
 
 class data_prefetcher():
+    """Based on prefetcher from the APEX example
+       https://github.com/NVIDIA/apex/blob/5b5d41034b506591a316c308c3d2cd14d5187e23/examples/imagenet/main_amp.py#L265
+    """
     def __init__(self, loader):
         self.loader = iter(loader)
         self.stream = torch.cuda.Stream()
         self.mean = torch.tensor([0.485 * 255, 0.456 * 255, 0.406 * 255]).cuda().view(1,3,1,1)
         self.std = torch.tensor([0.229 * 255, 0.224 * 255, 0.225 * 255]).cuda().view(1,3,1,1)
-        # With Amp, it isn't necessary to manually convert data to half.
-        # if args.fp16:
-        #     self.mean = self.mean.half()
-        #     self.std = self.std.half()
         self.preload()
 
     def preload(self):
@@ -397,27 +396,9 @@ class data_prefetcher():
             self.next_input = None
             self.next_target = None
             return
-        # if record_stream() doesn't work, another option is to make sure device inputs are created
-        # on the main stream.
-        # self.next_input_gpu = torch.empty_like(self.next_input, device='cuda')
-        # self.next_target_gpu = torch.empty_like(self.next_target, device='cuda')
-        # Need to make sure the memory allocated for next_* is not still in use by the main stream
-        # at the time we start copying to next_*:
-        # self.stream.wait_stream(torch.cuda.current_stream())
         with torch.cuda.stream(self.stream):
             self.next_input = self.next_input.cuda(non_blocking=True)
             self.next_target = self.next_target.cuda(non_blocking=True)
-            # more code for the alternative if record_stream() doesn't work:
-            # copy_ will record the use of the pinned source tensor in this side stream.
-            # self.next_input_gpu.copy_(self.next_input, non_blocking=True)
-            # self.next_target_gpu.copy_(self.next_target, non_blocking=True)
-            # self.next_input = self.next_input_gpu
-            # self.next_target = self.next_target_gpu
-
-            # With Amp, it isn't necessary to manually convert data to half.
-            # if args.fp16:
-            #     self.next_input = self.next_input.half()
-            # else:
             self.next_input = self.next_input.float()
             self.next_input = self.next_input.sub_(self.mean).div_(self.std)
 
@@ -425,6 +406,8 @@ class data_prefetcher():
         return self
 
     def __next__(self):
+        """The iterator was added on top of the orignal example to align it with DALI iterator
+        """
         torch.cuda.current_stream().wait_stream(self.stream)
         input = self.next_input
         target = self.next_target
