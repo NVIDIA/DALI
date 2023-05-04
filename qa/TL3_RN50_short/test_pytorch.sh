76d0af21c2ba65109e34065dfe9fa8fd273457b2
Janusz Lisiecki <jlisiecki@nvidia.com>
2023-05-04 11:33:21 +0200

    Fix
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL3_RN50_short/test_pytorch.sh
+++ qa/TL3_RN50_short/test_pytorch.sh
@@ -13,10 +13,10 @@ cd /opt/dali/docs/examples/use_cases/pytorch/resnet50
 NUM_GPUS=$(nvidia-smi -L | wc -l)
 
 if [ ! -d "val" ]; then
-   cp -r /data/imagenet/val-jpeg/ val
+   ln -sf /data/imagenet/val-jpeg/ val
 fi
 if [ ! -d "train" ]; then
-   cp -r /data/imagenet/train-jpeg/ train
+   ln -sf /data/imagenet/train-jpeg/ train
 fi
 
 LOG=dali.log
