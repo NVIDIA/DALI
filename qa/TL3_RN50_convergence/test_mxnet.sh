3464c04f60119ea0a5da3f73415772b45a7e57d8
Janusz Lisiecki <jlisiecki@nvidia.com>
2023-05-04 13:13:13 +0200

    Fix
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL3_RN50_convergence/test_mxnet.sh
+++ qa/TL3_RN50_convergence/test_mxnet.sh
@@ -16,8 +16,7 @@ cat tmp2.log | grep -o "0\.[0-9]*" > dali.log
 best=`awk 'BEGIN { max = -inf } { if ($1 > max) { max = $1 } } END { print max }' dali.log`
 mean=`awk 'BEGIN { sum = 0; n = 0 } { sum += $1; n += 1 } END { print sum / n }' tmp2.log`
 
-rm tmp2.log
-rm tmp2.log tmp3.log
+rm -rf tmp2.log
 
 if [[ `echo "$best $threshold" | awk '{ print ($1 >= $2) ? "1" : "0" }'` -eq "0" ]]; then
     echo "acc = $best; TEST FAILED"
