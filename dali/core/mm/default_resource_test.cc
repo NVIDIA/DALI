aa342bf804f3a9358dfce5e5058c4e373ff8406c
Michal Zientkiewicz <michalz@nvidia.com>
2023-05-08 14:57:43 +0200

    Disable tests that check memory utilization on integrated GPUs.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/core/mm/default_resource_test.cc
+++ dali/core/mm/default_resource_test.cc
@@ -379,14 +379,22 @@ static void ReleaseUnusedTestImpl(ssize_t max_alloc_size = std::numeric_limits<s
 }
 
 TEST(MMDefaultResource, ReleaseUnusedBasic) {
-  size_t free, total;
-  CUDA_CALL(cudaMemGetInfo(&free, &total));
-  ReleaseUnusedTestImpl(align_down(free / 2, 64 << 20));
+  cudaDeviceProp device_prop;
+  CUDA_CALL(cudaGetDeviceProperties(&device_prop, 0));
+  if (device_prop.integrated)
+    GTEST_SKIP() << "The memory usage on integrated GPUs cannot be reliably tracked.";
+
+  ReleaseUnusedTestImpl(256 << 20);
 }
 
 TEST(MMDefaultResource, ReleaseUnusedMaxMem) {
   if (!UseVMM())
     GTEST_SKIP() << "Cannot reliably test ReleaseUnused with max mem usage without VMM support";
+
+  cudaDeviceProp device_prop;
+  CUDA_CALL(cudaGetDeviceProperties(&device_prop, 0));
+  if (device_prop.integrated)
+    GTEST_SKIP() << "The memory usage on integrated GPUs cannot be reliably tracked.";
 
   ReleaseUnusedTestImpl();
 }
