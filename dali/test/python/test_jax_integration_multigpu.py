1a27c57c05020fa4b175afd9535490f718ca3ae6
Albert Wolant <awolant@nvidia.com>
2023-05-31 10:47:55 +0200

    Add comment to the test
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- dali/test/python/test_jax_integration_multigpu.py
+++ dali/test/python/test_jax_integration_multigpu.py
@@ -91,6 +91,26 @@ def test_dali_sequential_sharded_tensors_to_jax_sharded_array_manuall():
 
         # then
         # Assert that all values are as expected
+        # In first iteration, first shard should be:
+        # [[0 0 0 0 0]
+        #  [1 1 1 1 1]
+        #  [2 2 2 2 2]
+        #  [3 3 3 3 3]]
+        # And second shrad should be:
+        # [[4 4 4 4 4]
+        #  [5 5 5 5 5]
+        #  [6 6 6 6 6]
+        #  [7 7 7 7 7]]
+        # Then, in second iteration first shard should be:
+        # [[4 4 4 4 4]
+        #  [5 5 5 5 5]
+        #  [6 6 6 6 6]
+        #  [7 7 7 7 7]]
+        # And second shard should be:
+        # [[ 8  8  8  8  8]
+        #  [ 9  9  9  9  9]
+        #  [10 10 10 10 10]
+        #  [11 11 11 11 11]]
         assert jax.numpy.array_equal(
             jax_array.device_buffers[0],
             jax.numpy.stack([
