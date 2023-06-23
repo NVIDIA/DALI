06b7383ef44459b979f10d4205403386d0bc7d17
Albert Wolant <44801854+awolant@users.noreply.github.com>
2023-06-23 18:45:48 +0200

    Disable JAX iterator tests in ASAN build (#4920)
    * Disable JAX iterator tests in ASAN build
    
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL0_FW_iterators/test_jax.sh
+++ qa/TL0_FW_iterators/test_jax.sh
@@ -12,9 +12,13 @@ do_once() {
 test_body() {
     # General tests for iterators
     ${python_invoke_test} -m '(?:^|[\b_\./-])[Tt]est.*jax*' test_fw_iterators.py
-    
-    # More specific JAX tests
-    ${python_new_invoke_test} -s jax test_integration
+
+    # it takes very long time to run it with sanitizers on and provides little value so turn it off
+    if [ -z "$DALI_ENABLE_SANITIZERS" ]; then
+
+        # More specific JAX tests
+        ${python_new_invoke_test} -s jax test_integration
+    fi
 }
 
 pushd ../..
