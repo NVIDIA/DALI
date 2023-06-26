757a447b1acb65a679ca9509dc41765f0dc26d58
Janusz Lisiecki <jlisiecki@nvidia.com>
2023-06-26 21:53:20 +0200

    Extends guards against intercepting by asan certain functions
    - makes sure that __deregister_frame_info_bases doesn't use
      intercepted free/malloc from asan as they call unwind functions
      that try to obtain the same mutex as the __deregister_frame_info_bases
      holds leading to a deadlock
    - restores previously excluded JAX tests from asan runs
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_FW_iterators/test_jax.sh
+++ qa/TL0_FW_iterators/test_jax.sh
@@ -13,12 +13,8 @@ test_body() {
     # General tests for iterators
     ${python_invoke_test} -m '(?:^|[\b_\./-])[Tt]est.*jax*' test_fw_iterators.py
 
-    # it takes very long time to run it with sanitizers on and provides little value so turn it off
-    if [ -z "$DALI_ENABLE_SANITIZERS" ]; then
-
-        # More specific JAX tests
-        ${python_new_invoke_test} -s jax test_integration
-    fi
+    # More specific JAX tests
+    ${python_new_invoke_test} -s jax test_integration
 }
 
 pushd ../..
