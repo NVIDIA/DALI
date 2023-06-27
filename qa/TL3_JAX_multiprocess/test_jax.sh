18560875551db11098bec45347795a663aa29c0a
Albert Wolant <awolant@nvidia.com>
2023-06-27 16:42:56 +0200

    Remove leftovers
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL3_JAX_multiprocess/test_jax.sh
+++ qa/TL3_JAX_multiprocess/test_jax.sh
@@ -4,11 +4,6 @@ function CLEAN_AND_EXIT {
     exit $1
 }
 
-pushd /opt/dali/dali/test/python/
-
-# Multiprocess tests
-export NCCL_DEBUG=INFO
-
 python -c "import jax; print(jax.devices()); assert jax.device_count() > 0"
 
 CLEAN_AND_EXIT ${PIPESTATUS[0]}
