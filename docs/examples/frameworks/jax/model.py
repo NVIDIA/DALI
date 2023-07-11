9c3dcb6a6d92143a53e88122a1ecebf3c41813cf
Albert Wolant <awolant@nvidia.com>
2023-07-11 14:45:00 +0200

    Fix and refactor the tutorial
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- docs/examples/frameworks/jax/model.py
+++ docs/examples/frameworks/jax/model.py
@@ -15,38 +15,19 @@
 # This file is modified version of mnist_classifier_fromscratch.py from JAX codebase.
 # File in its unchanged form is included.
 
-import numpy.random as npr
 from jax import jit, grad
-from jax.scipy.special import logsumexp
 import jax.numpy as jnp
+from mnist_classifier_fromscratch import init_random_params as jax_init_random_params
+from mnist_classifier_fromscratch import loss, predict
+
 
 layer_sizes = [784, 1024, 1024, 10]
 param_scale = 0.1
 step_size = 0.001
 
 
-def init_random_params(scale=param_scale, layer_sizes=layer_sizes, rng=npr.RandomState(0)):
-    return [(scale * rng.randn(m, n), scale * rng.randn(n))
-            for m, n, in zip(layer_sizes[:-1], layer_sizes[1:])]
-
-
-def predict(params, inputs):
-    activations = inputs
-    for w, b in params[:-1]:
-        outputs = jnp.dot(activations, w) + b
-        activations = jnp.tanh(outputs)
-
-    final_w, final_b = params[-1]
-    logits = jnp.dot(activations, final_w) + final_b
-    return logits - logsumexp(logits, axis=1, keepdims=True)
-
-
-def loss(params, batch):
-    inputs = batch['images']
-    targets = batch['labels']
-
-    preds = predict(params, inputs)
-    return -jnp.mean(jnp.sum(preds * targets, axis=1))
+def init_random_params():
+    return jax_init_random_params(param_scale, layer_sizes)
 
 
 def accuracy(params, iterator):
@@ -64,6 +45,17 @@ def accuracy(params, iterator):
 
 @jit
 def update(params, batch, step_size=step_size):
-    grads = grad(loss)(params, batch)
-    return [(w - step_size * dw, b - step_size * db)
-            for (w, b), (dw, db) in zip(params, grads)]
+    grads = grad(loss)(params, (batch['images'], batch['labels']))
+    
+    updated_params = []
+    
+    for params, updates in zip(params, grads):
+        w, b = params
+        dw, db = updates
+        
+        new_w = w - step_size * dw
+        new_b = b - step_size * db
+        
+        updated_params.append((new_w, new_b))
+    
+    return updated_params
