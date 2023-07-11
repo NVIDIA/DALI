9364e0b5da0824d8a7981d7386ed120f37170276
Albert Wolant <awolant@nvidia.com>
2023-07-11 12:48:27 +0200

    Add original JAX example file
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- docs/examples/frameworks/jax/model.py
+++ docs/examples/frameworks/jax/model.py
@@ -1,21 +1,19 @@
-# Copyright 2018 The JAX Authors.
+# Copyright (c) 2019-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
 
-"""A basic MNIST example using Numpy and JAX.
 
-The primary aim here is simplicity and minimal dependencies.
-"""
+# This file is modified version of mnist_classifier_fromscratch.py from JAX codebase.
+# File in its unchanged form is included.
 
 import numpy.random as npr
 from jax import jit, grad
