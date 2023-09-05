30de38d0b30429602906868f8707683f6c7ca8d8
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2023-09-05 11:26:12 +0200

    Move to the upstream CV-CUDA 0.4 (#5032)
    - moves to the upstream version of the CV-CUDA v0.4
    - utilizes CV_CUDA_SRC_PATERN to build only relevant for DALI CV-CUDA ops
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- cmake/Dependencies.common.cmake
+++ cmake/Dependencies.common.cmake
@@ -263,6 +263,8 @@ endif()
 if (BUILD_CVCUDA)
   set(DALI_BUILD_PYTHON ${BUILD_PYTHON})
   set(BUILD_PYTHON OFF)
+  # for now we use only median blur from CV-CUDA
+  set(CV_CUDA_SRC_PATERN medianblur median_blur)
   check_and_add_cmake_submodule(${PROJECT_SOURCE_DIR}/third_party/cvcuda)
   list(APPEND DALI_LIBS cvcuda nvcv_types)
   set(BUILD_PYTHON ${DALI_BUILD_PYTHON})
