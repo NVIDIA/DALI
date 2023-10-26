9de0a97a71c02fa9627ca513ad5e7c589c150728
Krzysztof Lecki <klecki@nvidia.com>
2023-10-26 16:02:39 +0200

    Generalize `__module__` handling and hide private modules docs (#5112)
    After refactoring, the documentation for ops API started listing all the
    internal modules. Modules that start with `_` are no longer visible
    in Sphinx doc.
    
    Moving the custom operator classes to the dedicated submodules caused
    the docs to render the path to the implementation module and not the
    one we want to present in the API - due to the `__module__` change.
    
    As the module with the original implementation is still useful, a
    generalized way of adjusting the `__module__` was introduced, keeping
    the original information in the operator under `_impl_module`.
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/python/nvidia/dali/internal.py
+++ dali/python/nvidia/dali/internal.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2020-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import importlib
 import sys
 import types
@@ -42,3 +56,20 @@ Parameters
                 f"which is not a module, but {m}")
         root = m
     return root
+
+
+def _adjust_operator_module(operator, api_module, submodule, impl_overwrite=None):
+    """Adjust the __module__ of `operator` to point into the submodule of `api_module`
+    pointed by the list of in `submodule`, for example:
+        api_module = <nvidia.dali.ops module>
+        submodule = ["experimental", "readers"]
+
+    The original module where the operator code was generated is saved as `_impl_module` to allow
+    access to it.
+
+    If the operator has base class defined by hand, but it is generated with an automatic wrapper
+    generator, we can point to the original implementation module via `impl_overwrite`.
+    """
+    module = get_submodule(api_module, submodule)
+    operator._impl_module = operator.__module__ if impl_overwrite is None else impl_overwrite
+    operator.__module__ = module.__name__
--- dali/python/nvidia/dali/plugin/pytorch.py
+++ dali/python/nvidia/dali/plugin/pytorch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2017-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -712,6 +712,7 @@ class DALIRaggedIterator(_DaliBaseIterator):
 
 class TorchPythonFunction(ops.PythonFunctionBase):
     schema_name = "TorchPythonFunction"
+    _impl_module = "nvidia.dali.plugin.pytorch"
     ops.register_cpu_op('TorchPythonFunction')
     ops.register_gpu_op('TorchPythonFunction')
 
