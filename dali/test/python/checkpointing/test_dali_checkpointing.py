a70dd57a1c31e5265b7b1c5794278b0449cf70fa
Szymon Karpiński <skarpinski@nvidia.com>
2023-11-06 11:27:40 +0100

    Fix trivial testcases
    Signed-off-by: Szymon Karpiński <skarpinski@nvidia.com>

--- dali/test/python/checkpointing/test_dali_checkpointing.py
+++ dali/test/python/checkpointing/test_dali_checkpointing.py
@@ -308,13 +308,13 @@ class VideoConfig:
     (1, 3),
     (0, 2),
     (
-        BaseDecoderConfig(shard_id=0, num_shards=1, stick_to_shard=True, pad_last_batch=True, 
+        BaseDecoderConfig(shard_id=0, num_shards=1, stick_to_shard=True, pad_last_batch=True,
                           random_shuffle=True),
-        BaseDecoderConfig(shard_id=4, num_shards=7, stick_to_shard=True, pad_last_batch=True, 
+        BaseDecoderConfig(shard_id=4, num_shards=7, stick_to_shard=True, pad_last_batch=True,
                           random_shuffle=False),
-        BaseDecoderConfig(shard_id=6, num_shards=7, stick_to_shard=False, pad_last_batch=False, 
+        BaseDecoderConfig(shard_id=6, num_shards=7, stick_to_shard=False, pad_last_batch=False,
                           random_shuffle=False),
-        BaseDecoderConfig(shard_id=0, num_shards=2, stick_to_shard=False, pad_last_batch=False, 
+        BaseDecoderConfig(shard_id=0, num_shards=2, stick_to_shard=False, pad_last_batch=False,
                           random_shuffle=True),
     ),
     (
@@ -325,7 +325,8 @@ class VideoConfig:
 def test_video_reader(num_epochs, batch_size, iters_into_epoch,
                       config: BaseDecoderConfig, video: VideoConfig):
 
-    files = 2 * [os.path.join(get_dali_extra_path(), 'db/video/multiple_framerate/10/10fps.mp4')]
+    files = [os.path.join(get_dali_extra_path(), f'db/video/multiple_framerate/{f}/{f}fps.mp4')
+             for f in (10, 50)]
 
     @pipeline_def(batch_size=batch_size, device_id=0,
                 num_threads=4, enable_checkpointing=True)
@@ -348,7 +349,7 @@ def test_video_reader(num_epochs, batch_size, iters_into_epoch,
             shard_id=config.shard_id,
             stick_to_shard=config.stick_to_shard,
             pad_last_batch=config.pad_last_batch,
-            
+
             sequence_length=video.sequence_length,
             stride=video.stride,
             step=video.step)
@@ -358,7 +359,13 @@ def test_video_reader(num_epochs, batch_size, iters_into_epoch,
     p = pipeline()
     p.build()
 
+    assert p.reader_meta()['Reader']['epoch_size'] // config.num_shards > 2, \
+           "Trivial test case: at least 2 samples per shard required"
+
     iterations_in_epoch = calculate_iterations_in_epoch(p, batch_size, config.num_shards)
+
+    assert iterations_in_epoch >= iters_into_epoch, "Not enough iterations in epoch"
+
     for epoch in range(num_epochs):
         for i in range(iterations_in_epoch):
             p.run()
