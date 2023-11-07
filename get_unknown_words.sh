94e37d6d661296cc906cc461eba22a20d6b6ad2c
Krzysztof Lecki <klecki@nvidia.com>
2023-11-07 19:29:05 +0100

    Some additions
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- get_unknown_words.sh
+++ get_unknown_words.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v $PWD:/workdir ghcr.io/streetsidesoftware/cspell:latest --words-only --unique --quiet "**" | sort --ignore-case >> project-words.txt
+docker run -it -v $PWD:/workdir ghcr.io/streetsidesoftware/cspell:latest --words-only --unique --quiet --no-color "**" | sort --ignore-case > project-words.txt
