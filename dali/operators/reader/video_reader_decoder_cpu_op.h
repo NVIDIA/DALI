c7cbc8436a37a0466e0133b5b5d15c338255c866
Szymon Karpiński <skarpinski@nvidia.com>
2023-11-14 16:34:06 +0100

    Add checkpointing to experimental video reader
    Signed-off-by: Szymon Karpiński <skarpinski@nvidia.com>

--- dali/operators/reader/video_reader_decoder_cpu_op.h
+++ dali/operators/reader/video_reader_decoder_cpu_op.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2021, 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@
 #include "dali/operators/reader/loader/video/video_loader_decoder_cpu.h"
 
 namespace dali {
-class VideoReaderDecoderCpu : public DataReader<CPUBackend, VideoSample<CPUBackend>> {
+class VideoReaderDecoderCpu : public DataReader<CPUBackend, VideoSample<CPUBackend>, VideoSample<CPUBackend>, true> {
  public:
   explicit VideoReaderDecoderCpu(const OpSpec &spec);
 
