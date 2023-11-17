dfca5e96db44cecda517f67010e2779b3c4a7ea0
Krzysztof Lecki <klecki@nvidia.com>
2023-11-17 11:49:45 +0100

    Expose Tensors types stubs in nvidia.dali.tensors module (#5153)
    Base for the Python Tensor and TensorList types interface files,
    generated using stubgen (for `nvidia.dali.backend_impl` module)
    and inserted as `nvidia.dali.tensors`.
    
    As those types are generated in the backend, this is needed for
    tools like intellsense and mypy (working on static information)
    to work.
    
    C-based types (or types not guaranteed to be present, like np.array), 
    are removed from signatures and left as a comment or replaced with Any.
    
    Additionally, the `__module__` for Tensor types is adjusted to
    `nvidia.dali.tensors` on the backend level. It allows pybind to generate
    the documentation pointing to the desired module, rather than
    the `backend_impl` one. 
    We still have stubs which allow to find the Tensor types in the
    `backend` and `backend_impl` modules, keeping the backward
    compatibility.
    
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/python/backend_impl.cc
+++ dali/python/backend_impl.cc
@@ -19,6 +19,7 @@
 #include "dali/core/common.h"
 #include "dali/core/cuda_utils.h"
 #include "dali/core/device_guard.h"
+#include "pybind11/pytypes.h"
 #if SHM_WRAPPER_ENABLED
 #include "dali/core/os/shared_mem.h"
 #endif
@@ -62,6 +63,18 @@ void __backend_impl_force_tls_align_fun(void) {}
 
 
 using namespace pybind11::literals; // NOLINT
+
+/**
+ * @brief Override the default __module__ of Tensor classes from nvidia.dali.backend_impl
+ * with the user-friendly Python module.
+ * This definition must be provided as a first one for the Tensor classes, so all following
+ * definitions can look it up when pybind is generating the signatures, otherwise the annotations
+ * will contain the backend_impl module path.
+ */
+static std::string tensor_module_impl(const py::object object) {
+  (void)object;
+  return "nvidia.dali.tensors";
+}
 
 static void* ctypes_void_ptr(const py::object& object) {
   auto ptr_as_int = getattr(object, "value", py::none());
@@ -442,6 +455,7 @@ void ExposeTensor(py::module &m) {
       )code");
 
   auto tensor_cpu_binding = py::class_<Tensor<CPUBackend>>(m, "TensorCPU", py::buffer_protocol())
+    .def_property_readonly_static("__module__", tensor_module_impl)
     .def(py::init([](py::capsule &capsule, string layout = "") {
           auto t = std::make_unique<Tensor<CPUBackend>>();
           FillTensorFromDlPack(capsule, t.get(), layout);
@@ -636,6 +650,7 @@ void ExposeTensor(py::module &m) {
   py::implicitly_convertible<py::capsule&, Tensor<CPUBackend>>();
 
   auto tensor_gpu_binding = py::class_<Tensor<GPUBackend>>(m, "TensorGPU")
+    .def_property_readonly_static("__module__", tensor_module_impl)
     .def(py::init([](py::capsule &capsule, string layout = "") {
           auto t = std::make_unique<Tensor<GPUBackend>>();
           FillTensorFromDlPack(capsule, t.get(), layout);
@@ -915,6 +930,7 @@ void ExposeTensorList(py::module &m) {
   auto tensor_list_cpu_class =
       py::class_<TensorList<CPUBackend>, std::shared_ptr<TensorList<CPUBackend>>>(
           m, "TensorListCPU", py::buffer_protocol())
+    .def_property_readonly_static("__module__", tensor_module_impl)
     .def(py::init([](py::capsule &capsule, string layout = "") {
             auto t = std::make_shared<TensorList<CPUBackend>>();
             FillTensorFromDlPack(capsule, t.get(), layout);
@@ -1190,6 +1206,7 @@ void ExposeTensorList(py::module &m) {
   auto tensor_list_gpu_class =
       py::class_<TensorList<GPUBackend>, std::shared_ptr<TensorList<GPUBackend>>>(
           m, "TensorListGPU", py::buffer_protocol())
+    .def_property_readonly_static("__module__", tensor_module_impl)
     .def(py::init([](py::capsule &capsule, string layout = "") {
             auto t = std::make_shared<TensorList<GPUBackend>>();
             FillTensorFromDlPack(capsule, t.get(), layout);
