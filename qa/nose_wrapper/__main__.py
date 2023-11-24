c6409056ad4870cddfcef9fca4fb3d41f27d3abb
Janusz Lisiecki <jlisiecki@nvidia.com>
2023-11-24 09:13:48 +0100

    Fix Python3.11 tests
    - moves to a newer PaddlePaddle version that supports Python3.11
    - moves to a newer OpenCV version that supports Python3.11
    - fixes deprecated the usage of inspect.getargspec on nose
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/nose_wrapper/__main__.py
+++ qa/nose_wrapper/__main__.py
@@ -6,14 +6,23 @@ import nose.inspector
 import nose.loader
 import nose.suite
 import nose.plugins.attrib
+import inspect
+
 if sys.version_info >= (3, 10) and not hasattr(collections, "Callable"):
     nose.case.collections = collections.abc
     nose.inspector.collections = collections.abc
     nose.loader.collections = collections.abc
     nose.suite.collections = collections.abc
     nose.plugins.attrib.collections = collections.abc
+if sys.version_info >= (3, 11):
 
-if sys.argv[0].endswith('__main__.py'):
-    sys.argv[0] = '%s -m nose_wrapper' % sys.executable
+    def legacy_getargspec(fun):
+        args, varargs, varkw, defaults, _, _, _ = inspect.getfullargspec(fun)
+        return (args, varargs, varkw, defaults)
+
+    inspect.getargspec = legacy_getargspec
+
+if sys.argv[0].endswith("__main__.py"):
+    sys.argv[0] = "%s -m nose_wrapper" % sys.executable
 
 run_exit()
