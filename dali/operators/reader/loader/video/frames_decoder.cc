ccebeabb2d50253ababff552b2649a155cb1b419
Kamil Tokarski <ktokarski@nvidia.com>
2023-11-27 15:21:55 +0100

    Update FFmpeg to 6.1 (#5186)
    * Set DALI_DEPS_VERSION to updated FFmpeg (6.0 -> 6.1)
    * Replace deprecated key_frames memebr usage with a flag read
    * Stop using removed flags in FFmpeg recipe for conda
    * Adjust libs version in conde recipe
    ---------
    
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- dali/operators/reader/loader/video/frames_decoder.cc
+++ dali/operators/reader/loader/video/frames_decoder.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2021-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -353,7 +353,7 @@ void FramesDecoder::BuildIndex() {
   int last_keyframe = -1;
   while (ReadRegularFrame(nullptr, false)) {
     IndexEntry entry;
-    entry.is_keyframe = av_state_->frame_->key_frame;
+    entry.is_keyframe = av_state_->frame_->flags & AV_FRAME_FLAG_KEY;
     entry.pts = av_state_->frame_->pts;
     entry.is_flush_frame = false;
 
