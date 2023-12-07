2134a41fe072ab763bca210b042cc981b6c6a23c
Michal Zientkiewicz <michalz@nvidia.com>
2023-12-08 00:08:20 +0100

    Refactoring; add taskflow as a third party module.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/pipeline/executor/executor2/tf/exec_graph.h
+++ dali/pipeline/executor/executor2/tf/exec_graph.h
@@ -12,16 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef DALI_PIPELINE_EXECUTOR2_EXEC_GRAPH_H_
-#define DALI_PIPELINE_EXECUTOR2_EXEC_GRAPH_H_
+#ifndef DALI_PIPELINE_EXECUTOR2_TF_EXEC_GRAPH_H_
+#define DALI_PIPELINE_EXECUTOR2_TF_EXEC_GRAPH_H_
 
 #include <cassert>
 #include <memory>
 #include <variant>
 
-#include "graph.h"
+#include "../graph.h"
 #include "dali/pipeline/operator/operator.h"
 #include "dali/core/cuda_event_pool.h"
+
+#include "third_party/taskflow/taskflow/taskflow.hpp"  // TODO(michalz): Add it to cmake
 
 namespace dali {
 namespace exec2 {
@@ -59,66 +61,40 @@ class CUDAEventLease : public CUDAEvent {
   CUDAEventPool *owner_;
 };
 
-struct ExecDataNode {
-  template <typename Backend>
-  std::shared_ptr<TensorList<Backend>> get(AccessOrder order) {
-    return get(order, Backend());
-  }
+class ExecNode;
 
-  std::shared_ptr<TensorList<CPUBackend>> get(AccessOrder order, CPUBackend) {
-    if (cpu_ready)
-        order.wait(cpu_ready);
-    return cpu_data;
-  }
-
-  std::shared_ptr<TensorList<GPUBackend>> get(AccessOrder order, GPUBackend) {
-    if (gpu_ready)
-        order.wait(gpu_ready);
-    return gpu_data;
-  }
-
-  struct OutputQueueEntry {
-    std::shared_ptr<TensorList<CPUBackend>> cpu;
-    std::shared_ptr<TensorList<GPUBackend>> gpu;
-
-    CUDAEventLease ready;
-  };
-
-  std::mutex mtx;
-  std::condition_variable read, write;
-
-  template <typename Backend>
-  void push(std::shared_ptr<TensorList<Backend>> tl, AccessOrder writeOrder)
-  {
-    static_assert(std::is_same_v<Backend, CPUBackend> || std::is_same_v<Backend, GPUBackend>);
-
-    std::lock_guard g(mtx);
-    if constexpr (std::is_same_v<Backend, GPUBackend>)
-      queue.emplace(CUDAEventPool::instance().Get());
-
-  }
-
-  int max_queue_depth = 1;
-
-  std::queue<OutputQueueEntry> queue;
-
+struct ExecEdge {
   ExecNode *producer = nullptr;
+  ExecNode *consumer = nullptr;
+
+  int producer_output_idx = 0;
+  int consumer_input_idx = 0;
 };
 
-struct ExecNode {
-  OperatorNode *op_node = nullptr;
+class ExecNode {
+ public:
+  OperatorNode *op_node_ = nullptr;
+
+  std::vector<ExecEdge *> inputs_, outputs_;
+
   OperatorBase *op_instance = nullptr;
-  std::vector<ExecDataNode *> inputs, outputs;
+
+  Workspace &current_ws() { return ws_[ws_idx_]; }
+  void switch_ws() { ws_idx_ = 1 - ws_idx_; }
+
+ private:
+  Workspace ws_[2];
+  int ws_idx_ = 0;
 
 
 };
 
 struct ExecGraph {
-  std::vector<std::unique_ptr<ExecDataNode>> data_nodes;
-  std::vector<std::unique_ptr<ExecNode>> op_nodes;
+  std::list<ExecEdge> edges;
+  std::list<ExecNode> nodes;
 };
 
 }  // namespace exec2
 }  // namespace dali
 
-#endif  // DALI_PIPELINE_EXECUTOR2_EXEC_GRAPH_H_
+#endif  // DALI_PIPELINE_EXECUTOR2_EXEC_TF_GRAPH_H_
