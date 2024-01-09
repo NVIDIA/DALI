d747041abf88396da6944abdeab5dab3b110c7dd
Michał Zientkiewicz <michalz@nvidia.com>
2024-01-09 15:58:55 +0100

    Fix missing Shutdown method warning in debug pipeline. (#5267)
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/python/backend_impl.cc
+++ dali/python/backend_impl.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2017-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -1604,7 +1604,8 @@ void ExposePipelineDebug(py::module &m) {
       .def("AddMultipleOperators", &PipelineDebug::AddMultipleOperators)
       .def("RunOperatorCPU", &PipelineDebug::RunOperator<CPUBackend>)
       .def("RunOperatorGPU", &PipelineDebug::RunOperator<GPUBackend>)
-      .def("RunOperatorMixed", &PipelineDebug::RunOperator<MixedBackend>);
+      .def("RunOperatorMixed", &PipelineDebug::RunOperator<MixedBackend>)
+      .def("Shutdown", [](PipelineDebug *){});
 }
 
 template <typename Backend>
