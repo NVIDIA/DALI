7b2526ba64a6247c768ffa1be9d774a5c570afc8
Michal Zientkiewicz <michalz@nvidia.com>
2024-01-09 16:53:50 +0100

    [WIP]
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/pipeline/executor/executor2/tf/tf_exec.cc
+++ dali/pipeline/executor/executor2/tf/tf_exec.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "executor2.h"
+#include "tf_exec.h"
 
 namespace dali {
 namespace exec2 {
