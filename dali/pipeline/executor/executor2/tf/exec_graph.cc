40cf6238c6548ccc84b257ff0dc12738fb5254a3
Michal Zientkiewicz <michalz@nvidia.com>
2024-02-06 11:18:33 +0100

    [WIP]
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

---
+++ dali/pipeline/executor/executor2/tf/exec_graph.cc
@@ -0,0 +1,156 @@
+// Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "sched_graph.cc"
+
+namespace dali {
+namespace exec2 {
+
+
+void ExecNode::schedule(std::shared_ptr<ExecGraph> eg, tf::Taskflow &flow) {
+  main_task = flow.emplace([this]() {
+    runs++;
+    for (auto &out : ws.out) {
+      if (!out)
+        out = std::make_shared<Buffer>("unnamed");
+      assert(out->readers == 0);
+      out->writers++;
+    }
+
+    for (auto &e : inputs) {
+      assert(!e.producer || ws.in[e.consumer_input_idx]);
+    }
+
+    for (auto &in : ws.in) {
+      if (in)
+        assert(in->writers == 0);
+    }
+
+    // Run the operation
+    definition->task_fn(ws);
+    // Reset the inputs once we're done
+    for (auto &in : ws.in) {
+      if (in) {
+        in->readers--;
+        in.reset();
+      }
+    }
+    for (auto &out : ws.out) {
+      if (out)
+        out->writers--;
+    }
+
+    for (int i = 0, nout = outputs.size(); i < nout; i++) {
+      const ExecEdge &out_edge = outputs[i];
+      if (out_edge.consumer) {
+        Workspace &consumer_ws = out_edge.consumer->ws;
+        auto &buf = ws.out[out_edge.producer_output_idx];
+        if (buf)
+          buf->readers++;
+        consumer_ws.in[out_edge.consumer_input_idx] = buf;
+      }
+    }
+    for (auto &out : ws.out)
+      out.reset();
+  });
+
+  for (auto &in : inputs) {
+    if (!in.producer)  // external input?
+      continue;
+    assert(!in.producer->main_task.empty());
+    // Obviously, this task succeeds any producer task
+    main_task.succeed(in.producer->main_task);
+
+    // If the preceding task has a "release_outputs" task, our current task needs to precede it -
+    // that is, This way the producer will not have its output queue semaphore lowered until all
+    // consumers are done.
+    if (!in.producer->release_output.empty())
+      main_task.precede(in.producer->release_output);
+  }
+  main_task.acquire(definition->concurrency).release(definition->concurrency);
+  auto &output_queue = definition->output_queue;
+  if (output_queue.has_value()) {
+    main_task.acquire(*output_queue);
+    release_output = flow.emplace([]() {}).release(*output_queue);
+  }
+}
+
+
+void assert_valid(ExecGraph &eg) {
+  for (auto &exec_node : eg.nodes) {
+    for (int i = 0; i < exec_node.outputs.size(); i++) {
+      auto *e = exec_node.outputs[i];
+      assert(e->producer == &exec_node);
+      assert(e->consumer->inputs[e->consumer_input_idx] == e);
+    }
+    for (int i = 0; i < exec_node.inputs.size(); i++) {
+      auto *e = exec_node.inputs[i];
+      assert(e->consumer == &exec_node);
+      if (e->producer) {
+        bool found = false;
+        for (auto &out_e : e->producer->outputs) {
+          if (out_e == e) {
+            found = true;
+            break;
+          }
+        }
+        assert(found);
+      }
+    }
+  }
+}
+
+void assert_valid(SchedGraph &eg) {
+  for (auto &sched_node : eg.nodes) {
+    assert(sched_node.definition);
+    auto &exec_node = *sched_node.definition;
+
+    auto validate_edges = [](auto &sched_edges, auto &exec_edges) {
+      assert(sched_edges.size() == exec_edges.size());
+      for (int i = 0; i < sched_edges.size(); i++) {
+        assert(sched_edges[i].producer->definition == exec_edges[i]->producer);
+        assert(sched_edges[i].consumer->definition == exec_edges[i]->consumer);
+        assert(sched_edges[i].producer_output_idx == exec_edges[i]->producer_output_idx);
+        assert(sched_edges[i].consumer_input_idx == exec_edges[i]->consumer_input_idx);
+      }
+    };
+
+    validate_edges(sched_node.inputs, exec_node.inputs);
+    validate_edges(sched_node.outputs, exec_node.outputs);
+
+    for (int i = 0; i < sched_node.outputs.size(); i++) {
+      auto &e = sched_node.outputs[i];
+      assert(e.producer == &sched_node);
+      assert(e.consumer->inputs[e.consumer_input_idx] == e);
+    }
+    for (int i = 0; i < sched_node.inputs.size(); i++) {
+      auto &e = sched_node.inputs[i];
+      assert(e.consumer == &sched_node);
+      if (e.producer) {
+        bool found = false;
+        for (auto &out_e : e.producer->outputs) {
+          if (out_e == e) {
+            found = true;
+            break;
+          }
+        }
+        assert(found);
+      }
+    }
+  }
+}
+
+
+}  // namespace exec2
+}  // namespace dali
