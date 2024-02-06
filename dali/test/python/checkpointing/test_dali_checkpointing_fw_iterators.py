7f1302f87dcec42948f366c09f01c33ffebfff5e
Szymon Karpiński <skarpinski@nvidia.com>
2024-02-06 09:01:05 +0100

    Add assert(len...)
    Signed-off-by: Szymon Karpiński <skarpinski@nvidia.com>

--- dali/test/python/checkpointing/test_dali_checkpointing_fw_iterators.py
+++ dali/test/python/checkpointing/test_dali_checkpointing_fw_iterators.py
@@ -265,6 +265,8 @@ class FwTestBase:
             a = observe(it, steps)
             b = observe(it_restored, steps)
 
+            assert len(a) == len(b)
+
             for x, y in zip(a, b):
                 if x is None or y is None:
                     assert x is None and y is None
