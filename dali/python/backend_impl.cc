716feadbfe3babf78f0cf510fe80ea8eac80bdb6
szalpal <mszolucha@nvidia.com>
2024-02-08 17:10:54 +0100

    Interpolation models documentation upgrade
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- dali/python/backend_impl.cc
+++ dali/python/backend_impl.cc
@@ -1794,8 +1794,14 @@ PYBIND11_MODULE(backend_impl, m) {
   // DALIInterpType
   py::enum_<DALIInterpType>(types_m, "DALIInterpType", "Interpolation mode\n<SPHINX_IGNORE>")
     .value("INTERP_NN", DALI_INTERP_NN)
-    .value("INTERP_LINEAR", DALI_INTERP_LINEAR)
-    .value("INTERP_CUBIC", DALI_INTERP_CUBIC)
+    .value("INTERP_LINEAR", DALI_INTERP_LINEAR,
+           "Linear interpolation in DALI is separable with regards to dimensions. "
+           "Therefore, `DALI_INTERP_LINEAR` mode captures all: Linear, Bi-linear, "
+           "Tri-linear etc. interpolation modes.")
+    .value("INTERP_CUBIC", DALI_INTERP_CUBIC,
+           "Cubic interpolation in DALI is separable with regards to dimensions. "
+           "Therefore, `DALI_INTERP_CUBIC` mode captures all: Cubic, Bi-cubic, "
+           "Tri-cubic etc. interpolation modes.")
     .value("INTERP_LANCZOS3", DALI_INTERP_LANCZOS3)
     .value("INTERP_TRIANGULAR", DALI_INTERP_TRIANGULAR)
     .value("INTERP_GAUSSIAN", DALI_INTERP_GAUSSIAN)
