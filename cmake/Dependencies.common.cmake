2151ad8eafe72728914f01a689224c3b44992fd8
Joaquin Anton <janton@nvidia.com>
2024-02-16 09:52:08 +0100

    Set rpath at cmake level (do not wait for bundle-wheel) (#5323)
    Improvement: Set rpath during build and not only ater during bundle-wheel. This allows for doing pip install directly from build directory without the need to build a wheel, and still be able to detect nvimgcodec installed in the python directory.
    
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- cmake/Dependencies.common.cmake
+++ cmake/Dependencies.common.cmake
@@ -279,6 +279,7 @@ if(BUILD_NVIMAGECODEC)
     nvimgcodec_headers
     URL      https://developer.download.nvidia.com/compute/nvimgcodec/redist/nvimgcodec/linux-x86_64/nvimgcodec-linux-x86_64-0.2.0.6-archive.tar.xz
     URL_HASH SHA512=6577a8ff5589400cdf5767aa4a507245527a96e48065f23f626dfc6ba13b136960cacfe7f61c345dc158ed0352e1e971834aec6fd98038649b08b250c3306aeb
+    DOWNLOAD_EXTRACT_TIMESTAMP 1
   )
   FetchContent_Populate(nvimgcodec_headers)
   set(nvimgcodec_SEARCH_PATH "${nvimgcodec_headers_SOURCE_DIR}/${CUDA_VERSION_MAJOR}/include")
