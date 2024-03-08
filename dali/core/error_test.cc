5e5931d64fa4bb0af7b607e331c7d272c0e5d9d3
Krzysztof Lecki <klecki@nvidia.com>
2024-03-08 15:21:46 +0100

    Adjust file and line test
    Signed-off-by: Krzysztof Lecki <klecki@nvidia.com>

--- dali/core/error_test.cc
+++ dali/core/error_test.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2018-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ TEST(Error, EnforceFailed) {
     FAIL() << "Exception was expeceted";
   } catch (DALIException &e) {
     std::string msg = e.what();
-    EXPECT_NE(msg.find(file_and_line), std::string::npos)
+    EXPECT_NE(e.GetFileAndLine().find(file_and_line), std::string::npos)
       << "File/line spec not found in error `what()`, which is:\n" << msg;
     EXPECT_NE(msg.find(message), std::string::npos)
       << "Original message not found in error `what()`, which is:\n" << msg;
