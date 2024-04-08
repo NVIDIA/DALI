c753dd7f3a2919e578c4834e5e4c91644edac193
Kamil Tokarski <ktokarski@nvidia.com>
2024-04-08 14:38:48 +0200

    Update flags for conda ffmpeg build
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- conda/third_party/dali_ffmpeg/recipe/build.sh
+++ conda/third_party/dali_ffmpeg/recipe/build.sh
@@ -47,7 +47,8 @@ unset SUBDIR
     --disable-filters \
     --disable-bsfs \
     --disable-decoder=ipu \
-    --enable-bsf=h264_mp4toannexb,hevc_mp4toannexb,mpeg4_unpack_bframes
+    --enable-bsf=h264_mp4toannexb,hevc_mp4toannexb,mpeg4_unpack_bframes \
+    --disable-lzma
 # adds | sed 's/\(.*{\)/DALI_\1/' | to the version file generation command - it prepends "DALI_" to the symbol version
 sed -i 's/\$\$(M)sed '\''s\/MAJOR\/\$(lib$(NAME)_VERSION_MAJOR)\/'\'' \$\$< | \$(VERSION_SCRIPT_POSTPROCESS_CMD) > \$\$\@/\$\$(M)sed '\''s\/MAJOR\/\$(lib$(NAME)_VERSION_MAJOR)\/'\'' \$\$< | sed '\''s\/\\(\.*{\\)\/DALI_\\1\/'\'' | \$(VERSION_SCRIPT_POSTPROCESS_CMD) > \$\$\@/' ffbuild/library.mak
 make -j"$(nproc --all)"
