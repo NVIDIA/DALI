952c7fcd96fb00918b5705d5afa63d28c1df562a
Kamil Tokarski <ktokarski@nvidia.com>
2024-04-15 19:08:58 +0200

    Add license note
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- docs/examples/custom_operations/jax_operator_multi_gpu_process_1.py
+++ docs/examples/custom_operations/jax_operator_multi_gpu_process_1.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This is counterpart of the jax_operator_multi_gpu notebook,
 # the notebook and this script run as a group,
 # the notebook runs as process 0, this code runs as process 1
