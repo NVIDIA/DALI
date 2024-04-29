cc9950af0ad895dcbe70944736c7dde734cd09e8
Michal Zientkiewicz <michalz@nvidia.com>
2024-04-29 17:32:51 +0200

    Make shared IterationData a first class member of Workspace. Make operator traces thread-safe.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

---
+++ dali/pipeline/workspace/iteration_data.h
@@ -0,0 +1,82 @@
+// Copyright (c) 2023-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DALI_PIPELINE_WORKSPACE_ITERATION_DATA_H_
+#define DALI_PIPELINE_WORKSPACE_ITERATION_DATA_H_
+
+#include <memory>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+namespace dali {
+
+/**
+ * Operator Traces is a mechanism, where an Operator can provide any arbitrary information to
+ * the end user. Under `trace_name` key, the Operator assigns `trace_value` as the information
+ * mentioned above. Using the provided API, user will be able to retrieve this information after
+ * the iteration ends - at the same time when he's able to access outputs from the pipeline.
+ *
+ * @see daliGetOperatorTrace, daliGetNumOperatorTraces
+ *
+ * Here are few examples of these traces, but essentially sky is the limit:
+ *   - "execution_time" -> "432 sec"
+ *   - "number_of_unprocessed_samples" -> "100"
+ *   - "next_batch_ready" -> "true"
+ */
+using operator_trace_map_t = std::unordered_map<
+  std::string /* trace_name */, std::string /* trace_value */>;
+
+class OperatorTraces {
+ public:
+  /** Gets operator traces for a single operator.
+   *
+   * This function is thread safe, however, the map returned by it is not.
+   * The operator must ensure that it does not attempt to modify the map from multiple threads.
+   */
+  operator_trace_map_t &Get(const std::string &operator_name) {
+    std::lock_guard g(mtx_);
+    return map_[operator_name];
+  }
+
+  /** Obtains a copy of trace maps for all operators.
+   *
+   * This function is thread safe.
+   */
+  auto GetCopy() const {
+    std::lock_guard g(mtx_);
+    return map_;
+  }
+
+ private:
+  mutable std::mutex mtx_;
+  std::unordered_map<
+        std::string /* op_name */,
+        operator_trace_map_t /* per-operator traces */
+  > map_;
+};
+
+/**
+ * Contains the data of an iteration. This data is shared across all Workspaces that belong to
+ * a single iteration.
+ */
+struct IterationData {
+  int64_t iteration_index = 0;
+  OperatorTraces operator_traces;
+};
+
+}  // namespace dali
+
+#endif  // DALI_PIPELINE_WORKSPACE_ITERATION_DATA_H_
+
