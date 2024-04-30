cd8375b85a6d1092b17633a9b0befb3a1023836e
Joaquin Anton <janton@nvidia.com>
2024-04-30 11:58:24 +0200

    Add pip dependencies to video_plugin test
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/TL0_video_plugin/test.sh
+++ qa/TL0_video_plugin/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages='${python_test_runner_package}'
+pip_packages='${python_test_runner_package} scikit-build ninja cmake'
 target_dir=./dali/test/python
 
 # reduce the lenght of the sanitizers tests as much as possible
@@ -22,7 +22,7 @@ test_body() {
     ${python_invoke_test} test_dali_video_plugin.py:TestDaliVideoPluginLoadFail
 
     # Installing the video plugin
-    pip install ../../../nvidia-dali-video*.tar.gz
+    pip install -v ../../../nvidia-dali-video*.tar.gz
 
     ${python_invoke_test} test_dali_video_plugin.py:TestDaliVideoPluginLoadOk
 }
