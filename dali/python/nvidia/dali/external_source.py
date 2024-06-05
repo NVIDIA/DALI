1953479bdda654428916b54b6182de19497a6f98
Janusz Lisiecki <jlisiecki@nvidia.com>
2024-06-05 13:47:57 +0200

    Add missing `blocking` argument documentation to the external source operator
    - as the external source operator documentation is written
      in Python, it missed the `blocking` argument description
      which was defined in the native schema but not copied to
      the python description
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/python/nvidia/dali/external_source.py
+++ dali/python/nvidia/dali/external_source.py
@@ -460,6 +460,11 @@ class ExternalSource:
         .. note::
             This is applicable only when copying data to and from GPU memory.
 
+    `blocking`: bool, optional
+        If True, this operator will block until the data is available
+        (e.g. by calling ``feed_input``).  If False, the operator will raise an error,
+        if the data is not available.
+
     `no_copy` : bool, optional
         Determines whether DALI should copy the buffer when feed_input is called.
 
