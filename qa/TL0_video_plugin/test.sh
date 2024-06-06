af7c207e1067c4ce7c83b11c6002f2594c31f1b4
Albert Wolant <awolant@nvidia.com>
2024-06-06 14:30:11 +0200

    Fix tests
    Signed-off-by: Albert Wolant <awolant@nvidia.com>

--- qa/TL0_video_plugin/test.sh
+++ qa/TL0_video_plugin/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages='${python_test_runner_package} scikit-build ninja cmake'
+pip_packages='${python_test_runner_package} scikit-build ninja cmake opencv-python'
 target_dir=./dali/test/python
 
 # reduce the lenght of the sanitizers tests as much as possible
