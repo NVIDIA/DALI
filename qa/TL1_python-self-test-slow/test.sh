c712c8ef6ac6ae0bab03feb98a3218b52057ae54
Joaquin Anton <janton@nvidia.com>
2024-06-07 13:03:11 +0200

    Unpin librosa as it currently causes an issue about: cannot allocate memory in static TLS block
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- qa/TL1_python-self-test-slow/test.sh
+++ qa/TL1_python-self-test-slow/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa==0.8.1 scipy nvidia-ml-py==11.450.51 numba lz4'
+pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa scipy nvidia-ml-py==11.450.51 numba lz4'
 target_dir=./dali/test/python
 
 test_body() {
--- qa/TL1_python-self-test_conda/test_nofw.sh
+++ qa/TL1_python-self-test_conda/test_nofw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa==0.8.1 scipy nvidia-ml-py==11.450.51 numba lz4 psutil dill cloudpickle astropy'
+pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa scipy nvidia-ml-py==11.450.51 numba lz4 psutil dill cloudpickle astropy'
 target_dir=./dali/test/python
 
 # test_body definition is in separate file so it can be used without setup
