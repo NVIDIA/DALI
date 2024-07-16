771bb5b099728fa971fc914d54a1e2b435da0945
Joaquin Anton <janton@nvidia.com>
2024-07-16 18:26:32 +0200

    Use nvimagecodec in sequence parser
    Signed-off-by: Joaquin Anton <janton@nvidia.com>

--- dali/operators/reader/parser/sequence_parser.cc
+++ dali/operators/reader/parser/sequence_parser.cc
@@ -14,83 +14,90 @@
 
 #include <vector>
 #include <memory>
-#include <utility>
-#include <opencv2/imgcodecs.hpp>
 #include "dali/operators/reader/parser/sequence_parser.h"
-#include "dali/util/ocv.h"
 
 namespace dali {
 
-std::pair<cv::Mat, TensorShape<>>
-DecodeOpenCV(DALIImageType image_type, const uint8_t *encoded_buffer, size_t length) {
-  int flags = 0;
-  if (image_type == DALI_ANY_DATA) {
-    // Note: IMREAD_UNCHANGED always ignores orientation
-    flags |= cv::IMREAD_UNCHANGED;
-  } else if (image_type == DALI_GRAY) {
-    flags |= cv::IMREAD_GRAYSCALE | cv::IMREAD_IGNORE_ORIENTATION;
-  } else {
-    flags |= cv::IMREAD_COLOR | cv::IMREAD_IGNORE_ORIENTATION;
-  }
-
-  // Decode image to tmp cv::Mat
-  cv::Mat decoded_image = cv::imdecode(
-    cv::Mat(1, length, CV_8UC1, const_cast<unsigned char*>(encoded_buffer)), flags);
-
-  int W = decoded_image.cols;
-  int H = decoded_image.rows;
-  int C = decoded_image.channels();
-  DALI_ENFORCE(decoded_image.data != nullptr, "Unsupported image type.");
-  TensorShape<> decoded_shape{H, W, C};
-
-  if (image_type == DALI_ANY_DATA && C == 4) {
-    // Special case for ANY_DATA and 4 channels -> Convert to RGBA
-    // Note: ANY_DATA with 1 or 3 channels is already forced to DALI_GRAY and DALI_RGB respectively.
-    cv::cvtColor(decoded_image, decoded_image, cv::COLOR_BGRA2RGBA);
-  } else if (image_type == DALI_RGB || image_type == DALI_YCbCr) {
-    // if different image type needed (e.g. RGB), permute from BGR
-    OpenCvColorConversion(DALI_BGR, decoded_image, image_type, decoded_image);
-  }
-  return {decoded_image, decoded_shape};
-}
-
-
 void SequenceParser::Parse(const TensorSequence& data, SampleWorkspace* ws) {
   Index seq_length = data.tensors.size();
-  auto file_name = data.tensors[0].GetSourceInfo();
-  cv::Mat decoded_img;
-  TensorShape<> decoded_shape;
-  try {
-    std::tie(decoded_img, decoded_shape) =
-        DecodeOpenCV(image_type_, const_cast<unsigned char *>(data.tensors[0].data<uint8_t>()),
-                     data.tensors[0].size());
-  } catch (std::exception &e) {
-    DALI_FAIL(e.what(), ". File: ", file_name);
-  }
-  TensorShape<> seq_shape{seq_length, decoded_shape[0], decoded_shape[1], decoded_shape[2]};
-  const auto frame_size = volume(decoded_shape);
+  TensorShape<4> seq_shape;
+  seq_shape[0] = seq_length;
+  int64_t& nchannels = seq_shape[3];
   auto& sequence = ws->Output<CPUBackend>(0);
-  sequence.SetLayout("FHWC");
-  sequence.Resize(seq_shape, DALI_UINT8);
-  auto view_0 = sequence.SubspaceTensor(0);
-  std::memcpy(view_0.raw_mutable_data(), decoded_img.data, frame_size * sizeof(uint8_t));
 
-  // Decode and copy rest of the frames
-  for (int64_t frame_idx = 1; frame_idx < seq_length; frame_idx++) {
-    auto view_i = sequence.SubspaceTensor(frame_idx);
-    auto file_name = data.tensors[frame_idx].GetSourceInfo();
-    try {
-      TensorShape<> decoded_shape_i;
-      std::tie(decoded_img, decoded_shape_i) = DecodeOpenCV(
-          image_type_, const_cast<unsigned char *>(data.tensors[frame_idx].data<uint8_t>()),
-          data.tensors[frame_idx].size());
-      DALI_ENFORCE(decoded_shape == decoded_shape_i,
-                   make_string("Frames should have same dimensions: ", decoded_shape,
-                               " != ", decoded_shape_i));
-    } catch (std::exception &e) {
-      DALI_FAIL(e.what(), ". File: ", file_name);
+  nvimgcodecDecodeParams_t decode_params = {NVIMGCODEC_STRUCTURE_TYPE_DECODE_PARAMS,
+                                            sizeof(nvimgcodecDecodeParams_t), nullptr};
+  decode_params.apply_exif_orientation = 1;
+  decode_params.enable_roi = 0;
+
+  for (int i = 0; i < static_cast<int>(data.tensors.size()); i++) {
+    nvimgcodecImageInfo_t info{NVIMGCODEC_STRUCTURE_TYPE_IMAGE_INFO, sizeof(nvimgcodecImageInfo_t),
+                               nullptr};
+
+    auto file_name = data.tensors[i].GetSourceInfo();
+    auto encoded_stream = imgcodec::NvImageCodecCodeStream::FromHostMem(
+        instance_, data.tensors[i].data<uint8_t>(), data.tensors[i].size());
+    CHECK_NVIMGCODEC(nvimgcodecCodeStreamGetImageInfo(encoded_stream, &info));
+
+    int64_t new_nchannels = image_type_ == DALI_GRAY                           ? 1 :
+                            image_type_ == DALI_RGB || image_type_ == DALI_BGR ? 3 :
+                            info.num_planes > 1                                ? info.num_planes :
+                                                  info.plane_info[0].num_channels;
+    if (i == 0) {
+      seq_shape[1] = info.plane_info[0].height;
+      seq_shape[2] = info.plane_info[0].width;
+      nchannels = new_nchannels;
+      sequence.SetLayout("FHWC");
+      sequence.Resize(seq_shape, DALI_UINT8);
+    } else {
+      DALI_ENFORCE(info.plane_info[0].height == seq_shape[1],
+                   "All frames expected to have same height");
+      DALI_ENFORCE(info.plane_info[0].width == seq_shape[2],
+                   "All frames expected to have same width");
+      DALI_ENFORCE(new_nchannels == nchannels,
+                   "All frames expected to have same number of channels");
     }
-    std::memcpy(view_i.raw_mutable_data(), decoded_img.data, frame_size * sizeof(uint8_t));
+
+    auto view_i = sequence.SubspaceTensor(i);
+    info.buffer_size = volume(view_i.shape());
+    info.buffer = view_i.raw_mutable_data();
+
+    // Decode to format
+    info.color_spec = NVIMGCODEC_COLORSPEC_SRGB;
+    if (image_type_ == DALI_ANY_DATA) {
+      info.sample_format = NVIMGCODEC_SAMPLEFORMAT_I_UNCHANGED;
+    } else if (image_type_ == DALI_GRAY) {
+      info.sample_format = NVIMGCODEC_SAMPLEFORMAT_P_Y;
+    } else if (image_type_ == DALI_RGB) {
+      info.sample_format = NVIMGCODEC_SAMPLEFORMAT_I_RGB;
+    } else if (image_type_ == DALI_BGR) {
+      info.sample_format = NVIMGCODEC_SAMPLEFORMAT_I_BGR;
+    } else {
+      DALI_FAIL(
+          "Format not supported. Only RGB, BGR, GRAY or ANY_DATA are supported on this operator");
+    }
+    info.cuda_stream = nullptr;
+    info.region.ndim = 0;
+    info.buffer_kind = NVIMGCODEC_IMAGE_BUFFER_KIND_STRIDED_HOST;
+    info.num_planes = 1;
+    info.plane_info[0].height = seq_shape[1];
+    info.plane_info[0].width = seq_shape[2];
+    info.plane_info[0].num_channels = seq_shape[3];
+    info.plane_info[0].row_stride = seq_shape[2] * seq_shape[3];
+    info.plane_info[0].sample_type = NVIMGCODEC_SAMPLE_DATA_TYPE_UINT8;
+    info.plane_info[0].precision = 0;
+    auto image = imgcodec::NvImageCodecImage::Create(instance_, &info);
+
+    nvimgcodecFuture_t decode_future;
+    auto enc_stream = encoded_stream.get();
+    auto img_handle = image.get();
+    nvimgcodecDecoderDecode(decoder_, &enc_stream, &img_handle, 1, &decode_params, &decode_future);
+
+    size_t status_size;
+    nvimgcodecProcessingStatus_t decode_status;
+    nvimgcodecFutureGetProcessingStatus(decode_future, &decode_status, &status_size);
+    nvimgcodecFutureDestroy(decode_future);
+    DALI_ENFORCE(decode_status == NVIMGCODEC_PROCESSING_STATUS_SUCCESS);
   }
 }
 
