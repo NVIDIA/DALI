b0a6a07351f080e395100d9ce05674298a63c218
Michal Zientkiewicz <michalz@nvidia.com>
2024-07-17 17:32:15 +0200

    Restrict use of pinned when there's no GPU.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/pipeline/data/buffer.cc
+++ dali/pipeline/data/buffer.cc
@@ -64,11 +64,11 @@ DLL_PUBLIC shared_ptr<uint8_t> AllocBuffer(size_t bytes, bool pinned,
 
 DLL_PUBLIC bool RestrictPinnedMemUsage() {
   static bool val = []() {
-    /* int n = 0;
-    if (cudaGetDeviceCount(&n) != CUDA_SUCCESS)
+    int n = 0;
+    if (cudaGetDeviceCount(&n) != cudaSuccess)
       return true;
     if (n == 0)
-      return true; */
+      return true;
     const char *env = getenv("DALI_RESTRICT_PINNED_MEM");
     return env && atoi(env);
   }();
