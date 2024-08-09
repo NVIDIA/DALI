db968c59679194c168ff7c6acc981b08792422b9
Janusz Lisiecki <jlisiecki@nvidia.com>
2024-08-09 12:56:46 +0200

    Review fixes
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_decoder_perf/test.sh
+++ qa/TL1_decoder_perf/test.sh
@@ -12,11 +12,11 @@ function CLEAN_AND_EXIT {
 test_body() {
   # SPEC:DA-11356-002_v04
   if [ "$(uname -p)" == "x86_64" ]; then
-    # Hooper
+    # Hopper
     MIN_PERF=19000;
     python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 10 -t 10000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
   else
-    # GraceHooper
+    # GraceHopper
     MIN_PERF=29000;
     python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 10 -t 10000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
   fi
