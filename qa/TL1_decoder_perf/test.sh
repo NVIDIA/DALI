0e54fe1adf9de45d260cefc177c729eb61e94b3c
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2024-10-04 11:58:12 +0200

    Increase number of the decoder bench iterations (#5655)
    - increases the number of decoder bench iterations to
      make the test more stable
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_decoder_perf/test.sh
+++ qa/TL1_decoder_perf/test.sh
@@ -14,11 +14,11 @@ test_body() {
   if [ "$(uname -p)" == "x86_64" ]; then
     # Hopper
     MIN_PERF=19000;
-    python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 10 -t 10000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
+    python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
   else
     # GraceHopper
     MIN_PERF=29000;
-    python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 10 -t 10000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
+    python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
   fi
   PERF=$(grep "fps" ${LOG} | awk '{print $1}')
 
