8b75ca297bbf6142e359b226f0b08ca7577a53f1
Janusz Lisiecki <jlisiecki@nvidia.com>
2024-10-10 15:14:12 +0200

    Another approach
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- dali/operators/reader/loader/video_loader.cc
+++ dali/operators/reader/loader/video_loader.cc
@@ -564,6 +564,8 @@ void VideoLoader::read_file() {
 
   bool is_first_frame = true;
   int last_key_frame = -1;
+  int previous_last_key_frame = -1;
+  bool previous_last_key_frame_updated = false;
   bool key = false;
   bool seek_must_succeed = false;
   // how many key frames following the last requested frames we saw so far
@@ -588,22 +590,6 @@ void VideoLoader::read_file() {
     auto frame = av_rescale_q(pkt->pts - file.start_time_,
                               file.stream_base_,
                               file.frame_base_);
-    if (seek_before_last_key) {
-      if (last_key_frame <= frame) {
-        LOG_LINE << "Failed to seek to one before the last keyframe: " << last_key_frame
-                 << " , arrived at "<< last_key_frame << ". Trying --last_key_frame\n";
-        --last_key_frame;
-        DALI_ENFORCE(frame != 0 && last_key_frame > 0,
-                     "Rewound to the frame 0 and the decoding hasn't started, "
-                     "please check if your video is not corrupted");
-        seek(file, last_key_frame - 1);
-        continue;
-      }
-      LOG_LINE << "Seek to one before the last keyframe: " << last_key_frame
-               << " , arrived at "<< frame << "\n";
-      seek_before_last_key = false;
-      last_key_frame = -1;
-    }
     LOG_LINE << "Frame candidate " << frame << " (for " << req.frame  <<" )...\n";
 
     file.last_frame_ = frame;
@@ -624,18 +610,30 @@ void VideoLoader::read_file() {
                     "already 0");
         }
 
+      if (previous_last_key_frame < 0) {
+        previous_last_key_frame = last_key_frame - 1;
+        previous_last_key_frame_updated = true;
+      }
+      if (previous_last_key_frame_updated == false) {
+        --previous_last_key_frame;
+      }
       LOG_LINE << "Decoding not started, seek to preceding key frame, "
                << "current frame " << frame
-               << ", last key frame " << last_key_frame
+               << ", look fir a key frame before " << previous_last_key_frame
                << ", is_key " << key << std::endl;
-      seek(file, last_key_frame - 1);
+      seek(file, previous_last_key_frame);
+      previous_last_key_frame_updated = false;
       frames_send = 0;
-      seek_before_last_key = true;
+      last_key_frame = -1;
       continue;
     }
 
     if (key) {
       last_key_frame = frame;
+      if (frame < previous_last_key_frame) {
+        previous_last_key_frame = frame;
+        previous_last_key_frame_updated = true;
+      }
     }
 
     int pkt_frames = 1;
