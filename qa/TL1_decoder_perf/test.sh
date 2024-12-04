19257585f7690a12002ac592211193aa4ca36f17
Joaquin Anton Guirao <janton@nvidia.com>
2024-12-04 09:38:51 +0100

    Add --experimental_decoder option
    Signed-off-by: Joaquin Anton Guirao <janton@nvidia.com>

--- qa/TL1_decoder_perf/test.sh
+++ qa/TL1_decoder_perf/test.sh
@@ -3,9 +3,11 @@
 pip_packages='numpy'
 target_dir=./internal_tools
 
-LOG="dali.log"
+LOG1="dali_legacy.log"
+LOG2="dali_nvimgcodec.log"
 function CLEAN_AND_EXIT {
-    rm -rf ${LOG}
+    rm -rf ${LOG1}
+    rm -rf ${LOG2}
     exit $1
 }
 
@@ -15,12 +17,15 @@ test_body() {
     # Hopper
     MIN_PERF=19000;
     # use taskset to avoid inefficient data migration between cores we don't want to use
-    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
+    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG1}
+    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 --experimental_decoder | tee ${LOG2}
+
   else
     # GraceHopper
     MIN_PERF=29000;
     # use taskset to avoid inefficient data migration between cores we don't want to use
-    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
+    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG1}
+    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 --experimental_decoder | tee ${LOG2}
   fi
 
   # Regex Explanation:
@@ -28,10 +33,15 @@ test_body() {
   # \K: Resets the start of the match, so anything before \K is not included in the output.
   # [0-9]+(\.[0-9]+)?: Matches the number, with an optional decimal part.
   # (?= frames/sec): ensures " frames/sec" follows the number, but doesn't include it.
-  PERF=$(grep -oP 'Total Throughput: \K[0-9]+(\.[0-9]+)?(?= frames/sec)' ${LOG})
+  PERF1=$(grep -oP 'Total Throughput: \K[0-9]+(\.[0-9]+)?(?= frames/sec)' ${LOG1})
+  PERF2=$(grep -oP 'Total Throughput: \K[0-9]+(\.[0-9]+)?(?= frames/sec)' ${LOG2})
 
-  PERF_RESULT=$(echo "$PERF $MIN_PERF" | awk '{if ($1>=$2) {print "OK"} else { print "FAIL" }}')
-  if [[ "$PERF_RESULT" == "OK" ]]; then
+  PERF_RESULT1=$(echo "$PERF1 $MIN_PERF" | awk '{if ($1>=$2) {print "OK"} else { print "FAIL" }}')
+  PERF_RESULT2=$(echo "$PERF2 $MIN_PERF" | awk '{if ($1>=$2) {print "OK"} else { print "FAIL" }}')
+  # Ensure that PERF2 is no less than 5% smaller than PERF1
+  PERF_RESULT3=$(echo "$PERF2 $PERF1" | awk '{if ($1 >= $2 * 0.95) {print "OK"} else { print "FAIL" }}')
+
+  if [[ "$PERF_RESULT1" == "OK" && "$PERF_RESULT2" == "OK" && "$PERF_RESULT3" == "OK" ]]; then
     CLEAN_AND_EXIT 0
   fi
 
