058f183d5229c7724e761ebbb72e704f1aed6049
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2024-12-13 11:14:28 +0100

    Add usage of taskset in the TL1_decoder_perf (#5738)
    - DALI uses more threads than just value in the `num_threads`
      argument. In effect, the actual number of CPU cores used is
      greater than intended. To ensure that we don't use more cores,
      and avoid inefficient data migration between ones affined
      with the GPU we test and a distant one a usage of task set
      is added to pin cores available for the test.
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL1_decoder_perf/test.sh
+++ qa/TL1_decoder_perf/test.sh
@@ -14,11 +14,13 @@ test_body() {
   if [ "$(uname -p)" == "x86_64" ]; then
     # Hopper
     MIN_PERF=19000;
-    python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
+    # use taskset to avoid inefficient data migration between cores we don't want to use
+    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
   else
     # GraceHopper
     MIN_PERF=29000;
-    python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
+    # use taskset to avoid inefficient data migration between cores we don't want to use
+    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
   fi
 
   # Regex Explanation:
