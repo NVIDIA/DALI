faf11a0c6e511b3927d2a19dd3ea26162ec0b7ee
Joaquin Anton <janton@nvidia.com>
2025-01-17 11:06:32 +0100

    Fix test_dali_proxy usage (#5784)
    Signed-off-by: Joaquin Anton Guirao <janton@nvidia.com>

--- qa/TL0_python_self_test_frameworks/test_pytorch.sh
+++ qa/TL0_python_self_test_frameworks/test_pytorch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 # used pip packages
-pip_packages='${python_test_runner_package} numpy librosa torch psutil'
+pip_packages='${python_test_runner_package} numpy librosa torch psutil torchvision'
 target_dir=./dali/test/python
 
 test_body() {
@@ -14,10 +14,10 @@ test_body() {
     ${python_invoke_test} test_backend_impl_torch_dlpack.py
     ${python_invoke_test} test_dali_fork_torch.py
     ${python_invoke_test} test_copy_to_external_torch.py
-    ${python_invoke_test} test_dali_proxy.py
     ${python_invoke_test} --attr 'pytorch' test_external_source_impl_utils.py
     ${python_invoke_test} --attr 'pytorch' test_pipeline_debug.py
     ${python_invoke_test} --attr 'pytorch' test_functional_api.py
+    ${python_new_invoke_test} -s . test_dali_proxy
 }
 
 pushd ../..
