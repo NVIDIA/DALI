230217f0cb6080c8de01187803c51c0e1682423d
Michał Zientkiewicz <michalz@nvidia.com>
2025-02-12 11:13:08 +0100

    Fix handling of empty batches in GPU arithmetic operators. (#5818)
    - Add an early exit to GPU arithmetic operators with an empty output.
    - Add error checking after kernel launches in arithmetic ops.
    
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/operators/math/expressions/expression_impl_gpu_unary.cuh
+++ dali/operators/math/expressions/expression_impl_gpu_unary.cuh
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2019-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -55,6 +55,7 @@ struct InvokerUnOp {
                      dim3 block, cudaStream_t stream, bool is_flat_idx) {
     assert(is_flat_idx);  // should always be for unary ops
     ExecuteTiledUnOp<op, Result, Input><<<grid, block, 0, stream>>>(samples, tiles);
+    CUDA_CALL(cudaGetLastError());
   }
 };
 
