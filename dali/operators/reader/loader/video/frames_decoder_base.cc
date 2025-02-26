e1c5b211ae7694962589258db8a126d6cda125eb
Joaquin Anton Guirao <janton@nvidia.com>
2025-02-26 14:35:13 +0100

    More code review fixes
    Signed-off-by: Joaquin Anton Guirao <janton@nvidia.com>

--- dali/operators/reader/loader/video/frames_decoder_base.cc
+++ dali/operators/reader/loader/video/frames_decoder_base.cc
@@ -79,7 +79,8 @@ int64_t seek_memory_video_file(void *data_ptr, int64_t new_position, int origin)
 int AvState::OpenFile(const std::string& filename) {
   LOG_LINE << "Opening file " << filename << std::endl;
   CloseInput();
-  DALI_ENFORCE((ctx_ = avformat_alloc_context()), "Could not alloc avformat context");
+  ctx_ = avformat_alloc_context();
+  DALI_ENFORCE(ctx_, "Could not alloc avformat context");
   int ret = avformat_open_input(&ctx_, filename.c_str(), nullptr, nullptr);
   if (ret < 0) {
     CloseInput();
@@ -90,7 +91,8 @@ int AvState::OpenFile(const std::string& filename) {
 int AvState::OpenMemoryFile(MemoryVideoFile &memory_video_file) {
   LOG_LINE << "Opening memory file" << std::endl;
   CloseInput();
-  DALI_ENFORCE((ctx_ = avformat_alloc_context()), "Could not alloc avformat context");
+  ctx_ = avformat_alloc_context();
+  DALI_ENFORCE(ctx_, "Could not alloc avformat context");
 
   static constexpr int DEFAULT_AV_BUFFER_SIZE = (1 << 15);
   uint8_t *avio_buffer = static_cast<uint8_t *>(av_malloc(DEFAULT_AV_BUFFER_SIZE));
@@ -667,13 +669,14 @@ void FramesDecoderBase::Reset() {
   int ret = -1;
   if (memory_video_file_) {
     memory_video_file_->Seek(0, SEEK_SET);
-    DALI_ENFORCE((ret = av_state_->OpenMemoryFile(*memory_video_file_)) >= 0,
+    ret = av_state_->OpenMemoryFile(*memory_video_file_);
+    DALI_ENFORCE(ret >= 0,
                  make_string("Could not open video file from memory buffer due to: ",
                              detail::av_error_string(ret)));
   } else {
-    DALI_ENFORCE(
-        (ret = av_state_->OpenFile(Filename())) >= 0,
-        make_string("Could not open video file \"", Filename(),
+    ret = av_state_->OpenFile(Filename());
+    DALI_ENFORCE(ret >= 0,
+                 make_string("Could not open video file \"", Filename(),
                     "\" due to: ", detail::av_error_string(ret)));
   }
 
