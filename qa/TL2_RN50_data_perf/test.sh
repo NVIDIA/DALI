08e0d8601d80c801fb116287e1476f16c464036b
Janusz Lisiecki <jlisiecki@nvidia.com>
2025-02-28 10:47:11 +0100

    More fixes
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL2_RN50_data_perf/test.sh
+++ qa/TL2_RN50_data_perf/test.sh
@@ -8,6 +8,17 @@ do_once() {
 }
 
 test_body() {
+    start=`date +%s`
+    (sleep 10 && pkill -HUP ls && true) &
+    (ls /data/imagenet/train-jpeg > /dev/null && pkill -HUP sleep) &
+    wait
+    end=`date +%s`
+    runtime=$((end-start))
+    echo "Data access time: $runtime seconds"
+    if [ $runtime -gt 3 ]; then
+        echo "Data access time is greater than 3 seconds, skipping the test"
+        return 0
+    fi
     # test code
     python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2 --decoder_type "legacy"
     python test_RN50_data_pipeline.py --gpus ${NUM_GPUS} -b 256 --workers 3 --prefetch 2 --decoder_type "experimental"
