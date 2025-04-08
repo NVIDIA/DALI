3cf03cefa9abf6c12965b9388aa3c78e76cadee3
Joaquin Anton Guirao <janton@nvidia.com>
2025-04-08 21:19:38 +0200

    Fixes
    Signed-off-by: Joaquin Anton Guirao <janton@nvidia.com>

--- qa/TL1_decoder_perf/test.sh
+++ qa/TL1_decoder_perf/test.sh
@@ -3,11 +3,9 @@
 pip_packages='numpy'
 target_dir=./internal_tools
 
-LOG1="dali_legacy.log"
-LOG2="dali_nvimgcodec.log"
+LOG="dali.log"
 function CLEAN_AND_EXIT {
-    rm -rf ${LOG1}
-    rm -rf ${LOG2}
+    rm -rf ${LOG}
     exit $1
 }
 
@@ -17,15 +15,13 @@ test_body() {
     # Hopper
     MIN_PERF=19000;
     # use taskset to avoid inefficient data migration between cores we don't want to use
-    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG1}
-    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 --experimental_decoder | tee ${LOG2}
+    taskset --cpu-list 0-127 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 70 --hw_load 0.12 | tee ${LOG}
 
   else
     # GraceHopper
     MIN_PERF=29000;
     # use taskset to avoid inefficient data migration between cores we don't want to use
-    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG1}
-    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 --experimental_decoder | tee ${LOG2}
+    taskset --cpu-list 0-71 python hw_decoder_bench.py --width_hint 6000 --height_hint 6000 -b 408 -d 0 -g gpu -w 100 -t 100000 -i ${DALI_EXTRA_PATH}/db/single/jpeg -p rn50 -j 72 --hw_load 0.11 | tee ${LOG}
   fi
 
   # Regex Explanation:
@@ -33,45 +29,14 @@ test_body() {
   # \K: Resets the start of the match, so anything before \K is not included in the output.
   # [0-9]+(\.[0-9]+)?: Matches the number, with an optional decimal part.
   # (?= frames/sec): ensures " frames/sec" follows the number, but doesn't include it.
-  PERF1=$(grep -oP 'Total Throughput: \K[0-9]+(\.[0-9]+)?(?= frames/sec)' ${LOG1})
-  PERF2=$(grep -oP 'Total Throughput: \K[0-9]+(\.[0-9]+)?(?= frames/sec)' ${LOG2})
+  PERF=$(grep -oP 'Total Throughput: \K[0-9]+(\.[0-9]+)?(?= frames/sec)' ${LOG})
+  PERF_RESULT=$(echo "$PERF $MIN_PERF" | awk '{if ($1>=$2) {print "OK"} else { print "FAIL" }}')
+  echo "PERF_RESULT=${PERF_RESULT}"
 
-  PERF_RESULT1=$(echo "$PERF1 $MIN_PERF" | awk '{if ($1>=$2) {print "OK"} else { print "FAIL" }}')
-  PERF_RESULT2=$(echo "$PERF2 $MIN_PERF" | awk '{if ($1>=$2) {print "OK"} else { print "FAIL" }}')
-  # Ensure that PERF2 is no less than 5% smaller than PERF1
-  PERF_RESULT3=$(echo "$PERF2 $PERF1" | awk '{if ($1 >= $2 * 0.95) {print "OK"} else { print "FAIL" }}')
-
-  echo "PERF_RESULT1=${PERF_RESULT1}"
-  echo "PERF_RESULT2=${PERF_RESULT2}"
-  echo "PERF_RESULT3=${PERF_RESULT3}"
-
-  # If nvImageCodec>=0.5.0 enforce the performance requirements. Otherwise, we check only the legacy decoder
-  if pip show nvidia-nvimgcodec-cu12 > /dev/null 2>&1; then
-      NVIMGCODEC_VERSION=$(pip show nvidia-nvimgcodec-cu12 | grep ^Version: | awk '{print $2}')
-  elif pip show nvidia-nvimgcodec-cu11 > /dev/null 2>&1; then
-      NVIMGCODEC_VERSION=$(pip show nvidia-nvimgcodec-cu11 | grep ^Version: | awk '{print $2}')
+  if [[ "$PERF_RESULT" == "OK" ]]; then
+    CLEAN_AND_EXIT 0
   else
-      echo "Neither nvidia-nvimgcodec-cu11 nor nvidia-nvimgcodec-cu12 is installed"
-      exit 1
-  fi
-  NVIMGCODEC_VERSION_WITHOUT_EXTRA=$(echo "$NVIMGCODEC_VERSION" | awk -F '.' '{print $1 "." $2 "." $3}')
-  if [[ "$NVIMGCODEC_VERSION_WITHOUT_EXTRA" =~ ^([0-9]+)\.([0-9]+)\.([0-9]+)$ ]]; then
-    IFS='.' read -r MAJOR MINOR PATCH <<< "$NVIMGCODEC_VERSION_WITHOUT_EXTRA"
-    if [[ $MAJOR -gt 0 || ($MAJOR -eq 0 && $MINOR -ge 5) ]]; then
-      if [[ "$PERF_RESULT1" == "OK" && "$PERF_RESULT2" == "OK" && "$PERF_RESULT3" == "OK" ]]; then
-        CLEAN_AND_EXIT 0
-      else
-        CLEAN_AND_EXIT 4
-      fi
-    else
-      if [[ "$PERF_RESULT1" == "OK" ]]; then
-        CLEAN_AND_EXIT 0
-      else
-        CLEAN_AND_EXIT 4
-      fi
-    fi
-  else
-    CLEAN_AND_EXIT 3
+    CLEAN_AND_EXIT 4
   fi
 }
 pushd ../..
