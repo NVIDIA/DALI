abd8cbe0082abbd7dc2ddb98fe0481540bf75dd4
Joaquin Anton Guirao <janton@nvidia.com>
2025-04-15 17:55:08 +0200

    Fix video reader tests
    Signed-off-by: Joaquin Anton Guirao <janton@nvidia.com>

--- dali/test/python/test_video_reader.py
+++ dali/test/python/test_video_reader.py
@@ -148,7 +148,7 @@ def compare_experimental_to_legacy_reader(device, batch_size, **kwargs):
 
 
 @cartesian_params(
-    ["cpu"],  # TODO(janton): use all devices
+    devices,
     batch_sizes,
     sequence_lengths,
     pad_modes_supported_by_legacy_reader,
@@ -158,10 +158,11 @@ def test_compare_experimental_to_legacy_reader_filenames(
     device, batch_size, sequence_length, pad_mode, image_type
 ):
     labels = [np.random.randint(0, 100) for _ in range(len(VIDEO_FILES))]
+    files = sorted(VIDEO_FILES)
     compare_experimental_to_legacy_reader(
         device=device,
         batch_size=batch_size,
-        filenames=VIDEO_FILES,
+        filenames=files,
         sequence_length=sequence_length,
         enable_timestamps=True,
         enable_frame_num=True,
