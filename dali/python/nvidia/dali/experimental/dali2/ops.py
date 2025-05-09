ed102eaf4e5ad6a9edff90d9a7db2403f810243c
Michal Zientkiewicz <michalz@nvidia.com>
2025-05-09 18:00:53 +0200

    [WIP]
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/python/nvidia/dali/experimental/dali2/ops.py
+++ dali/python/nvidia/dali/experimental/dali2/ops.py
@@ -12,11 +12,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from . import _device
+
 class Operator:
     def __init__(self, max_batch_size, name=None, **kwargs):
         self._name = name
         self._max_batch_size = max_batch_size
         self._init_args = kwargs
+        self._device = _device.Device(
+            name=kwargs.get("device", "cpu"),
+            device_id=kwargs.get("device_id", 0)
+        )
+        self._minipipe = None
+        self._input_meta = []
+        self._arg_meta = []
+
+    def run(self, *inputs, **args):
+        if self._minipipe is None or not self.is_compatible(inputs, args):
+            self._set_meta(inputs, args)
+            import nvidia.dali as dali
+            self._minipipe = dali.Pipeline(
+                 batch_size=self._max_batch_size,
+                 num_threads=1,
+                 device_id=None if self._device.device_type == "cpu" else self._device.device_id)
+            with self._minipipe:
+                op = self.legacy_op(max_batch_size=self._max_batch_size, name=self._name, **self._init_args)
+
+    def _set_meta(self, inputs, args):
+        self._input_meta = [self._make_meta(input) for input in inputs]
+        self._arg_meta = [self._make_meta(arg) for arg in args]
+
+    def is_compatible(self, inputs, args):
+        return self._input_meta == [input.meta for input in inputs] and self._arg_meta == [arg.meta for arg in args]
+
+    def _make_meta(self, x):
+        is_batch = False
+        if isinstance(x, _expression.Expression):
+            is_batch = x.is_batch
+        elif isinstance(x, TensorList):
+            is_batch = True
+        else:
+            is_batch = False
+
+
 
 
 def initialize():
