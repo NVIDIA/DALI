881e6b17173fcdec3f6a8e7a29eb52d6f4f4b52d
Janusz Lisiecki <jlisiecki@nvidia.com>
2025-05-13 12:25:37 +0200

    Disable conda tests when sanitizers are enabled
    - the conda tests are causing issues with sanitizers,
      so we skip them when DALI_ENABLE_SANITIZERS is set.
    - see  https://github.com/conda-forge/zstandard-feedstock/pull/61#issuecomment-2841916192
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/TL0_plugin_manager/test.sh
+++ qa/TL0_plugin_manager/test.sh
@@ -3,10 +3,13 @@
 pip_packages='${python_test_runner_package} numpy'
 target_dir=./dali/test/python
 
-# populate epilog and prolog with variants to enable/disable conda
-# every test will be executed for bellow configs
-prolog=(: enable_conda)
-epilog=(: disable_conda)
+# don't test conda with snitizers
+if [ -z "$DALI_ENABLE_SANITIZERS" ]; then
+  # populate epilog and prolog with variants to enable/disable conda
+  # every test will be executed for bellow configs
+  prolog=(: enable_conda)
+  epilog=(: disable_conda)
+fi
 
 test_body() {
     ${python_invoke_test} test_plugin_manager.py
