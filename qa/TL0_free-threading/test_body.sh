8cff79319ebf0f165610bc3eb66db4e18a6496e1
Michał Szołucha <mszolucha@nvidia.com>
2025-05-13 16:40:24 +0200

    Turn off -e in the place that needs it
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- qa/TL0_free-threading/test_body.sh
+++ qa/TL0_free-threading/test_body.sh
@@ -2,11 +2,13 @@
 
 # PYTHON_GIL can be set to 0 only if Python is compiled with --disable-gil.
 # Check if Python is compiled with --disable-gil.
+set +e
 python3 -c 'import sysconfig ; exit(sysconfig.get_config_var("Py_GIL_DISABLED"))'
 # Set PYTHON_GIL accordingly.
 if [ $? -ne 0  ]; then
         export PYTHON_GIL=0
 fi
+set -e
 
 test_py_with_framework() {
     ${python_new_invoke_test} -A '!slow' -s free-threading
