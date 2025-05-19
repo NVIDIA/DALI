f20223b3ac578d3d03b1ad0f2aa10aedf2681172
Janusz Lisiecki <jlisiecki@nvidia.com>
2025-05-19 19:19:05 +0200

    Add early test exit when DEADLYSIGNAL is detected in the log
    - in some cases, the sanitizer causes DEADLYSIGNAL and hangs the
      test with an enormous amount of logs. This PR adds early exit
      in such a situation.
    - https://github.com/google/sanitizers/issues/837
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>

--- qa/test_template.sh
+++ qa/test_template.sh
@@ -7,8 +7,11 @@ then
     # script. Turn this off to the gather_pip_packages process
     source qa/test_template_impl.sh
 else
+    # early exit if DEADLYSIGNAL is detected in the log
+    # https://github.com/google/sanitizers/issues/837
     source qa/test_template_impl.sh 2>&1 | perl -pe 'use POSIX strftime;
                     $|=1;
                     select((select(STDERR), $| = 1)[0]);
-                    print strftime "[%Y-%m-%d %H:%M:%S] ", localtime'
+                    print strftime "[%Y-%m-%d %H:%M:%S] ", localtime;
+                    if (/DEADLYSIGNAL/) {exit 1}' || exit 1
 fi
