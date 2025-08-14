af775a6d3a4210cab16c72bd60c0b2a4bb20bcee
Kamil Tokarski <ktokarski@nvidia.com>
2025-08-14 21:02:14 +0200

    Fix for cu12
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- qa/TL0_python-self-test-operators_1/test_numba_cuda.sh
+++ qa/TL0_python-self-test-operators_1/test_numba_cuda.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 # used pip packages
 
-# there's no cu13 extra for numba-cuda and numba-cuda requires cuda-bindings
-NUMBA_PKG=$(if [[ $DALI_CUDA_MAJOR_VERSION == "13" ]]; then echo "numba_cuda cuda-bindings==13.*"; else echo "numba_cuda[cu${DALI_CUDA_MAJOR_VERSION}]"; fi)
+# there's no cuXX extra for the latest numba-cuda and numba-cuda requires cuda-bindings
+NUMBA_PKG="numba_cuda cuda-bindings==${DALI_CUDA_MAJOR_VERSION}.*"
 pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa scipy nvidia-ml-py==11.450.51 numba lz4 ${NUMBA_PKG}'
 
 target_dir=./dali/test/python
