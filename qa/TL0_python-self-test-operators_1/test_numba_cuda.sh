cb27a0012b76c40a026072f432b7d296da4dc3df
Kamil Tokarski <ktokarski@nvidia.com>
2025-08-17 22:03:09 +0200

    Fix
    Signed-off-by: Kamil Tokarski <ktokarski@nvidia.com>

--- qa/TL0_python-self-test-operators_1/test_numba_cuda.sh
+++ qa/TL0_python-self-test-operators_1/test_numba_cuda.sh
@@ -2,8 +2,7 @@
 # used pip packages
 
 # there's no cuXX extra for the latest numba-cuda and numba-cuda requires cuda-bindings
-NUMBA_PKG="numba_cuda cuda-bindings==${DALI_CUDA_MAJOR_VERSION}.*"
-pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa scipy nvidia-ml-py==11.450.51 numba lz4 ${NUMBA_PKG}'
+pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pillow librosa scipy nvidia-ml-py==11.450.51 numba lz4 numba_cuda cuda-bindings==${DALI_CUDA_MAJOR_VERSION}.*'
 
 target_dir=./dali/test/python
 
