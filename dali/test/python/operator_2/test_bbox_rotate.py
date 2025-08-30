Fix rotateCorners (y points down in image). Require input image shape as normalized rotation doesn't work if aspect ratio != 1. Allow boxes to be normalized or pixel coords with flag.
Signed-off-by: Bryce Ferenczi <frenzi@hotmail.com.au>

dali/test/python/operator_2/test_bbox_rotate.py
@@ -38,26 +38,34 @@ def show_outputs(images: np.ndarray, boxes: np.ndarray, labels: np.ndarray, file
 
 
 @pipeline_def(num_threads=1, batch_size=1, device_id=0)
-def coco_rotate_pipeline(angle=45.0, keep_size=False, ltrb=True):
-    image, boxes, labels = fn.readers.coco(
-        file_root=str(file_root), annotations_file=str(train_annotations), ltrb=ltrb, ratio=True
+def coco_rotate_pipeline(angle=45.0, keep_size=False, ltrb=True, ratio=False):
+    image_enc, boxes, labels = fn.readers.coco(
+        file_root=str(file_root), annotations_file=str(train_annotations), ltrb=ltrb, ratio=ratio
     )
-    image = fn.decoders.image(image, device="cpu")
+    image = fn.decoders.image(image_enc, device="cpu")
     image_rot = fn.rotate(image, angle=angle, keep_size=keep_size)
     boxes_rot, labels_rot = fn.bbox_rotate(
-        boxes, labels, angle=angle, ltrb=ltrb, keep_size=keep_size
+        boxes,
+        labels,
+        angle=angle,
+        input_shape=image.shape(),
+        bbox_layout="xyXY" if ltrb else "xyWH",
+        keep_size=keep_size,
+        bbox_normalized=ratio,
     )
     return image, boxes, labels, image_rot, boxes_rot, labels_rot
 
 
 def test_bbox_rotate():
-    pipeline = coco_rotate_pipeline()
+    ratio = False
+    pipeline = coco_rotate_pipeline(ratio=ratio)
     pipeline.build()
     outs = pipeline.run()
     images, boxes, labels, images_rot, boxes_rot, labels_rot = map(lambda x: np.array(x[0]), outs)
-    images_whwh = np.tile(np.array(images.shape[1::-1])[None], 2)
-    boxes *= images_whwh
-    images_rot_whwh = np.tile(np.array(images_rot.shape[1::-1])[None], 2)
-    boxes_rot *= images_rot_whwh
+    if ratio:
+        images_whwh = np.tile(np.array(images.shape[1::-1])[None], 2)
+        boxes *= images_whwh
+        images_rot_whwh = np.tile(np.array(images_rot.shape[1::-1])[None], 2)
+        boxes_rot *= images_rot_whwh
     show_outputs(images, boxes, labels, "original.webp")
     show_outputs(images_rot, boxes_rot, labels_rot, "rotated.webp")