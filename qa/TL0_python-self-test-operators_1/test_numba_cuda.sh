49c61988e585f7a866ad9df47ebd194d4358ebf1
Janusz Lisiecki <39967756+JanuszL@users.noreply.github.com>
2025-09-19 11:35:59 +0200

    Add nvcc/nvjitlink version compatibility check to numba CUDA test (#6035)
    - Adds a check for nvcc and nvjitlink wheel package versions before
      running numba_cuda test. Auto-upgrades the lower version to
      match the higher one to prevent PTX version mismatch error
    
    Signed-off-by: Janusz Lisiecki <jlisiecki@nvidia.com>
    Signed-off-by: Tony Reina <tony.reina@exthymic.com>

--- qa/TL0_python-self-test-operators_1/test_numba_cuda.sh
+++ qa/TL0_python-self-test-operators_1/test_numba_cuda.sh
@@ -5,6 +5,30 @@ pip_packages='${python_test_runner_package} dataclasses numpy opencv-python pill
 target_dir=./dali/test/python
 
 test_body() {
+  # make sure nvcc and nvjitlink are the same version, if the nvjitlink is lower than nvcc
+  # we get the PTX version mismatch error
+  if [[ "$DALI_CUDA_MAJOR_VERSION" == "12" ]]; then
+    nvcc_pkg="nvidia-cuda-nvcc-cu12"
+    nvjitlink_pkg="nvidia-nvjitlink-cu12"
+  else
+    # with CUDA 13 cuXX suffix is not used and CUDA version is defined by the major version
+    nvcc_pkg="nvidia-cuda-nvcc"
+    nvjitlink_pkg="nvidia-nvjitlink"
+  fi
+  nvcc_ver=$(pip show $nvcc_pkg | awk '/^Version: /{print $2}') || true
+  nvjitlink_ver=$(pip show $nvjitlink_pkg | awk '/^Version: /{print $2}') || true
+  if [[ -n "$nvcc_ver" && -n "$nvjitlink_ver" ]]; then
+    nvcc_majmin=$(echo $nvcc_ver | awk -F. '{print $1"."$2}')
+    nvjitlink_majmin=$(echo $nvjitlink_ver | awk -F. '{print $1"."$2}')
+    if [[ "$nvcc_majmin" != "$nvjitlink_majmin" ]]; then
+      awk_comp=$(awk -v a="$nvcc_majmin" -v b="$nvjitlink_majmin" 'BEGIN{print (a<b)?1:0}')
+      if [[ "$awk_comp" == "1" ]]; then
+        pip install --upgrade "${nvcc_pkg}~=$nvjitlink_majmin"
+      else
+        pip install --upgrade "${nvjitlink_pkg}~=$nvcc_majmin"
+      fi
+    fi
+  fi
   ${python_new_invoke_test} -A '!slow' -s operator_1 test_numba_func
 }
 
