7c27767579cd4d177607b5ff592b8aaf3e4815c3
Michał Zientkiewicz <michalz@nvidia.com>
2025-10-13 15:21:23 +0200

    Rename DALI2 to dynamic (#6064)
    * Rename DALI2 to dynamic
    * import dynamic as ndd
    
    ---------
    Signed-off-by: Michał Zientkiewicz <mzient@gmail.com>

--- dali/test/python/experimental_mode/test_batch.py
+++ dali/test/python/experimental_mode/test_batch.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import nvidia.dali.experimental.dali2 as D
+import nvidia.dali.experimental.dynamic as ndd
 import numpy as np
 from nose_utils import attr
 from nose2.tools import params
@@ -21,7 +21,7 @@ import test_tensor
 
 
 def asnumpy(batch_or_tensor):
-    if isinstance(batch_or_tensor, D.Batch):
+    if isinstance(batch_or_tensor, ndd.Batch):
         return [test_tensor.asnumpy(t) for t in batch_or_tensor.tensors]
     else:
         return test_tensor.asnumpy(batch_or_tensor)
@@ -32,16 +32,16 @@ def test_batch_construction(device_type):
     t0 = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.int32)
     t1 = np.array([[7, 8, 9], [10, 11, 12]], dtype=np.int32)
 
-    b = D.batch(
+    b = ndd.batch(
         [
             t0,
             t1,
         ],
-        device=D.Device(device_type),
+        device=ndd.Device(device_type),
         layout="AB",
     )
 
-    assert isinstance(b, D.Batch)
+    assert isinstance(b, ndd.Batch)
     assert b.device.device_type == device_type
     assert b.layout == "AB"
     assert np.array_equal(asnumpy(b.tensors[0]), t0)
@@ -59,10 +59,10 @@ def test_batch_construction(device_type):
 @params(("cpu",), ("gpu",))
 def test_batch_from_empty_list(device_type):
     with assert_raises(ValueError, glob="Element type"):
-        D.batch([], device=device_type)
-    b = D.batch([], dtype=D.int32, device=device_type)
-    assert isinstance(b, D.Batch)
-    assert b.dtype == D.int32
+        ndd.batch([], device=device_type)
+    b = ndd.batch([], dtype=ndd.int32, device=device_type)
+    assert isinstance(b, ndd.Batch)
+    assert b.dtype == ndd.int32
     assert b.shape == []
     assert b.ndim == 0
 
@@ -72,17 +72,17 @@ def test_batch_as_batch(device_type):
     t0 = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.int32)
     t1 = np.array([[7, 8, 9], [10, 11, 12]], dtype=np.int32)
 
-    b0 = D.batch(
+    b0 = ndd.batch(
         [
             t0,
             t1,
         ],
-        device=D.Device(device_type),
+        device=ndd.Device(device_type),
         layout="AB",
     )
 
-    b1 = D.batch(b0)
-    b2 = D.as_batch(b0)
+    b1 = ndd.batch(b0)
+    b2 = ndd.as_batch(b0)
     assert b1.dtype == b0.dtype
     assert b1.shape == b0.shape
     assert b1.layout == b0.layout
@@ -93,15 +93,15 @@ def test_batch_as_batch(device_type):
     assert not b1.tensors[0]._is_same_tensor(b0.tensors[0])
     assert b2.tensors[0]._is_same_tensor(b0.tensors[0])
 
-    b3 = D.as_batch(b0, dtype=D.float32)
-    assert b3.dtype == D.float32
+    b3 = ndd.as_batch(b0, dtype=ndd.float32)
+    assert b3.dtype == ndd.float32
     assert b3.shape == b0.shape
     assert b3.layout == b0.layout
 
 
 def test_broadcast():
     a = np.array([1, 2, 3])
-    b = D.Batch.broadcast(a, 5).evaluate()
+    b = ndd.Batch.broadcast(a, 5).evaluate()
     for i, t in enumerate(b._backend):
         assert np.array_equal(np.array(t), a)
 
@@ -121,24 +121,24 @@ def test_batch_construction_with_torch_tensor(device_type):
     import torch
 
     data = torch.tensor([[1, 2, 3], [4, 5, 6]], device=device_type, dtype=torch.int32)
-    b = D.as_batch(data)
-    assert b.device == D.Device("gpu" if device_type == "cuda" else device_type)
-    assert b.dtype == D.int32
+    b = ndd.as_batch(data)
+    assert b.device == ndd.Device("gpu" if device_type == "cuda" else device_type)
+    assert b.dtype == ndd.int32
     assert b.batch_size == 2
     assert b.ndim == 1
     assert b.shape == [(3,), (3,)]
     assert b.layout is None
-    ref = torch.from_dlpack(D.as_tensor(b)._backend)
+    ref = torch.from_dlpack(ndd.as_tensor(b)._backend)
     assert torch.equal(data[0], ref[0])
     assert torch.equal(data[1], ref[1])
 
 
 @params(("cpu",), ("gpu",))
 def test_batch_construction_with_tensor(device_type):
-    t = D.tensor(np.array([1, 2, 3], dtype=np.uint8), device=device_type, layout="X")
-    b = D.Batch([t])
-    assert b.device == D.Device(device_type)
-    assert b.dtype == D.uint8
+    t = ndd.tensor(np.array([1, 2, 3], dtype=np.uint8), device=device_type, layout="X")
+    b = ndd.Batch([t])
+    assert b.device == ndd.Device(device_type)
+    assert b.dtype == ndd.uint8
     assert b.layout == "X"
     assert b.batch_size == 1
     assert b.ndim == 1
@@ -151,21 +151,21 @@ def test_batch_construction_with_conversion(device_type):
     data_i32 = [np.int32([1]), np.int32([1, 2, 0x1000001])]
     data_fp32 = [np.float32([1]), np.float32([1.25, 2.2, 0x1000000])]
     # loss of precision --------------------------^
-    orig = D.Batch(data, device=device_type).evaluate()
+    orig = ndd.Batch(data, device=device_type).evaluate()
     # convert from a list of tensors
-    i32 = D.Batch(data, device=device_type, dtype=D.int32).evaluate()
+    i32 = ndd.Batch(data, device=device_type, dtype=ndd.int32).evaluate()
     # convert from a TensorList object
-    fp32 = D.Batch(orig._backend, device=device_type, dtype=D.float32)
+    fp32 = ndd.Batch(orig._backend, device=device_type, dtype=ndd.float32)
     fp32.evaluate()
-    assert orig.dtype == D.float64
-    assert orig.device == D.Device(device_type)
-    assert orig._backend.dtype == D.float64.type_id
-    assert i32.dtype == D.int32
-    assert i32.device == D.Device(device_type)
-    assert i32._backend.dtype == D.int32.type_id
-    assert fp32.dtype == D.float32
-    assert fp32.device == D.Device(device_type)
-    assert fp32._backend.dtype == D.float32.type_id
+    assert orig.dtype == ndd.float64
+    assert orig.device == ndd.Device(device_type)
+    assert orig._backend.dtype == ndd.float64.type_id
+    assert i32.dtype == ndd.int32
+    assert i32.device == ndd.Device(device_type)
+    assert i32._backend.dtype == ndd.int32.type_id
+    assert fp32.dtype == ndd.float32
+    assert fp32.device == ndd.Device(device_type)
+    assert fp32._backend.dtype == ndd.float32.type_id
     assert batch_equal(data, asnumpy(orig))
     assert batch_equal(data_i32, asnumpy(i32))
     assert batch_equal(data_fp32, asnumpy(fp32))
@@ -173,11 +173,11 @@ def test_batch_construction_with_conversion(device_type):
 
 @params(("cpu",), ("gpu",))
 def test_batch_properties_clone(device_type):
-    t = D.tensor(np.array([1, 2, 3], dtype=np.uint8))
-    src = D.Batch([t], device=device_type, layout="X")
-    b = D.batch(src)
-    assert b.device == D.Device(device_type)
-    assert b.dtype == D.uint8
+    t = ndd.tensor(np.array([1, 2, 3], dtype=np.uint8))
+    src = ndd.Batch([t], device=device_type, layout="X")
+    b = ndd.batch(src)
+    assert b.device == ndd.Device(device_type)
+    assert b.dtype == ndd.uint8
     assert b.layout == "X"
     assert b.batch_size == 1
     assert b.ndim == 1
@@ -185,77 +185,77 @@ def test_batch_properties_clone(device_type):
 
 
 def test_batch_subscript_broadcast():
-    b = D.as_batch(
+    b = ndd.as_batch(
         [
-            D.tensor([[1, 2, 3], [4, 5, 6]], dtype=D.int32),
-            D.tensor([[7, 8, 9], [10, 11, 12]], dtype=D.int32),
+            ndd.tensor([[1, 2, 3], [4, 5, 6]], dtype=ndd.int32),
+            ndd.tensor([[7, 8, 9], [10, 11, 12]], dtype=ndd.int32),
         ],
         layout="XY",
     )
     b11 = b.slice[1, 1]
     assert b11.layout is None
-    assert b11.dtype == D.int32
-    assert isinstance(b11, D.Batch)
+    assert b11.dtype == ndd.int32
+    assert isinstance(b11, ndd.Batch)
     assert asnumpy(b11.tensors[0]) == 5
     assert asnumpy(b11.tensors[1]) == 11
 
 
 def test_batch_partial_slice():
-    b = D.as_batch(
+    b = ndd.as_batch(
         [
-            D.tensor([[1, 2, 3], [4, 5, 6]], dtype=D.int32),
-            D.tensor([[7, 8, 9], [10, 11, 12]], dtype=D.int32),
+            ndd.tensor([[1, 2, 3], [4, 5, 6]], dtype=ndd.int32),
+            ndd.tensor([[7, 8, 9], [10, 11, 12]], dtype=ndd.int32),
         ],
         layout="XY",
     )
     b11 = b.slice[..., 1]
     assert b11.layout == "X"
-    assert b11.dtype == D.int32
-    assert isinstance(b11, D.Batch)
+    assert b11.dtype == ndd.int32
+    assert isinstance(b11, ndd.Batch)
     assert np.array_equal(asnumpy(b11.tensors[0]), np.array([2, 5], dtype=np.int32))
     assert np.array_equal(asnumpy(b11.tensors[1]), np.array([8, 11], dtype=np.int32))
 
 
 def test_batch_slice():
-    b = D.as_batch(
+    b = ndd.as_batch(
         [
-            D.tensor([[1, 2, 3], [4, 5, 6]], dtype=D.uint16),
-            D.tensor([[7, 8, 9, 10], [11, 12, 13, 14]], dtype=D.uint16),
+            ndd.tensor([[1, 2, 3], [4, 5, 6]], dtype=ndd.uint16),
+            ndd.tensor([[7, 8, 9, 10], [11, 12, 13, 14]], dtype=ndd.uint16),
         ],
         layout="XY",
     )
     sliced = b.slice[..., 1:-1]
     assert sliced.layout == "XY"
-    assert sliced.dtype == D.uint16
+    assert sliced.dtype == ndd.uint16
     assert np.array_equal(asnumpy(sliced.tensors[0]), np.array([[2], [5]], dtype=np.uint16))
     assert np.array_equal(asnumpy(sliced.tensors[1]), np.array([[8, 9], [12, 13]], dtype=np.uint16))
 
 
 def test_batch_subscript_per_sample():
-    b = D.as_batch(
+    b = ndd.as_batch(
         [
-            D.tensor([[1, 2, 3], [4, 5, 6]], dtype=D.int32),
-            D.tensor([[7, 8, 9, 10], [11, 12, 13, 14]], dtype=D.int32),
+            ndd.tensor([[1, 2, 3], [4, 5, 6]], dtype=ndd.int32),
+            ndd.tensor([[7, 8, 9, 10], [11, 12, 13, 14]], dtype=ndd.int32),
         ]
     )
     # unzipped indices (1, 1), (0, 2)
-    i = D.as_batch([1, 0])
-    j = D.as_batch([1, 2])
+    i = ndd.as_batch([1, 0])
+    j = ndd.as_batch([1, 2])
     b11 = b.slice[i, j]
-    assert isinstance(b11, D.Batch)
+    assert isinstance(b11, ndd.Batch)
     assert asnumpy(b11.tensors[0]) == 5
     assert asnumpy(b11.tensors[1]) == 9
 
 
 def test_batch_to_gpu():
     input = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.int32)
-    t_cpu = D.tensor(input)
+    t_cpu = ndd.tensor(input)
     t_gpu = t_cpu.gpu()
-    assert t_gpu.device == D.Device("gpu")
-    b_gpu = D.Batch([t_gpu])
+    assert t_gpu.device == ndd.Device("gpu")
+    b_gpu = ndd.Batch([t_gpu])
     b_gpu.evaluate()
-    assert b_gpu.device == D.Device("gpu")
-    assert b_gpu.dtype == D.int32
+    assert b_gpu.device == ndd.Device("gpu")
+    assert b_gpu.dtype == ndd.int32
     assert b_gpu.batch_size == 1
     assert b_gpu.ndim == 2
     assert b_gpu.shape == [(2, 3)]
--- dali/test/python/experimental_mode/test_type.py
+++ dali/test/python/experimental_mode/test_type.py
@@ -12,125 +12,125 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import nvidia.dali.experimental.dali2 as dali2
+import nvidia.dali.experimental.dynamic as ndd
 import nvidia.dali.types as types
 
 
 def test_type_ids():
-    assert dali2.int8.type_id == types.INT8
-    assert dali2.uint8.type_id == types.UINT8
-    assert dali2.int16.type_id == types.INT16
-    assert dali2.uint16.type_id == types.UINT16
-    assert dali2.int32.type_id == types.INT32
-    assert dali2.uint32.type_id == types.UINT32
-    assert dali2.int64.type_id == types.INT64
-    assert dali2.uint64.type_id == types.UINT64
-    assert dali2.float16.type_id == types.FLOAT16
-    assert dali2.float32.type_id == types.FLOAT
-    assert dali2.float64.type_id == types.FLOAT64
-    assert dali2.bool.type_id == types.BOOL
-    assert dali2.InterpType.type_id == types.INTERP_TYPE
-    assert dali2.DataType.type_id == types.DATA_TYPE
-    assert dali2.ImageType.type_id == types.IMAGE_TYPE
+    assert ndd.int8.type_id == types.INT8
+    assert ndd.uint8.type_id == types.UINT8
+    assert ndd.int16.type_id == types.INT16
+    assert ndd.uint16.type_id == types.UINT16
+    assert ndd.int32.type_id == types.INT32
+    assert ndd.uint32.type_id == types.UINT32
+    assert ndd.int64.type_id == types.INT64
+    assert ndd.uint64.type_id == types.UINT64
+    assert ndd.float16.type_id == types.FLOAT16
+    assert ndd.float32.type_id == types.FLOAT
+    assert ndd.float64.type_id == types.FLOAT64
+    assert ndd.bool.type_id == types.BOOL
+    assert ndd.InterpType.type_id == types.INTERP_TYPE
+    assert ndd.DataType.type_id == types.DATA_TYPE
+    assert ndd.ImageType.type_id == types.IMAGE_TYPE
 
 
 def test_type_ids_to_type():
-    assert dali2.dtype(types.INT8) == dali2.int8
-    assert dali2.dtype(types.UINT8) == dali2.uint8
-    assert dali2.dtype(types.INT16) == dali2.int16
-    assert dali2.dtype(types.UINT16) == dali2.uint16
-    assert dali2.dtype(types.INT32) == dali2.int32
-    assert dali2.dtype(types.UINT32) == dali2.uint32
-    assert dali2.dtype(types.INT64) == dali2.int64
-    assert dali2.dtype(types.UINT64) == dali2.uint64
-    assert dali2.dtype(types.FLOAT16) == dali2.float16
-    assert dali2.dtype(types.FLOAT) == dali2.float32
-    assert dali2.dtype(types.FLOAT64) == dali2.float64
-    assert dali2.dtype(types.BOOL) == dali2.bool
-    assert dali2.dtype(types.INTERP_TYPE) == dali2.InterpType
-    assert dali2.dtype(types.DATA_TYPE) == dali2.DataType
-    assert dali2.dtype(types.IMAGE_TYPE) == dali2.ImageType
+    assert ndd.dtype(types.INT8) == ndd.int8
+    assert ndd.dtype(types.UINT8) == ndd.uint8
+    assert ndd.dtype(types.INT16) == ndd.int16
+    assert ndd.dtype(types.UINT16) == ndd.uint16
+    assert ndd.dtype(types.INT32) == ndd.int32
+    assert ndd.dtype(types.UINT32) == ndd.uint32
+    assert ndd.dtype(types.INT64) == ndd.int64
+    assert ndd.dtype(types.UINT64) == ndd.uint64
+    assert ndd.dtype(types.FLOAT16) == ndd.float16
+    assert ndd.dtype(types.FLOAT) == ndd.float32
+    assert ndd.dtype(types.FLOAT64) == ndd.float64
+    assert ndd.dtype(types.BOOL) == ndd.bool
+    assert ndd.dtype(types.INTERP_TYPE) == ndd.InterpType
+    assert ndd.dtype(types.DATA_TYPE) == ndd.DataType
+    assert ndd.dtype(types.IMAGE_TYPE) == ndd.ImageType
 
 
 def test_type_names():
-    assert dali2.int8.name == "i8"
-    assert dali2.uint8.name == "u8"
-    assert dali2.int16.name == "i16"
-    assert dali2.uint16.name == "u16"
-    assert dali2.int32.name == "i32"
-    assert dali2.uint32.name == "u32"
-    assert dali2.int64.name == "i64"
-    assert dali2.uint64.name == "u64"
-    assert dali2.float16.name == "f16"
-    assert dali2.float32.name == "f32"
-    assert dali2.float64.name == "f64"
-    assert dali2.bool.name == "bool"
-    assert dali2.bfloat16.name == "bfloat16"
-    assert dali2.DataType.name == "DataType"
-    assert dali2.ImageType.name == "ImageType"
-    assert dali2.InterpType.name == "InterpType"
+    assert ndd.int8.name == "i8"
+    assert ndd.uint8.name == "u8"
+    assert ndd.int16.name == "i16"
+    assert ndd.uint16.name == "u16"
+    assert ndd.int32.name == "i32"
+    assert ndd.uint32.name == "u32"
+    assert ndd.int64.name == "i64"
+    assert ndd.uint64.name == "u64"
+    assert ndd.float16.name == "f16"
+    assert ndd.float32.name == "f32"
+    assert ndd.float64.name == "f64"
+    assert ndd.bool.name == "bool"
+    assert ndd.bfloat16.name == "bfloat16"
+    assert ndd.DataType.name == "DataType"
+    assert ndd.ImageType.name == "ImageType"
+    assert ndd.InterpType.name == "InterpType"
 
 
 def test_type_names_to_type():
-    assert dali2.dtype("i8") is dali2.int8
-    assert dali2.dtype("u8") is dali2.uint8
-    assert dali2.dtype("i16") is dali2.int16
-    assert dali2.dtype("u16") is dali2.uint16
-    assert dali2.dtype("i32") is dali2.int32
-    assert dali2.dtype("u32") is dali2.uint32
-    assert dali2.dtype("i64") is dali2.int64
-    assert dali2.dtype("u64") is dali2.uint64
-    assert dali2.dtype("f16") is dali2.float16
-    assert dali2.dtype("f32") is dali2.float32
-    assert dali2.dtype("f64") is dali2.float64
-    assert dali2.dtype("bool") is dali2.bool
-    assert dali2.dtype("bfloat16") is dali2.bfloat16
-    assert dali2.dtype("DataType") is dali2.DataType
-    assert dali2.dtype("ImageType") is dali2.ImageType
-    assert dali2.dtype("InterpType") is dali2.InterpType
+    assert ndd.dtype("i8") is ndd.int8
+    assert ndd.dtype("u8") is ndd.uint8
+    assert ndd.dtype("i16") is ndd.int16
+    assert ndd.dtype("u16") is ndd.uint16
+    assert ndd.dtype("i32") is ndd.int32
+    assert ndd.dtype("u32") is ndd.uint32
+    assert ndd.dtype("i64") is ndd.int64
+    assert ndd.dtype("u64") is ndd.uint64
+    assert ndd.dtype("f16") is ndd.float16
+    assert ndd.dtype("f32") is ndd.float32
+    assert ndd.dtype("f64") is ndd.float64
+    assert ndd.dtype("bool") is ndd.bool
+    assert ndd.dtype("bfloat16") is ndd.bfloat16
+    assert ndd.dtype("DataType") is ndd.DataType
+    assert ndd.dtype("ImageType") is ndd.ImageType
+    assert ndd.dtype("InterpType") is ndd.InterpType
 
 
 def test_type_bytes():
-    assert dali2.int8.bytes == 1
-    assert dali2.uint8.bytes == 1
-    assert dali2.int16.bytes == 2
-    assert dali2.uint16.bytes == 2
-    assert dali2.int32.bytes == 4
-    assert dali2.uint32.bytes == 4
-    assert dali2.int64.bytes == 8
-    assert dali2.uint64.bytes == 8
-    assert dali2.float16.bytes == 2
-    assert dali2.float32.bytes == 4
-    assert dali2.float64.bytes == 8
-    assert dali2.bool.bytes == 1
-    assert dali2.InterpType.bytes == 4
-    assert dali2.DataType.bytes == 4
-    assert dali2.ImageType.bytes == 4
+    assert ndd.int8.bytes == 1
+    assert ndd.uint8.bytes == 1
+    assert ndd.int16.bytes == 2
+    assert ndd.uint16.bytes == 2
+    assert ndd.int32.bytes == 4
+    assert ndd.uint32.bytes == 4
+    assert ndd.int64.bytes == 8
+    assert ndd.uint64.bytes == 8
+    assert ndd.float16.bytes == 2
+    assert ndd.float32.bytes == 4
+    assert ndd.float64.bytes == 8
+    assert ndd.bool.bytes == 1
+    assert ndd.InterpType.bytes == 4
+    assert ndd.DataType.bytes == 4
+    assert ndd.ImageType.bytes == 4
 
 
 def test_type_exponent_bits():
-    assert dali2.int8.exponent_bits is None
-    assert dali2.uint8.exponent_bits is None
-    assert dali2.int16.exponent_bits is None
-    assert dali2.uint16.exponent_bits is None
-    assert dali2.int32.exponent_bits is None
-    assert dali2.uint32.exponent_bits is None
-    assert dali2.int64.exponent_bits is None
-    assert dali2.uint64.exponent_bits is None
-    assert dali2.float16.exponent_bits == 5
-    assert dali2.float32.exponent_bits == 8
-    assert dali2.float64.exponent_bits == 11
+    assert ndd.int8.exponent_bits is None
+    assert ndd.uint8.exponent_bits is None
+    assert ndd.int16.exponent_bits is None
+    assert ndd.uint16.exponent_bits is None
+    assert ndd.int32.exponent_bits is None
+    assert ndd.uint32.exponent_bits is None
+    assert ndd.int64.exponent_bits is None
+    assert ndd.uint64.exponent_bits is None
+    assert ndd.float16.exponent_bits == 5
+    assert ndd.float32.exponent_bits == 8
+    assert ndd.float64.exponent_bits == 11
 
 
 def test_type_significand_bits():
-    assert dali2.int8.significand_bits is None
-    assert dali2.uint8.significand_bits is None
-    assert dali2.int16.significand_bits is None
-    assert dali2.uint16.significand_bits is None
-    assert dali2.int32.significand_bits is None
-    assert dali2.uint32.significand_bits is None
-    assert dali2.int64.significand_bits is None
-    assert dali2.uint64.significand_bits is None
-    assert dali2.float16.significand_bits == 10
-    assert dali2.float32.significand_bits == 23
-    assert dali2.float64.significand_bits == 52
+    assert ndd.int8.significand_bits is None
+    assert ndd.uint8.significand_bits is None
+    assert ndd.int16.significand_bits is None
+    assert ndd.uint16.significand_bits is None
+    assert ndd.int32.significand_bits is None
+    assert ndd.uint32.significand_bits is None
+    assert ndd.int64.significand_bits is None
+    assert ndd.uint64.significand_bits is None
+    assert ndd.float16.significand_bits == 10
+    assert ndd.float32.significand_bits == 23
+    assert ndd.float64.significand_bits == 52
