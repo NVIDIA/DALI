19611144b216957bf30928794acb4d0bb49f2d42
Szymon Karpiński <skarpinski@nvidia.com>
2025-11-21 15:54:02 +0100

    Refactor
    Signed-off-by: Szymon Karpiński <skarpinski@nvidia.com>

--- docs/autodoc_submodules.py
+++ docs/autodoc_submodules.py
@@ -206,88 +206,68 @@ def single_module_file(module, funs_in_module, references):
         result += f"   {full_name}\n"
     return result
 
-
-def fn_autodoc(out_filename, generated_path, references):
+def write_toctree(all_modules, relative_generated_path, out_filename):
     all_modules_str = ".. toctree::\n   :hidden:\n\n"
-    all_modules = get_modules(fn_modules)
     for module in all_modules:
-        dali_module = sys.modules[module]
-        # Take all public members of given module
-        funs_in_module = get_functions(dali_module)
-        if len(funs_in_module) == 0:
-            continue
-
         # As the top-level file is included from a directory above generated_path
         # we need to provide the relative path to the per-module files
         # the rest is within the same directory, so there is no need for that
-        all_modules_str += f"   {generated_path / module}\n"
-
-        single_module_str = single_module_file(
-            module, funs_in_module, references
-        )
-        with open(generated_path / (module + ".rst"), "w") as module_file:
-            module_file.write(single_module_str)
-
-        for fun in funs_in_module:
-            full_name = f"{module}.{fun}"
-            if (
-                module in exclude_fn_members
-                and fun in exclude_fn_members[module]
-            ):
-                continue
-            with open(
-                generated_path / (full_name + ".rst"), "w"
-            ) as function_file:
-                single_file_str = single_fun_file(full_name, references)
-                function_file.write(single_file_str)
-
+        all_modules_str += f"   {relative_generated_path / module}\n"
     with open(out_filename, "w") as f:
-        f.write(all_modules_str)
+        f.write(all_modules_str)    
+
+def write_module_file(generated_path, module, funs_in_module, references):
+    if len(funs_in_module) == 0:
+        return
+    single_module_str = single_module_file(
+        module, funs_in_module, references
+    )
+    with open(generated_path / (module + ".rst"), "w") as module_file:
+        module_file.write(single_module_str)
+
+def write_function_files(module, funs_in_module, file_content_generator, generated_path, references):
+    for fun in funs_in_module:
+        full_name = f"{module}.{fun}"
+        with open(
+            generated_path / (full_name + ".rst"), "w"
+        ) as function_file:
+            single_file_str = file_content_generator(full_name, references)
+            function_file.write(single_file_str)
+
+def fn_autodoc(out_filename, generated_path, references):    
+    all_modules = get_modules(fn_modules)
+    write_toctree(all_modules, generated_path, out_filename)
+    for module in all_modules:
+        dali_module = sys.modules[module]
+        funs_in_module = [
+            fun for fun in get_functions(dali_module) if module not in exclude_fn_members or fun not in exclude_fn_members[module]
+        ]
+        write_module_file(generated_path, module, funs_in_module, references)
+        write_function_files(module, funs_in_module, single_fun_file, generated_path, references)
+
+
+
 
 
 def dynamic_autodoc(out_filename, generated_path, relative_generated_path, references):
-    all_modules_str = ".. toctree::\n   :hidden:\n\n"
     all_modules = get_modules(dynamic_modules)
+    write_toctree(all_modules, relative_generated_path, out_filename)
     for module in all_modules:
         dali_module = sys.modules[module]
-        # Take all public members of given module
-        funs_in_module = get_functions(dali_module)
-        if len(funs_in_module) == 0:
-            continue
-
-        # Skip non-operators
         funs_in_module = [
             fun
-            for fun in funs_in_module
+            for fun in get_functions(dali_module)
             if hasattr(getattr(dali_module, fun), "schema")
         ]
 
-        # As the top-level file is included from a directory above generated_path
-        # we need to provide the relative path to the per-module files
-        # the rest is within the same directory, so there is no need for that
-        all_modules_str += f"   {os.path.join(relative_generated_path, module)}\n"
+        write_module_file(generated_path, module, funs_in_module, references)
+        write_function_files(module, funs_in_module, single_fun_file, generated_path, references)
 
-        single_module_str = single_module_file(
-            module, funs_in_module, references
-        )
-        with open(generated_path / (module + ".rst"), "w") as module_file:
-            module_file.write(single_module_str)
-
-        for fun in funs_in_module:
-            full_name = f"{module}.{fun}"
-            with open(
-                generated_path / (full_name + ".rst"), "w"
-            ) as function_file:
-                single_file_str = single_fun_file(full_name, references)
-                function_file.write(single_file_str)
-
-    with open(out_filename, "w") as f:
-        f.write(all_modules_str)
 
 
 def dynamic_readers_autodoc(out_filename, generated_path, relative_generated_path, references):
-    all_modules_str = ".. toctree::\n   :hidden:\n\n"
     all_modules = [m for m in get_modules(dynamic_modules) if "readers" in m]
+    write_toctree(all_modules, relative_generated_path, out_filename)
     for module in all_modules:
         dali_module = sys.modules[module]
         readers_in_module = [
@@ -297,22 +277,5 @@ def dynamic_readers_autodoc(out_filename, generated_path, relative_generated_pat
             and issubclass(v, nvidia.dali.experimental.dynamic.ops.Reader)
         ]
 
-        # As the top-level file is included from a directory above generated_path
-        # we need to provide the relative path to the per-module files
-        # the rest is within the same directory, so there is no need for that
-        all_modules_str += f"   {os.path.join(relative_generated_path, module)}\n"
-        single_module_str = single_module_file(
-            module, readers_in_module, references
-        )
-        with open(generated_path / (module + ".rst"), "w") as module_file:
-            module_file.write(single_module_str)
-
-        for reader in readers_in_module:
-            full_name = f"{module}.{reader}"
-            with open(
-                generated_path / (full_name + ".rst"), "w"
-            ) as function_file:
-                function_file.write(single_class_op_file(full_name, references))
-
-    with open(out_filename, "w") as f:
-        f.write(all_modules_str)
+        write_module_file(generated_path, module, readers_in_module, references)
+        write_function_files(module, readers_in_module, single_class_op_file, generated_path, references)
