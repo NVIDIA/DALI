52415706c33a6de434ea653d3da30136b889b459
Michał Zientkiewicz <michalz@nvidia.com>
2025-12-03 18:12:09 +0100

    Add proper broadcasting logic to constant_value operator family. (#6104)
    * Add proper broadcasting logic to constant_value (`full` operator)
    * Extend tests - use scalars and various partial broadcasting patterns
    
    ---------
    
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/operators/generic/constant_value.cc
+++ dali/operators/generic/constant_value.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,14 +16,64 @@
 #include <vector>
 #include "dali/core/convert.h"
 #include "dali/pipeline/operator/op_schema.h"
+#include "dali/kernels/common/utils.h"
 
 namespace dali {
+
+template <typename T, typename U>
+void RepeatInner(T *out, int64_t out_size, const U *in, int64_t in_size) {
+  if (is_pow2(in_size)) {
+    for (ptrdiff_t i = 0; i < out_size; i++) {
+      out[i] = ConvertSat<T>(in[i & (in_size - 1)]);
+    }
+  } else {
+    ptrdiff_t i = 0;
+    for (; i + in_size <= out_size; i += in_size) {
+      for (ptrdiff_t j = 0; j < in_size; j++) {
+        out[i + j] = ConvertSat<T>(in[j]);
+      }
+    }
+    for (ptrdiff_t j = 0; i < out_size; i++, j++) {
+      out[i] = ConvertSat<T>(in[j]);
+    }
+  }
+}
+
+template <typename T, typename U>
+void Broadcast(
+      int ndim, T *out,
+      const int64_t *out_shape,
+      const int64_t *out_strides,
+      const U *in,
+      const int64_t *in_strides) {
+  if (ndim == 0) {
+    *out = ConvertSat<T>(*in);
+  } else if (ndim == 1) {
+    int64_t in_stride = *in_strides;
+    int64_t out_stride = *out_strides;
+    for (ptrdiff_t i = 0, extent = out_shape[0]; i < extent; i++) {
+      out[i * out_stride] = ConvertSat<T>(in[i * in_stride]);
+    }
+  } else {
+    int64_t in_stride = *in_strides;
+    int64_t out_stride = *out_strides;
+    for (ptrdiff_t i = 0, extent = out_shape[0]; i < extent; i++) {
+      Broadcast(
+        ndim - 1,
+        out + i * out_stride,
+        out_shape + 1,
+        out_strides + 1,
+        in + i * in_stride,
+        in_strides + 1);
+    }
+  }
+}
 
 template <>
 void ConstantValue<CPUBackend>::RunImpl(Workspace &ws) {
   auto &output = ws.Output<CPUBackend>(0);
-  const auto& out_shape = output.shape();
-  int nsamples = out_shape.size();
+  const auto& out_shapes = output.shape();
+  int nsamples = out_shapes.size();
   auto dtype = output.type();
   auto &tp = ws.GetThreadPool();
   if (has_fill_value_) {
@@ -35,10 +85,35 @@ void ConstantValue<CPUBackend>::RunImpl(Workspace &ws) {
           tp.AddWork([&, s](int thread_idx) {
             const auto* fill_value_data = fill_value.tensor<FillValueType>(s);
             auto* out_data = output.mutable_tensor<OutputType>(s);
-            auto out_sz = out_shape.tensor_size(s);
-            auto fill_value_sz = fill_value_sh.tensor_size(s);
-            for (int64_t i = 0; i < out_sz; i++) {
-              out_data[i] = ConvertSat<OutputType>(fill_value_data[i % fill_value_sz]);
+            auto out_shape = out_shapes.tensor_shape_span(s);
+            auto in_shape = fill_value.tensor_shape_span(s);
+            if (in_shape.empty() || in_shape.back() == volume(in_shape)) {
+              RepeatInner(out_data, volume(out_shape), fill_value_data, volume(in_shape));
+            } else {
+              TensorShape<> in_strides;
+              in_strides.resize(out_shape.size());
+              int i = in_shape.size() - 1;
+              int j = out_shape.size() - 1;
+              ptrdiff_t stride = 1;
+              for (; i >= 0 && j >= 0; i--, j--) {
+                if (in_shape[i] == out_shape[j]) {
+                  in_strides[j] = stride;
+                  stride *= in_shape[i];
+                } else {
+                  assert(in_shape[i] == 1);
+                  in_strides[j] = 0;
+                }
+              }
+              for (; j >= 0; j--) {
+                in_strides[j] = 0;
+              }
+              int ndim = out_shape.size();
+              TensorShape<> out_strides;
+              kernels::CalcStrides(out_strides, out_shape);
+              Broadcast(
+                  ndim,
+                  out_data, out_shape.data(), out_strides.data(),
+                  fill_value_data, in_strides.data());
             }
           });
         }
@@ -59,7 +134,7 @@ void ConstantValue<CPUBackend>::RunImpl(Workspace &ws) {
       for (int s = 0; s < nsamples; s++) {
         tp.AddWork([&, value, s](int thread_idx) {
           auto* out_data = output.mutable_tensor<T>(s);
-          auto out_sz = out_shape.tensor_size(s);
+          auto out_sz = out_shapes.tensor_size(s);
           std::fill(out_data, out_data + out_sz, value);
         });
       }
