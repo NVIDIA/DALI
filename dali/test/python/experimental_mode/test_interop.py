c12c14756a2190f4acfe71015abbc21e139aa5ad
Rostan Tabet <rtabet@nvidia.com>
2025-12-15 16:13:49 +0000

    Add interop tests with PyTorch
    Signed-off-by: Rostan Tabet <rtabet@nvidia.com>

--- dali/test/python/experimental_mode/test_interop.py
+++ dali/test/python/experimental_mode/test_interop.py
@@ -2,6 +2,8 @@ import warnings
 
 import numpy as np
 import nvidia.dali.experimental.dynamic as ndd
+from nose2.tools import params
+from nose_utils import attr
 
 
 def test_numpy_dlpack():
@@ -26,17 +28,14 @@ def test_numpy_array_interface():
 
 def test_numpy_nocopy():
     ndd_tensor = ndd.tensor([1, 2, 3], device="cpu")
-    np_array1 = np.asarray(ndd_tensor)
-    np_array2 = np.asarray(ndd_tensor, copy=True)
+    np_array1 = np.asarray(ndd_tensor)  # no copy
+    np_array2 = np.array(ndd_tensor)  # copy
 
     np_array1[0] = 42
     np_array2[1] = 0
     result = np.array(ndd_tensor)
 
-    np.testing.assert_array_equal(
-        result,
-        [42, 2, 3],
-    )
+    np.testing.assert_array_equal(result, [42, 2, 3])
 
 
 def test_cuda_array_interface_exists():
@@ -45,3 +44,29 @@ def test_cuda_array_interface_exists():
 
     assert not hasattr(cpu_tensor, "__cuda_array_interface__")
     assert hasattr(gpu_tensor, "__cuda_array_interface__")
+
+
+@attr("pytorch")
+def test_torch_dlpack():
+    import torch
+
+    ndd_tensor = ndd.tensor([1, 2, 3], device="cpu")
+    torch_tensor = torch.from_dlpack(ndd_tensor)
+
+    np.testing.assert_array_equal(torch_tensor, [1, 2, 3])
+
+
+@attr("pytorch")
+@params(("cpu",), ("gpu",))
+def test_torch_nocopy(device: str):
+    import torch
+
+    ndd_tensor = ndd.tensor([1, 2, 3], device=device)
+    torch_tensor1 = torch.as_tensor(ndd_tensor)  # no copy
+    torch_tensor2 = torch.tensor(ndd_tensor)  # copy
+
+    torch_tensor1[0] = 42
+    torch_tensor2[1] = 0
+    result = torch.tensor(ndd_tensor, device="cpu")
+
+    np.testing.assert_array_equal(result, [42, 2, 3])
