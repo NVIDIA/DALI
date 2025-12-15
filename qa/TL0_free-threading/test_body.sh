4c2255bb4bbd7e236e3d3eddba91eb437910b467
Michał Szołucha <mszolucha@nvidia.com>
2025-12-15 13:18:43 +0100

    Apply review remakrs
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- qa/TL0_free-threading/test_body.sh
+++ qa/TL0_free-threading/test_body.sh
@@ -1,15 +1,8 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 echo "test_body.sh Sanity check: PYTHON_GIL=${PYTHON_GIL}"
 
-test_py_with_framework() {
+test_no_fw() {
     ${python_new_invoke_test} -A '!slow' -s free-threading
 }
 
-test_no_fw() {
-    test_py_with_framework
-}
-
-run_all() {
-  test_no_fw
-}
