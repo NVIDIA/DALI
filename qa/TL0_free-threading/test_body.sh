04e1d38ab4508eb74ffaf08fe0b72d0950e8645a
Michał Szołucha <mszolucha@nvidia.com>
2025-12-15 15:53:36 +0100

    Apply review remarks
    Signed-off-by: Michał Szołucha <mszolucha@nvidia.com>

--- qa/TL0_free-threading/test_body.sh
+++ qa/TL0_free-threading/test_body.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -xe
 
-echo "test_body.sh Sanity check: PYTHON_GIL=${PYTHON_GIL}"
-
 test_no_fw() {
     ${python_new_invoke_test} -A '!slow' -s free-threading
 }
--- qa/TL0_free-threading/test_nofw.sh
+++ qa/TL0_free-threading/test_nofw.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-echo "test_nofw.sh Sanity check: PYTHON_GIL=${PYTHON_GIL}"
-
 # used pip packages
 pip_packages='${python_test_runner_package} numpy'
 
