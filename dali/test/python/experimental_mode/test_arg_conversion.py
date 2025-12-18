c5129999138bd146138fd95c5f7221cba0b0178a
Rostan Tabet <rtabet@nvidia.com>
2025-12-18 12:44:45 +0000

    Fix test_arg_conversion.py
    Signed-off-by: Rostan Tabet <rtabet@nvidia.com>
    Co-authored-by: Michal Zientkiewicz <michalz@nvidia.com>

--- dali/test/python/experimental_mode/test_arg_conversion.py
+++ dali/test/python/experimental_mode/test_arg_conversion.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import nvidia.dali.experimental.dynamic as ndd
-import numpy as np
 import os
+
+import numpy as np
+import nvidia.dali.experimental.dynamic as ndd
 from test_utils import get_dali_extra_path
 
 
@@ -51,7 +52,7 @@ def test_arg_conversion():
     def check_not_converted(args):
         nonlocal test_calls
         test_calls += 1
-        assert args["size"] is size, "size should be passed as-is"
+        assert args["size"]._storage is size._storage, "size should be passed as-is"
 
     _conversion_test_op(check_not_converted)(img, size=size).evaluate()
     assert test_calls == 3, "Argument check function not called"
@@ -81,7 +82,7 @@ def test_arg_conversion_batch():
     def check_not_converted(args):
         nonlocal test_calls
         test_calls += 1
-        assert args["size"] is size, "size should be passed as-is"
+        assert args["size"]._storage is size._storage, "size should be passed as-is"
 
     _conversion_test_op(check_not_converted)(imgs, size=size).evaluate()
     assert test_calls == 3, "Argument check function not called"
