f421f0bf4bd502bf7176340951af3b7c3c348802
Rostan Tabet <rtabet@nvidia.com>
2026-01-05 11:41:03 +0000

    Translate data loading examples to dynamic mode
    Signed-off-by: Rostan Tabet <rtabet@nvidia.com>

---
+++ docs/examples/general/data_loading/recordio_reader/index.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from doc_index import doc, doc_entry, op_reference
+
+doc(
+    title="MXNet RecordIO Reader",
+    underline_char="=",
+    options=":maxdepth: 1",
+    entries=[
+        doc_entry(
+            "Pipeline Mode <pipeline_mode.ipynb>",
+            op_reference(
+                "fn.readers.mxnet",
+                "Example of reading data stored in the MXNet RecordIO format",
+            ),
+        ),
+        doc_entry(
+            "Dynamic Mode <dynamic_mode.ipynb>",
+            op_reference(
+                "dynamic.readers.MXNet",
+                "Example of reading data stored in the MXNet RecordIO format",
+            ),
+        ),
+    ],
+)
