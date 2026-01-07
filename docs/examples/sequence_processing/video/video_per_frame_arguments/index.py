e005f277a0db6236fe3dce00cd5e339efc4e414f
Rostan Tabet <rtabet@nvidia.com>
2026-01-07 12:26:12 +0000

    Translate video examples to dynamic mode
    Signed-off-by: Rostan Tabet <rtabet@nvidia.com>

---
+++ docs/examples/sequence_processing/video/video_per_frame_arguments/index.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from doc_index import doc, doc_entry, op_reference
+
+doc(
+    title="Processing video with image processing operators",
+    underline_char="=",
+    options=":maxdepth: 1",
+    entries=[
+        doc_entry(
+            "Pipeline Mode <pipeline_mode.ipynb>",
+            [
+                op_reference("fn.readers.video", "Processing video in DALI"),
+                op_reference("fn.per_frame", "Using per-frame operator"),
+                op_reference("fn.gaussian_blur", "Per-frame blur"),
+                op_reference("fn.rotate", "Per-frame rotation"),
+                op_reference("fn.warp_affine", "Per-frame affine transforms"),
+                op_reference("fn.transforms", "Transform builders"),
+            ],
+        ),
+        doc_entry(
+            "Dynamic Mode <dynamic_mode.ipynb>",
+            [
+                op_reference(
+                    "dynamic.readers.Video", "Processing video in DALI"
+                ),
+                op_reference("dynamic.per_frame", "Using per-frame operator"),
+                op_reference("dynamic.gaussian_blur", "Per-frame blur"),
+                op_reference("dynamic.rotate", "Per-frame rotation"),
+                op_reference(
+                    "dynamic.warp_affine", "Per-frame affine transforms"
+                ),
+            ],
+        ),
+    ],
+)
