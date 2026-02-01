93a415a89ec8c246d2b13c234e88dd793ba3d51a
Rostan Tabet <rtabet@nvidia.com>
2026-02-01 20:27:28 +0000

    Fix optical flow paths in TL1_jupyter_notebooks_TU
    Signed-off-by: Rostan Tabet <rtabet@nvidia.com>

--- qa/TL1_jupyter_notebooks_TU/test.sh
+++ qa/TL1_jupyter_notebooks_TU/test.sh
@@ -5,13 +5,17 @@ pip_packages='jupyter matplotlib numpy'
 target_dir=./docs/examples/
 
 test_body() {
-    test_files=("sequence_processing/optical_flow_example.ipynb")
+    test_files=(
+        "sequence_processing/optical_flow/pipeline_mode.ipynb"
+        "sequence_processing/optical_flow/dynamic_mode.ipynb"
+    )
 
     # test code
-    echo $test_files | xargs -i jupyter nbconvert \
-                   --to notebook --inplace --execute \
-                   --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
-                   --ExecutePreprocessor.timeout=600 {}
+    for f in ${test_files[@]}; do
+        jupyter nbconvert --to notebook --inplace --execute \
+                        --ExecutePreprocessor.kernel_name=python${PYVER:0:1} \
+                        --ExecutePreprocessor.timeout=600 $f
+    done
 }
 
 pushd ../..
