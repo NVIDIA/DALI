2805751893aeebd89583006a29c772b99d48f1d4
Rostan Tabet <rtabet@nvidia.com>
2026-02-06 17:31:07 +0000

    Include ndd.pytorch.nodes only if torchdata is available
    Signed-off-by: Rostan Tabet <rtabet@nvidia.com>

--- dali/python/nvidia/dali/experimental/dynamic/pytorch/__init__.py
+++ dali/python/nvidia/dali/experimental/dynamic/pytorch/__init__.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import nodes as nodes  # noqa: F401
+import importlib
+
+if importlib.util.find_spec("torchdata") is not None:
+    __all__ = ["nodes"]
+    from . import nodes as nodes  # noqa: F401
