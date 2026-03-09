cd17d61c55409c93528cdfba70941f7dcfe0e3b5
Michal Zientkiewicz <michalz@nvidia.com>
2026-03-09 15:22:19 +0100

    Make result of AtScopeExit non-discardable.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- include/dali/core/call_at_exit.h
+++ include/dali/core/call_at_exit.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -35,6 +35,7 @@ struct CallAtExit {
 }  // namespace detail
 
 template <typename Callable>
+[[nodiscard("The callback would be called immediately.")]]
 detail::CallAtExit<Callable> AtScopeExit(Callable &&c) {
   return detail::CallAtExit<Callable>(std::forward<Callable>(c));
 }
