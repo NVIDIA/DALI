50b862af282a00b6beef39b05d0451082e44361e
Michal Zientkiewicz <michalz@nvidia.com>
2026-03-10 11:07:20 +0100

    Fix -newtp launch script.
    Signed-off-by: Michal Zientkiewicz <michalz@nvidia.com>

--- qa/TL0_python-self-test-core-newtp/test.sh
+++ qa/TL0_python-self-test-core-newtp/test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 export DALI_USE_NEW_THREAD_POOL=1
-source ../TL0_python-self-test-core/test.sh
+pushd ../TL0_python-self-test-core
+bash -e ../TL0_python-self-test-core/test.sh
+popd
