cc44d31c2feb420b30a21e33ff452782840f03c7
Marek Dabek <mdabek@nvidia.com>
2026-04-09 14:45:53 +0200

    Revert "DLPack capsule fix for PyTorch 2.7.1"
    This reverts commit f076eb9ae32a9aa4ac651f13651ca348fe00189a.

--- dali/python/backend_impl.cc
+++ dali/python/backend_impl.cc
@@ -181,18 +181,19 @@ void CheckContiguousTensor(const TStrides &strides, int num_strides,
                            const TShape &shape, int num_extents, size_t element_size) {
   DALI_ENFORCE(num_strides == num_extents,
     "There should be exactly as many strides as there are extents in array shape.");
-  for (int i = 0; i < num_extents; i++)
-    if (shape[i] == 0)
-      return;  // The volume is 0, the strides will never be used to compute an actual address
-
   int64_t stride_from_shape = element_size;
+  int64_t stride_from_shape_collapsed = 1;
   int64_t last_non_one_dim = 1;
   for (int i = num_strides - 1; i >= 0; i--) {
-    DALI_ENFORCE(shape[i] == 1 ||  // ignore unit extents - the stride won't be used anyway
-                 strides[i] == stride_from_shape,
+    DALI_ENFORCE(strides[i] == stride_from_shape || strides[i] == stride_from_shape_collapsed,
         make_string("Strided data not supported. Dimension ", i, " has stride ", strides[i],
         " whereas densely packed data of this shape would have a stride ", stride_from_shape));
     stride_from_shape *= shape[i];
+    // for shapes [1, 1, 5] leading dimensions may not contribute to stride
+    if (shape[i] != 1) {
+      stride_from_shape_collapsed *= last_non_one_dim;
+      last_non_one_dim = shape[i];
+    }
   }
 }
 
