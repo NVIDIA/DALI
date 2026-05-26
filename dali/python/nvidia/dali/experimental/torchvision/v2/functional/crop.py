39d2e5da32ded837c3b581e1178de133c09e57da
Marek Dabek <mdabek@nvidia.com>
2026-05-26 18:20:55 +0200

    Crop parameters and validation
    Signed-off-by: Marek Dabek <mdabek@nvidia.com>

--- dali/python/nvidia/dali/experimental/torchvision/v2/functional/crop.py
+++ dali/python/nvidia/dali/experimental/torchvision/v2/functional/crop.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import operator
 from typing import List
 
 import nvidia.dali.experimental.dynamic as ndd
